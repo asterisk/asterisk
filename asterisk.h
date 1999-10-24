502eefae87438c42736b4d57aff7ff5a7a746c0e
Mark Spencer <markster@digium.com>
1999-10-24 16:47:59 +0000

    Version 0.1.0 from FTP
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@8 65c4cc65-6c06-0410-ace0-fbb531ad65f3

---
+++ asterisk.h
@@ -0,0 +1,34 @@
+/*
+ * Asterisk -- A telephony toolkit for Linux.
+ *
+ * General Definitions for Asterisk top level program
+ * 
+ * Copyright (C) 1999, Adtran Inc. and Linux Support Services, LLC
+ *
+ * Mark Spencer <markster@linux-support.net>
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License
+ */
+
+#ifndef _ASTERISK_H
+#define _ASTERISK_H
+
+#define AST_CONFIG_DIR "/etc/asterisk"
+#define AST_MODULE_DIR "/usr/lib/asterisk/modules"
+#define AST_SPOOL_DIR  "/var/spool/asterisk"
+#define AST_VAR_DIR    "/var/lib/asterisk"
+#define AST_LOG_DIR	   "/var/log/asterisk"
+
+#define AST_CONFIG_FILE "asterisk.conf"
+
+#define AST_SOUNDS AST_VAR_DIR "/sounds"
+
+/* Provided by module.c */
+extern int load_modules(void);
+/* Provided by pbx.c */
+extern int load_pbx(void);
+/* Provided by logger.c */
+extern int init_logger(void);
+
+#endif
