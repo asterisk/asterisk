445e08d4d4a45e642d87087f1314c92d04a8a0ba
Mark Spencer <markster@digium.com>
1999-12-01 05:25:45 +0000

    Version 0.1.1 from FTP
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@68 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/ixjuser.h
+++ channels/ixjuser.h
@@ -1,8 +1,8 @@
 /******************************************************************************
 $Id$
 $Log$
-Revision 1.1  1999/12/01 05:25:58  markster
-Version 0.1.0 from FTP
+Revision 1.2  1999/12/01 05:25:58  markster
+Version 0.1.1 from FTP
 
 Revision 1.1  1999/12/01 05:25:58  markster
 Start on the Internet Phone Jack channel
