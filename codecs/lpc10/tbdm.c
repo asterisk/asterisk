e077f63d6a223279089cb8c63b1e3fb8ee69f3cc
Mark Spencer <markster@digium.com>
2000-01-05 08:20:36 +0000

    Version 0.1.10 from FTP
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@183 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- codecs/lpc10/tbdm.c
+++ codecs/lpc10/tbdm.c
@@ -1,8 +1,8 @@
 /*
 
 $Log$
-Revision 1.8  2000/01/05 08:20:40  markster
-Version 0.1.9 from FTP
+Revision 1.9  2000/01/05 08:20:40  markster
+Version 0.1.10 from FTP
 
 Revision 1.2  2000/01/05 08:20:40  markster
 Some OSS fixes and a few lpc changes to make it actually work
@@ -30,8 +30,8 @@ extern int tbdm_(real *speech, integer *lpita, integer *tau, integer *ltau, real
 /* 	TBDM Version 49 */
 
 /* $Log$
- * Revision 1.8  2000/01/05 08:20:40  markster
- * Version 0.1.9 from FTP
+ * Revision 1.9  2000/01/05 08:20:40  markster
+ * Version 0.1.10 from FTP
  *
 /* Revision 1.2  2000/01/05 08:20:40  markster
 /* Some OSS fixes and a few lpc changes to make it actually work
