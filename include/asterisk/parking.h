fb5cc266e253a1ae6fbb9e033f6db88d90a36c60
Mark Spencer <markster@digium.com>
2002-10-11 14:55:03 +0000

    Version 0.3.0 from FTP
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@533 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- include/asterisk/parking.h
+++ include/asterisk/parking.h
@@ -24,15 +24,19 @@
 	host.  If the call is not picked up within a specified period of
 	time, then the call will return to the last step that it was in 
 	(in terms of exten, priority and context)
+	\param timeout is a timeout in milliseconds
+	\param extout is a parameter to an int that will hold the parked location, or NULL if you want
 */
-extern int ast_park_call(struct ast_channel *chan, struct ast_channel *host);
+extern int ast_park_call(struct ast_channel *chan, struct ast_channel *host, int timeout, int *extout);
 //! Park a call via a masqueraded channel
 /*! \param rchan the real channel to be parked
     \param host the channel to have the parking read to
 	Masquerade the channel rchan into a new, empty channel which is then
 	parked with ast_park_call
+	\param timeout is a timeout in milliseconds
+	\param extout is a parameter to an int that will hold the parked location, or NULL if you want
 */
-extern int ast_masq_park_call(struct ast_channel *rchan, struct ast_channel *host);
+extern int ast_masq_park_call(struct ast_channel *rchan, struct ast_channel *host, int timeout, int *extout);
 
 //! Determine system parking extension
 /*! Returns the call parking extension for drivers that provide special
