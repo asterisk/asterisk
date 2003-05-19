3e6d45aebe394b26e042d10a76b9fa74bd1a73db
Martin Pycko <martinp@digium.com>
2003-05-19 23:43:44 +0000

    Fix the flash/*0 when incoming call comes on FXO -> FXS
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@1044 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_zap.c
+++ channels/chan_zap.c
@@ -2884,8 +2884,9 @@ static struct ast_frame *zt_handle_event(struct ast_channel *ast)
 			case SIG_SF_FEATDMF:
 			case SIG_SF_FEATB:
 				/* FGD MF *Must* wait for wink */
-				res = ioctl(p->subs[SUB_REAL].zfd, ZT_DIAL, &p->dop);
-				if (res < 0) {
+				if (strlen(p->dop.dialstr)) {
+					res = ioctl(p->subs[SUB_REAL].zfd, ZT_DIAL, &p->dop);
+				else if (res < 0) {
 					ast_log(LOG_WARNING, "Unable to initiate dialing on trunk channel %d\n", p->channel);
 					p->dop.dialstr[0] = '\0';
 					return NULL;
