e492a6ef882b2e07fb1fd268f33d08d2d1457891
Mark Spencer <markster@digium.com>
2003-08-19 18:54:56 +0000

    Revert killer change to config.h
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@1376 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- codecs/gsm/inc/config.h
+++ codecs/gsm/inc/config.h
@@ -9,29 +9,29 @@
 #ifndef	CONFIG_H
 #define	CONFIG_H
 
-//#define	SIGHANDLER_T	int 		/* signal handlers are void	*/
-//#define HAS_SYSV_SIGNAL	1		/* sigs not blocked/reset?	*/
+/*efine	SIGHANDLER_T	int 		/* signal handlers are void	*/
+/*efine HAS_SYSV_SIGNAL	1		/* sigs not blocked/reset?	*/
 
 #define	HAS_STDLIB_H	1		/* /usr/include/stdlib.h	*/
-//#define	HAS_LIMITS_H	1		/* /usr/include/limits.h	*/
+/*efine	HAS_LIMITS_H	1		/* /usr/include/limits.h	*/
 #define	HAS_FCNTL_H	1		/* /usr/include/fcntl.h		*/
-//#define	HAS_ERRNO_DECL	1		/* errno.h declares errno	*/
+/*efine	HAS_ERRNO_DECL	1		/* errno.h declares errno	*/
 
 #define	HAS_FSTAT 	1		/* fstat syscall		*/
 #define	HAS_FCHMOD 	1		/* fchmod syscall		*/
 #define	HAS_CHMOD 	1		/* chmod syscall		*/
 #define	HAS_FCHOWN 	1		/* fchown syscall		*/
 #define	HAS_CHOWN 	1		/* chown syscall		*/
-//#define	HAS__FSETMODE 	1		/* _fsetmode -- set file mode	*/
+/*efine	HAS__FSETMODE 	1		/* _fsetmode -- set file mode	*/
 
 #define	HAS_STRING_H 	1		/* /usr/include/string.h 	*/
-//#define	HAS_STRINGS_H	1		/* /usr/include/strings.h 	*/
+/*efine	HAS_STRINGS_H	1		/* /usr/include/strings.h 	*/
 
 #define	HAS_UNISTD_H	1		/* /usr/include/unistd.h	*/
 #define	HAS_UTIME	1		/* POSIX utime(path, times)	*/
-//#define	HAS_UTIMES	1		/* use utimes()	syscall instead	*/
+/*efine	HAS_UTIMES	1		/* use utimes()	syscall instead	*/
 #define	HAS_UTIME_H	1		/* UTIME header file		*/
-//#define	HAS_UTIMBUF	1		/* struct utimbuf		*/
-//#define	HAS_UTIMEUSEC   1		/* microseconds in utimbuf?	*/
+/*efine	HAS_UTIMBUF	1		/* struct utimbuf		*/
+/*efine	HAS_UTIMEUSEC   1		/* microseconds in utimbuf?	*/
 
 #endif	/* CONFIG_H */
