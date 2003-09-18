4ac2cab96204035936a733cd11dba843c24c3c2d
Martin Pycko <martinp@digium.com>
2003-09-18 21:17:14 +0000

    Add NoCDR application; when called there won't be generated CDR record for that call
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@1528 65c4cc65-6c06-0410-ace0-fbb531ad65f3

---
+++ apps/app_cdr.c
@@ -0,0 +1,65 @@
+/*
+ * Asterisk -- A telephony toolkit for Linux.
+ *
+ * Applictions connected with CDR engine
+ * 
+ * Copyright (C) 2003, Digium
+ *
+ * Martin Pycko <martinp@digium.com
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License
+ */
+
+#include <asterisk/channel.h>
+#include <asterisk/module.h>
+#include <asterisk/pbx.h>
+#include <stdlib.h>
+
+
+static char *tdesc = "Make sure asterisk doesn't save CDR for a certain call";
+
+static char *nocdr_descrip = "NoCDR(): makes sure there won't be any CDR written for a certain call";
+static char *nocdr_app = "NoCDR";
+static char *nocdr_synopsis = "Make sure asterisk doesn't save CDR for a certain call";
+
+STANDARD_LOCAL_USER;
+
+LOCAL_USER_DECL;
+
+static int nocdr_exec(struct ast_channel *chan, void *data)
+{
+	if (chan->cdr) {
+		free(chan->cdr);
+		chan->cdr = NULL;
+	}
+	return 0;
+}
+
+int unload_module(void)
+{
+	STANDARD_HANGUP_LOCALUSERS;
+	return ast_unregister_application(nocdr_app);
+}
+
+int load_module(void)
+{
+	return ast_register_application(nocdr_app, nocdr_exec, nocdr_synopsis, nocdr_descrip);
+}
+
+char *description(void)
+{
+	return tdesc;
+}
+
+int usecount(void)
+{
+	int res;
+	STANDARD_USECOUNT(res);
+	return res;
+}
+
+char *key()
+{
+	return ASTERISK_GPL_KEY;
+}
