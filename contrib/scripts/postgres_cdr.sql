5b13ea0278af5fe1c4d42ff5614e7025bbf4cccc
Mark Spencer <markster@digium.com>
2003-12-02 23:42:39 +0000

    Fix SQL description for longer channels
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@1815 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- contrib/scripts/postgres_cdr.sql
+++ contrib/scripts/postgres_cdr.sql
@@ -14,19 +14,19 @@
 CREATE TABLE cdr (
         AcctId		BIGSERIAL PRIMARY KEY,
 	calldate	TIMESTAMP with time zone NOT NULL DEFAULT now(),
-	clid		VARCHAR(45) NOT NULL default '',
-	src		VARCHAR(45) NOT NULL default '',
-	dst		VARCHAR(45) NOT NULL default '',
-	dcontext	VARCHAR(45) NOT NULL default '',
-	channel		VARCHAR(45) NOT NULL default '',
-	dstchannel	VARCHAR(45) NOT NULL default '',
-	lastapp		VARCHAR(45) NOT NULL default '',
-	lastdata	VARCHAR(45) NOT NULL default '',
+	clid		VARCHAR(80) NOT NULL default '',
+	src		VARCHAR(80) NOT NULL default '',
+	dst		VARCHAR(80) NOT NULL default '',
+	dcontext	VARCHAR(80) NOT NULL default '',
+	channel		VARCHAR(80) NOT NULL default '',
+	dstchannel	VARCHAR(80) NOT NULL default '',
+	lastapp		VARCHAR(80) NOT NULL default '',
+	lastdata	VARCHAR(80) NOT NULL default '',
 	duration	INTEGER NOT NULL default '0',
 	billsec		INTEGER NOT NULL default '0',
 	disposition	VARCHAR(45) NOT NULL default '',
 	amaflags	INTEGER NOT NULL default '0',
-	accountcode	VARCHAR(45) NOT NULL default '',
-	uniqueid	VARCHAR(45) NOT NULL default ''
+	accountcode	VARCHAR(20) NOT NULL default '',
+	uniqueid	VARCHAR(32) NOT NULL default ''
 );
 
