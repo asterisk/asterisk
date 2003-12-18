a2418cd15dab6540c64e1a561fe7fa149db8efd2
Jeremy McNamara <jj@nufone.net>
2003-12-18 18:24:36 +0000

    don't complain on type=h323 itself :)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@1864 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_h323.c
+++ channels/chan_h323.c
@@ -212,7 +212,9 @@ static struct oh323_alias *build_alias(char *name, struct ast_variable *v)
 			} else if (!strcasecmp(v->name, "secret")) {
 				strncpy(alias->secret,  v->value, sizeof(alias->secret)-1);
 			} else {
+				if (strcasecmp(v->name, "h323")) { 	
 					ast_log(LOG_WARNING, "Keyword %s does not make sense in type=h323\n", v->value);
+				}
 			}
 			v = v->next;
 		}
