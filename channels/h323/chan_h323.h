e46d9edc98ea390524c16d9d1bd6c67b543394d6
Jeremy McNamara <jj@nufone.net>
2004-01-13 08:46:46 +0000

    Not needed here
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@2016 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/h323/chan_h323.h
+++ channels/h323/chan_h323.h
@@ -109,8 +109,6 @@ typedef struct rtp_info {
 	unsigned int port;
 } rtp_info_t;
 
-static struct sockaddr_in bindaddr;
-
 /* This is a callback prototype function, called pass
    DTMF down the RTP. */
 typedef int (*send_digit_cb)(unsigned, char);
