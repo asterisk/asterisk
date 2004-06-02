2587e6faa4c50cc712daaeea1297f4ed8a88940c
James Golovich <james@gnuinter.net>
2004-06-02 16:13:25 +0000

    Code formatting fixes in chan_zap.c
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@3128 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_zap.c
+++ channels/chan_zap.c
@@ -1459,10 +1459,10 @@ static int zt_call(struct ast_channel *ast, char *rdest, int timeout)
 			/* nick@dccinc.com 4/3/03 mods to allow for deferred dialing */
 			c = strchr(dest, '/');
 			if (c)
-			  c++;
+				c++;
 			if (c && (strlen(c) < p->stripmsd)) {
-			  ast_log(LOG_WARNING, "Number '%s' is shorter than stripmsd (%d)\n", c, p->stripmsd);
-			  c = NULL;
+				ast_log(LOG_WARNING, "Number '%s' is shorter than stripmsd (%d)\n", c, p->stripmsd);
+				c = NULL;
 			}
 			if (c) {
 				p->dop.op = ZT_DIAL_OP_REPLACE;
@@ -3076,19 +3076,19 @@ static struct ast_frame *zt_handle_event(struct ast_channel *ast)
 						/* nick@dccinc.com 4/3/03 - fxo should be able to do deferred dialing */
 						res = ioctl(p->subs[SUB_REAL].zfd, ZT_DIAL, &p->dop);
 						if (res < 0) {
-						  ast_log(LOG_WARNING, "Unable to initiate dialing on trunk channel %d\n", p->channel);
-						  p->dop.dialstr[0] = '\0';
-						  return NULL;
+							ast_log(LOG_WARNING, "Unable to initiate dialing on trunk channel %d\n", p->channel);
+							p->dop.dialstr[0] = '\0';
+							return NULL;
 						} else {
-						  ast_log(LOG_DEBUG, "Sent FXO deferred digit string: %s\n", p->dop.dialstr);
-						  p->subs[index].f.frametype = AST_FRAME_NULL;
-						  p->subs[index].f.subclass = 0;
-						  p->dialing = 1;
+							ast_log(LOG_DEBUG, "Sent FXO deferred digit string: %s\n", p->dop.dialstr);
+							p->subs[index].f.frametype = AST_FRAME_NULL;
+							p->subs[index].f.subclass = 0;
+							p->dialing = 1;
 						}
 						p->dop.dialstr[0] = '\0';
-					   ast_setstate(ast, AST_STATE_DIALING);
+						ast_setstate(ast, AST_STATE_DIALING);
 					} else
-					   ast_setstate(ast, AST_STATE_UP);
+						ast_setstate(ast, AST_STATE_UP);
 					return &p->subs[index].f;
 				case AST_STATE_DOWN:
 					ast_setstate(ast, AST_STATE_RING);
