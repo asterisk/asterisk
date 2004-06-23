df93792f9c5febb0baf3751449261f991cb96ef7
Mark Spencer <markster@digium.com>
2004-06-23 22:01:46 +0000

    Extend delay slightly
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@3292 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_zap.c
+++ channels/chan_zap.c
@@ -3196,7 +3196,7 @@ static struct ast_frame *zt_handle_event(struct ast_channel *ast)
 				if (strlen(p->dop.dialstr) > 4) {
 					memset(p->echorest, 'w', sizeof(p->echorest) - 1);
 					p->echorest[sizeof(p->echorest) - 1] = '\0';
-					strcpy(p->echorest + (p->echotraining / 400) + 1, p->dop.dialstr + strlen(p->dop.dialstr) - 2);
+					strcpy(p->echorest + (p->echotraining / 401) + 1, p->dop.dialstr + strlen(p->dop.dialstr) - 2);
 					p->echobreak = 1;
 					p->dop.dialstr[strlen(p->dop.dialstr)-2] = '\0';
 				} else
