fb8ebe63659a1db70159aec65c3820c2f243c103
James Golovich <james@gnuinter.net>
2004-07-01 17:34:42 +0000

    Reverse login on voicemail email sending.  Last change made it send email only if there was no address specified
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@3371 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_voicemail.c
+++ apps/app_voicemail.c
@@ -833,7 +833,7 @@ static int sendmail(char *srcemail, struct ast_vm_user *vmu, int msgnum, char *m
 	time_t t;
 	struct tm tm;
 	struct vm_zone *the_zone = NULL;
-	if (vmu && !ast_strlen_zero(vmu->email)) {
+	if (vmu && ast_strlen_zero(vmu->email)) {
 		ast_log(LOG_WARNING, "E-mail address missing for mailbox [%s].  E-mail will not be sent.\n", vmu->mailbox);
 		return(0);
 	}
