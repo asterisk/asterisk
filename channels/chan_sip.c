12b3b6c86edc9acbf94495ce3656e3853e67ea98
Mark Spencer <markster@digium.com>
2004-07-08 13:04:42 +0000

    Use proper bind address
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@3394 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -2229,9 +2229,9 @@ static struct sip_pvt *sip_alloc(char *callid, struct sockaddr_in *sin, int useg
 	} else {
 		memcpy(&p->ourip, &__ourip, sizeof(p->ourip));
 	}
-	p->rtp = ast_rtp_new_with_bindaddr(sched, io, 1, 0, p->ourip);
+	p->rtp = ast_rtp_new_with_bindaddr(sched, io, 1, 0, __ourip);
 	if (videosupport)
-		p->vrtp = ast_rtp_new_with_bindaddr(sched, io, 1, 0, p->ourip);
+		p->vrtp = ast_rtp_new_with_bindaddr(sched, io, 1, 0, __ourip);
 	p->branch = rand();	
 	p->tag = rand();
 	
