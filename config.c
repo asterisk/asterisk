442e1403a8cbf7867f7347e70f5499871c291710
Mark Spencer <markster@digium.com>
2004-12-11 20:03:42 +0000

    Restore ignoring missing include files (bug #2825)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@4428 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- config.c
+++ config.c
@@ -303,8 +303,7 @@ static int cfg_process(struct ast_config *tmp, struct ast_category **_tmpc, stru
 						if(arg && cur) {
 							ast_log(LOG_WARNING, "Including files with explicit config engine no longer permitted.  Please use extconfig.conf to specify all mappings\n");
 						} else {
-							if (!__ast_load(cur, tmp, _tmpc, _last, includelevel + 1))
-								return -1;
+							__ast_load(cur, tmp, _tmpc, _last, includelevel + 1);
 						}
 					} else
 						ast_log(LOG_WARNING, "Maximum Include level (%d) exceeded\n", includelevel);
