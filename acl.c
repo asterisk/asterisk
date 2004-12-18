c14a82f2fe205ca2df8c7602cae9ea79a24eed4e
Mark Spencer <markster@digium.com>
2004-12-18 21:54:58 +0000

    Improve ACL performance (thanks sailer) (bug #3084)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@4477 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- acl.c
+++ acl.c
@@ -245,157 +245,28 @@ int ast_lookup_iface(char *iface, struct in_addr *address) {
 
 int ast_ouraddrfor(struct in_addr *them, struct in_addr *us)
 {
-#if defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__)
-	struct sockaddr_in *sin;
-	struct sockaddr *sa;
-	struct {
-		struct	rt_msghdr m_rtm;
-		char	m_space[512];
-	} m_rtmsg;
-	char iabuf[INET_ADDRSTRLEN];
-	char *cp, *p;
-	int i, l, s, seq, flags;
-	pid_t pid = getpid();
-	static int routeseq;	/* Protected by "routeseq_lock" mutex */
+	int s;
+	struct sockaddr_in sin;
+	socklen_t slen;
 
-	p = ast_strdupa(ast_inet_ntoa(iabuf, sizeof(iabuf), *them));
-	memset(us, 0, sizeof(struct in_addr));
-
-	memset(&m_rtmsg, 0, sizeof(m_rtmsg));
-	m_rtmsg.m_rtm.rtm_type = RTM_GET;
-	m_rtmsg.m_rtm.rtm_version = RTM_VERSION;
-	ast_mutex_lock(&routeseq_lock);
-	seq = ++routeseq;
-	ast_mutex_unlock(&routeseq_lock);
-	m_rtmsg.m_rtm.rtm_seq = seq;
-	m_rtmsg.m_rtm.rtm_addrs = RTA_DST | RTA_IFA;
-	m_rtmsg.m_rtm.rtm_msglen = sizeof(struct rt_msghdr) + sizeof(struct sockaddr_in);
-	sin = (struct sockaddr_in *)m_rtmsg.m_space;
-	sin->sin_family = AF_INET;
-	sin->sin_len = sizeof(struct sockaddr_in);
-	sin->sin_addr = *them;
-
-	if ((s = socket(PF_ROUTE, SOCK_RAW, AF_UNSPEC)) < 0) {
-		ast_log(LOG_ERROR, "Error opening routing socket\n");
+	s = socket(PF_INET, SOCK_DGRAM, 0);
+	if (s == -1) {
+		ast_log(LOG_WARNING, "Cannot create socket\n");
 		return -1;
 	}
-	flags = fcntl(s, F_GETFL);
-	fcntl(s, F_SETFL, flags | O_NONBLOCK);
-	if (write(s, (char *)&m_rtmsg, m_rtmsg.m_rtm.rtm_msglen) < 0) {
-		ast_log(LOG_ERROR, "Error writing to routing socket: %s\n", strerror(errno));
-		close(s);
+        sin.sin_family = AF_INET;
+        sin.sin_port = 5060;
+	sin.sin_addr = *them;
+	if (connect(s, (struct sockaddr *)&sin, sizeof(sin))) {
+		ast_log(LOG_WARNING, "Cannot connect\n");
 		return -1;
 	}
-	do {
-		l = read(s, (char *)&m_rtmsg, sizeof(m_rtmsg));
-	} while (l > 0 && (m_rtmsg.m_rtm.rtm_seq != seq || m_rtmsg.m_rtm.rtm_pid != pid));
-	if (l < 0) {
-		if (errno != EAGAIN)
-			ast_log(LOG_ERROR, "Error reading from routing socket\n");
-		close(s);
+	slen = sizeof(sin);
+	if (getsockname(s, (struct sockaddr *)&sin, &slen)) {
+		ast_log(LOG_WARNING, "Cannot get socket name\n");
 		return -1;
 	}
 	close(s);
-
-	if (m_rtmsg.m_rtm.rtm_version != RTM_VERSION) {
-		ast_log(LOG_ERROR, "Unsupported route socket protocol version\n");
-		return -1;
-	}
-
-	if (m_rtmsg.m_rtm.rtm_msglen != l)
-		ast_log(LOG_WARNING, "Message length mismatch, in packet %d, returned %d\n",
-				m_rtmsg.m_rtm.rtm_msglen, l);
-
-	if (m_rtmsg.m_rtm.rtm_errno) {
-		ast_log(LOG_ERROR, "RTM_GET got %s (%d)\n",
-				strerror(m_rtmsg.m_rtm.rtm_errno), m_rtmsg.m_rtm.rtm_errno);
-		return -1;
-	}
-
-	cp = (char *)m_rtmsg.m_space;
-	if (m_rtmsg.m_rtm.rtm_addrs)
-		for (i = 1; i; i <<= 1)
-			if (m_rtmsg.m_rtm.rtm_addrs & i) {
-				sa = (struct sockaddr *)cp;
-				if (i == RTA_IFA && sa->sa_family == AF_INET) {
-					sin = (struct sockaddr_in *)sa;
-					*us = sin->sin_addr;
-					ast_log(LOG_DEBUG, "Found route to %s, output from our address %s.\n", p, ast_inet_ntoa(iabuf, sizeof(iabuf), *us));
-					return 0;
-				}
-				cp += sa->sa_len > 0 ?
-					  (1 + ((sa->sa_len - 1) | (sizeof(long) - 1))) :
-					  sizeof(long);
-			}
-
-	ast_log(LOG_DEBUG, "No route found for address %s!\n", p);
-	return -1;
-#else
-	FILE *PROC;
-	unsigned int remote_ip;
-	int res = 1;
-	char line[256];
-	remote_ip = them->s_addr;
-	
-	PROC = fopen("/proc/net/route","r");
-	if (!PROC) {
-		bzero(us,sizeof(struct in_addr));
-		return -1;
-	}
-	/* First line contains headers */
-	fgets(line,sizeof(line),PROC);
-
-	while (!feof(PROC)) {
-		char iface[256];
-		unsigned int dest, gateway, mask;
-		int i,fieldnum;
-		char *fields[40];
-
-		fgets(line,sizeof(line),PROC);
-
-		fieldnum = 0;
-		for (i=0;i<sizeof(line);i++) {
-			char *offset;
-
-			fields[fieldnum++] = line + i;
-			offset = strchr(line + i,'\t');
-			if (offset == NULL) {
-				/* Exit loop */
-				break;
-			} else if (fieldnum >= 9) {
-				/* Short-circuit: can't break at 8, since the end of field 7 is figured when fieldnum=8 */
-				break;
-			} else {
-				*offset = '\0';
-				i = offset - line;
-			}
-		}
-		if (fieldnum >= 8) {
-
-			sscanf(fields[0],"%s",iface);
-			sscanf(fields[1],"%x",&dest);
-			sscanf(fields[2],"%x",&gateway);
-			sscanf(fields[7],"%x",&mask);
-#if 0
-			{ char iabuf[INET_ADDRSTRLEN]; 
-			printf("Addr: %s %08x Dest: %08x Mask: %08x\n", ast_inet_ntoa(iabuf, sizeof(iabuf), *them), remote_ip, dest, mask); }
-#endif		
-			/* Looks simple, but here is the magic */
-			if (((remote_ip & mask) ^ dest) == 0) {
-				res = ast_lookup_iface(iface,us);
-				break;
-			}
-		}
-	}
-	fclose(PROC);
-	if (res == 1) {
-		ast_log(LOG_WARNING, "Yikes!  No default route?!!\n");
-		bzero(us,sizeof(struct in_addr));
-		return -2;
-	} else if (res) {
-		/* We've already warned in subroutine */
-		return -1;
- 	}
+	*us = sin.sin_addr;
 	return 0;
-#endif
 }
