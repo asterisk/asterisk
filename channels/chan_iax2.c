ccd7a7ac5448fdef6090460e38d47d161d010145
Mark Spencer <markster@digium.com>
2005-01-28 22:08:24 +0000

    Reduce complexity of check for f->samples / 8 not being 0
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@4910 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_iax2.c
+++ channels/chan_iax2.c
@@ -3027,7 +3027,7 @@ static unsigned int calc_timestamp(struct chan_iax2_pvt *p, unsigned int ts, str
 				* next multiple of frame size (so our
 				* silent periods are multiples of
 				* frame size too) */
-				if (f->samples / 8) /* check to make sure we dont core dump */
+				if (f->samples >= 8) /* check to make sure we dont core dump */
 				{
 					int diff = ms % (f->samples / 8);
 					if (diff)
