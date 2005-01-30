f85cf0138cfaca9669e1682ef5346411fee67c1c
Mark Spencer <markster@digium.com>
2005-01-30 06:09:28 +0000

    Add setrdnis app (bug #3460)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@4924 65c4cc65-6c06-0410-ace0-fbb531ad65f3

---
+++ apps/app_setrdnis.c
@@ -0,0 +1,95 @@
+/*
+ * Asterisk -- A telephony toolkit for Linux.
+ *
+ * App to set rdnis
+ * 
+ * Copyright (C) 1999 - 2005, Digium, Inc.
+ *
+ * Mark Spencer <markster@digium.com>
+ * Oliver Daudey <traveler@xs4all.nl>
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License
+ */
+ 
+#include <asterisk/lock.h>
+#include <asterisk/file.h>
+#include <asterisk/logger.h>
+#include <asterisk/channel.h>
+#include <asterisk/pbx.h>
+#include <asterisk/module.h>
+#include <asterisk/translate.h>
+#include <asterisk/image.h>
+#include <asterisk/callerid.h>
+#include <asterisk/utils.h>
+#include <string.h>
+#include <stdlib.h>
+
+static char *tdesc = "Set RDNIS Number";
+
+static char *app = "SetRDNIS";
+
+static char *synopsis = "Set RDNIS Number";
+
+static char *descrip = 
+"  SetRDNIS(cnum): Set RDNIS Number on a call to a new\n"
+"value.  Always returns 0\n";
+
+STANDARD_LOCAL_USER;
+
+LOCAL_USER_DECL;
+
+static int setrdnis_exec(struct ast_channel *chan, void *data)
+{
+	struct localuser *u;
+	char *opt, *n, *l;
+	char tmp[256];
+	if (data)
+		strncpy(tmp, (char *)data, sizeof(tmp) - 1);
+	else
+		tmp[0] = '\0';
+	opt = strchr(tmp, '|');
+	if (opt)
+		*opt = '\0';
+	LOCAL_USER_ADD(u);
+	n = l = NULL;
+	ast_callerid_parse(tmp, &n, &l);
+	if (l) {
+		ast_shrink_phone_number(l);
+		ast_mutex_lock(&chan->lock);
+		if (chan->cid.cid_rdnis)
+			free(chan->cid.cid_rdnis);
+		chan->cid.cid_rdnis = (l[0]) ? strdup(l) : NULL;
+		ast_mutex_unlock(&chan->lock);
+	}
+	LOCAL_USER_REMOVE(u);
+	return 0;
+}
+
+int unload_module(void)
+{
+	STANDARD_HANGUP_LOCALUSERS;
+	return ast_unregister_application(app);
+}
+
+int load_module(void)
+{
+	return ast_register_application(app, setrdnis_exec, synopsis, descrip);
+}
+
+char *description(void)
+{
+	return tdesc;
+}
+
+int usecount(void)
+{
+	int res;
+	STANDARD_USECOUNT(res);
+	return res;
+}
+
+char *key()
+{
+	return ASTERISK_GPL_KEY;
+}
