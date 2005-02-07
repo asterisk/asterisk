59814ed3e56b186471e0a558ede26678077488e3
Russell Bryant <russell@russellbryant.com>
2005-02-07 03:53:36 +0000

    Fix wav append mode (bug #3498)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/v1-0@4975 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- file.c
+++ file.c
@@ -844,8 +844,12 @@ struct ast_filestream *ast_writefile(char *filename, char *type, char *comment, 
 		return NULL;
 	}
 	/* set the O_TRUNC flag if and only if there is no O_APPEND specified */
-	if (!(flags & O_APPEND)) 
+	if (flags & O_APPEND){ 
+		/* We really can't use O_APPEND as it will break WAV header updates */
+		flags &= ~O_APPEND;
+	}else{
 		myflags = O_TRUNC;
+	}
 	
 	myflags |= O_WRONLY | O_CREAT;
 
