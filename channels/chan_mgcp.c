864f6a50b835fd6c083fa6bab9389297637cbb1a
Kevin P. Fleming <kpfleming@digium.com>
2005-04-05 21:40:37 +0000

    ensure that MGCP AMA flags are passed into CDR (bug #3962)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@5414 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_mgcp.c
+++ channels/chan_mgcp.c
@@ -3710,6 +3710,7 @@ static struct mgcp_gateway *build_gateway(char *cat, struct ast_variable *v)
 					strncpy(e->mailbox, mailbox, sizeof(e->mailbox) - 1);
 					snprintf(e->rqnt_ident, sizeof(e->rqnt_ident), "%08x", rand());
 					e->msgstate = -1;
+					e->amaflags = amaflags;
 					e->capability = capability;
 					e->parent = gw;
 					e->dtmfmode = dtmfmode;
@@ -3812,6 +3813,7 @@ static struct mgcp_gateway *build_gateway(char *cat, struct ast_variable *v)
 						e->msgstate = -1;
 						e->parent = gw;
 					}
+					e->amaflags = amaflags;
 					e->capability = capability;
 					e->dtmfmode = dtmfmode;
 					e->adsi = adsi;
