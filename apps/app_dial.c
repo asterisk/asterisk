e87a53037e4e13c7c2406c5ec84e95f84aa032ee
Mark Spencer <markster@digium.com>
2005-04-09 18:54:58 +0000

    Forward text frames before answer (bug #3717)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@5440 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_dial.c
+++ apps/app_dial.c
@@ -513,6 +513,10 @@ static struct ast_channel *wait_for_answer(struct ast_channel *in, struct localu
 								!(ast_test_flag(outgoing, DIAL_RINGBACKONLY|DIAL_MUSICONHOLD))) {
 						if (ast_write(in, f))
 							ast_log(LOG_WARNING, "Unable to forward image\n");
+					} else if (single && (f->frametype == AST_FRAME_TEXT) && 
+								!(ast_test_flag(outgoing, DIAL_RINGBACKONLY|DIAL_MUSICONHOLD))) {
+						if (ast_write(in, f))
+							ast_log(LOG_WARNING, "Unable to text\n");
 					} else if (single && (f->frametype == AST_FRAME_HTML) && !ast_test_flag(outgoing, DIAL_NOFORWARDHTML))
 						ast_channel_sendhtml(in, f->subclass, f->data, f->datalen);
 
