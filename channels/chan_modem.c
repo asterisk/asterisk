a657d3a7fa3d5c140f31b2f627fb7e8f40ce7d5b
Russell Bryant <russell@russellbryant.com>
2005-07-26 14:40:41 +0000

    fix build issue for FreeBSD (bug #4802)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@6221 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_modem.c
+++ channels/chan_modem.c
@@ -12,15 +12,14 @@
  */
 
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/socket.h>
+#include <ctype.h>
+#include <string.h>
 #include <sys/time.h>
 #include <errno.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <sys/termios.h>
