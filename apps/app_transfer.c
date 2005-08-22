9d899a2075a297b4efa9520c08747cadb014fac8
Kevin P. Fleming <kpfleming@digium.com>
2005-08-22 20:53:38 +0000

    add status output to Transfer() application (issue #4810, with mods)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@6355 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_transfer.c
+++ apps/app_transfer.c
@@ -25,21 +25,28 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 #include "asterisk/channel.h"
 #include "asterisk/pbx.h"
 #include "asterisk/module.h"
+#include "asterisk/options.h"
 
-static char *tdesc = "Transfer";
+static const char *tdesc = "Transfer";
 
-static char *app = "Transfer";
+static const char *app = "Transfer";
 
-static char *synopsis = "Transfer caller to remote extension";
+static const char *synopsis = "Transfer caller to remote extension";
 
-static char *descrip = 
+static const char *descrip = 
 "  Transfer([Tech/]dest):  Requests the remote caller be transfered\n"
 "to a given extension. If TECH (SIP, IAX2, LOCAL etc) is used, only\n"
 "an incoming call with the same channel technology will be transfered.\n"
 "Note that for SIP, if you transfer before call is setup, a 302 redirect\n"
 "SIP message will be returned to the caller.\n"
+"\nThe result of the application will be reported in the TRANSFERSTATUS\n"
+"channel variable:\n"
+"       SUCCESS       Transfer succeeded\n"
+"       FAILURE      Transfer failed\n"
+"       UNSUPPORTED  Transfer unsupported by channel driver\n"
 "Returns -1 on hangup, or 0 on completion regardless of whether the\n"
-"transfer was successful.  If the transfer was *not* supported or\n"
+"transfer was successful.\n\n"
+"Old depraciated behaviour: If the transfer was *not* supported or\n"
 "successful and there exists a priority n + 101,\n"
 "then that priority will be taken next.\n" ;
 
@@ -49,40 +56,60 @@ LOCAL_USER_DECL;
 
 static int transfer_exec(struct ast_channel *chan, void *data)
 {
-	int res=0;
+	int res;
 	int len;
 	struct localuser *u;
 	char *slash;
+	char *tech = NULL;
 	char *dest = data;
-	if (!data || !strlen(data)) {
+	char *status;
+
+	if (!dest || ast_strlen_zero(dest)) {
 		ast_log(LOG_WARNING, "Transfer requires an argument ([Tech/]destination)\n");
-		res = 1;
+		pbx_builtin_setvar_helper(chan, "TRANSFERSTATUS", "FAILURE");
+		return 0;
 	}
-	if ((slash = strchr((char *)data, '/')) && (len = (slash - (char *)data))) {
+
+	if ((slash = strchr(dest, '/')) && (len = (slash - dest))) {
+		tech = dest;
 		dest = slash + 1;
 		/* Allow execution only if the Tech/destination agrees with the type of the channel */
-		if (strncasecmp(chan->type, (char *)data, len))
+		if (strncasecmp(chan->type, tech, len)) {
+			pbx_builtin_setvar_helper(chan, "TRANSFERSTATUS", "FAILURE");
 			return 0;
+		}
 	}
-	LOCAL_USER_ADD(u);
-	if (!res) {
-		res = ast_transfer(chan, dest);
 
+	/* Check if the channel supports transfer before we try it */
+	if (!chan->tech->transfer) {
+		pbx_builtin_setvar_helper(chan, "TRANSFERSTATUS", "UNSUPPORTED");
+		return 0;
 	}
+
+	LOCAL_USER_ADD(u);
+
+	res = ast_transfer(chan, dest);
+
 	if (!res) {
-		/* Look for a "busy" place */
-		if (ast_exists_extension(chan, chan->context, chan->exten, chan->priority + 101, chan->cid.cid_num))
-			chan->priority += 100;
-	}
-	if (res > 0)
+		status = "FAILURE";
+		if (option_priority_jumping)
+			ast_goto_if_exists(chan, chan->context, chan->exten, chan->priority + 101);
+	} else {
+		status = "SUCCESS";
 		res = 0;
+	}
+
+	pbx_builtin_setvar_helper(chan, "TRANSFERSTATUS", status);
+
 	LOCAL_USER_REMOVE(u);
+
 	return res;
 }
 
 int unload_module(void)
 {
 	STANDARD_HANGUP_LOCALUSERS;
+
 	return ast_unregister_application(app);
 }
 
@@ -93,13 +120,15 @@ int load_module(void)
 
 char *description(void)
 {
-	return tdesc;
+	return (char *) tdesc;
 }
 
 int usecount(void)
 {
 	int res;
+
 	STANDARD_USECOUNT(res);
+
 	return res;
 }
 
