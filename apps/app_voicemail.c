bfac0241be0ae0732ea30f896643cf5b289de4c9
Russell Bryant <russell@russellbryant.com>
2005-08-24 15:39:25 +0000

    fix crash related to saving a changed password (bug #4976)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/v1-0@6392 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_voicemail.c
+++ apps/app_voicemail.c
@@ -675,7 +675,7 @@ static void vm_change_password(struct ast_vm_user *vmu, char *newpassword)
 					}
 				} else {
 					/* Put it back like it was */
-					fprintf(configout, orig);
+					fprintf(configout, "%s", orig);
 				}
 			}
         }
