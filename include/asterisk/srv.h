7085e27835bb45c5831c7362faf002fe81d9a95d
Kevin P. Fleming <kpfleming@digium.com>
2005-08-25 23:23:26 +0000

    doc updates and copyright header cleanup (issue #5026)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@6411 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- include/asterisk/srv.h
+++ include/asterisk/srv.h
@@ -1,14 +1,16 @@
 /*
  * Asterisk -- A telephony toolkit for Linux.
  *
- * ENUM support
+ * DNS SRV record support
  * 
- * Copyright (C) 1999, Mark Spencer
+ * Copyright (C) 1999-2005, Digium, Inc.
  *
- * Mark Spencer <markster@linux-support.net>
+ * Mark Spencer <markster@digium.com>
  *
  * This program is free software, distributed under the terms of
  * the GNU General Public License
+ * 
+ * $Revision$
  */
 
 #ifndef _ASTERISK_SRV_H
@@ -16,7 +18,23 @@
 
 struct ast_channel;
 
-/* Lookup entry in SRV records Returns 1 if found, 0 if not found, -1 on hangup */
+/*!
+  \file srv.h
+  \brief Support for DNS SRV records, used in to locate SIP services.
+	Note: The Asterisk DNS SRV record support is broken, it only
+	supports the first DNS SRV record and will give no load 
+	balancing or failover support.
+*/
+
+/*! Lookup entry in SRV records Returns 1 if found, 0 if not found, -1 on hangup 
+	Only do SRV record lookup if you get a domain without a port. If you get a port #, it's a DNS host name.
+*/
+/*!	\param	chan Ast channel
+	\param	host host name (return value)
+	\param	hostlen Length of string "host"
+	\param	port Port number (return value)
+	\param service Service tag for SRV lookup (like "_sip._udp" or "_stun._udp"
+*/
 extern int ast_get_srv(struct ast_channel *chan, char *host, int hostlen, int *port, const char *service);
 
 #endif
