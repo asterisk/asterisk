cfabcabae88c498f524e251f1ef014aa6bdcde31
Mark Spencer <markster@digium.com>
2005-09-25 02:28:31 +0000

    Null out call number on release complete when number is unallocated (bug #4633)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@6639 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_zap.c
+++ channels/chan_zap.c
@@ -5196,6 +5196,8 @@ static void *ss_thread(void *data)
 			chan->hangupcause = AST_CAUSE_UNALLOCATED;
 			ast_hangup(chan);
 			p->exten[0] = '\0';
+			/* Since we send release complete here, we won't get one */
+			p->call = NULL;
 		}
 		return NULL;
 		break;
