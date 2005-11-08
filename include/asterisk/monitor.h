8f7c94ab0f9f212a14d84d000cfa3f027231546b
Kevin P. Fleming <kpfleming@digium.com>
2005-11-08 03:19:05 +0000

    whitespace cleanup, don't include unnecessary header
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@7016 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- include/asterisk/monitor.h
+++ include/asterisk/monitor.h
@@ -23,36 +23,33 @@
 #ifndef _ASTERISK_MONITOR_H
 #define _ASTERISK_MONITOR_H
 
-#include <stdio.h>
-
 #include "asterisk/channel.h"
 
 struct ast_channel;
 
 /*! Responsible for channel monitoring data */
-struct ast_channel_monitor
-{
+struct ast_channel_monitor {
 	struct ast_filestream *read_stream;
 	struct ast_filestream *write_stream;
-	char read_filename[ FILENAME_MAX ];
-	char write_filename[ FILENAME_MAX ];
-	char filename_base[ FILENAME_MAX ];
+	char read_filename[FILENAME_MAX];
+	char write_filename[FILENAME_MAX];
+	char filename_base[FILENAME_MAX];
 	int filename_changed;
 	char *format;
 	int joinfiles;
-	int (*stop)( struct ast_channel *chan, int need_lock);
+	int (*stop)(struct ast_channel *chan, int need_lock);
 };
 
 /* Start monitoring a channel */
-int ast_monitor_start(	struct ast_channel *chan, const char *format_spec,
-						const char *fname_base, int need_lock );
+int ast_monitor_start(struct ast_channel *chan, const char *format_spec,
+		      const char *fname_base, int need_lock );
 
 /* Stop monitoring a channel */
-int ast_monitor_stop( struct ast_channel *chan, int need_lock);
+int ast_monitor_stop(struct ast_channel *chan, int need_lock);
 
 /* Change monitoring filename of a channel */
-int ast_monitor_change_fname(	struct ast_channel *chan,
-								const char *fname_base, int need_lock );
+int ast_monitor_change_fname(struct ast_channel *chan,
+			     const char *fname_base, int need_lock);
 
 void ast_monitor_setjoinfiles(struct ast_channel *chan, int turnon);
 
