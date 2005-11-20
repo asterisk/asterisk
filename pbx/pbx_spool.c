c203681525fa8ca60f860d35508616317a22fab7
Josh Roberson <josh@asteriasgi.com>
2005-11-20 10:32:28 +0000

    Make conform to coding guidelines... thanks drumkilla :)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@7145 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- pbx/pbx_spool.c
+++ pbx/pbx_spool.c
@@ -202,15 +202,13 @@ static int apply_outgoing(struct outgoing *o, char *fn, FILE *f)
 				} else if (!strcasecmp(buf, "setvar") || !strcasecmp(buf, "set")) {
 					c2 = c;
 					strsep(&c2, "=");
-					if (c2)
-					{
+					if (c2) {
 						var = ast_variable_new(c, c2);
 						if (var) {
 							var->next = o->vars;
 							o->vars = var;
 						}
-					}
-					else
+					} else
 						ast_log(LOG_WARNING, "Malformed \"%s\" argument.  Should be \"%s: variable=value\"\n", buf, buf);
 				} else if (!strcasecmp(buf, "account")) {
 					var = ast_variable_new("CDR(accountcode|r)", c);
