08acdc6a86132f7eed9852e261c45bdc9dc99723
Kevin P. Fleming <kpfleming@digium.com>
2005-11-21 19:34:09 +0000

    backport fix from HEAD branch
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/v1-2@7186 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_cut.c
+++ apps/app_cut.c
@@ -36,7 +36,6 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 #include "asterisk/channel.h"
 #include "asterisk/pbx.h"
 #include "asterisk/module.h"
-#include "asterisk/version.h"
 #include "asterisk/app.h"
 
 /* Maximum length of any variable */
