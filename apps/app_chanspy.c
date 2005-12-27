8df1e4866addaf21a9419bde399824be05438426
Russell Bryant <russell@russellbryant.com>
2005-12-27 21:07:08 +0000

    backport fix for permissions of created recordings (issue #6067)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.2@7663 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_chanspy.c
+++ apps/app_chanspy.c
@@ -440,7 +440,7 @@ static int chanspy_exec(struct ast_channel *chan, void *data)
 	if (recbase) {
 		char filename[512];
 		snprintf(filename,sizeof(filename),"%s/%s.%d.raw",ast_config_AST_MONITOR_DIR, recbase, (int)time(NULL));
-		if ((fd = open(filename, O_CREAT | O_WRONLY, O_TRUNC)) <= 0) {
+		if ((fd = open(filename, O_CREAT | O_WRONLY, O_TRUNC, 0644)) <= 0) {
 			ast_log(LOG_WARNING, "Cannot open %s for recording\n", filename);
 			fd = 0;
 		}
