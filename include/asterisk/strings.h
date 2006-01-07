33323ac720234c5ed8d2e38496acb3acee8379de
Tilghman Lesher <tilghman@meg.abyt.es>
2006-01-07 15:45:47 +0000

    Bug 6164 - Allow ast_skip_blanks on const strings without spewing warning
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@7854 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- include/asterisk/strings.h
+++ include/asterisk/strings.h
@@ -42,11 +42,11 @@ static force_inline int ast_strlen_zero(const char *s)
   \return a pointer to the first non-whitespace character
  */
 AST_INLINE_API(
-char *ast_skip_blanks(char *str),
+char *ast_skip_blanks(const char *str),
 {
 	while (*str && *str < 33)
 		str++;
-	return str;
+	return (char *)str;
 }
 )
 
