d7f70ca1c2ac098a1a003d7f38c85a0142412e33
Russell Bryant <russell@russellbryant.com>
2006-01-11 05:47:21 +0000

    Merged revisions 7972 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.2
    ........
    r7972 | russell | 2006-01-11 00:46:39 -0500 (Wed, 11 Jan 2006) | 2 lines
    
    fix mem leak on module unload (issue #6190)
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@7973 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -13218,7 +13218,6 @@ int unload_module()
 				ast_softhangup(p->owner, AST_SOFTHANGUP_APPUNLOAD);
 			p = p->next;
 		}
-		iflist = NULL;
 		ast_mutex_unlock(&iflock);
 	} else {
 		ast_log(LOG_WARNING, "Unable to lock the interface list\n");
