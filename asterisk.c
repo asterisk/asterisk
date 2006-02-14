4f144aa5e31ddd3f41f1550a6b9d20e6bcfc0b17
BJ Weschke <bweschke@btwtech.com>
2006-02-14 18:45:34 +0000

    #ifdef'd the prctl fix to only try and compile on linux systems. Thanks rizzo for pointing this out.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.2@9961 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- asterisk.c
+++ asterisk.c
@@ -2210,11 +2210,15 @@ int main(int argc, char *argv[])
 
 #endif /* __CYGWIN__ */
 
+#ifdef linux
+
 	if (geteuid() && option_dumpcore) {
 		if (prctl(PR_SET_DUMPABLE, 1, 0, 0, 0) < 0) {
 			ast_log(LOG_WARNING, "Unable to set the process for core dumps after changing to a non-root user. %s\n", strerror(errno));
 		}	
 	}
+
+#endif
 
 	term_init();
 	printf(term_end());
