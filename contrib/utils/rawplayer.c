688e35587300140a87e414ea6c7e405ed9e2d7a8
Russell Bryant <russell@russellbryant.com>
2006-03-14 16:10:44 +0000

    catch read/write errors and exit if they occur (issue #6721)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@12893 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- contrib/utils/rawplayer.c
+++ contrib/utils/rawplayer.c
@@ -1,6 +1,10 @@
 /*
   Rawplayer.c simple raw file stdout player
   (c) Anthony C Minessale II <anthmct@yahoo.com>
+
+  2006-03-10: Bruno Rocha <bruno@3gnt.net>
+  - include <stdlib.h> to remove compiler warning on some platforms
+  - check for read/write errors (avoid 100% CPU usage in some asterisk failures)
 */
 
 #define BUFLEN 320
@@ -8,21 +12,25 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 static int deliver_file(char *path, int fdout) {
-	int fd = 0, bytes = 0;
+	int fd = 0, bytes = 0, error = 0;
 	short buf[BUFLEN];
 
 	if ((fd = open(path,O_RDONLY))) {
-		while ((bytes=read(fd, buf, BUFLEN))) {
-			write(fdout, buf, bytes);
+		while ((bytes=read(fd, buf, BUFLEN)) > 0) {
+			if(write(fdout, buf, bytes) < 0){
+				error = -2;
+				break;
+			}
 		}
 		if(fd)
 			close(fd);
 	} else 
 		return -1;
 	
-	return 0;
+	return error;
 }
 
 
