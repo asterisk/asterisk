ed2918cfd1e2dd445f58c37ae73eb1f5d46a0987
Russell Bryant <russell@russellbryant.com>
2006-04-24 21:32:41 +0000

    check for automake before aclocal, so that the error message says automake is missing instead of aclocal, since that's where aclocal comes from
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@22327 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,10 +12,10 @@ check_for_app() {
 uname -s | grep -q FreeBSD
 if [ $? = 0 ]
 then
-	check_for_app aclocal19
 	check_for_app autoconf259
 	check_for_app autoheader259
 	check_for_app automake19
+	check_for_app aclocal19
 	echo "Generating the configure script ..."
 	aclocal19 2>/dev/null
 	autoconf259
@@ -25,10 +25,10 @@ else
 	export AUTOCONF_VERSION=2.59
 	export AUTOMAKE_VERSION=1.9
 
-	check_for_app aclocal
 	check_for_app autoconf
 	check_for_app autoheader
 	check_for_app automake
+	check_for_app aclocal
 	echo "Generating the configure script ..."
 	aclocal 2>/dev/null
 	autoconf
