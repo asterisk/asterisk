8682a7378d28d755899d0d3670c18fa233ce51e1
Russell Bryant <russell@russellbryant.com>
2006-04-29 04:13:07 +0000

    add a small optimization for deleting all the members of a list
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@23271 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- pbx/pbx_dundi.c
+++ pbx/pbx_dundi.c
@@ -559,10 +559,8 @@ static int dundi_lookup_local(struct dundi_result *dr, struct dundi_mapping *map
 				newvariable = ast_var_assign("IPADDR", ipaddr);
 				AST_LIST_INSERT_HEAD(&headp, newvariable, entries);
 				pbx_substitute_variables_varshead(&headp, map->dest, dr[anscnt].dest, sizeof(dr[anscnt].dest));
-				while (!AST_LIST_EMPTY(&headp)) {           /* List Deletion. */
-					newvariable = AST_LIST_REMOVE_HEAD(&headp, entries);
+				while ((newvariable = AST_LIST_REMOVE_HEAD(&headp, entries)))
 					ast_var_delete(newvariable);
-				}
 			} else
 				dr[anscnt].dest[0] = '\0';
 			anscnt++;
