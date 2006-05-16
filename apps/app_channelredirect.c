185cebac91773ce013ea94744e60700114d5ddf3
Olle Johansson <oej@edvina.net>
2006-05-16 19:20:50 +0000

    Adding check of option_debug
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@27436 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_channelredirect.c
+++ apps/app_channelredirect.c
@@ -105,7 +105,8 @@ static int asyncgoto_exec(struct ast_channel *chan, void *data)
 		goto chanquit;
 	}
 
-	ast_log(LOG_DEBUG, "Attempting async goto (%s) to %s|%s|%d\n", args.channel, S_OR(context, chan2->context), S_OR(exten, chan2->exten), prio);
+	if (option_debug > 1)
+		ast_log(LOG_DEBUG, "Attempting async goto (%s) to %s|%s|%d\n", args.channel, S_OR(context, chan2->context), S_OR(exten, chan2->exten), prio);
 
 	if (ast_async_goto_if_exists(chan2, S_OR(context, chan2->context), S_OR(exten, chan2->exten), prio))
 		ast_log(LOG_WARNING, "%s failed for %s\n", app, args.channel);
