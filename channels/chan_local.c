f0e022bbadd9b9dac7701fa0a6f1e24fff24eb2d
Joshua Colp <jcolp@digium.com>
2006-05-22 16:33:03 +0000

    Preserve presentation bit when going through chan_local (issue #7002 reported by acunningham)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.2@29464 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_local.c
+++ channels/chan_local.c
@@ -344,6 +344,8 @@ static int local_call(struct ast_channel *ast, char *dest, int timeout)
 	else
 		p->chan->cid.cid_ani = NULL;
 
+	p->chan->cid.cid_pres = p->owner->cid.cid_pres;
+
 	strncpy(p->chan->language, p->owner->language, sizeof(p->chan->language) - 1);
 	strncpy(p->chan->accountcode, p->owner->accountcode, sizeof(p->chan->accountcode) - 1);
 	p->chan->cdrflags = p->owner->cdrflags;
