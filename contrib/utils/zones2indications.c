de0c61807d8e3940827022db6bab8bad3a1b776a
Russell Bryant <russell@russellbryant.com>
2006-06-05 15:56:35 +0000

    use the standard Asterisk copyright header
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@32306 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- contrib/utils/zones2indications.c
+++ contrib/utils/zones2indications.c
@@ -1,22 +1,24 @@
 /*
- * zones2indications: print libtonozone data as Asterisk indications.conf
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. 
+ * Asterisk -- An open source telephony toolkit.
  *
- * Author: Tzafrir Cohen <tzafrir.cohen@xorcom.com>
+ * Copyright (C) 1999 - 2006, Digium, Inc.
+ *
+ * Tzafrir Cohen <tzafrir.cohen@xorcom.com>
+ *
+ * See http://www.asterisk.org for more information about
+ * the Asterisk project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
  */
+
+/*! \file
+ * \brief print libtonozone data as Asterisk indications.conf
+ */ 
 
 #include <stdio.h>
 #include <stdlib.h>
