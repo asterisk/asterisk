1fb6111c18f82834dace28553a8ccbc0f0e22933
Joshua Colp <jcolp@digium.com>
2006-07-06 15:55:05 +0000

    Merged revisions 37173 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.2
    ........
    r37173 | file | 2006-07-06 11:48:07 -0400 (Thu, 06 Jul 2006) | 2 lines
    
    Instead of giving the scheduled item ID on a peer expiration, give the time until they expire (issue #7455 reported by slavon)
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@37174 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -9631,7 +9631,7 @@ static int _sip_show_peer(int type, int fd, struct mansession *s, struct message
 		ast_cli(fd, "  Dynamic      : %s\n", (ast_test_flag(&peer->flags[1], SIP_PAGE2_DYNAMIC)?"Yes":"No"));
 		ast_cli(fd, "  Callerid     : %s\n", ast_callerid_merge(cbuf, sizeof(cbuf), peer->cid_name, peer->cid_num, "<unspecified>"));
 		ast_cli(fd, "  MaxCallBR    : %d kbps\n", peer->maxcallbitrate);
-		ast_cli(fd, "  Expire       : %d\n", peer->expire);
+		ast_cli(fd, "  Expire       : %ld\n", ast_sched_when(sched, peer->expire));
 		ast_cli(fd, "  Insecure     : %s\n", insecure2str(ast_test_flag(&peer->flags[0], SIP_INSECURE_PORT), ast_test_flag(&peer->flags[0], SIP_INSECURE_INVITE)));
 		ast_cli(fd, "  Nat          : %s\n", nat2str(ast_test_flag(&peer->flags[0], SIP_NAT)));
 		ast_cli(fd, "  ACL          : %s\n", (peer->ha?"Yes":"No"));
