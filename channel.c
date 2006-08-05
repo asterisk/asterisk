5e3b7eec7d628b1c12c5c0f0b8593c76d9fccff5
Russell Bryant <russell@russellbryant.com>
2006-08-05 05:07:39 +0000

    suppress a compiler warning about the usage of a potentially uninitialized variable
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.2@38903 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channel.c
+++ channel.c
@@ -3440,7 +3440,7 @@ enum ast_bridge_result ast_channel_bridge(struct ast_channel *c0, struct ast_cha
 	}
 
 	for (/* ever */;;) {
-		struct timeval now;
+		struct timeval now = { 0, };
 		int to;
 
 		to = -1;
