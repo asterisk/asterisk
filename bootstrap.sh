2eb7c010c71b41f6d40706581cb0ac410930dac6
Russell Bryant <russell@russellbryant.com>
2006-08-25 16:19:58 +0000

    we use autoconf 2.60.
    Note that this only affects people regenerating the configure script.
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@41088 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,17 +12,17 @@ check_for_app() {
 uname -s | grep -q FreeBSD
 if [ $? = 0 ]
 then
-	check_for_app autoconf259
-	check_for_app autoheader259
+	check_for_app autoconf260
+	check_for_app autoheader260
 	check_for_app automake19
 	check_for_app aclocal19
 	echo "Generating the configure script ..."
 	aclocal19 2>/dev/null
-	autoconf259
-	autoheader259
+	autoconf260
+	autoheader260
 	automake19 --add-missing --copy 2>/dev/null
 else
-	AUTOCONF_VERSION=2.59
+	AUTOCONF_VERSION=2.60
 	AUTOMAKE_VERSION=1.9
 	export AUTOCONF_VERSION
 	export AUTOMAKE_VERSION
