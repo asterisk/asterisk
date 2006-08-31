7399d40ec7fbfbdbdae917a5d607555469ae2f8d
Anthony LaMantia <alamantia@digium.com>
2006-08-31 20:49:26 +0000

    fix for bugid: 0007830
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@41630 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_record.c
+++ apps/app_record.c
@@ -302,6 +302,10 @@ static int record_exec(struct ast_channel *chan, void *data)
 			}
 		} else if ((f->frametype == AST_FRAME_DTMF) &&
 		    (f->subclass == terminator)) {
+			 res = ast_writestream(s,f);
+		         if(res) {
+			 	ast_log(LOG_WARNING,"Problem writing final recoard frame\n");
+			 }		 
 			ast_frfree(f);
 			break;
 		}
