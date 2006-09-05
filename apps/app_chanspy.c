68de3c0b2916379f55c3d3e9cd4d916d0e3cfb23
Joshua Colp <jcolp@digium.com>
2006-09-05 20:02:48 +0000

    Merge in last round of spy fixes. This should hopefully eliminate all the issues people have been seeing by distinctly separating what each component (core/spy) is responsible for. Core is responsible for adding a spy to a channel, feeding frames to the spy, removing the spy from a channel, and telling the spy to stop. Spy is responsible for reading frames in, and cleaning up after itself.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.2@42054 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_chanspy.c
+++ apps/app_chanspy.c
@@ -207,21 +207,6 @@ static int start_spying(struct ast_channel *chan, struct ast_channel *spychan, s
 	return res;
 }
 
-static void stop_spying(struct ast_channel *chan, struct ast_channel_spy *spy) 
-{
-	/* If our status has changed to DONE, then the channel we're spying on is gone....
-	   DON'T TOUCH IT!!!  RUN AWAY!!! */
-	if (spy->status == CHANSPY_DONE)
-		return;
-
-	if (!chan)
-		return;
-
-	ast_mutex_lock(&chan->lock);
-	ast_channel_spy_remove(chan, spy);
-	ast_mutex_unlock(&chan->lock);
-};
-
 /* Map 'volume' levels from -4 through +4 into
    decibel (dB) settings for channel drivers
 */
@@ -338,7 +323,13 @@ static int channel_spy(struct ast_channel *chan, struct ast_channel *spyee, int 
 			}
 		}
 		ast_deactivate_generator(chan);
-		stop_spying(spyee, &csth.spy);
+
+		if (csth.spy.chan) {
+			csth.spy.status = CHANSPY_DONE;
+			ast_mutex_lock(&csth.spy.chan->lock);
+			ast_channel_spy_remove(csth.spy.chan, &csth.spy);
+			ast_mutex_unlock(&csth.spy.chan->lock);
+		}
 
 		if (option_verbose >= 2) {
 			ast_verbose(VERBOSE_PREFIX_2 "Done Spying on channel %s\n", name);
@@ -347,7 +338,7 @@ static int channel_spy(struct ast_channel *chan, struct ast_channel *spyee, int 
 		running = 0;
 	}
 
-	ast_mutex_destroy(&csth.spy.lock);
+	ast_channel_spy_free(&csth.spy);
 
 	return running;
 }
