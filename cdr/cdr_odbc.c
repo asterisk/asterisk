7a0a5b1480778658382b6c211719640b850e5e6e
Joshua Colp <jcolp@digium.com>
2006-10-23 00:27:39 +0000

    Merged revisions 45927 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.2
    ........
    r45927 | file | 2006-10-22 20:25:28 -0400 (Sun, 22 Oct 2006) | 2 lines
    
    Don't leak memory mmmk?
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@45928 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- cdr/cdr_odbc.c
+++ cdr/cdr_odbc.c
@@ -340,7 +340,6 @@ static int odbc_load_module(void)
 		goto out;
 	}
 
-	ast_config_destroy(cfg);
 	if (option_verbose > 2) {
 		ast_verbose( VERBOSE_PREFIX_3 "cdr_odbc: dsn is %s\n",dsn);
 		if (username)
@@ -365,6 +364,8 @@ static int odbc_load_module(void)
 		ast_log(LOG_ERROR, "cdr_odbc: Unable to register ODBC CDR handling\n");
 	}
 out:
+	if (cfg)
+		ast_config_destroy(cfg);
 	ast_mutex_unlock(&odbc_lock);
 	return res;
 }
