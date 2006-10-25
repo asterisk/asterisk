1b3411f2ff9b3fab98dc2ce2fd8cfd0c7f670551
Olle Johansson <oej@edvina.net>
2006-10-25 20:21:05 +0000

    Old todo: Don't add Contact headers on BYE and CANCEL.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@46277 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -5632,7 +5632,9 @@ static int reqprep(struct sip_request *req, struct sip_pvt *p, int sipmethod, in
 		add_header(req, "From", ot);
 		add_header(req, "To", of);
 	}
-	add_header(req, "Contact", p->our_contact);
+	/* Do not add Contact for BYE and Cancel requests */
+	if (sipmethod != SIP_BYE && sipmethod != SIP_CANCEL)
+		add_header(req, "Contact", p->our_contact);
 	copy_header(req, orig, "Call-ID");
 	add_header(req, "CSeq", tmp);
 
