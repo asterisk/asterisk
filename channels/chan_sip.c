2b6d3952e13d3683691439ccfdd27ef80590e105
Olle Johansson <oej@edvina.net>
2006-11-10 21:39:58 +0000

    Proper fix for adding debug...
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@47475 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -2018,9 +2018,11 @@ static int __sip_autodestruct(void *data)
 	if (p->owner) {
 		ast_log(LOG_WARNING, "Autodestruct on dialog '%s' with owner in place (Method: %s)\n", p->callid, sip_methods[p->method].text);
 		ast_queue_hangup(p->owner);
-	} else if (p->refer)
+	} else if (p->refer) {
+		if (option_debug > 2)
+			ast_log(LOG_DEBUG, "Finally hanging up channel after transfer: %s\n", p->callid);
 		transmit_request_with_auth(p, SIP_BYE, 0, XMIT_RELIABLE, 1);
-	else 
+	} else 
 		sip_destroy(p);
 	return 0;
 }
