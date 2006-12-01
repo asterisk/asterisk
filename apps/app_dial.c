359a553961ce2834e51ea1b953ef1fe76bf1d811
Kevin P. Fleming <kpfleming@digium.com>
2006-12-01 23:39:59 +0000

    Merged revisions 48193 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ................
    r48193 | kpfleming | 2006-12-01 17:37:28 -0600 (Fri, 01 Dec 2006) | 10 lines
    
    Merged revisions 48192 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.2
    
    ........
    r48192 | kpfleming | 2006-12-01 17:30:59 -0600 (Fri, 01 Dec 2006) | 2 lines
    
    if Dial() is going to send music-on-hold to the calling party, it has to send PROGRESS first to ensure that the reverse audio path has been setup first (BE-106)
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@48194 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_dial.c
+++ apps/app_dial.c
@@ -1430,6 +1430,7 @@ static int dial_exec_full(struct ast_channel *chan, void *data, struct ast_flags
 		if (ast_test_flag(outgoing, OPT_MUSICBACK)) {
 			moh = 1;
 			ast_moh_start(chan, opt_args[OPT_ARG_MUSICBACK], NULL);
+			ast_indicate(chan, AST_CONTROL_PROGRESS);
 		} else if (ast_test_flag(outgoing, OPT_RINGBACK)) {
 			ast_indicate(chan, AST_CONTROL_RINGING);
 			sentringing++;
