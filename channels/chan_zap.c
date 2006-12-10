f6cf1458adad637a1b04d098079b41fda9cf7cef
Steve Murphy <murf@digium.com>
2006-12-10 03:04:18 +0000

    Merged revisions 48371 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.2
    ........
    r48371 | murf | 2006-12-09 19:14:13 -0700 (Sat, 09 Dec 2006) | 1 line
    
    This version applies the patch suggested by stevens in bug 7836 (make inbound channel RINGING state consistent with other channels).
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@48372 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_zap.c
+++ channels/chan_zap.c
@@ -3508,8 +3508,11 @@ static int attempt_transfer(struct zt_pvt *p)
 		   stop if now if appropriate */
 		if (ast_bridged_channel(p->subs[SUB_THREEWAY].owner))
 			ast_queue_control(p->subs[SUB_THREEWAY].owner, AST_CONTROL_UNHOLD);
-		if (p->subs[SUB_THREEWAY].owner->_state == AST_STATE_RINGING) {
+		if (p->subs[SUB_REAL].owner->_state == AST_STATE_RINGING) {
 			ast_indicate(ast_bridged_channel(p->subs[SUB_REAL].owner), AST_CONTROL_RINGING);
+		}
+		if (p->subs[SUB_THREEWAY].owner->_state == AST_STATE_RING) {
+			tone_zone_play_tone(p->subs[SUB_THREEWAY].zfd, ZT_TONE_RINGTONE);
 		}
 		if (p->subs[SUB_REAL].owner->cdr) {
 			/* Move CDR from second channel to current one */
@@ -3533,8 +3536,12 @@ static int attempt_transfer(struct zt_pvt *p)
 		unalloc_sub(p, SUB_THREEWAY);
 	} else if (ast_bridged_channel(p->subs[SUB_THREEWAY].owner)) {
 		ast_queue_control(p->subs[SUB_REAL].owner, AST_CONTROL_UNHOLD);
-		if (p->subs[SUB_REAL].owner->_state == AST_STATE_RINGING)
+		if (p->subs[SUB_THREEWAY].owner->_state == AST_STATE_RINGING) {
 			ast_indicate(ast_bridged_channel(p->subs[SUB_THREEWAY].owner), AST_CONTROL_RINGING);
+		}
+		if (p->subs[SUB_REAL].owner->_state == AST_STATE_RING) {
+			tone_zone_play_tone(p->subs[SUB_REAL].zfd, ZT_TONE_RINGTONE);
+		}
 		if (p->subs[SUB_THREEWAY].owner->cdr) {
 			/* Move CDR from second channel to current one */
 			p->subs[SUB_REAL].owner->cdr = 
