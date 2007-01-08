46700b62af160f760b29c4e10449f0fb9a95dbfd
Joshua Colp <jcolp@digium.com>
2007-01-08 18:21:31 +0000

    Disable the more intense packet2packet bridging until the bugs can be worked out.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@50032 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/rtp.c
+++ main/rtp.c
@@ -90,6 +90,9 @@ static struct sockaddr_in rtcpdebugaddr;	/*!< Debug RTCP packets to/from this ho
 static int nochecksums;
 #endif
 
+/* Uncomment this to enable more intense native bridging, but note: this is currently buggy */
+/* #define P2P_INTENSE */
+
 /*!
  * \brief Structure representing a RTP session.
  *
@@ -2879,6 +2882,7 @@ static enum ast_bridge_result bridge_native_loop(struct ast_channel *c0, struct 
 }
 
 /*! \brief P2P RTP Callback */
+#ifdef P2P_INTENSE
 static int p2p_rtp_callback(int *id, int fd, short events, void *cbdata)
 {
 	int res = 0, hdrlen = 12;
@@ -2936,6 +2940,12 @@ static int p2p_callback_enable(struct ast_channel *chan, struct ast_rtp *rtp, in
 
 	return 1;
 }
+#else
+static int p2p_callback_enable(struct ast_channel *chan, struct ast_rtp *rtp, int *fds, int **iod)
+{
+	return 0;
+}
+#endif
 
 /*! \brief Helper function to switch a channel and RTP stream out of callback mode */
 static int p2p_callback_disable(struct ast_channel *chan, struct ast_rtp *rtp, int *fds, int **iod)
