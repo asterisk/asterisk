089486cfcfe66f5db1e337ec610b4c6046deeca7
Joshua Colp <jcolp@digium.com>
2007-02-02 16:59:53 +0000

    Merged revisions 53117 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.2
    ........
    r53117 | file | 2007-02-02 10:58:09 -0600 (Fri, 02 Feb 2007) | 2 lines
    
    Pass the glob expanded filename to process_text_line so that error messages contain the actual filename, not the original include one. (issue #8959 reported by tzafrir)
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@53118 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/config.c
+++ main/config.c
@@ -923,7 +923,7 @@ static struct ast_config *config_text_file_load(const char *database, const char
 				if (process_buf) {
 					char *buf = ast_strip(process_buf);
 					if (!ast_strlen_zero(buf)) {
-						if (process_text_line(cfg, &cat, buf, lineno, filename, withcomments)) {
+						if (process_text_line(cfg, &cat, buf, lineno, fn, withcomments)) {
 							cfg = NULL;
 							break;
 						}
