4331761b5ef77cfa50578311b5b33961be6ba9a8
Joshua Colp <jcolp@digium.com>
2007-02-16 22:20:55 +0000

    Merged revisions 55002 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ................
    r55002 | file | 2007-02-16 17:18:46 -0500 (Fri, 16 Feb 2007) | 10 lines
    
    Merged revisions 54999 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.2
    
    ........
    r54999 | file | 2007-02-16 17:13:45 -0500 (Fri, 16 Feb 2007) | 2 lines
    
    Do not send indications through ast_indicate in chan_agent but instead go directly to the technology. This way when indications are emulated they happen on the Agent channel and do not screw up formats on the channels. (issue #8439 reported by punkgode)
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@55003 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_agent.c
+++ channels/chan_agent.c
@@ -593,7 +593,7 @@ static int agent_indicate(struct ast_channel *ast, int condition, const void *da
 	int res = -1;
 	ast_mutex_lock(&p->lock);
 	if (p->chan)
-		res = ast_indicate_data(p->chan, condition, data, datalen);
+		res = p->chan->tech->indicate ? p->chan->tech->indicate(p->chan, condition, data, datalen) : -1;
 	else
 		res = 0;
 	ast_mutex_unlock(&p->lock);
