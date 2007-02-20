62cb480f286f0e110021ab960e6d0099c783552c
Joshua Colp <jcolp@digium.com>
2007-02-20 23:12:55 +0000

    Merged revisions 55688 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r55688 | file | 2007-02-20 18:08:45 -0500 (Tue, 20 Feb 2007) | 2 lines
    
    Don't test our own address against the localnet settings. At least one person has had issues as a result of this from #7051 so I'm reversing it. (issue #8821 reported by kokoskarokoska)
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@55697 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -1925,16 +1925,14 @@ static void build_via(struct sip_pvt *p)
  */
 static enum sip_result ast_sip_ouraddrfor(struct in_addr *them, struct in_addr *us)
 {
-	struct sockaddr_in theirs, ours;
+	struct sockaddr_in theirs;
 
 	/* Get our local information */
 	ast_ouraddrfor(them, us);
 	theirs.sin_addr = *them;
-	ours.sin_addr = *us;
 
 	if (localaddr && externip.sin_addr.s_addr &&
-	    ast_apply_ha(localaddr, &theirs) &&
-	    !ast_apply_ha(localaddr, &ours)) {
+	    ast_apply_ha(localaddr, &theirs)) {
 		if (externexpire && time(NULL) >= externexpire) {
 			struct ast_hostent ahp;
 			struct hostent *hp;
