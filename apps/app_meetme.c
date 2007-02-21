bb3aa2ee419b9bac7893e26e6e77d5156a575223
Russell Bryant <russell@russellbryant.com>
2007-02-21 20:22:33 +0000

    Simplify the last change to app_meetme, and move the call to dispose_conf() up into the block where we know a conf exists.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@55951 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_meetme.c
+++ apps/app_meetme.c
@@ -2536,8 +2536,6 @@ static int conf_exec(struct ast_channel *chan, void *data)
 					/* Run the conference */
 					res = conf_run(chan, cnf, confflags.flags, optargs);
 				}
-			}
-			if (cnf) {
 				dispose_conf(cnf);
 				cnf = NULL;
 			}
