acf96db39fe2411c163783394c0e939797fa42cb
Joshua Colp <jcolp@digium.com>
2007-02-23 21:57:07 +0000

    Merged revisions 56457 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r56457 | file | 2007-02-23 16:53:41 -0500 (Fri, 23 Feb 2007) | 2 lines
    
    Change log notice to debug. It is possible for a scheduled item to execute and be deleted at close to the same time and unavoidable. If this happens this message creeps up.
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@56458 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/sched.c
+++ main/sched.c
@@ -281,7 +281,8 @@ int ast_sched_del(struct sched_context *con, int id)
 	ast_mutex_unlock(&con->lock);
 
 	if (!s) {
-		ast_log(LOG_NOTICE, "Attempted to delete nonexistent schedule entry %d!\n", id);
+		if (option_debug)
+			ast_log(LOG_DEBUG, "Attempted to delete nonexistent schedule entry %d!\n", id);
 #ifdef DO_CRASH
 		CRASH;
 #endif
