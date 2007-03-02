e23433e50ed8f23af594a756b3bbfb08ae865a13
Joshua Colp <jcolp@digium.com>
2007-03-02 17:09:18 +0000

    Merged revisions 57477 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ................
    r57477 | file | 2007-03-02 12:06:52 -0500 (Fri, 02 Mar 2007) | 10 lines
    
    Merged revisions 57475 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.2
    
    ........
    r57475 | file | 2007-03-02 12:02:46 -0500 (Fri, 02 Mar 2007) | 2 lines
    
    If a SIP message comes in and goes to a method handler that requires additional values that may not be present then send back an error.
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@57478 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -15324,6 +15324,12 @@ static int handle_request(struct sip_pvt *p, struct sip_request *req, struct soc
 		}
 	}
 
+	if (!e && (p->method == SIP_INVITE || p->method == SIP_SUBSCRIBE || p->method == SIP_REGISTER || p->method == SIP_NOTIFY)) {
+		transmit_response(p, "503 Server error", req);
+		sip_scheddestroy(p, DEFAULT_TRANS_TIMEOUT);
+		return -1;
+	}
+
 	/* Handle various incoming SIP methods in requests */
 	switch (p->method) {
 	case SIP_OPTIONS:
