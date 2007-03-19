7246207bd88b57b2f3ef8f07df7ba508ac40b38c
Tilghman Lesher <tilghman@meg.abyt.es>
2007-03-19 22:29:56 +0000

    Oops, this should have been a %d all along
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@59049 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- funcs/func_strings.c
+++ funcs/func_strings.c
@@ -283,7 +283,7 @@ static int acf_sprintf(struct ast_channel *chan, char *cmd, char *data, char *bu
 				formatbuf[&arg.format[i] - formatstart + 1] = '\0';
 
 				/* Convert the argument into the required type */
-				if (sscanf(arg.var[argcount++], "%i", &tmpi) != 1) {
+				if (sscanf(arg.var[argcount++], "%d", &tmpi) != 1) {
 					ast_log(LOG_ERROR, "Argument '%s' is not an integer number for format '%s'\n", arg.var[argcount - 1], formatbuf);
 					goto sprintf_fail;
 				}
