7ca58639653b444ad06600b5de9ae244d9d1addd
Tilghman Lesher <tilghman@meg.abyt.es>
2007-03-27 23:36:49 +0000

    Oops
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.2@59283 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_voicemail.c
+++ apps/app_voicemail.c
@@ -1262,7 +1262,7 @@ static int store_file(char *dir, char *mailboxuser, char *mailboxcontext, int ms
 		lseek(fd, 0, SEEK_SET);
 		printf("Length is %d\n", fdlen);
 		fdm = mmap(NULL, fdlen, PROT_READ | PROT_WRITE, MAP_SHARED,fd, 0);
-		if (fdm != MAP_FAILED) {
+		if (fdm == MAP_FAILED) {
 			ast_log(LOG_WARNING, "Memory map failed!\n");
 			goto yuck;
 		} 
