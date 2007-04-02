6408918920cf58aa9b6d538f5b86534bcb3d0002
Russell Bryant <russell@russellbryant.com>
2007-04-02 15:39:07 +0000

    Merged revisions 59608 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.2
    ........
    r59608 | russell | 2007-04-01 17:35:25 -0500 (Sun, 01 Apr 2007) | 6 lines
    
    Add the SO_REUSEADDR flag to sockets handled by netsock.  This is needed by
    the patch that went in for issue 7874.  chan_iax2 needs to be able to create
    socket that is lisetning on INADDR_ANY, but also be able to bind sockets to
    specific addresses.  (Thanks to Stevenson on the asterisk-dev mailing list
    for explaining why this flag was needed.)
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@59654 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/netsock.c
+++ main/netsock.c
@@ -125,6 +125,7 @@ struct ast_netsock *ast_netsock_bindaddr(struct ast_netsock_list *list, struct i
 	int *ioref;
 	
 	struct ast_netsock *ns;
+	const int reuseFlag = 1;
 	
 	/* Make a UDP socket */
 	netsocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
@@ -132,6 +133,9 @@ struct ast_netsock *ast_netsock_bindaddr(struct ast_netsock_list *list, struct i
 	if (netsocket < 0) {
 		ast_log(LOG_ERROR, "Unable to create network socket: %s\n", strerror(errno));
 		return NULL;
+	}
+	if (setsockopt(netsocket, SOL_SOCKET, SO_REUSEADDR, (char *)&reuseFlag, sizeof reuseFlag) < 0) {
+			ast_log(LOG_WARNING, "Error setting SO_REUSEADDR on sockfd '%d'\n", netsocket);
 	}
 	if (bind(netsocket,(struct sockaddr *)bindaddr, sizeof(struct sockaddr_in))) {
 		ast_log(LOG_ERROR, "Unable to bind to %s port %d: %s\n", ast_inet_ntoa(bindaddr->sin_addr), ntohs(bindaddr->sin_port), strerror(errno));
