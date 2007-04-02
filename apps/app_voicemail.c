a6d7b13160cf8590f4d810b259bb878276c31eb1
Joshua Colp <jcolp@digium.com>
2007-04-02 19:01:04 +0000

    Merged revisions 59724 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ................
    r59724 | file | 2007-04-02 14:58:24 -0400 (Mon, 02 Apr 2007) | 10 lines
    
    Merged revisions 59723 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.2
    
    ........
    r59723 | file | 2007-04-02 14:55:25 -0400 (Mon, 02 Apr 2007) | 2 lines
    
    Increase the maximum size for a string of mailboxes to 1024. (issue #9270 reported by rtucker)
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@59725 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_voicemail.c
+++ apps/app_voicemail.c
@@ -2891,7 +2891,7 @@ static int leave_voicemail(struct ast_channel *chan, char *ext, struct leave_vm_
 	char fmt[80];
 	char *context;
 	char ecodes[16] = "#";
-	char tmp[256] = "", *tmpptr;
+	char tmp[1024] = "", *tmpptr;
 	struct ast_vm_user *vmu;
 	struct ast_vm_user svm;
 	const char *category = NULL;
