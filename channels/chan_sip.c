721f85d0848b1ca1edc33cc22e9c43eb2f84e882
Joshua Colp <jcolp@digium.com>
2007-04-24 16:10:10 +0000

    Merged revisions 61772 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ................
    r61772 | file | 2007-04-24 12:07:02 -0400 (Tue, 24 Apr 2007) | 10 lines
    
    Merged revisions 61771 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.2
    
    ........
    r61771 | file | 2007-04-24 12:05:06 -0400 (Tue, 24 Apr 2007) | 2 lines
    
    Allow RFC2833 to be sent in the response SDP when an INVITE comes in without SDP. (issue #9546 reported by mcrawford)
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@61773 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -9675,6 +9675,7 @@ static enum check_auth_result check_user_ok(struct sip_pvt *p, char *of,
 			p->noncodeccapability |= AST_RTP_DTMF;
 		else
 			p->noncodeccapability &= ~AST_RTP_DTMF;
+		p->jointnoncodeccapability = p->noncodeccapability;
 		if (p->t38.peercapability)
 			p->t38.jointcapability &= p->t38.peercapability;
 		p->maxcallbitrate = user->maxcallbitrate;
@@ -9811,6 +9812,7 @@ static enum check_auth_result check_peer_ok(struct sip_pvt *p, char *of,
 			p->noncodeccapability |= AST_RTP_DTMF;
 		else
 			p->noncodeccapability &= ~AST_RTP_DTMF;
+		p->jointnoncodeccapability = p->noncodeccapability;
 		if (p->t38.peercapability)
 			p->t38.jointcapability &= p->t38.peercapability;
 	}
