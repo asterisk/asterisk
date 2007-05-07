71befa9884eaab7260614c3214e1b33fc7da1384
Joshua Colp <jcolp@digium.com>
2007-05-07 21:39:52 +0000

    Properly handle what happens during a masquerade in relation to group counting. (issue #9657 reported by ramonpeek)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.2@63285 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- app.c
+++ app.c
@@ -1113,6 +1113,20 @@ int ast_app_group_match_get_count(char *groupmatch, char *category)
 	return count;
 }
 
+int ast_app_group_update(struct ast_channel *old, struct ast_channel *new)
+{
+	struct ast_group_info *gi = NULL;
+
+	AST_LIST_LOCK(&groups);
+	AST_LIST_TRAVERSE(&groups, gi, list) {
+		if (gi->chan == old)
+			gi->chan = new;
+	}
+	AST_LIST_UNLOCK(&groups);
+
+	return 0;
+}
+
 int ast_app_group_discard(struct ast_channel *chan)
 {
 	struct ast_group_info *gi = NULL;
