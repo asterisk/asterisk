42c63dd2edbc6e97aa922bb19b4dd23c6dd8d4d1
Joshua Colp <jcolp@digium.com>
2007-05-20 14:48:31 +0000

    Add the adsistub file to the Asterisk makefile, fix a stub definition, and no longer make the symbols from res_adsi global since they don't need to be.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@65233 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_adsi.c
+++ res/res_adsi.c
@@ -1175,7 +1175,7 @@ static int unload_module(void)
 	return -1;
 }
 
-AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_GLOBAL_SYMBOLS, "ADSI Resource",
+AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_DEFAULT, "ADSI Resource",
 		.load = load_module,
 		.unload = unload_module,
 		.reload = reload,
