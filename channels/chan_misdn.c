6e2816fbea979428ca227accd1d0f00f5359f340
Automerge Script <automerge@asterisk.org>
2007-06-05 11:17:13 +0000

    automerge commit
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.2-netsec@67238 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_misdn.c
+++ channels/chan_misdn.c
@@ -2404,6 +2404,16 @@ enum ast_bridge_result  misdn_bridge (struct ast_channel *c0,
 	else
 		return -1;
 
+	int p1_b, p2_b;
+
+	misdn_cfg_get(ch1->bc->port, MISDN_CFG_BRIDGING, &p1_b, sizeof(int));
+	misdn_cfg_get(ch2->bc->port, MISDN_CFG_BRIDGING, &p2_b, sizeof(int));
+	
+	if ( ! p1_b || ! p2_b) {
+		ast_log(LOG_NOTICE, "Falling back to Asterisk bridging\n");
+		return AST_BRIDGE_FAILED;
+	}
+
 	int bridging;
 	misdn_cfg_get( 0, MISDN_GEN_BRIDGING, &bridging, sizeof(int));
 	if (bridging) {
@@ -2842,6 +2852,7 @@ static struct ast_channel *misdn_new(struct chan_list *chlist, int state,  char 
 		
 		int bridging;
 		misdn_cfg_get( 0, MISDN_GEN_BRIDGING, &bridging, sizeof(int));
+		
 		if (bridging)
 			tmp->tech = &misdn_tech;
 		else
