f5f018a209b57826bf4891e00c2dceae3d0798e2
Christian Richter <christian.richter@beronet.com>
2007-06-20 15:42:39 +0000

    forgot one place ..
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.2@70342 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/misdn/isdn_lib.c
+++ channels/misdn/isdn_lib.c
@@ -3394,7 +3394,7 @@ int misdn_lib_send_event(struct misdn_bchannel *bc, enum event_e event )
 			clean_up_bc(bc);
 			
 			if (channel>0)
-				empty_chan_in_stack(stack,bc->channel);
+				empty_chan_in_stack(stack,channel);
 			
 			bc->in_use=0;
 		}
