aff5425deac1ca59f1275b66e7c46c19495570d3
Brett Bryant <bbryant@digium.com>
2007-06-22 14:56:36 +0000

    Merged revisions 71066 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ................
    r71066 | bbryant | 2007-06-22 09:53:08 -0500 (Fri, 22 Jun 2007) | 18 lines
    
    Merged revisions 71064 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.2
    
    ........
    r71064 | bbryant | 2007-06-22 09:39:34 -0500 (Fri, 22 Jun 2007) | 10 lines
    
    Fixed infinite loop when controlling terminal was lost
    and return value of input function wasn't checked for
    errors. This would cause 100% cpu to be taken up.
    
    (closes issue #9654, issue #10010)
    Reported by: mnicholson, and eserra
    
    Idea for the patch from mnicholson, patched by me
    
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@71067 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/asterisk.c
+++ main/asterisk.c
@@ -2232,6 +2232,9 @@ static void ast_remotecontrol(char * data)
 	for (;;) {
 		ebuf = (char *)el_gets(el, &num);
 
+		if (!ebuf)
+			break;
+
 		if (!ast_strlen_zero(ebuf)) {
 			if (ebuf[strlen(ebuf)-1] == '\n')
 				ebuf[strlen(ebuf)-1] = '\0';
@@ -2957,12 +2960,14 @@ int main(int argc, char *argv[])
 
 		for (;;) {
 			buf = (char *)el_gets(el, &num);
-			if (buf) {
-				if (buf[strlen(buf)-1] == '\n')
-					buf[strlen(buf)-1] = '\0';
+			if (!buf)
+				break;
 
-				consolehandler((char *)buf);
-			} else if (ast_opt_remote && (write(STDOUT_FILENO, "\nUse EXIT or QUIT to exit the asterisk console\n",
+			if (buf[strlen(buf)-1] == '\n')
+				buf[strlen(buf)-1] = '\0';
+			consolehandler((char *)buf);
+
+			if (!buf && ast_opt_remote && (write(STDOUT_FILENO, "\nUse EXIT or QUIT to exit the asterisk console\n",
 				   strlen("\nUse EXIT or QUIT to exit the asterisk console\n")) < 0)) {
 				/* Whoa, stdout disappeared from under us... Make /dev/null's */
 				int fd;
