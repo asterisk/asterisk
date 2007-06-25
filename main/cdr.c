879bad257f2378f69365e04d905f907e23f43732
Joshua Colp <jcolp@digium.com>
2007-06-25 01:07:31 +0000

    Fix it so 1.4 actually compiles on my box.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@71422 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/cdr.c
+++ main/cdr.c
@@ -525,7 +525,7 @@ void ast_cdr_merge(struct ast_cdr *to, struct ast_cdr *from)
 	if (ast_test_flag(from, AST_CDR_FLAG_LOCKED)) {
 		discard_from = 1;
 		if (lto) {
-			struct ast_cdr *llfrom;
+			struct ast_cdr *llfrom = NULL;
 			/* insert the from stuff after lto */
 			lto->next = from;
 			lfrom = from;
@@ -540,7 +540,7 @@ void ast_cdr_merge(struct ast_cdr *to, struct ast_cdr *from)
 		} else {
 			/* save copy of the current *to cdr */
 			struct ast_cdr tcdr;
-			struct ast_cdr *llfrom;
+			struct ast_cdr *llfrom = NULL;
 			memcpy(&tcdr, to, sizeof(tcdr));
 			/* copy in the locked from cdr */
 			memcpy(to, from, sizeof(*to));
