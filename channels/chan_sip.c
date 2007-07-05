134a556c9fc6afc4eadd1f1a438b293669e989b0
Russell Bryant <russell@russellbryant.com>
2007-07-05 23:59:50 +0000

    Merged revisions 73598 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r73598 | russell | 2007-07-05 18:59:22 -0500 (Thu, 05 Jul 2007) | 3 lines
    
    Fix a crash in chan_sip.  Don't try to stop the monitor thread if it was never
    started.  (closes issue #10124, reported by gzero, fixed by me)
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@73599 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -18460,7 +18460,7 @@ static int unload_module(void)
 	dialoglist_unlock();
 
 	ast_mutex_lock(&monlock);
-	if (monitor_thread && (monitor_thread != AST_PTHREADT_STOP)) {
+	if (monitor_thread && (monitor_thread != AST_PTHREADT_STOP) && (monitor_thread != AST_PTHREADT_NULL)) {
 		pthread_cancel(monitor_thread);
 		pthread_kill(monitor_thread, SIGURG);
 		pthread_join(monitor_thread, NULL);
