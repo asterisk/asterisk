cc19ba80f5d01b7f1deb91da0423fc00e1ff2f9b
Kevin P. Fleming <kpfleming@digium.com>
2007-07-05 22:29:37 +0000

    Merged revisions 73548 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ................
    r73548 | kpfleming | 2007-07-05 17:20:44 -0500 (Thu, 05 Jul 2007) | 10 lines
    
    Merged revisions 73547 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.2
    
    ........
    r73547 | kpfleming | 2007-07-05 17:11:51 -0500 (Thu, 05 Jul 2007) | 2 lines
    
    we shouldn't allow G.723.1 endpoints to use VAD, just like we don't support it for G.729
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@73550 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -6449,6 +6449,9 @@ static void add_codec_to_sdp(const struct sip_pvt *p, int codec, int sample_rate
 	if (codec == AST_FORMAT_G729A) {
 		/* Indicate that we don't support VAD (G.729 annex B) */
 		ast_build_string(a_buf, a_size, "a=fmtp:%d annexb=no\r\n", rtp_code);
+	} else if (codec == AST_FORMAT_G723_1) {
+		/* Indicate that we don't support VAD (G.723.1 annex A) */
+		ast_build_string(a_buf, a_size, "a=fmtp:%d annexa=no\r\n", rtp_code);
 	} else if (codec == AST_FORMAT_ILBC) {
 		/* Add information about us using only 20/30 ms packetization */
 		ast_build_string(a_buf, a_size, "a=fmtp:%d mode=%d\r\n", rtp_code, fmt.cur_ms);
