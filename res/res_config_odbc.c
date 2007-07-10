760649be7569f9fee4923a27c91be76022df065f
Russell Bryant <russell@russellbryant.com>
2007-07-10 15:32:21 +0000

    Merged revisions 74314 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ................
    r74314 | russell | 2007-07-10 10:31:41 -0500 (Tue, 10 Jul 2007) | 11 lines
    
    Merged revisions 74313 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.2
    
    ........
    r74313 | russell | 2007-07-10 10:30:20 -0500 (Tue, 10 Jul 2007) | 3 lines
    
    Only use ESCAPE when LIKE is used.  
    (issue #10075, this part reported by jmls on IRC, patch by me)
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@74315 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_config_odbc.c
+++ res/res_config_odbc.c
@@ -109,7 +109,8 @@ static struct ast_variable *realtime_odbc(const char *database, const char *tabl
 		newval = va_arg(aq, const char *);
 	}
 	va_end(aq);
-	snprintf(sql + strlen(sql), sizeof(sql) - strlen(sql), " ESCAPE '\\'");
+	if (strcasestr(sql, "LIKE"))
+		snprintf(sql + strlen(sql), sizeof(sql) - strlen(sql), " ESCAPE '\\'");
 
 	res = SQLPrepare(stmt, (unsigned char *)sql, SQL_NTS);
 	if ((res != SQL_SUCCESS) && (res != SQL_SUCCESS_WITH_INFO)) {
@@ -266,7 +267,8 @@ static struct ast_config *realtime_multi_odbc(const char *database, const char *
 	if (initfield)
 		snprintf(sql + strlen(sql), sizeof(sql) - strlen(sql), " ORDER BY %s", initfield);
 	va_end(aq);
-	snprintf(sql + strlen(sql), sizeof(sql) - strlen(sql), " ESCAPE '\\'");
+	if (strcasestr(sql, "LIKE"))
+		snprintf(sql + strlen(sql), sizeof(sql) - strlen(sql), " ESCAPE '\\'");
 
 	res = SQLPrepare(stmt, (unsigned char *)sql, SQL_NTS);
 	if ((res != SQL_SUCCESS) && (res != SQL_SUCCESS_WITH_INFO)) {
