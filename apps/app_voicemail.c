2600cd5d7b60dd3bc3e0ea3f2407152d28b9f3dd
Jason Parker <jparker@digium.com>
2007-07-10 15:39:39 +0000

    Merged revisions 74317 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    (Closes issue #10170)
    ................
    r74317 | qwell | 2007-07-10 10:38:32 -0500 (Tue, 10 Jul 2007) | 12 lines
    
    Merged revisions 74316 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.2
    
    ........
    r74316 | qwell | 2007-07-10 10:37:54 -0500 (Tue, 10 Jul 2007) | 4 lines
    
    Fix a small typo in description in of Voicemail() application.
    
    Issue 10170, patch by casper.
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@74318 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_voicemail.c
+++ apps/app_voicemail.c
@@ -495,7 +495,7 @@ static char *descrip_vm =
 	"           message. The units are whole-number decibels (dB).\n"
 	"    s    - Skip the playback of instructions for leaving a message to the\n"
 	"           calling party.\n"
-	"    u    - Play the 'unavailable greeting.\n";
+	"    u    - Play the 'unavailable' greeting.\n";
 
 static char *synopsis_vmain = "Check Voicemail messages";
 
