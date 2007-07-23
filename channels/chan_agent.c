46cb5961ea5d45c0b3eb6cabb5b1fe3d0976d70a
Automerge Script <automerge@asterisk.org>
2007-07-23 19:30:11 +0000

    automerge commit
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.2-netsec@76702 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_agent.c
+++ channels/chan_agent.c
@@ -217,9 +217,9 @@ static struct agent_pvt *agents = NULL;  /**< Holds the list of agents (loaded f
 			ast_set_read_format(ast, ast->readformat); \
 			ast_set_write_format(ast, ast->writeformat); \
 		} \
-		if (p->chan->readformat != ast->rawreadformat)  \
+		if (p->chan->readformat != ast->rawreadformat && !p->chan->generator)  \
 			ast_set_read_format(p->chan, ast->rawreadformat); \
-		if (p->chan->writeformat != ast->rawwriteformat) \
+		if (p->chan->writeformat != ast->rawwriteformat && !p->chan->generator) \
 			ast_set_write_format(p->chan, ast->rawwriteformat); \
 	} \
 } while(0)
