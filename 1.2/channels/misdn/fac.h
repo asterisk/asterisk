1952f00c95da4ccc7a5d83b46e35eb00f975c942
Jason Parker <jparker@digium.com>
2007-07-24 17:11:41 +0000

    Gotta love it when a public server that we use in a post-commit hook "dies", causing issues like this.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/tags/1.2.23@76818 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- 1.2/channels/misdn/fac.h
+++
@@ -1,8 +0,0 @@
-#ifndef FAC_H
-#define FAC_H
-
-void fac_enc( unsigned char **ntmsg, msg_t * msg, enum facility_type type,  union facility fac, struct misdn_bchannel *bc);
-
-void fac_dec( unsigned char *p, Q931_info_t *qi, enum facility_type *type,  union facility *fac, struct misdn_bchannel *bc);
-
-#endif
