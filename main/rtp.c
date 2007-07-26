f1aadc8161913d72c72023685f1e831f615a6007
Luigi Rizzo <rizzo@icir.org>
2007-07-26 04:47:54 +0000

    add an entry for h263plus in an empty slot of the rtp types.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@77233 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/rtp.c
+++ main/rtp.c
@@ -1672,6 +1672,7 @@ static struct rtpPayloadType static_RTP_PT[MAX_RTP_PT] = {
 	[31] = {1, AST_FORMAT_H261},
 	[34] = {1, AST_FORMAT_H263},
 	[97] = {1, AST_FORMAT_ILBC},
+	[98] = {1, AST_FORMAT_H263_PLUS},
 	[99] = {1, AST_FORMAT_H264},
 	[101] = {0, AST_RTP_DTMF},
 	[102] = {1, AST_FORMAT_T140},	/* Real time text chat */
