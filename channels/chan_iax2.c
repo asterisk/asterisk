171791dd8b660c8bead4ba41f49cd6589b4d44bf
Russell Bryant <russell@russellbryant.com>
2007-08-02 16:58:26 +0000

    Merged revisions 77939 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r77939 | russell | 2007-08-02 11:56:04 -0500 (Thu, 02 Aug 2007) | 4 lines
    
    Add another sanity check to vnak_retransmit().  This check ensures that frames
    that have already been marked for deletion don't get retransmitted.
    (closes issue #10361, patch from mihai)
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@77940 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_iax2.c
+++ channels/chan_iax2.c
@@ -6266,7 +6266,8 @@ static void vnak_retransmit(int callno, int last)
 	AST_LIST_TRAVERSE(&frame_queue, f, list) {
 		/* Send a copy immediately */
 		if ((f->callno == callno) && iaxs[f->callno] &&
-			((unsigned char ) (f->oseqno - last) < 128)) {
+			((unsigned char ) (f->oseqno - last) < 128) &&
+			(f->retries >= 0)) {
 			send_packet(f);
 		}
 	}
