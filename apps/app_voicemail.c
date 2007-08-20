e18bdff54a5b4f2fb4555b5fe8321f86d424312a
Tilghman Lesher <tilghman@meg.abyt.es>
2007-08-20 02:46:02 +0000

    Merged revisions 79998 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r79998 | tilghman | 2007-08-19 21:42:49 -0500 (Sun, 19 Aug 2007) | 2 lines
    
    Missing curly braces.  Oops.  (Reported by snuffy via IRC)
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@79999 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_voicemail.c
+++ apps/app_voicemail.c
@@ -3063,10 +3063,11 @@ static int leave_voicemail(struct ast_channel *chan, char *ext, struct leave_vm_
 			if (ast_streamfile(chan, prefile, chan->language) > -1) 
 				res = ast_waitstream(chan, ecodes);
 #ifdef ODBC_STORAGE
-			if(success == -1) 
-				/*We couldn't retrieve the file from the database, but we found it on the file system. Let's put it in the database*/
+			if (success == -1) {
+				/* We couldn't retrieve the file from the database, but we found it on the file system. Let's put it in the database. */
 				ast_debug(1, "Greeting not retrieved from database, but found in file storage. Inserting into database\n");
 				store_file(prefile, vmu->mailbox, vmu->context, -1);
+			}
 #endif
 		} else {
 			ast_debug(1, "%s doesn't exist, doing what we can\n", prefile);
