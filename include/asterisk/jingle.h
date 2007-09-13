43d63b102735f32355b7690a74577e6f9da6cab5
Philippe Sultan <philippe.sultan@gmail.com>
2007-09-13 15:10:08 +0000

    Assign namespace properly
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@82318 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- include/asterisk/jingle.h
+++ include/asterisk/jingle.h
@@ -39,7 +39,7 @@
 
 #define JINGLE_NS "http://www.xmpp.org/extensions/xep-0166.html#ns"
 #define JINGLE_DTMF_NS "http://www.xmpp.org/extensions/xep-0181.html#ns"
-#define JINGLE_DTMF_NS_ERROR "http://www.xmpp.org/extensions/xep-0181.html#ns"
+#define JINGLE_DTMF_NS_ERRORS "http://www.xmpp.org/extensions/xep-0181.html#ns-errors"
 #define GOOGLE_NS "http://www.google.com/session"
 
 #define JINGLE_SID "sid"
