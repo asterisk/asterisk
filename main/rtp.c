3dcf938f73baa4b17d06a8db6a85419bebb08a2b
Joshua Colp <jcolp@digium.com>
2007-10-08 15:37:46 +0000

    Update codec information as well as address when doing hold reinvites. (issue #10868) Reported by: mavince
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@85023 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/rtp.c
+++ main/rtp.c
@@ -2949,6 +2949,11 @@ static enum ast_bridge_result bridge_native_loop(struct ast_channel *c0, struct 
 				memcpy(&ac0, &t0, sizeof(ac0));
 				ast_rtp_get_peer(p1, &t1);
 				memcpy(&ac1, &t1, sizeof(ac1));
+				/* Update codec information */
+				if (pr0->get_codec)
+					oldcodec0 = codec0 = pr0->get_codec(c0);
+				if (pr1->get_codec)
+					oldcodec1 = codec1 = pr1->get_codec(c1);
 				ast_indicate_data(other, fr->subclass, fr->data, fr->datalen);
 				ast_frfree(fr);
 			} else {
