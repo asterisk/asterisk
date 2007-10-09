474e0e8d129072b058eff1930d9673aca3d738d0
Joshua Colp <jcolp@digium.com>
2007-10-09 14:30:16 +0000

    Don't perform a reinvite if a transfer is in progress. (issue #10915) Reported by: ramonpeek
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@85093 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -17262,7 +17262,7 @@ static int sip_set_rtp_peer(struct ast_channel *chan, struct ast_rtp *rtp, struc
 			changed = 1;
 		}
 	}
-	if (changed && !ast_test_flag(&p->flags[0], SIP_GOTREFER)) {
+	if (changed && !ast_test_flag(&p->flags[0], SIP_GOTREFER) && !ast_test_flag(&p->flags[0], SIP_DEFER_BYE_ON_TRANSFER)) {
 		if (chan->_state != AST_STATE_UP) {	/* We are in early state */
 			if (!ast_test_flag(&p->flags[0], SIP_NO_HISTORY))
 				append_history(p, "ExtInv", "Initial invite sent with remote bridge proposal.");
