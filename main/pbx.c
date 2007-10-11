c0fcb59d38b2b795ce585ed24b823f61df36b297
Tilghman Lesher <tilghman@meg.abyt.es>
2007-10-11 04:40:56 +0000

    Merged revisions 85356 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r85356 | tilghman | 2007-10-10 23:35:33 -0500 (Wed, 10 Oct 2007) | 2 lines
    
    A dollar sign by itself, not indicating a start of a variable or expression prematurely ends substitution (closes issue #10939)
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@85357 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/pbx.c
+++ main/pbx.c
@@ -1645,6 +1645,8 @@ static void pbx_substitute_variables_helper_full(struct ast_channel *c, struct v
 				nextexp = nextthing;
 				pos = nextexp - whereweare;
 				break;
+			default:
+				pos = 1;
 			}
 		}
 
@@ -1800,8 +1802,7 @@ static void pbx_substitute_variables_helper_full(struct ast_channel *c, struct v
 				count -= length;
 				cp2 += length;
 			}
-		} else
-			break;
+		}
 	}
 }
 
