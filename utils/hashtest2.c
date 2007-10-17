65786f15fa90a61166f27808238a6764b58cd41d
Steve Murphy <murf@digium.com>
2007-10-17 19:58:20 +0000

    more stub routines to allow linkage in stand-alone environment, with thread debugs turned on
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@86180 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- utils/hashtest2.c
+++ utils/hashtest2.c
@@ -61,6 +61,22 @@ int64_t ast_mark(int prof_id, int x)
 {
 }
 
+void ast_cli(int fd, const char *fmt, ...);
+void ast_cli(int fd, const char *fmt, ...)
+{
+}
+
+struct ast_cli_entry {
+	char * const cmda; /* just something to satisfy compile & link; will never be used */
+};
+
+int ast_cli_register_multiple(struct ast_cli_entry *e, int len);
+int ast_cli_register_multiple(struct ast_cli_entry *e, int len)
+{
+ return 0;
+}
+
+
 /* my OBJECT */
 struct ht_element 
 {
