72a8e08bf515cc1a03a31180c197fbe6b81e743e
Joshua Colp <jcolp@digium.com>
2007-11-26 16:20:04 +0000

    Revert change for 11348 until it can be looked at even more.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@89582 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/utils.c
+++ main/utils.c
@@ -1140,7 +1140,7 @@ long int ast_random(void)
 		int read_res = read(dev_urandom_fd, &res, sizeof(res));
 		if (read_res > 0) {
 			res = res < 0 ? ~res : res;
-			return res % ((long)RAND_MAX + 1);
+			return res;
 		}
 	}
 #endif
