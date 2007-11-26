daea9fd8e892cc71f45a756e046e3ec628087fda
Kevin P. Fleming <kpfleming@digium.com>
2007-11-26 17:20:36 +0000

    when parsing application options that take arguments, don't indicate that the option was supplied unless a non-zero-length argument was found for it
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@89586 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/app.c
+++ main/app.c
@@ -1394,7 +1394,6 @@ int ast_app_parse_options(const struct ast_app_option *options, struct ast_flags
 	s = optstr;
 	while (*s) {
 		curarg = *s++ & 0x7f;	/* the array (in app.h) has 128 entries */
-		ast_set_flag(flags, options[curarg].flag);
 		argloc = options[curarg].arg_index;
 		if (*s == '(') {
 			/* Has argument */
@@ -1411,6 +1410,8 @@ int ast_app_parse_options(const struct ast_app_option *options, struct ast_flags
 		} else if (argloc) {
 			args[argloc - 1] = NULL;
 		}
+		if (!argloc || !ast_strlen_zero(args[argloc - 1]))
+			ast_set_flag(flags, options[curarg].flag);
 	}
 
 	return res;
