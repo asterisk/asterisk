ec846d6cf29e35d7b072d575d604ec3572c2a892
Russell Bryant <russell@russellbryant.com>
2007-12-05 00:49:19 +0000

    Merged revisions 91074 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r91074 | russell | 2007-12-04 18:48:47 -0600 (Tue, 04 Dec 2007) | 4 lines
    
    When DEBUG_THREADS is enabled, we only have the details about who is holding
    a lock that we are waiting on for a mutex, not rwlocks.  This should fix the
    problem where people have reported "core show locks" crashing sometimes.
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@91077 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/utils.c
+++ main/utils.c
@@ -756,6 +756,10 @@ static char *handle_show_locks(struct ast_cli_entry *e, int cmd, struct ast_cli_
 			if (!lock_info->locks[i].pending)
 				continue;
 
+			/* We only have further details for mutexes right now */
+			if (lock_info->locks[i].type != AST_MUTEX)
+				continue;
+
 			lock = lock_info->locks[i].lock_addr;
 
 			ast_reentrancy_lock(lock);
