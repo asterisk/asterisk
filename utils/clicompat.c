ff8134d30f76dd9e3e95ca96d9107417192352c8
Luigi Rizzo <rizzo@icir.org>
2007-12-10 08:35:35 +0000

    simplify this file
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@92103 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- utils/clicompat.c
+++ utils/clicompat.c
@@ -1,16 +1,16 @@
+/*
+ * Stubs for some cli functions used by the test routines.
+ * $Revision$
+ */
 void ast_cli(int fd, const char *fmt, ...);
 void ast_cli(int fd, const char *fmt, ...)
 {
 }
 
-struct ast_cli_entry {
-       char * const cmda; /* just something to satisfy compile & link; will never be used */
-};
+struct ast_cli_entry;
 
 int ast_cli_register_multiple(struct ast_cli_entry *e, int len);
 int ast_cli_register_multiple(struct ast_cli_entry *e, int len)
 {
- return 0;
+	return 0;
 }
-
-
