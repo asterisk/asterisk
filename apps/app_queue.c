8bf68432a08eebe3851abff729de5d62cba34869
Mark Michelson <mmichelson@digium.com>
2007-12-10 16:30:46 +0000

    Merged revisions 92202 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r92202 | mmichelson | 2007-12-10 10:29:44 -0600 (Mon, 10 Dec 2007) | 7 lines
    
    If there are no members in a queue, then the loop where the datastore for detecting
    duplicate dialed numbers will be skipped, meaning the datastore isn't created. This means
    that when we try to free it, there's a crash. This stops that crash from occurring.
    
    (closes issue #11499, reported by slavon, patched by eliel)
    
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@92203 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_queue.c
+++ apps/app_queue.c
@@ -2912,8 +2912,10 @@ static int try_calling(struct queue_ent *qe, const char *options, char *announce
 	if (use_weight)
 		ao2_unlock(queues);
 	lpeer = wait_for_answer(qe, outgoing, &to, &digit, numbusies, ast_test_flag(&(bridge_config.features_caller), AST_FEATURE_DISCONNECT), forwardsallowed);
-	ast_channel_datastore_remove(qe->chan, datastore);
-	ast_channel_datastore_free(datastore);
+	if (datastore) {
+		ast_channel_datastore_remove(qe->chan, datastore);
+		ast_channel_datastore_free(datastore);
+	}
 	ao2_lock(qe->parent);
 	if (qe->parent->strategy == QUEUE_STRATEGY_RRMEMORY) {
 		store_next_rr(qe, outgoing);
