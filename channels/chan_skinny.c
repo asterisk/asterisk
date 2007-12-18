ff5ffd02cf754232932afa0d8ba34796fdb9fa78
Jason Parker <jparker@digium.com>
2007-12-18 22:42:41 +0000

    FreeBSD also does not have byte swap functions.
    Issue 11586, patch by sobomax.
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@93764 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_skinny.c
+++ channels/chan_skinny.c
@@ -107,7 +107,7 @@ static char version_id[16] = "P002F202";
 #define htolel(x) (x)
 #define htoles(x) (x)
 #else
-#if defined(SOLARIS) || defined(__Darwin__) || defined(__NetBSD__) || defined(__OpenBSD__)
+#if defined(SOLARIS) || defined(__Darwin__) || defined(__NetBSD__) || defined(__OpenBSD__) || defined(__FreeBSD__)
 #define __bswap_16(x) \
 	((((x) & 0xff00) >> 8) | \
 	 (((x) & 0x00ff) << 8))
