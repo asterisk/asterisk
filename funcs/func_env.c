f3e2f0bb0a0bed2960871087e3555b5acb239a78
Tilghman Lesher <tilghman@meg.abyt.es>
2007-12-31 20:33:21 +0000

    Merged revisions 95470 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r95470 | tilghman | 2007-12-31 14:27:26 -0600 (Mon, 31 Dec 2007) | 3 lines
    
    Allow the default "0" to be returned if the STAT fails
    (Closes issue #11659)
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@95490 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- funcs/func_env.c
+++ funcs/func_env.c
@@ -73,7 +73,7 @@ static int stat_read(struct ast_channel *chan, const char *cmd, char *data,
 
 	action = strsep(&data, ",");
 	if (stat(data, &s)) {
-		return -1;
+		return 0;
 	} else {
 		switch (*action) {
 		case 'e':
