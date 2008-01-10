91762636b9986ed6e2ac74d464213719751a48b6
Joshua Colp <jcolp@digium.com>
2008-01-10 15:07:12 +0000

    Don't try to copy the category from the group if no category exists. (closes issue #11724) Reported by: IgorG Patches:       group_count.v1.patch uploaded by IgorG (license 20)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@97697 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- funcs/func_groupcount.c
+++ funcs/func_groupcount.c
@@ -58,7 +58,8 @@ static int group_count_function_read(struct ast_channel *chan, char *cmd,
 		}
 		if (gi) {
 			ast_copy_string(group, gi->group, sizeof(group));
-			ast_copy_string(category, gi->category, sizeof(category));
+			if (!ast_strlen_zero(gi->category))
+				ast_copy_string(category, gi->category, sizeof(category));
 		}
 		ast_app_group_list_unlock();
 	}
