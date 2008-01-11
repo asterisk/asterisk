38da1d3afc2ece87f9822c995c02e04b765dda99
Joshua Colp <jcolp@digium.com>
2008-01-11 17:27:58 +0000

    Merged revisions 98219 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r98219 | file | 2008-01-11 13:22:53 -0400 (Fri, 11 Jan 2008) | 4 lines
    
    Ensure the return value of ast_bridge_call is passed back up as the application return value. This is needed for transfers to function so the PBX core knows to continue execution.
    (closes issue #10327)
    Reported by: kkiely
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@98220 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_followme.c
+++ apps/app_followme.c
@@ -1012,7 +1012,6 @@ static int app_exec(struct ast_channel *chan, void *data)
 		pbx_builtin_setvar_helper(caller, "ANSWEREDTIME", toast);
 		if (outbound)
 			ast_hangup(outbound);
-		res = 1;
 	}
 
 	outrun:
