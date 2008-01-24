ccdd466fab3a9ebfb81c1df8827b8ae55a8d7649
Russell Bryant <russell@russellbryant.com>
2008-01-24 17:24:08 +0000

    Merged revisions 100164 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r100164 | russell | 2008-01-24 11:22:09 -0600 (Thu, 24 Jan 2008) | 2 lines
    
    Update main Asterisk copyright info to 2008
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@100169 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/asterisk.c
+++ main/asterisk.c
@@ -1,7 +1,7 @@
 /*
  * Asterisk -- An open source telephony toolkit.
  *
- * Copyright (C) 1999 - 2006, Digium, Inc.
+ * Copyright (C) 1999 - 2008, Digium, Inc.
  *
  * Mark Spencer <markster@digium.com>
  *
@@ -30,8 +30,8 @@
  *
  * \section copyright Copyright and author
  *
- * Copyright (C) 1999 - 2006, Digium, Inc.
- * Asterisk is a trade mark registered by Digium, Inc.
+ * Copyright (C) 1999 - 2008, Digium, Inc.
+ * Asterisk is a trademark registered by Digium, Inc.
  *
  * \author Mark Spencer <markster@digium.com>
  * Also see \ref AstCREDITS
@@ -136,7 +136,7 @@ int daemon(int, int);  /* defined in libresolv of all places */
 
 /*! \brief Welcome message when starting a CLI interface */
 #define WELCOME_MESSAGE \
-    ast_verbose("Asterisk %s, Copyright (C) 1999 - 2007 Digium, Inc. and others.\n" \
+    ast_verbose("Asterisk %s, Copyright (C) 1999 - 2008 Digium, Inc. and others.\n" \
                 "Created by Mark Spencer <markster@digium.com>\n" \
                 "Asterisk comes with ABSOLUTELY NO WARRANTY; type 'core show warranty' for details.\n" \
                 "This is free software, with components licensed under the GNU General Public\n" \
@@ -2393,7 +2393,7 @@ static int show_version(void)
 }
 
 static int show_cli_help(void) {
-	printf("Asterisk %s, Copyright (C) 1999 - 2007, Digium, Inc. and others.\n", ast_get_version());
+	printf("Asterisk %s, Copyright (C) 1999 - 2008, Digium, Inc. and others.\n", ast_get_version());
 	printf("Usage: asterisk [OPTIONS]\n");
 	printf("Valid Options:\n");
 	printf("   -V              Display version number and exit\n");
