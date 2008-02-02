b095b7468548a2529efcc84d8d6eb1938ed24c53
Russell Bryant <russell@russellbryant.com>
2008-02-02 06:10:20 +0000

    The documentation page has to be in its own comment block to work, apparently. Fix it up!
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@102037 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- include/asterisk/event.h
+++ include/asterisk/event.h
@@ -20,6 +20,9 @@
  * \file
  * \author Russell Bryant <russell@digium.com>
  * \ref AstGenericEvents
+ */
+
+/*!
  * \page AstGenericEvents Generic event system
  *
  * The purpose of this API is to provide a generic way to share events between
