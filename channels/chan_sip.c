f26bac62e65406fb77258c602bca17ba4f9aeacc
Joshua Colp <jcolp@digium.com>
2008-02-06 17:59:23 +0000

    Only consider a T.38-only INVITE compatible if we have both a joint capability between us and them and if they provided T.38.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@102725 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -5416,7 +5416,7 @@ static int process_sdp(struct sip_pvt *p, struct sip_request *req)
 	}
 	if (!newjointcapability) {
 		/* If T.38 was not negotiated either, totally bail out... */
-		if (!p->t38.jointcapability) {
+		if (!p->t38.jointcapability || !p->t38.peercapability) {
 			ast_log(LOG_NOTICE, "No compatible codecs, not accepting this offer!\n");
 			/* Do NOT Change current setting */
 			return -1;
