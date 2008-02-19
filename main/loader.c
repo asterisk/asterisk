ee5e39e04bf6c60119c7f0c4f016de9f31f11148
Joshua Colp <jcolp@digium.com>
2008-02-19 21:54:09 +0000

    Add a log message that appears when you try to unload a module that isn't loaded. (closes issue #12033) Reported by: jamesgolovich Patches:       asterisk-loader.diff.txt uploaded by jamesgolovich (license 176)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@103828 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/loader.c
+++ main/loader.c
@@ -478,6 +478,7 @@ int ast_unload_resource(const char *resource_name, enum ast_module_unload_mode f
 
 	if (!(mod = find_resource(resource_name, 0))) {
 		AST_LIST_UNLOCK(&module_list);
+		ast_log(LOG_WARNING, "Unload failed, '%s' could not be found\n", resource_name);
 		return 0;
 	}
 
