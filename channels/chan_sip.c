5410140ae139cae4452ab406d682dc1389e37738
Joshua Colp <jcolp@digium.com>
2008-02-25 15:19:58 +0000

    Merged revisions 104082 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r104082 | file | 2008-02-25 11:17:18 -0400 (Mon, 25 Feb 2008) | 6 lines
    
    Due to recent changes tag will no longer be NULL if not present so we have to use ast_strlen_zero to see if it's actually blank.
    (closes issue #12061)
    Reported by: flefoll
    Patches:
          chan_sip.c.br14.patch_pedantic_no_totag uploaded by flefoll (license 244)
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@104083 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -5831,7 +5831,7 @@ static struct sip_pvt *find_call(struct sip_request *req, struct sockaddr_in *si
 			found = (!strcmp(p->callid, callid));
 		else 
 			found = (!strcmp(p->callid, callid) && 
-			(!pedanticsipchecking || !tag || ast_strlen_zero(p->theirtag) || !strcmp(p->theirtag, tag))) ;
+			(!pedanticsipchecking || ast_strlen_zero(tag) || ast_strlen_zero(p->theirtag) || !strcmp(p->theirtag, tag))) ;
 
 		ast_debug(5, "= %s Their Call ID: %s Their Tag %s Our tag: %s\n", found ? "Found" : "No match", p->callid, p->theirtag, p->tag);
 
