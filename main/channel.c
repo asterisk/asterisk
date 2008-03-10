541039ce1b55e05b3d3e049e50c4ee05009c8552
Russell Bryant <russell@russellbryant.com>
2008-03-10 16:59:13 +0000

    Merged revisions 107099 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r107099 | russell | 2008-03-10 11:58:57 -0500 (Mon, 10 Mar 2008) | 3 lines
    
    Fix a race condition where the generator can go away
    (closes issue #12175, reported by edantie, patched by me)
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@107100 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/channel.c
+++ main/channel.c
@@ -1711,8 +1711,12 @@ static int generator_force(const void *data)
 	ast_channel_lock(chan);
 	tmp = chan->generatordata;
 	chan->generatordata = NULL;
-	generate = chan->generator->generate;
+	if (chan->generator)
+		generate = chan->generator->generate;
 	ast_channel_unlock(chan);
+
+	if (!tmp || !generate)
+		return 0;
 
 	res = generate(chan, tmp, 0, 160);
 
