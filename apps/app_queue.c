0ee953a24966300f05a7ab7628d7ccae01239cd0
Joshua Colp <jcolp@digium.com>
2008-03-28 00:56:44 +0000

    Merged revisions 111565 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ........
    r111565 | file | 2008-03-27 21:55:47 -0300 (Thu, 27 Mar 2008) | 2 lines
    
    Forgetting to unregister a manager action is bad, mmmk?
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@111566 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_queue.c
+++ apps/app_queue.c
@@ -6181,7 +6181,7 @@ static int unload_module(void)
 	ast_cli_unregister_multiple(cli_queue, sizeof(cli_queue) / sizeof(struct ast_cli_entry));
 	res = ast_manager_unregister("QueueStatus");
 	res |= ast_manager_unregister("Queues");
-	res |= ast_manager_unregister("QueueStatus");
+	res |= ast_manager_unregister("QueueRule");
 	res |= ast_manager_unregister("QueueSummary");
 	res |= ast_manager_unregister("QueueAdd");
 	res |= ast_manager_unregister("QueueRemove");
