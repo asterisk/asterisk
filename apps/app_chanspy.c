9facdcb64f3d5c6689f26caf23bf7be323273642
Mark Michelson <mmichelson@digium.com>
2008-04-17 21:04:40 +0000

    Merged revisions 114226 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r114226 | mmichelson | 2008-04-17 16:03:29 -0500 (Thu, 17 Apr 2008) | 9 lines
    
    Declaration of the peer channel in this scope was making it so the peer variable defined
    in the outer scope was never set properly, therefore making iterating through the channel
    list always restart from the beginning. This bug would have affected anyone who called
    chanspy without specifying a first argument.
    
    (closes issue #12461)
    Reported by: stever28
    
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@114227 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_chanspy.c
+++ apps/app_chanspy.c
@@ -620,7 +620,6 @@ static int common_exec(struct ast_channel *chan, const struct ast_flags *flags,
 			char *ext;
 			char *form_enforced;
 			int ienf = !myenforced;
-			struct ast_channel *peer;
 
 			peer = peer_chanspy_ds->chan;
 
