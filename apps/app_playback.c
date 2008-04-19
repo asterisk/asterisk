5955dbc3c151e252cf6bb838e72efc777fc16abe
Tilghman Lesher <tilghman@meg.abyt.es>
2008-04-19 13:57:17 +0000

    Ensure that help text terminates with a newline
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@114299 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_playback.c
+++ apps/app_playback.c
@@ -374,7 +374,7 @@ static int __say_init(int fd, int argc, char *argv[])
 static struct ast_cli_entry cli_playback[] = {
         { { "say", "load", NULL },
 	__say_init, "set/show the say mode",
-	"say load new|old" },
+	"Usage: say load [new|old]\n    Set/show the say mode\n" },
 };
 
 static int playback_exec(struct ast_channel *chan, void *data)
