694a6b4abb75c6d232ff50e962b1a331357dddf1
Russell Bryant <russell@russellbryant.com>
2008-04-23 17:16:32 +0000

    Fix find_callno_locked() to actually return the callno locked in some more cases.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@114587 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_iax2.c
+++ channels/chan_iax2.c
@@ -1353,7 +1353,8 @@ static int __find_callno(unsigned short callno, unsigned short dcallno, struct s
 					res = x;
 				}
 			}
-			ast_mutex_unlock(&iaxsl[x]);
+			if (res && !return_locked)
+				ast_mutex_unlock(&iaxsl[x]);
 		}
 		for (x=TRUNK_CALL_START;(res < 1) && (x<maxtrunkcall);x++) {
 			ast_mutex_lock(&iaxsl[x]);
@@ -1363,7 +1364,8 @@ static int __find_callno(unsigned short callno, unsigned short dcallno, struct s
 					res = x;
 				}
 			}
-			ast_mutex_unlock(&iaxsl[x]);
+			if (res && !return_locked)
+				ast_mutex_unlock(&iaxsl[x]);
 		}
 	}
 	if ((res < 1) && (new >= NEW_ALLOW)) {
