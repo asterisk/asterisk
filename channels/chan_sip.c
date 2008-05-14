29b1d7356730d4a419d0f66828ef9078f1db3119
Olle Johansson <oej@edvina.net>
2008-05-14 12:32:57 +0000

    Add support for codec settings in originate via call file and manager.
    This is to enable video and text in originated calls. Development sponsored
    by Omnitor AB, Sweden. (http://www.omnitor.se)
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@116229 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -11976,7 +11976,8 @@ static void receive_message(struct sip_pvt *p, struct sip_request *req)
 	struct ast_frame f;
 	const char *content_type = get_header(req, "Content-Type");
 
-	if (strcmp(content_type, "text/plain")) { /* No text/plain attachment */
+ 	if (strncmp(content_type, "text/plain", strlen("text/plain"))) { 
+	//if (strcmp(content_type, "text/plain")) { /* No text/plain attachment */
 		transmit_response(p, "415 Unsupported Media Type", req); 
 		if (!p->owner)
 			sip_scheddestroy(p, DEFAULT_TRANS_TIMEOUT);
