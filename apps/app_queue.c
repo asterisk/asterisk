8c4f56f424745fd873ee65f57c50cc6e21ad687a
Mark Michelson <mmichelson@digium.com>
2008-06-17 15:58:21 +0000

    Merged revisions 123275 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
    r123275 | mmichelson | 2008-06-17 10:57:43 -0500 (Tue, 17 Jun 2008) | 20 lines
    
    Merged revisions 123274 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.4
    
    ........
    r123274 | mmichelson | 2008-06-17 10:56:55 -0500 (Tue, 17 Jun 2008) | 12 lines
    
    davidw pointed out that the holdtime calculation used by
    app_queue does not use "boxcar" filtering as the comments
    say. The term "boxcar" means that the number of samples used
    to calculate stays constant, with new samples replacing the
    oldest ones. The queue holdtime calculation uses all holdtime
    samples collected since the queue was loaded, so the comment
    has been changed to be accurate.
    
    (closes issue #12781)
    Reported by: davidw
    
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@123276 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_queue.c
+++ apps/app_queue.c
@@ -467,7 +467,7 @@ struct call_queue {
 	int minannouncefrequency;           /*!< The minimum number of seconds between position announcements (def. 15) */
 	int periodicannouncefrequency;      /*!< How often to play periodic announcement */
 	int roundingseconds;                /*!< How many seconds do we round to? */
-	int holdtime;                       /*!< Current avg holdtime, based on recursive boxcar filter */
+	int holdtime;                       /*!< Current avg holdtime, based on an exponential average */
 	int callscompleted;                 /*!< Number of queue calls completed */
 	int callsabandoned;                 /*!< Number of queue calls abandoned */
 	int servicelevel;                   /*!< seconds setting for servicelevel*/
@@ -1910,7 +1910,7 @@ static void recalc_holdtime(struct queue_ent *qe, int newholdtime)
 {
 	int oldvalue;
 
-	/* Calculate holdtime using a recursive boxcar filter */
+	/* Calculate holdtime using an exponential average */
 	/* Thanks to SRT for this contribution */
 	/* 2^2 (4) is the filter coefficient; a higher exponent would give old entries more weight */
 
