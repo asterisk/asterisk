09c659d7d0c920e78ba7e4cbe8254925fe7e95e9
Mark Michelson <mmichelson@digium.com>
2008-06-27 00:22:03 +0000

    Make this compile with dev-mode on
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@125666 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_queue.c
+++ apps/app_queue.c
@@ -3539,7 +3539,7 @@ static int try_calling(struct queue_ent *qe, const char *options, char *announce
 		/* Begin Monitoring */
 		if (qe->parent->monfmt && *qe->parent->monfmt) {
 			if (!qe->parent->montype) {
-				char *monexec, *monargs;
+				const char *monexec, *monargs;
 				ast_debug(1, "Starting Monitor as requested.\n");
 				if ((monexec = pbx_builtin_getvar_helper(qe->chan, "MONITOR_EXEC")) || (monargs = pbx_builtin_getvar_helper(qe->chan, "MONITOR_EXEC_ARGS")))
 					which = qe->chan;
