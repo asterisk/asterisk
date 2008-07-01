a904d0f74f2b65f0392de62663a2ff440ca49bb5
Tilghman Lesher <tilghman@meg.abyt.es>
2008-07-01 20:25:37 +0000

    Disable the old, slow search for matching callno in chan_iax2 (but allow it to be reenabled for debugging)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@127133 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_iax2.c
+++ channels/chan_iax2.c
@@ -1563,6 +1563,7 @@ static int __find_callno(unsigned short callno, unsigned short dcallno, struct s
 			return res;
 		}
 
+#ifdef IAX_OLD_FIND
 		/* If we get here, we SHOULD NOT find a call structure for this
 		   callno; if we do, it means that there is a call structure that
 		   has a peer callno but did NOT get entered into the hash table,
@@ -1601,6 +1602,7 @@ static int __find_callno(unsigned short callno, unsigned short dcallno, struct s
 		if (res) {
 			ast_log(LOG_WARNING, "Old call search code found call number %d that was not in hash table!\n", res);
 		}
+#endif
 	}
 	if (!res && (new >= NEW_ALLOW)) {
 		int start, found = 0;
