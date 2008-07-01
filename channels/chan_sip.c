8e0a99b7e3d33ae641f330e12b1df0c432472008
Olle Johansson <oej@edvina.net>
2008-07-01 07:49:15 +0000

    Fix bad XML for hold notification. Reported by: gowen72 Patches:       hold.patch uploaded by gowen72 (license 432) (closes issue #12942)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@126735 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -7372,7 +7372,7 @@ static int transmit_state_notify(struct sip_pvt *p, int state, int full, int tim
 		ast_build_string(&t, &maxbytes, "<state>%s</state>\n", statestring);
 		if (state == AST_EXTENSION_ONHOLD) {
 			ast_build_string(&t, &maxbytes, "<local>\n<target uri=\"%s\">\n"
-			                                "<param pname=\"+sip.rendering\" pvalue=\"no\">\n"
+			                                "<param pname=\"+sip.rendering\" pvalue=\"no\"/>\n"
 			                                "</target>\n</local>\n", mto);
 		}
 		ast_build_string(&t, &maxbytes, "</dialog>\n</dialog-info>\n");
