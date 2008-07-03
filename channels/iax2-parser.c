10f0255389c80c59ead07bfa882d9b01f830c832
Brett Bryant <bbryant@digium.com>
2008-07-03 22:47:48 +0000

    Fix one more file that got changed.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@127934 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/iax2-parser.c
+++ channels/iax2-parser.c
@@ -447,11 +447,7 @@ void iax_showframe(struct iax_frame *f, struct ast_iax2_full_hdr *fhi, int rx, s
 		"PROVISN",
 		"FWDWNLD",
 		"FWDATA ",
-		"TXMEDIA",
-		"RTENC  ",
-		"RTDEC  ",
-		"RTPAUSE",
-		"RTRESP "
+		"TXMEDIA"
 	};
 	const char *cmds[] = {
 		"(0?)",
