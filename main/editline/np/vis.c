a2a2d2d0a00d3c6fe0af056f1c6de1901f15b4dd
Kevin P. Fleming <kpfleming@digium.com>
2008-07-03 22:23:32 +0000

    Merged revisions 127903 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
    r127903 | kpfleming | 2008-07-03 17:23:04 -0500 (Thu, 03 Jul 2008) | 20 lines
    
    Merged revisions 127892,127895 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.4
    
    ........
    r127892 | kpfleming | 2008-07-03 17:18:38 -0500 (Thu, 03 Jul 2008) | 6 lines
    
    a couple of small Solaris-related fixes
    
    (closes issue #11885)
    Reported by: snuffy, asgaroth
    
    
    ........
    r127895 | kpfleming | 2008-07-03 17:20:16 -0500 (Thu, 03 Jul 2008) | 3 lines
    
    remove this, it has been moved to the main Makefile
    
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@127905 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/editline/np/vis.c
+++ main/editline/np/vis.c
@@ -44,6 +44,7 @@ __RCSID("$NetBSD: vis.c,v 1.22 2002/03/23 17:38:27 christos Exp $");
 #include <assert.h>
 #include "np/vis.h"
 #include <stdlib.h>
+#include <stdint.h>
 
 #ifdef __weak_alias
 __weak_alias(strsvis,_strsvis)
@@ -167,8 +168,8 @@ do {									      \
 	if (isc) break;							      \
 	if (isextra || ((c & 0177) == ' ') || (flag & VIS_OCTAL)) {	      \
 		*dst++ = '\\';						      \
-		*dst++ = (u_char)(((u_int32_t)(u_char)c >> 6) & 03) + '0';    \
-		*dst++ = (u_char)(((u_int32_t)(u_char)c >> 3) & 07) + '0';    \
+		*dst++ = (u_char)(((uint32_t)(u_char)c >> 6) & 03) + '0';     \
+		*dst++ = (u_char)(((uint32_t)(u_char)c >> 3) & 07) + '0';     \
 		*dst++ =			     (c	      & 07) + '0';    \
 	} else {							      \
 		if ((flag & VIS_NOSLASH) == 0) *dst++ = '\\';		      \
