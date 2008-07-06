207b9c07e422d2d566f9beadebcfe0a1329abcf3
Olle Johansson <oej@edvina.net>
2008-07-06 19:56:10 +0000

    Merged revisions 128491 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ........
    r128491 | oej | 2008-07-06 21:14:06 +0200 (SÃ¶n, 06 Jul 2008) | 3 lines
    
    - Remove unused variable "expiry"
    - Set global_outboundproxy.force at reload.
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@128523 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -213,7 +213,6 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 static int min_expiry = DEFAULT_MIN_EXPIRY;        /*!< Minimum accepted registration time */
 static int max_expiry = DEFAULT_MAX_EXPIRY;        /*!< Maximum accepted registration time */
 static int default_expiry = DEFAULT_DEFAULT_EXPIRY;
-static int expiry = DEFAULT_EXPIRY;
 
 #ifndef MAX
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
@@ -20376,7 +20375,8 @@ static int reload_config(enum channelreloadreason reason)
 	memset(&default_prefs, 0 , sizeof(default_prefs));
 	memset(&global_outboundproxy, 0, sizeof(struct sip_proxy));
 	global_outboundproxy.ip.sin_port = htons(STANDARD_SIP_PORT);
-	global_outboundproxy.ip.sin_family = AF_INET;	/* Type of address: IPv4 */
+	global_outboundproxy.ip.sin_family = AF_INET;	/*!< Type of address: IPv4 */
+	global_outboundproxy.force = FALSE;		/*!< Don't force proxy usage, use route: headers */
 	ourport_tcp = STANDARD_SIP_PORT;
 	ourport_tls = STANDARD_TLS_PORT;
 	bindaddr.sin_port = htons(STANDARD_SIP_PORT);
@@ -20398,7 +20398,6 @@ static int reload_config(enum channelreloadreason reason)
 	allow_external_domains = DEFAULT_ALLOW_EXT_DOM;				/* Allow external invites */
 	global_regcontext[0] = '\0';
 	global_regextenonqualify = DEFAULT_REGEXTENONQUALIFY;
-	expiry = DEFAULT_EXPIRY;
 	global_notifyringing = DEFAULT_NOTIFYRINGING;
 	global_limitonpeers = FALSE;		/*!< Match call limit on peers only */
 	global_notifyhold = FALSE;		/*!< Keep track of hold status for a peer */
