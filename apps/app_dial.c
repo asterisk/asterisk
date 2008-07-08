e46bb5f5bc277a6c8a003a3205b6c9c547ed72f1
Tilghman Lesher <tilghman@meg.abyt.es>
2008-07-08 20:27:47 +0000

    Cause SIP to return a 480 instead of a 404 when a sip peer exists, but is not registered. (closes issue #12885)  Reported by: ibc  Patches:        20080701__bug12885__2.diff.txt uploaded by Corydon76 (license 14)  Tested by: ibc
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@129149 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_dial.c
+++ apps/app_dial.c
@@ -341,6 +341,7 @@ static void hanguptree(struct dial_localuser *outgoing, struct ast_channel *exce
 			ast_cdr_failed(chan->cdr); \
 		numcongestion++; \
 		break; \
+	case AST_CAUSE_NO_ROUTE_DESTINATION: \
 	case AST_CAUSE_UNREGISTERED: \
 		if (chan->cdr) \
 			ast_cdr_failed(chan->cdr); \
