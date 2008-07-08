8fa66db1202fc8604c012c9e605021ee9f1b955b
Tilghman Lesher <tilghman@meg.abyt.es>
2008-07-08 00:02:11 +0000

    Merged revisions 128856 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r128856 | tilghman | 2008-07-07 19:01:30 -0500 (Mon, 07 Jul 2008) | 7 lines
    
    Check for non-NULL before stripping characters.
    (closes issue #12954)
     Reported by: bfsworks
     Patches: 
           20080701__bug12954.diff.txt uploaded by Corydon76 (license 14)
     Tested by: deti
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@128857 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_voicemail.c
+++ apps/app_voicemail.c
@@ -2421,7 +2421,7 @@ static void make_email_file(FILE *p, char *srcemail, struct ast_vm_user *vmu, in
 	char bound[256];
 	char dur[256];
 	struct ast_tm tm;
-	char enc_cidnum[256], enc_cidname[256];
+	char enc_cidnum[256] = "", enc_cidname[256] = "";
 	char *passdata2;
 	size_t len_passdata;
 	char *greeting_attachment; 
@@ -2433,8 +2433,12 @@ static void make_email_file(FILE *p, char *srcemail, struct ast_vm_user *vmu, in
 #define ENDL "\n"
 #endif
 
-	strip_control(cidnum, enc_cidnum, sizeof(enc_cidnum));
-	strip_control(cidname, enc_cidname, sizeof(enc_cidname));
+	if (cidnum) {
+		strip_control(cidnum, enc_cidnum, sizeof(enc_cidnum));
+	}
+	if (cidname) {
+		strip_control(cidname, enc_cidname, sizeof(enc_cidname));
+	}
 	gethostname(host, sizeof(host) - 1);
 
 	if (strchr(srcemail, '@'))
