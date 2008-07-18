701760473710fcc996aaad98f6fd6a3dd4f80779
Tilghman Lesher <tilghman@meg.abyt.es>
2008-07-18 17:11:10 +0000

    Merged revisions 131989 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
    r131989 | tilghman | 2008-07-18 12:10:34 -0500 (Fri, 18 Jul 2008) | 10 lines
    
    Merged revisions 131988 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.4
    
    ........
    r131988 | tilghman | 2008-07-18 12:10:01 -0500 (Fri, 18 Jul 2008) | 2 lines
    
    Oops
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@131990 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/sched.c
+++ main/sched.c
@@ -293,7 +293,11 @@ int _ast_sched_del(struct sched_context *con, int id, const char *file, int line
 
 	if (!s) {
 		ast_debug(1, "Attempted to delete nonexistent schedule entry %d!\n", id);
+#ifndef DEVMODE
+		ast_assert(s != NULL);
+#else
 		_ast_assert(0, "s != NULL", file, line, function);
+#endif
 		return -1;
 	}
 	
