19380639cbad3f9a60dcb2a2a045b1cb06b7e798
Mark Michelson <mmichelson@digium.com>
2008-07-25 14:41:40 +0000

    Merged revisions 133573 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
    r133573 | mmichelson | 2008-07-25 09:40:52 -0500 (Fri, 25 Jul 2008) | 15 lines
    
    Merged revisions 133572 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.4
    
    ........
    r133572 | mmichelson | 2008-07-25 09:40:10 -0500 (Fri, 25 Jul 2008) | 7 lines
    
    We need to make sure to null-terminate the "name"
    portion of SIP URI parameters so that there are no
    bogus comparisons.
    
    Thanks to bbryant for pointing this out.
    
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@133574 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -16288,6 +16288,7 @@ static int sip_uri_params_cmp(const char *input1, const char *input2)
 			if (!value2) {
 				goto fail;
 			}
+			*value2++ = '\0';
 			if (!strcasecmp(name1, name2)) {
 				if (strcasecmp(value1, value2)) {
 					goto fail;
@@ -16342,6 +16343,7 @@ static int sip_uri_params_cmp(const char *input1, const char *input2)
 		if (!value2) {
 			goto fail;
 		}
+		*value2++ = '\0';
 		if ((!strcasecmp(name2, "maddr") && !maddrmatch) ||
 				(!strcasecmp(name2, "ttl") && !ttlmatch) ||
 				(!strcasecmp(name2, "user") && !usermatch) ||
