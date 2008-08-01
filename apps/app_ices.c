f138b464500085b589b16274409740a76d5c7d1d
Michiel van Baak <michiel@vanbaak.info>
2008-08-01 11:57:38 +0000

    Merged revisions 135059 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
    r135059 | mvanbaak | 2008-08-01 13:47:34 +0200 (Fri, 01 Aug 2008) | 10 lines
    
    Merged revisions 135058 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.4
    
    ........
    r135058 | mvanbaak | 2008-08-01 13:43:46 +0200 (Fri, 01 Aug 2008) | 2 lines
    
    make app_ices compile on OpenBSD.
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@135060 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_ices.c
+++ apps/app_ices.c
@@ -92,15 +92,15 @@ static int icesencode(char *filename, int fd)
 	 * But many places has it in /usr/bin 
 	 * As a last-ditch effort, try to use PATH
 	 */
-	execl(path_LOCAL "ices2", "ices", filename, NULL);
-	execl(path_BIN "ices2", "ices", filename, NULL);
-	execlp("ices2", "ices", filename, NULL);
+	execl(path_LOCAL "ices2", "ices", filename, (char *)NULL);
+	execl(path_BIN "ices2", "ices", filename, (char *)NULL);
+	execlp("ices2", "ices", filename, (char *)NULL);
 
 	ast_debug(1, "Couldn't find ices version 2, attempting to use ices version 1.");
 
-	execl(path_LOCAL "ices", "ices", filename, NULL);
-	execl(path_BIN "ices", "ices", filename, NULL);
-	execlp("ices", "ices", filename, NULL);
+	execl(path_LOCAL "ices", "ices", filename, (char *)NULL);
+	execl(path_BIN "ices", "ices", filename, (char *)NULL);
+	execlp("ices", "ices", filename, (char *)NULL);
 
 	ast_log(LOG_WARNING, "Execute of ices failed, could not find command.\n");
 	close(fd);
