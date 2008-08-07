285609937a1bd43d223ce88bcad209b612492734
Tilghman Lesher <tilghman@meg.abyt.es>
2008-08-07 16:55:57 +0000

    Merged revisions 136488 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
    r136488 | tilghman | 2008-08-07 11:50:47 -0500 (Thu, 07 Aug 2008) | 7 lines
    
    Update persistent state on all exit conditions.
    (closes issue #12916)
     Reported by: sgenyuk
     Patches: 
           app_queue.patch.txt uploaded by neutrino88 (license 297)
     Tested by: sgenyuk, aragon
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@136489 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_queue.c
+++ apps/app_queue.c
@@ -2234,6 +2234,7 @@ static int ring_entry(struct queue_ent *qe, struct callattempt *tmp, int *busies
 		ast_verb(3, "Couldn't call %s\n", tmp->interface);
 		do_hang(tmp);
 		(*busies)++;
+		update_status(tmp->member->interface, ast_device_state(tmp->member->interface));
 		return 0;
 	} else if (qe->parent->eventwhencalled) {
 		char vars[2048];
@@ -2259,6 +2260,7 @@ static int ring_entry(struct queue_ent *qe, struct callattempt *tmp, int *busies
 		ast_verb(3, "Called %s\n", tmp->interface);
 	}
 
+	update_status(tmp->member->interface, ast_device_state(tmp->member->interface));
 	return 1;
 }
 
