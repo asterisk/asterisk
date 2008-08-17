8dcca4a2b066f66a515e177c35d006518166c5e5
Sean Bright <sean@malleable.com>
2008-08-17 14:14:46 +0000

    Merged revisions 138482 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ........
    r138482 | seanbright | 2008-08-17 10:12:11 -0400 (Sun, 17 Aug 2008) | 6 lines
    
    Move Uniqueid to the end of the event for those that rely on the position
    of the name/value pairs, pointed out by snuffy-home on #asterisk-commits.
    
    For those of you who rely on the position of name/value pairs in manager
    events... stop... that is why associative arrays were invented.
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@138483 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/features.c
+++ main/features.c
@@ -475,15 +475,16 @@ static int park_call_full(struct ast_channel *chan, struct ast_channel *peer, in
 	manager_event(EVENT_FLAG_CALL, "ParkedCall",
 		"Exten: %s\r\n"
 		"Channel: %s\r\n"
-		"Uniqueid: %s\r\n"
 		"From: %s\r\n"
 		"Timeout: %ld\r\n"
 		"CallerIDNum: %s\r\n"
-		"CallerIDName: %s\r\n",
-		pu->parkingexten, pu->chan->name, pu->chan->uniqueid, peer ? peer->name : "",
+		"CallerIDName: %s\r\n"
+		"Uniqueid: %s\r\n",
+		pu->parkingexten, pu->chan->name, peer ? peer->name : "",
 		(long)pu->start.tv_sec + (long)(pu->parkingtime/1000) - (long)time(NULL),
 		S_OR(pu->chan->cid.cid_num, "<unknown>"),
-		S_OR(pu->chan->cid.cid_name, "<unknown>")
+		S_OR(pu->chan->cid.cid_name, "<unknown>"),
+		pu->chan->uniqueid
 		);
 
 	if (peer && adsipark && ast_adsi_available(peer)) {
