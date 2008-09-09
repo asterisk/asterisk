09c3b90918b9dcd1fc8f41712a23b757f028f4da
Mark Michelson <mmichelson@digium.com>
2008-09-09 16:19:17 +0000

    When determining if codecs used by SIP peers allow the media to be natively bridged, use the jointcapability instead of the peercapability.
    It seems that the intent of using the peercapability was to
    expand the choice of codecs for the call to increase the
    chances of being able to native bridge the channels. The 
    problem is that if a codec were settled on for the native
    bridge and that wasn't a codec that was configured to be used
    by Asterisk for that peer, then Asterisk would send a 
    REINVITE with no codecs in the SDP which is a bug no matter
    how you slice it.
    
    
    (closes issue #13076)
    Reported by: ramonpeek
    Patches:
          13076.patch uploaded by putnopvut (license 60)
    Tested by: tbelder
    
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@142079 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -18315,7 +18315,7 @@ static int sip_sipredirect(struct sip_pvt *p, const char *dest)
 static int sip_get_codec(struct ast_channel *chan)
 {
 	struct sip_pvt *p = chan->tech_pvt;
-	return p->peercapability ? p->peercapability : p->capability;	
+	return p->jointcapability ? p->jointcapability : p->capability;	
 }
 
 /*! \brief Send a poke to all known peers 
