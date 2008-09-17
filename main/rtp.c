af35ef7d739e73026851834e8e988940f41650b0
Mark Michelson <mmichelson@digium.com>
2008-09-17 18:24:15 +0000

    Allow for "G.729" if offered in an SDP even though it is not RFC 3551 compliant. Some Cisco switches will send this in an SDP, and it doesn't hurt to be able to accept this.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@143337 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/rtp.c
+++ main/rtp.c
@@ -1348,6 +1348,7 @@ static struct {
 	{{1, AST_FORMAT_LPC10}, "audio", "LPC"},
 	{{1, AST_FORMAT_G729A}, "audio", "G729"},
 	{{1, AST_FORMAT_G729A}, "audio", "G729A"},
+	{{1, AST_FORMAT_G729A}, "audio", "G.729"},
 	{{1, AST_FORMAT_SPEEX}, "audio", "speex"},
 	{{1, AST_FORMAT_ILBC}, "audio", "iLBC"},
 	{{1, AST_FORMAT_G722}, "audio", "G722"},
