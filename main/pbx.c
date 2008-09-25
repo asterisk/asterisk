561fd11a1f4e5f81512040b8ff769aa6e64e3c37
Steve Murphy <murf@digium.com>
2008-09-25 22:23:21 +0000

    Merged revisions 144569 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ........
    r144569 | murf | 2008-09-25 16:21:28 -0600 (Thu, 25 Sep 2008) | 14 lines
    
    (closes issue #13557)
    Reported by: nickpeirson
    
    The user attached a patch, but the license is not yet
    recorded. I took the liberty of finding and replacing
    ALL index() calls with strchr() calls, and that
    involves more than just main/pbx.c;
    
    chan_oss, app_playback, func_cut also had calls
    to index(), and I changed them out. 1.4 had no
    references to index() at all.
    
    
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@144570 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/pbx.c
+++ main/pbx.c
@@ -1042,7 +1042,7 @@ static void pbx_destroy(struct ast_pbx *p)
  *     NULL
  *
  *   In the above, I could easily turn "N" into "23456789", but I think that a quick "if( *z >= '2' && *z <= '9' )" might take
- *   fewer CPU cycles than a call to index("23456789",*z), where *z is the char to match...
+ *   fewer CPU cycles than a call to strchr("23456789",*z), where *z is the char to match...
  *
  *   traversal is pretty simple: one routine merely traverses the alt list, and for each matching char in the pattern,  it calls itself
  *   on the corresponding next pointer, incrementing also the pointer of the string to be matched, and passing the total specificity and length.
@@ -1340,7 +1340,7 @@ static void new_find_extension(const char *str, struct scoreboard *score, struct
 					return; /* the first match is all we need */
 				}
 			}
-		} else if (index(p->x, *str)) {
+		} else if (strchr(p->x, *str)) {
 			ast_debug(4, "Nothing strange about this match\n");
 			NEW_MATCHER_CHK_MATCH;
 			NEW_MATCHER_RECURSE;
