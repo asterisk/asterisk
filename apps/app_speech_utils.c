fddf5f914ab7edebb2bb8352a7770d6974ffaece
Joshua Colp <jcolp@digium.com>
2008-10-08 14:53:51 +0000

    Merged revisions 147517 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
      r147517 | file | 2008-10-08 11:51:42 -0300 (Wed, 08 Oct 2008) | 2 lines
      
      If we receive DTMF make sure that the state of the speech structure goes back to being not ready. (issue #LUMENVOX-8)
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@147518 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_speech_utils.c
+++ apps/app_speech_utils.c
@@ -743,6 +743,7 @@ static int speech_background(struct ast_channel *chan, void *data)
 			speech->results->text = ast_strdup(dtmf);
 			speech->results->grammar = ast_strdup("dtmf");
 		}
+		ast_speech_change_state(speech, AST_SPEECH_STATE_NOT_READY);
 	}
 
 	/* See if it was because they hung up */
