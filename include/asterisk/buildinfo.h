4c83309c54d9463e669ff05ad51bc3d48811103a
Kevin P. Fleming <kpfleming@digium.com>
2008-10-30 16:53:11 +0000

    try to get this committed before the buildbot complains about a broken tree
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@152810 65c4cc65-6c06-0410-ace0-fbb531ad65f3

---
+++ include/asterisk/buildinfo.h
@@ -0,0 +1,29 @@
+/*
+ * Asterisk -- An open source telephony toolkit.
+ *
+ * Copyright (C) 1999 - 2006, Digium, Inc.
+ *
+ * Mark Spencer <markster@digium.com>
+ *
+ * See http://www.asterisk.org for more information about
+ * the Asterisk project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
+ */
+
+#ifndef _ASTERISK_BUILDINFO_H
+#define _ASTERISK_BUILDINFO_H
+
+extern const char *ast_build_hostname;
+extern const char *ast_build_kernel;
+extern const char *ast_build_machine;
+extern const char *ast_build_os;
+extern const char *ast_build_date;
+extern const char *ast_build_user;
+
+#endif
