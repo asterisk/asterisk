85034a7f8eee06ab4a757ad59570bb3b4b4bda6e
Jason Parker <jparker@digium.com>
2008-12-03 20:35:36 +0000

    Another place this is missing
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@160700 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/manager.c
+++ main/manager.c
@@ -1152,7 +1152,7 @@ static int action_getconfig(struct mansession *s, const struct message *m)
 		}
 	}
 	if (!ast_strlen_zero(category) && catcount == 0) /* TODO: actually, a config with no categories doesn't even get loaded */
-		astman_append(s, "No categories found");
+		astman_append(s, "No categories found\r\n");
 	ast_config_destroy(cfg);
 	astman_append(s, "\r\n");
 
