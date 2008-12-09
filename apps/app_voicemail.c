73b6cbf66cf917c3ee89d619e13fb15866672baa
Tilghman Lesher <tilghman@meg.abyt.es>
2008-12-09 21:57:09 +0000

    Merged revisions 162348 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
      r162348 | tilghman | 2008-12-09 15:53:25 -0600 (Tue, 09 Dec 2008) | 4 lines
      
      We appear to have documented tz= in the [general] section of voicemail.conf,
      without actually having implemented it.  Oops.
      (Reported by Olivier on the -users list)
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@162355 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_voicemail.c
+++ apps/app_voicemail.c
@@ -689,6 +689,7 @@ static char *app4 = "VMAuthenticate";
 
 static AST_LIST_HEAD_STATIC(users, ast_vm_user);
 static AST_LIST_HEAD_STATIC(zones, vm_zone);
+static char zonetag[80];
 static int maxsilence;
 static int maxmsg;
 static int maxdeletedmsg;
@@ -846,6 +847,7 @@ static void populate_defaults(struct ast_vm_user *vmu)
 	ast_copy_string(vmu->callback, callcontext, sizeof(vmu->callback));
 	ast_copy_string(vmu->dialout, dialcontext, sizeof(vmu->dialout));
 	ast_copy_string(vmu->exit, exitcontext, sizeof(vmu->exit));
+	ast_copy_string(vmu->zonetag, zonetag, sizeof(vmu->zonetag));
 	if (vmmaxsecs)
 		vmu->maxsecs = vmmaxsecs;
 	if (maxmsg)
@@ -10839,6 +10841,9 @@ static int load_config(int reload)
 			if (atoi(val)) {
 				adsiver = atoi(val);
 			}
+		}
+		if ((val = ast_variable_retrieve(cfg, "general", "zonetag"))) {
+			ast_copy_string(zonetag, val, sizeof(zonetag));
 		}
 		if ((val = ast_variable_retrieve(cfg, "general", "emailsubject"))) {
 			emailsubject = ast_strdup(val);
