5c111c073fb2c66e60605978c9700fdab2697e35
Michiel van Baak <michiel@vanbaak.info>
2008-12-10 11:36:28 +0000

    Merged revisions 162583 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ........
      r162583 | mvanbaak | 2008-12-10 12:34:09 +0100 (Wed, 10 Dec 2008) | 5 lines
      
      Make res_snmp.so compile on OpenBSD.
      
      OpenBSD uses an old version of gcc which throws an error
      if you use a macro that's not #defined
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@162584 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/snmp/agent.c
+++ res/snmp/agent.c
@@ -47,6 +47,14 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 #define HAVE_DMALLOC_H 0	/* XXX we shouldn't do this */
 #endif
 
+#if defined(__OpenBSD__)
+/*
+ * OpenBSD uses old "legacy" cc which has a rather pedantic builtin preprocessor.
+ * Using a macro which is not #defined throws an error.
+ */
+#define __NetBSD_Version__ 0
+#endif
+
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
 #include <net-snmp/agent/net-snmp-agent-includes.h>
