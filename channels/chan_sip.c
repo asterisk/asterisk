02ce4faaeb2b5b2aa57e9e0595dbc94a853cf39d
Joshua Colp <jcolp@digium.com>
2008-12-10 17:53:09 +0000

    Merged revisions 162738 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
      r162738 | file | 2008-12-10 13:50:43 -0400 (Wed, 10 Dec 2008) | 6 lines
      
      When a SIP peer unregisters set the expiry time back to 0 so that the 200 OK contains an expires of 0.
      (closes issue #13599)
      Reported by: hjourdain
      Patches:
            chan_sip.c.diff uploaded by hjourdain (license 583)
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@162739 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -11118,6 +11118,7 @@ static enum parse_register_result parse_register_contact(struct sip_pvt *pvt, st
 		ast_string_field_set(peer, useragent, "");
 		peer->sipoptions = 0;
 		peer->lastms = 0;
+		pvt->expiry = 0;
 
 		ast_verb(3, "Unregistered SIP '%s'\n", peer->name);
 
