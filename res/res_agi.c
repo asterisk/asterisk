d9fb8362bcd098028bb12dff240aef6c6c529cb5
Tilghman Lesher <tilghman@meg.abyt.es>
2008-12-11 16:51:27 +0000

    Don't wait forever, if there's a specified recording timeout. (closes issue #13885)  Reported by: bamby  Patches:        res_agi.c.patch uploaded by bamby (license 430)
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@163088 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_agi.c
+++ res/res_agi.c
@@ -970,7 +970,7 @@ static int handle_recordfile(struct ast_channel *chan, AGI *agi, int argc, char 
 		
 		start = ast_tvnow();
 		while ((ms < 0) || ast_tvdiff_ms(ast_tvnow(), start) < ms) {
-			res = ast_waitfor(chan, -1);
+			res = ast_waitfor(chan, ms - ast_tvdiff_ms(ast_tvnow(), start));
 			if (res < 0) {
 				ast_closestream(fs);
 				fdprintf(agi->fd, "200 result=%d (waitfor) endpos=%ld\n", res,sample_offset);
