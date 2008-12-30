f43bfb0ace58f9bcb9d63be33c8745f829ba6b93
Terry Wilson <twilson@digium.com>
2008-12-30 20:55:05 +0000

    Merged revisions 166908 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ........
      r166908 | twilson | 2008-12-30 14:50:05 -0600 (Tue, 30 Dec 2008) | 2 lines
      
      Fix some svn:keywords
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@166909 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_phoneprov.c
+++ res/res_phoneprov.c
@@ -34,7 +34,7 @@
 #ifdef SOLARIS
 #include <sys/sockio.h>
 #endif
-ASTERISK_FILE_VERSION(__FILE__, "$Revision: 96773 $")
+ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 
 #include "asterisk/file.h"
 #include "asterisk/paths.h"
