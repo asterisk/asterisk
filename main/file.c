5fe8bde41a289dae9adacec26cb5d4cceb795550
Russell Bryant <russell@russellbryant.com>
2009-01-07 22:35:36 +0000

    Fix the last couple of places where free() was improperly used directly.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@167566 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/file.c
+++ main/file.c
@@ -969,11 +969,11 @@ struct ast_filestream *ast_readfile(const char *filename, const char *type, cons
 		if (!bfile || (fs = get_filestream(f, bfile)) == NULL ||
 		    open_wrapper(fs) ) {
 			ast_log(LOG_WARNING, "Unable to open %s\n", fn);
-			if (fs)
-				ast_free(fs);
+			if (fs) {
+				ast_closestream(fs);
+			}
 			fs = NULL;
-			if (bfile)
-				fclose(bfile);
+			bfile = NULL;
 			free(fn);
 			continue;
 		}
@@ -1086,9 +1086,10 @@ struct ast_filestream *ast_writefile(const char *filename, const char *type, con
 					unlink(fn);
 					unlink(orig_fn);
 				}
-				if (fs)
-					ast_free(fs);
-				fs = NULL;
+				if (fs) {
+					ast_closestream(fs);
+					fs = NULL;
+				}
 				continue;
 			}
 			fs->trans = NULL;
