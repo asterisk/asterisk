0e782e9a28533f4330f68747741bdde8e5a6e17b
Terry Wilson <twilson@digium.com>
2009-01-09 20:30:21 +0000

    Merged revisions 168142 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ........
      r168142 | twilson | 2009-01-09 14:25:25 -0600 (Fri, 09 Jan 2009) | 7 lines
      
      Don't leak memory if phoneprov.conf does not exist
      
      (closes issue #14203)
      Reported by: jamesgolovich
      Patches: 
            asterisk-phoneprovleak.diff.txt uploaded by jamesgolovich (license 176)
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.1@168157 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_phoneprov.c
+++ res/res_phoneprov.c
@@ -943,6 +943,7 @@ static int set_config(void)
 
 	if (!(phoneprov_cfg = ast_config_load("phoneprov.conf", config_flags))) {
 		ast_log(LOG_ERROR, "Unable to load config phoneprov.conf\n");
+		ast_config_destroy(cfg);
 		return -1;
 	}
 
