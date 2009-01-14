7c1d2de899320272947ac163c4f93e98ecd2e1c6
Steve Murphy <murf@digium.com>
2009-01-14 19:34:35 +0000

    app_page was failing to compile in dev-mode on my gcc-4.2.4 system. This change gets rid of the warning.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@168608 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_page.c
+++ apps/app_page.c
@@ -125,7 +125,7 @@ static int page_exec(struct ast_channel *chan, void *data)
 	}
 
 	if (!(dial_list = ast_calloc(num_dials, sizeof(void *)))) {
-		ast_log(LOG_ERROR, "Can't allocate %ld bytes for dial list\n", (sizeof(void *) * num_dials));
+		ast_log(LOG_ERROR, "Can't allocate %ld bytes for dial list\n", (long)(sizeof(void *) * num_dials));
 		ast_module_user_remove(u);
 		return -1;
 	}
