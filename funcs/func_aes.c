dda3fd446f36f50f56008233354d452ee6c891c0
Mark Michelson <mmichelson@digium.com>
2009-01-28 00:17:55 +0000

    Fix some signedness problems in func_aes.c
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@171797 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- funcs/func_aes.c
+++ funcs/func_aes.c
@@ -98,8 +98,8 @@ static int aes_helper(struct ast_channel *chan, const char *cmd, char *data,
 		return -1;
 	}
 
-	ast_aes_encrypt_key(args.key, &ecx);   /* encryption:  plaintext -> encryptedtext -> base64 */
-	ast_aes_decrypt_key(args.key, &dcx);   /* decryption:  base64 -> encryptedtext -> plaintext */
+	ast_aes_encrypt_key((unsigned char *) args.key, &ecx);   /* encryption:  plaintext -> encryptedtext -> base64 */
+	ast_aes_decrypt_key((unsigned char *) args.key, &dcx);   /* decryption:  base64 -> encryptedtext -> plaintext */
 	tmp = ast_calloc(1, len);                     /* requires a tmp buffer for the base64 decode */
 	tmpP = tmp;
 	encrypt = strcmp("AES_DECRYPT", cmd);           /* -1 if encrypting, 0 if decrypting */
@@ -120,9 +120,9 @@ static int aes_helper(struct ast_channel *chan, const char *cmd, char *data,
 		memset(curblock, 0, AES_BLOCK_SIZE);
 		memcpy(curblock, tmpP, (data_len < AES_BLOCK_SIZE) ? data_len : AES_BLOCK_SIZE);
 		if (encrypt) {
-			ast_aes_encrypt(curblock, tmpP, &ecx);
+			ast_aes_encrypt(curblock, (unsigned char *) tmpP, &ecx);
 		} else {
-			ast_aes_decrypt(curblock, tmpP, &dcx);
+			ast_aes_decrypt(curblock, (unsigned char *) tmpP, &dcx);
 		}
 		tmpP += AES_BLOCK_SIZE;
 		data_len -= AES_BLOCK_SIZE;
