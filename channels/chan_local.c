37db651ba831a47d59482c719ba2630e2db841a3
Tilghman Lesher <tilghman@meg.abyt.es>
2009-03-16 17:38:50 +0000

    Merged revisions 182211,182278 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
      r182211 | tilghman | 2009-03-16 10:50:55 -0500 (Mon, 16 Mar 2009) | 14 lines
      
      Merged revisions 182208 via svnmerge from 
      https://origsvn.digium.com/svn/asterisk/branches/1.4
      
      ........
        r182208 | tilghman | 2009-03-16 10:39:15 -0500 (Mon, 16 Mar 2009) | 7 lines
        
        Fixup glare detection, to fix a memory leak of a local pvt structure.
        (closes issue #14656)
         Reported by: caspy
         Patches: 
               20090313__bug14656__2.diff.txt uploaded by tilghman (license 14)
         Tested by: caspy
      ........
    ................
      r182278 | tilghman | 2009-03-16 12:33:38 -0500 (Mon, 16 Mar 2009) | 7 lines
      
      Fix an off-by-one error in the FILE() function, and extend FILE()'s length parameter to work like variable substitution.
      Previously, FILE() returned one less character than specified, due to the
      terminating NULL.  Both the offset and length parameters now behave
      identically to the way variable substitution offsets and lengths also work.
      (closes issue #14670)
       Reported by: BMC
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.1@182280 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_local.c
+++ channels/chan_local.c
@@ -203,23 +203,17 @@ static int local_queue_frame(struct local_pvt *p, int isoutbound, struct ast_fra
 	/* Recalculate outbound channel */
 	other = isoutbound ? p->owner : p->chan;
 
-	/* Set glare detection */
-	ast_set_flag(p, LOCAL_GLARE_DETECT);
-	if (ast_test_flag(p, LOCAL_CANCEL_QUEUE)) {
-		/* We had a glare on the hangup.  Forget all this business,
-		return and destroy p.  */
-		ast_mutex_unlock(&p->lock);
-		p = local_pvt_destroy(p);
-		return -1;
-	}
 	if (!other) {
-		ast_clear_flag(p, LOCAL_GLARE_DETECT);
 		return 0;
 	}
 
 	/* do not queue frame if generator is on both local channels */
-	if (us && us->generator && other->generator)
+	if (us && us->generator && other->generator) {
 		return 0;
+	}
+
+	/* Set glare detection */
+	ast_set_flag(p, LOCAL_GLARE_DETECT);
 
 	/* Ensure that we have both channels locked */
 	while (other && ast_channel_trylock(other)) {
@@ -233,6 +227,17 @@ static int local_queue_frame(struct local_pvt *p, int isoutbound, struct ast_fra
 			ast_mutex_lock(&p->lock);
 		}
 		other = isoutbound ? p->owner : p->chan;
+	}
+
+	/* Since glare detection only occurs within this function, and because
+	 * a pvt flag cannot be set without having the pvt lock, this is the only
+	 * location where we could detect a cancelling of the queue. */
+	if (ast_test_flag(p, LOCAL_CANCEL_QUEUE)) {
+		/* We had a glare on the hangup.  Forget all this business,
+		return and destroy p.  */
+		ast_mutex_unlock(&p->lock);
+		p = local_pvt_destroy(p);
+		return -1;
 	}
 
 	if (other) {
