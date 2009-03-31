e2eaf7f862600b6b7008700ef1ae0f8688b8ead6
Mark Michelson <mmichelson@digium.com>
2009-03-31 19:45:30 +0000

    Fix Russian voicemail intro to say the word "messages" properly.
    (closes issue #14736)
    Reported by: chappell
    Patches:
          voicemail_no_messages.diff uploaded by chappell (license 8)
    
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@185468 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_voicemail.c
+++ apps/app_voicemail.c
@@ -298,11 +298,6 @@ Dutch also uses:
 Spanish also uses:
 \arg \b vm-youhaveno
 
-Ukrainian requires the following additional soundfile:
-\arg \b vm-nove		'nove'
-\arg \b vm-stare	'stare'
-\arg \b digits/ua/1e	'odne'
-
 Italian requires the following additional soundfile:
 
 For vm_intro_it:
@@ -5992,7 +5987,8 @@ static int vm_intro_multilang(struct ast_channel *chan, struct vm_state *vms, co
 	if (!res) {
 		if (lastnum == 0) {
 			res = ast_play_and_wait(chan, "vm-no");
-		} else {
+		}
+		if (!res) {
 			res = ast_say_counted_noun(chan, lastnum, "vm-message");
 		}
 	}
