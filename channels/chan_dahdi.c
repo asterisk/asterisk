04670fdac8b4d80b0bfd23c3378276a0ee5fbd4d
Kevin P. Fleming <kpfleming@digium.com>
2009-04-02 17:27:11 +0000

    Merged revisions 186101 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
      r186101 | kpfleming | 2009-04-02 12:26:07 -0500 (Thu, 02 Apr 2009) | 9 lines
      
      Merged revisions 186081 via svnmerge from 
      https://origsvn.digium.com/svn/asterisk/branches/1.4
      
      ........
        r186081 | kpfleming | 2009-04-02 12:21:29 -0500 (Thu, 02 Apr 2009) | 3 lines
        
        ensure that the buffer passed to DAHDI_SET_BUFINFO is fully initialized
      ........
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@186105 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_dahdi.c
+++ channels/chan_dahdi.c
@@ -11768,6 +11768,7 @@ static int start_pri(struct dahdi_pri *pri)
 			pri->dchanavail[i] |= DCHAN_NOTINALARM;
 		else
 			pri->dchanavail[i] &= ~DCHAN_NOTINALARM;
+		memset(&bi, 0, sizeof(bi));
 		bi.txbufpolicy = DAHDI_POLICY_IMMEDIATE;
 		bi.rxbufpolicy = DAHDI_POLICY_IMMEDIATE;
 		bi.numbufs = 32;
@@ -13326,6 +13327,7 @@ static int linkset_addsigchan(int sigchan)
 			return -1;
 		}
 
+		memset(&bi, 0, sizeof(bi));
 		bi.txbufpolicy = DAHDI_POLICY_IMMEDIATE;
 		bi.rxbufpolicy = DAHDI_POLICY_IMMEDIATE;
 		bi.numbufs = 32;
