4ce04d6865b933a5569f19a167d4226f79fc988d
Mark Michelson <mmichelson@digium.com>
2009-04-08 00:09:54 +0000

    Merged revisions 186842 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
      r186842 | mmichelson | 2009-04-07 19:09:28 -0500 (Tue, 07 Apr 2009) | 14 lines
      
      Merged revisions 186841 via svnmerge from 
      https://origsvn.digium.com/svn/asterisk/branches/1.4
      
      ........
        r186841 | mmichelson | 2009-04-07 19:09:04 -0500 (Tue, 07 Apr 2009) | 8 lines
        
        Fix a few typos of the word "frequency."
        
        (closes issue #14842)
        Reported by: jvandal
        Patches:
              frequency-typo.diff uploaded by jvandal (license 413)
      ........
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@186843 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- formats/format_wav.c
+++ formats/format_wav.c
@@ -136,7 +136,7 @@ static int check_header(FILE *f)
 		return -1;
 	}
 	if (ltohl(freq) != DEFAULT_SAMPLE_RATE) {
-		ast_log(LOG_WARNING, "Unexpected freqency %d\n", ltohl(freq));
+		ast_log(LOG_WARNING, "Unexpected frequency %d\n", ltohl(freq));
 		return -1;
 	}
 	/* Ignore the byte frequency */
