03c25bb848f0c4362bb06cce505fbdd8de8c6ca0
Tilghman Lesher <tilghman@meg.abyt.es>
2009-04-09 05:12:07 +0000

    Merged revisions 187302 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
      r187302 | tilghman | 2009-04-08 23:59:05 -0500 (Wed, 08 Apr 2009) | 14 lines
      
      Merged revisions 187300-187301 via svnmerge from 
      https://origsvn.digium.com/svn/asterisk/branches/1.4
      
      ........
        r187300 | tilghman | 2009-04-08 23:31:38 -0500 (Wed, 08 Apr 2009) | 3 lines
        
        Add debugging mode for diagnosing file descriptor leaks.
        (Related to issue #14625)
      ........
        r187301 | tilghman | 2009-04-08 23:32:40 -0500 (Wed, 08 Apr 2009) | 2 lines
        
        Oops, missed this file in the last commit.
      ........
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.2@187303 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/asterisk.c
+++ main/asterisk.c
@@ -3136,6 +3136,7 @@ int main(int argc, char *argv[])
 	ast_utils_init();
 	tdd_init();
 	ast_tps_init();
+	ast_fd_init();
 
 	if (getenv("HOME")) 
 		snprintf(filename, sizeof(filename), "%s/.asterisk_history", getenv("HOME"));
