4d64b0c93726a449a16810b1921892eca9feb44a
Richard Mudgett <rmudgett@digium.com>
2009-05-11 19:09:00 +0000

    Sent wrong message to clear a call we started if the other end has not responed yet.
    In the state MISDN_CALLING (i.e. SETUP was sent but no answer has arrived yet),
    it is not allowed to clear the call with RELEASE_COMPLETE.  It must be
    cleared with DISCONNECT.  A RELEASE_COMPLETE is only allowed as an answer
    to a SETUP.  (See Q.931 ch. 5.3.2, 5.3.2.a, 5.3.2.b)
    
    Patches:
        chan-misdn-ccstate7.patch uploaded by customer.
    
    JIRA ABE-1862
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@193613 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_misdn.c
+++ channels/chan_misdn.c
@@ -2610,11 +2610,10 @@ static int misdn_hangup(struct ast_channel *ast)
 
 	switch (p->state) {
 	case MISDN_INCOMING_SETUP:
-	case MISDN_CALLING:
 		/* This is the only place in misdn_hangup, where we 
 		 * can call release_chan, else it might create lot's of trouble
 		 * */
-		ast_log(LOG_NOTICE, "release channel, in CALLING/INCOMING_SETUP state.. no other events happened\n");
+		ast_log(LOG_NOTICE, "release channel, in INCOMING_SETUP state.. no other events happened\n");
 		release_chan(bc);
 
 		p->state = MISDN_CLEANING;
@@ -2637,6 +2636,7 @@ static int misdn_hangup(struct ast_channel *ast)
 			misdn_lib_send_event( bc, EVENT_DISCONNECT);
 		break;
 
+	case MISDN_CALLING:
 	case MISDN_ALERTING:
 	case MISDN_PROGRESS:
 	case MISDN_PROCEEDING:
