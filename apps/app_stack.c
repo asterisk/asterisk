f3fd77990d9df6b439ca917b4b6124b50589faa0
Tilghman Lesher <tilghman@meg.abyt.es>
2009-05-20 23:31:26 +0000

    Merged revisions 195839 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ........
      r195839 | tilghman | 2009-05-20 18:30:05 -0500 (Wed, 20 May 2009) | 3 lines
      
      If a variable had a blank value upon the initial setting, then it would do nothing.
      Identified by Dmitry Andrianov via private email, fixed by me.
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.2@195842 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_stack.c
+++ apps/app_stack.c
@@ -201,21 +201,20 @@ static int frame_set_var(struct ast_channel *chan, struct gosub_stack_frame *fra
 		}
 	}
 
-	if (!ast_strlen_zero(value)) {
-		if (!found) {
-			variables = ast_var_assign(var, "");
-			AST_LIST_INSERT_HEAD(&frame->varshead, variables, entries);
-			pbx_builtin_pushvar_helper(chan, var, value);
-		} else
-			pbx_builtin_setvar_helper(chan, var, value);
-
-		manager_event(EVENT_FLAG_DIALPLAN, "VarSet", 
-			"Channel: %s\r\n"
-			"Variable: LOCAL(%s)\r\n"
-			"Value: %s\r\n"
-			"Uniqueid: %s\r\n", 
-			chan->name, var, value, chan->uniqueid);
+	if (!found) {
+		variables = ast_var_assign(var, "");
+		AST_LIST_INSERT_HEAD(&frame->varshead, variables, entries);
+		pbx_builtin_pushvar_helper(chan, var, value);
+	} else {
+		pbx_builtin_setvar_helper(chan, var, value);
 	}
+
+	manager_event(EVENT_FLAG_DIALPLAN, "VarSet",
+		"Channel: %s\r\n"
+		"Variable: LOCAL(%s)\r\n"
+		"Value: %s\r\n"
+		"Uniqueid: %s\r\n",
+		chan->name, var, value, chan->uniqueid);
 	return 0;
 }
 
