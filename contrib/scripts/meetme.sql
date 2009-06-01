62dda18b91636ae95e8838e60c9f96a3c62707dd
Tilghman Lesher <tilghman@meg.abyt.es>
2009-06-01 18:44:21 +0000

    Merged revisions 198626 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ........
      r198626 | tilghman | 2009-06-01 13:40:35 -0500 (Mon, 01 Jun 2009) | 2 lines
      
      Add information for new meetme realtime fields
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.1@198628 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- contrib/scripts/meetme.sql
+++ contrib/scripts/meetme.sql
@@ -4,9 +4,17 @@
 
 CREATE TABLE meetme (
 	confno char(80) DEFAULT '0' NOT NULL,
+	-- Must set schedule=yes in meetme.conf to use starttime and endtime
+	starttime datetime NULL,
+	endtime datetime NULL,
+	-- PIN to enter the conference, if any
 	pin char(20) NULL,
+	-- PIN to enter the conference as an administrator, if any
 	adminpin char(20) NULL,
+	-- Current count of conference participants
 	members integer DEFAULT 0 NOT NULL,
-	PRIMARY KEY (confno)
+	-- Maximum conference participants allowed concurrently
+	maxusers integer DEFAULT 0 NOT NULL,
+	PRIMARY KEY (confno, starttime)
 );
 
