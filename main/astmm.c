140d84dfc550234541cdac8351e1c92cf982e992
Mark Michelson <mmichelson@digium.com>
2009-06-25 18:52:22 +0000

    Prevent false positives when freeing a NULL pointer with MALLOC_DEBUG enabled.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@203230 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/astmm.c
+++ main/astmm.c
@@ -157,9 +157,15 @@ static inline size_t __ast_sizeof_region(void *ptr)
 
 static void __ast_free_region(void *ptr, const char *file, int lineno, const char *func)
 {
-	int hash = HASH(ptr);
+	int hash;
 	struct ast_region *reg, *prev = NULL;
 	unsigned int *fence;
+
+	if (!ptr) {
+		return;
+	}
+
+	hash = HASH(ptr);
 
 	ast_mutex_lock(&reglock);
 	for (reg = regions[hash]; reg; reg = reg->next) {
