d4a4a577351cb1c41b240ac8dac9c44ea9322ef1
Tilghman Lesher <tilghman@meg.abyt.es>
2009-07-14 20:26:36 +0000

    Merged revisions 206567 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ........
      r206567 | tilghman | 2009-07-14 15:14:45 -0500 (Tue, 14 Jul 2009) | 6 lines
      
      Document all meetme realtime fields, and in the process, make some field lengths more consistent.
      (closes issue #15493)
       Reported by: lasko
       Patches: 
             meetme.diff uploaded by lasko (license 833)
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.2@206598 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- contrib/scripts/meetme.sql
+++ contrib/scripts/meetme.sql
@@ -4,17 +4,27 @@
 
 CREATE TABLE meetme (
 	confno char(80) DEFAULT '0' NOT NULL,
+	-- Web booking id for the conference
+	bookId char(50) NULL,
 	-- Must set schedule=yes in meetme.conf to use starttime and endtime
 	starttime datetime NULL,
 	endtime datetime NULL,
 	-- PIN to enter the conference, if any
-	pin char(20) NULL,
+	pin char(30) NULL,
+	-- Options to associate with normal users of the conference
+	opts char(100) NULL,
 	-- PIN to enter the conference as an administrator, if any
-	adminpin char(20) NULL,
+	adminpin char(30) NULL,
+	-- Options to associate with administrator users of the conference
+	adminopts char(100) NULL,
 	-- Current count of conference participants
 	members integer DEFAULT 0 NOT NULL,
 	-- Maximum conference participants allowed concurrently
 	maxusers integer DEFAULT 0 NOT NULL,
+	-- Recording of the conference, if any
+	recordingfilename char(255) NULL,
+	-- File format of the conference recording, if any
+	recordingformat char(10) NULL,
 	PRIMARY KEY (confno, starttime)
 );
 
