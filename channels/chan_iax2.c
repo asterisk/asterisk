0a3504f74bbee7d368e6ab3ad9eb3a7c88ae7ccb
David Vossel <dvossel@digium.com>
2009-09-18 23:19:50 +0000

    iax2 frame double free
    The iax frame's retrans sched id was written over right
    before iax2_frame_free was called.  In iax2_frame_free that
    retrans id is used to delete the sched item.  By writing over
    the retrans field before the sched item could be deleted, it was
    possible for a retransmit to occur on a freed frame.
    
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@219519 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_iax2.c
+++ channels/chan_iax2.c
@@ -3064,7 +3064,6 @@ static void __attempt_transmit(const void *data)
 		AST_LIST_REMOVE(&iaxq.queue, f, list);
 		iaxq.count--;
 		AST_LIST_UNLOCK(&iaxq.queue);
-		f->retrans = -1;
 		/* Free the IAX frame */
 		iax2_frame_free(f);
 	}
