2198f6c3810e030a923c6ce1b52ed853b83744e6
David Vossel <dvossel@digium.com>
2009-09-21 17:02:20 +0000

    Merged revisions 219721 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
      r219721 | dvossel | 2009-09-21 11:59:05 -0500 (Mon, 21 Sep 2009) | 9 lines
      
      Merged revisions 219720 via svnmerge from 
      https://origsvn.digium.com/svn/asterisk/branches/1.4
      
      ........
        r219720 | dvossel | 2009-09-21 11:55:53 -0500 (Mon, 21 Sep 2009) | 3 lines
        
        Reverting merge 219520. This change was not necessary.
      ........
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.1@219723 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_iax2.c
+++ channels/chan_iax2.c
@@ -1545,7 +1545,6 @@ static void iax2_destroy_helper(struct chan_iax2_pvt *pvt)
 static void iax2_frame_free(struct iax_frame *fr)
 {
 	AST_SCHED_DEL(sched, fr->retrans);
-	fr->retrans = -1;
 	iax_frame_free(fr);
 }
 
@@ -3219,6 +3218,7 @@ static void __attempt_transmit(const void *data)
 		AST_LIST_LOCK(&frame_queue);
 		AST_LIST_REMOVE(&frame_queue, f, list);
 		AST_LIST_UNLOCK(&frame_queue);
+		f->retrans = -1; /* this is safe because this is the scheduled function */
 		/* Free the IAX frame */
 		iax2_frame_free(f);
 	}
