9f1bf0f9bd2448b6b08e48ef8d4c008b362e85f2
Jeff Peeler <jpeeler@digium.com>
2009-10-09 23:11:42 +0000

    Fix interpretation of PRIREDIRECTIONREASON set by chan_sip.
    This commit is the simplest way to solve a problem that has already been solved
    in trunk with the "COLP/CONP and Redirecting party information into Asterisk"
    commit. In trunk the redirection reason is translated into a generic redirect 
    reason. I would have had to do the same fix except chan_sip never reads
    PRIREDIRECTREASON. So both chan_dahdi and chan_h323 have been modified to
    interpret the one different redirect reason of "no-answer" properly and set the
    ISDN reason code 2 of "no reply".
    
    (closes issue #15033)
    Reported by: steinwej
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.1@223405 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_h323.c
+++ channels/chan_h323.c
@@ -626,7 +626,8 @@ static int oh323_call(struct ast_channel *c, char *dest, int timeout)
 			pvt->options.redirect_reason = 0;
 		else if (!strcasecmp(addr, "BUSY"))
 			pvt->options.redirect_reason = 1;
-		else if (!strcasecmp(addr, "NO_REPLY"))
+		else if (!strcasecmp(addr, "NO_REPLY") || !strcasecmp(addr, "NOANSWER"))
+		/* the NOANSWER is to match diversion-reason from chan_sip, (which never reads PRIREDIRECTREASON) */
 			pvt->options.redirect_reason = 2;
 		else if (!strcasecmp(addr, "UNCONDITIONAL"))
 			pvt->options.redirect_reason = 15;
