503d9f8ab079eba7e14f7c5e97b8c856235c8fae
Mark Michelson <mmichelson@digium.com>
2009-10-09 18:29:42 +0000

    Merged revisions 223215 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
      r223215 | mmichelson | 2009-10-09 13:17:34 -0500 (Fri, 09 Oct 2009) | 9 lines
      
      Recorded merge of revisions 223213 via svnmerge from 
      https://origsvn.digium.com/svn/asterisk/branches/1.4
      
      ........
        r223213 | mmichelson | 2009-10-09 13:17:12 -0500 (Fri, 09 Oct 2009) | 3 lines
        
        Fix potential memory leak in app_dial.c
      ........
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.2@223257 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_dial.c
+++ apps/app_dial.c
@@ -1670,6 +1670,7 @@ static int dial_exec_full(struct ast_channel *chan, void *data, struct ast_flags
 			datastore->inheritance = DATASTORE_INHERIT_FOREVER;
 
 			if (!(dialed_interfaces = ast_calloc(1, sizeof(*dialed_interfaces)))) {
+				ast_datastore_free(datastore);
 				ast_free(tmp);
 				goto out;
 			}
