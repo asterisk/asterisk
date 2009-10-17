9d34d37a4bd0f19287ba99a43b295dc243e9e097
Jeff Peeler <jpeeler@digium.com>
2009-10-17 02:02:32 +0000

    fix typo, sorry
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.1@224336 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_dahdi.c
+++ channels/chan_dahdi.c
@@ -9164,12 +9164,12 @@ static struct dahdi_pvt *mkintf(int channel, const struct dahdi_chan_conf *conf,
 		ast_copy_string(tmp->context, conf->chan.context, sizeof(tmp->context));
 		ast_copy_string(tmp->parkinglot, conf->chan.parkinglot, sizeof(tmp->parkinglot));
 		tmp->cid_ton = 0;
-		if ((p->sig != SIG_PRI) || (p->sig != SIG_SS7) || (p->sig != SIG_BRI) || (p->sig != SIG_BRI_PTMP)) {
+		if ((tmp->sig != SIG_PRI) || (tmp->sig != SIG_SS7) || (tmp->sig != SIG_BRI) || (tmp->sig != SIG_BRI_PTMP)) {
 			ast_copy_string(tmp->cid_num, conf->chan.cid_num, sizeof(tmp->cid_num));
 			ast_copy_string(tmp->cid_name, conf->chan.cid_name, sizeof(tmp->cid_name));
 		} else {
-			p->cid_num[0] = '\0';
-			p->cid_name[0] = '\0';
+			tmp->cid_num[0] = '\0';
+			tmp->cid_name[0] = '\0';
 		}
 		ast_copy_string(tmp->mailbox, conf->chan.mailbox, sizeof(tmp->mailbox));
 		if (channel != CHAN_PSEUDO && !ast_strlen_zero(tmp->mailbox)) {
