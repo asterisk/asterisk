2400c2cc61afa96ea3c93b26612ea49e9d7f6d7c
Olle Johansson <oej@edvina.net>
2009-11-03 11:21:06 +0000

    Merged revisions 227091 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
    r227091 | oej | 2009-11-03 12:11:15 +0100 (Tis, 03 Nov 2009) | 15 lines
    
    Merged revisions 227088 via svnmerge from 
    https://origsvn.digium.com/svn/asterisk/branches/1.4
    
    ........
    r227088 | oej | 2009-11-03 11:29:59 +0100 (Tis, 03 Nov 2009) | 7 lines
    
    Use proper response code when violating Contact ACL's.
    
    https://reviewboard.asterisk.org/r/415/
    
    Thanks kpfleming for a quick review.
    (EDVX-003)
    
    ........
    
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.0@227102 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -302,6 +302,7 @@ enum xmittype {
 };
 
 enum parse_register_result {
+	PARSE_REGISTER_DENIED,
 	PARSE_REGISTER_FAILED,
 	PARSE_REGISTER_UPDATE,
 	PARSE_REGISTER_QUERY,
@@ -10636,10 +10637,10 @@ static enum parse_register_result parse_register_contact(struct sip_pvt *pvt, st
 	memcpy(&testsin.sin_addr, hp->h_addr, sizeof(testsin.sin_addr));
 	if (	ast_apply_ha(global_contact_ha, &testsin) != AST_SENSE_ALLOW ||
 			ast_apply_ha(peer->contactha, &testsin) != AST_SENSE_ALLOW) {
-		ast_log(LOG_WARNING, "Host '%s' disallowed by rule\n", host);
+		ast_log(LOG_WARNING, "Host '%s' disallowed by contact ACL (violating IP %s)\n", host, ast_inet_ntoa(testsin.sin_addr));
 		*peer->fullcontact = '\0';
 		ast_string_field_set(pvt, our_contact, "");
-		return PARSE_REGISTER_FAILED;
+		return PARSE_REGISTER_DENIED;
 	}
 
 	if (!ast_test_flag(&peer->flags[0], SIP_NAT_ROUTE)) {
@@ -11294,6 +11295,11 @@ static enum check_auth_result register_verify(struct sip_pvt *p, struct sockaddr
 				/* We have a successful registration attempt with proper authentication,
 				   now, update the peer */
 				switch (parse_register_contact(p, peer, req)) {
+				case PARSE_REGISTER_DENIED:
+					transmit_response_with_date(p, "603 Denied", req);
+					peer->lastmsgssent = -1;
+					res = 0;
+					break;
 				case PARSE_REGISTER_FAILED:
 					ast_log(LOG_WARNING, "Failed to parse contact info\n");
 					transmit_response_with_date(p, "400 Bad Request", req);
@@ -11333,6 +11339,11 @@ static enum check_auth_result register_verify(struct sip_pvt *p, struct sockaddr
 			if (sip_cancel_destroy(p))
 				ast_log(LOG_WARNING, "Unable to cancel SIP destruction.  Expect bad things.\n");
 			switch (parse_register_contact(p, peer, req)) {
+			case PARSE_REGISTER_DENIED:
+				transmit_response_with_date(p, "403 Forbidden (ACL)", req);
+				peer->lastmsgssent = -1;
+				res = 0;
+				break;
 			case PARSE_REGISTER_FAILED:
 				ast_log(LOG_WARNING, "Failed to parse contact info\n");
 				transmit_response_with_date(p, "400 Bad Request", req);
