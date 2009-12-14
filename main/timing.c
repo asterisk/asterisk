204f888fb3a22c407f901d46ed3f511938d3e7d6
Sean Bright <sean@malleable.com>
2009-12-14 16:10:30 +0000

    Merged revisions 234572 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ........
      r234572 | seanbright | 2009-12-14 11:08:09 -0500 (Mon, 14 Dec 2009) | 2 lines
      
      The default rate for 'timing test' is actually 50/sec, not 100/sec as advertised.
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.2@234575 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/timing.c
+++ main/timing.c
@@ -216,7 +216,7 @@ static char *timing_test(struct ast_cli_entry *e, int cmd, struct ast_cli_args *
 	case CLI_INIT:
 		e->command = "timing test";
 		e->usage = "Usage: timing test <rate>\n"
-		           "   Test a timer with a specified rate, 100/sec by default.\n"
+		           "   Test a timer with a specified rate, 50/sec by default.\n"
 		           "";
 		return NULL;
 	case CLI_GENERATE:
