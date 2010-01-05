0c6243420143be9f7d44be024c8dca3e22737016
Michiel van Baak <michiel@vanbaak.info>
2010-01-05 16:08:12 +0000

    Make CLI command 'mixmonitor start|stop <channel> work again.
    (closes issue #16534)
    Reported by: jlaguilar
    Fix as suggested by jlaguilar in the bugreport
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@237656 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_mixmonitor.c
+++ apps/app_mixmonitor.c
@@ -571,7 +571,7 @@ static char *handle_cli_mixmonitor(struct ast_cli_entry *e, int cmd, struct ast_
 
 	switch (cmd) {
 	case CLI_INIT:
-		e->command = "mixmonitor {start|stop} {<chan_name>} [args]";
+		e->command = "mixmonitor {start|stop}";
 		e->usage =
 			"Usage: mixmonitor <start|stop> <chan_name> [args]\n"
 			"       The optional arguments are passed to the MixMonitor\n"
