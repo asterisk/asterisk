7df5564a61f131991541de75323f885c702b63ba
Jeff Peeler <jpeeler@digium.com>
2010-01-08 17:18:41 +0000

    Stop trying to find a parking space after traversing the parkinglot one time.
    (closes issue #16428)
    Reported by: Yasuhiro Konishi
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@238583 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/features.c
+++ main/features.c
@@ -754,7 +754,7 @@ static struct parkeduser *park_space_reserve(struct ast_channel *chan,
 		for (i = start; 1; i++) {
 			if (i == parkinglot->parking_stop + 1) {
 				i = parkinglot->parking_start - 1;
-				continue;
+				break;
 			}
 
 			AST_LIST_TRAVERSE(&parkinglot->parkings, cur, list) {
@@ -762,8 +762,7 @@ static struct parkeduser *park_space_reserve(struct ast_channel *chan,
 					break;
 				}
 			}
-
-			if (!cur || i == start - 1) {
+			if (!cur) {
 				parking_space = i;
 				break;
 			}
