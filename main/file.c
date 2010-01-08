c3da049d25a096fd204784d070024c24c6e35a33
Matthew Nicholson <mnicholson@digium.com>
2010-01-08 19:20:44 +0000

    Properly calculate the remaining space in the output string when reducing format strings.
    (closes issue #16560)
    Reported by: goldwein
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@238629 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/file.c
+++ main/file.c
@@ -1367,6 +1367,7 @@ char *ast_format_str_reduce(char *fmts)
 	char *orig = fmts;
 	int i, j, x, first, found = 0;
 	int len = strlen(fmts) + 1;
+	int res;
 
 	if (AST_LIST_LOCK(&formats)) {
 		ast_log(LOG_WARNING, "Unable to lock format list\n");
@@ -1402,8 +1403,9 @@ char *ast_format_str_reduce(char *fmts)
 
 		/* special handling for the first entry */
 		if (first) {
-			fmts += snprintf(fmts, len, "%s", fmts_str[i]);
-			len -= (fmts - orig);
+			res = snprintf(fmts, len, "%s", fmts_str[i]);
+			fmts += res;
+			len -= res;
 			first = 0;
 			continue;
 		}
@@ -1418,8 +1420,9 @@ char *ast_format_str_reduce(char *fmts)
 		}
 
 		if (!found) {
-			fmts += snprintf(fmts, len, "|%s", fmts_str[i]);
-			len -= (fmts - orig);
+			res = snprintf(fmts, len, "|%s", fmts_str[i]);
+			fmts += res;
+			len -= res;
 		}
 	}
 
