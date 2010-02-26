022e262d719c3cf195dab9210cc97958f4615832
Russell Bryant <russell@russellbryant.com>
2010-02-26 08:07:09 +0000

    constification, remove include
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@249011 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- cdr/cdr_csv.c
+++ cdr/cdr_csv.c
@@ -32,8 +32,6 @@
 
 ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 
-#include <time.h>
-
 #include "asterisk/paths.h"	/* use ast_config_AST_LOG_DIR */
 #include "asterisk/config.h"
 #include "asterisk/channel.h"
@@ -133,7 +131,7 @@ static int load_config(int reload)
 	return 1;
 }
 
-static int append_string(char *buf, char *s, size_t bufsize)
+static int append_string(char *buf, const char *s, size_t bufsize)
 {
 	int pos = strlen(buf), spos = 0, error = -1;
 
