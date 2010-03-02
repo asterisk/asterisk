634224480d0779900e221fe419bddf8a9a1086a9
Alec L Davis <sivad.a@paradise.net.nz>
2010-03-02 09:05:43 +0000

    fixes ability to exit echo app
    when called from a ISDN channel, null frames prevent '#' exit.
    Now only echo back VOICE and DTMF frames
    
    (issue #16880)
    Reported by: alecdavis
    Patches: 
          echo_exit_1-6-1.diff.txt uploaded by alecdavis (license 585)
    Tested by: alecdavis
    
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.2@249844 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_echo.c
+++ apps/app_echo.c
@@ -62,16 +62,23 @@ static int echo_exec(struct ast_channel *chan, void *data)
 		if (!f) {
 			break;
 		}
-		f->delivery.tv_sec = 0;
-		f->delivery.tv_usec = 0;
-		if (ast_write(chan, f)) {
-			ast_frfree(f);
-			goto end;
-		}
-		if ((f->frametype == AST_FRAME_DTMF) && (f->subclass == '#')) {
-			res = 0;
-			ast_frfree(f);
-			goto end;
+		switch (f->frametype) {
+		case AST_FRAME_VOICE:
+		case AST_FRAME_DTMF:
+			f->delivery.tv_sec = 0;
+			f->delivery.tv_usec = 0;
+			if (ast_write(chan, f)) {
+				ast_frfree(f);
+				goto end;
+			}
+			if ((f->frametype == AST_FRAME_DTMF) && (f->subclass == '#')) {
+				res = 0;
+				ast_frfree(f);
+				goto end;
+			}
+			break;
+		default:
+			break;
 		}
 		ast_frfree(f);
 	}
