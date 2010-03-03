b5dbe2e99a66c19bbe120eb24373ca38f17c4c4b
Jeff Peeler <jpeeler@digium.com>
2010-03-03 19:09:21 +0000

    Merged revisions 250481 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
      r250481 | jpeeler | 2010-03-03 13:06:06 -0600 (Wed, 03 Mar 2010) | 22 lines
      
      Merged revisions 250480 via svnmerge from 
      https://origsvn.digium.com/svn/asterisk/branches/1.4
      
      ........
        r250480 | jpeeler | 2010-03-03 13:04:11 -0600 (Wed, 03 Mar 2010) | 15 lines
        
        Make sure to clear red alarm after polarity reversal.
        
        From the issue:
        The automatic overnight line tests (or manual ones) used on UK (BT) lines causes
        a red alarm on a dahdi / TDM400P connected channel. This is because the line
        uses voltage tests (battery loss) and polarity reversal. The polarity reversal
        causes chan_dahdi to initiate v23 CallerID processing but during this the event
        DAHDI_EVENT_NOALARM is ignored so that the alarm is never cleared.
        
        (closes issue #14163)
        Reported by: jedi98
        Patches: 
              chan_dahdi-1.4-inalarm.diff uploaded by jedi98 (license 653)
        Tested by: mattbrown, Chainsaw, mikeeccleston
      ........
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.1@250483 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_dahdi.c
+++ channels/chan_dahdi.c
@@ -7313,6 +7313,9 @@ static void *ss_thread(void *data)
 						if (i & DAHDI_IOMUX_SIGEVENT) {
 							res = dahdi_get_event(p->subs[idx].dfd);
 							ast_log(LOG_NOTICE, "Got event %d (%s)...\n", res, event2str(res));
+							if (res == DAHDI_EVENT_NOALARM) {
+								p->inalarm = 0;
+							}
 
 							if (p->cid_signalling == CID_SIG_V23_JP) {
 								if (res == DAHDI_EVENT_RINGBEGIN) {
@@ -7414,6 +7417,9 @@ static void *ss_thread(void *data)
 							if (i & DAHDI_IOMUX_SIGEVENT) {
 								res = dahdi_get_event(p->subs[idx].dfd);
 								ast_log(LOG_NOTICE, "Got event %d (%s)...\n", res, event2str(res));
+								if (res == DAHDI_EVENT_NOALARM) {
+									p->inalarm = 0;
+								}
 								res = 0;
 								/* Let us detect distinctive ring */
 		
@@ -7565,6 +7571,9 @@ static void *ss_thread(void *data)
 					if (i & DAHDI_IOMUX_SIGEVENT) {
 						res = dahdi_get_event(p->subs[idx].dfd);
 						ast_log(LOG_NOTICE, "Got event %d (%s)...\n", res, event2str(res));
+						if (res == DAHDI_EVENT_NOALARM) {
+							p->inalarm = 0;
+						}
 						/* If we get a PR event, they hung up while processing calerid */
 						if ( res == DAHDI_EVENT_POLARITY && p->hanguponpolarityswitch && p->polarity == POLARITY_REV) {
 							ast_log(LOG_DEBUG, "Hanging up due to polarity reversal on channel %d while detecting callerid\n", p->channel);
@@ -7634,6 +7643,9 @@ static void *ss_thread(void *data)
 						if (i & DAHDI_IOMUX_SIGEVENT) {
 							res = dahdi_get_event(p->subs[idx].dfd);
 							ast_log(LOG_NOTICE, "Got event %d (%s)...\n", res, event2str(res));
+							if (res == DAHDI_EVENT_NOALARM) {
+								p->inalarm = 0;
+							}
 							res = 0;
 							/* Let us detect callerid when the telco uses distinctive ring */
 
