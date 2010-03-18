5200b6e81e298afa48306c69771a906890d640f3
Philippe Sultan <philippe.sultan@gmail.com>
2010-03-18 15:59:19 +0000

    Prevent a crash when a buddy gets offline.
    (closes issue #16760)
    Reported by: fiddur
    Patches:
          248394.diff uploaded by fiddur (license 678)i with modifications by me
    Tested by: fiddur, phsultan
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@253261 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_jabber.c
+++ res/res_jabber.c
@@ -2395,11 +2395,17 @@ static void aji_handle_presence(struct aji_client *client, ikspak *pak)
 		ast_debug(3, "JABBER: Kinky! how did that happen %i\n", pak->show);
 	}
 
+    if (found) {
 	manager_event(EVENT_FLAG_USER, "JabberStatus",
 			"Account: %s\r\nJID: %s\r\nResource: %s\r\nStatus: %d\r\nPriority: %d"
 			"\r\nDescription: %s\r\n",
 			client->name, pak->from->partial, found->resource, found->status,
 			found->priority, found->description);
+    } else {
+	manager_event(EVENT_FLAG_USER, "JabberStatus",
+			"Account: %s\r\nJID: %s\r\nStatus: %d\r\n",
+			client->name, pak->from->partial, pak->show ? pak->show:IKS_SHOW_UNAVAILABLE);
+    }
 }
 
 /*!
