d75b120fbd7e4bb37c3a80e30d7fd895dd780316
Leif Madsen <leif@leifmadsen.com>
2010-03-18 17:54:21 +0000

    Merged revisions 253345 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ........
      r253345 | lmadsen | 2010-03-18 12:52:35 -0500 (Thu, 18 Mar 2010) | 7 lines
      
      Change usage of pipe to comma in UserEvent docs.
      
      Change the example usage of pipe as a separator to comma in the UserEvent
      documentation.
      
      (closes issue #16961)
      Reported by: jlpedrosa
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.2@253346 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_userevent.c
+++ apps/app_userevent.c
@@ -43,7 +43,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 			<para>Sends an arbitrary event to the manager interface, with an optional
 			<replaceable>body</replaceable> representing additional arguments. The
 			<replaceable>body</replaceable> may be specified as
-			a <literal>|</literal> delimited list of headers. Each additional
+			a <literal>,</literal> delimited list of headers. Each additional
 			argument will be placed on a new line in the event. The format of the
 			event will be:</para>
 			<para>    Event: UserEvent</para>
