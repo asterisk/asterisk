f4617ae2b534262484980b4fbff2e42f87543b23
Michiel van Baak <michiel@vanbaak.info>
2010-04-02 06:45:54 +0000

    Ignore Redial softkey when no previous dialed number is known
    (closes issue #17126)
    Reported by: wedhorn
    Patches: 
          skinny79xx_redial1.diff uploaded by wedhorn (license 30)
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@255851 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_skinny.c
+++ channels/chan_skinny.c
@@ -5746,16 +5746,7 @@ static int handle_soft_key_event_message(struct skinny_req *req, struct skinnyse
 			ast_verb(1, "Received Softkey Event: Redial(%d/%d)\n", instance, callreference);
 
 		if (ast_strlen_zero(l->lastnumberdialed)) {
-			ast_log(LOG_WARNING, "Attempted redial, but no previously dialed number found.\n");
-			l->hookstate = SKINNY_ONHOOK;
-			transmit_speaker_mode(d, SKINNY_SPEAKEROFF);
-			transmit_closereceivechannel(d, sub);
-			transmit_stopmediatransmission(d, sub);
-			transmit_speaker_mode(d, SKINNY_SPEAKEROFF);
-			transmit_clearpromptmessage(d, l->instance, sub->callid);
-			transmit_callstate(d, l->instance, sub->callid, SKINNY_ONHOOK);
-			transmit_selectsoftkeys(d, 0, 0, KEYDEF_ONHOOK);
-			transmit_activatecallplane(d, l);
+			ast_log(LOG_WARNING, "Attempted redial, but no previously dialed number found. Ignoring button.\n");
 			break;
 		}
 
