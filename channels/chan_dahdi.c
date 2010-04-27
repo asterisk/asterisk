28bf868fb3774527d60ae169b49db7a4293cd80d
Richard Mudgett <rmudgett@digium.com>
2010-04-27 22:37:40 +0000

    Merged revisions 259538 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
      r259538 | rmudgett | 2010-04-27 17:18:09 -0500 (Tue, 27 Apr 2010) | 18 lines
      
      Merged revisions 259531 via svnmerge from 
      https://origsvn.digium.com/svn/asterisk/branches/1.4
      
      ........
        r259531 | rmudgett | 2010-04-27 16:53:07 -0500 (Tue, 27 Apr 2010) | 11 lines
        
        DAHDI "WARNING" message is confusing and vague
        
        "WARNING[28406]: chan_dahdi.c:6873 ss_thread: CallerID feed failed: Success"
        
        Changed the warning to "Failed to decode CallerID on channel 'name'".  The
        message before it is likely more specific about why the CallerID decode
        failed.
        
        SWP-501
        AST-283
      ........
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.1@259615 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_dahdi.c
+++ channels/chan_dahdi.c
@@ -7341,9 +7341,14 @@ static void *ss_thread(void *data)
 							} else {
 								res = callerid_feed(cs, buf, res, AST_LAW(p));
 							}
-
 							if (res < 0) {
-								ast_log(LOG_WARNING, "CallerID feed failed on channel '%s'\n", chan->name);
+								/*
+								 * The previous diagnostic message output likely
+								 * explains why it failed.
+								 */
+								ast_log(LOG_WARNING,
+									"Failed to decode CallerID on channel '%s'\n",
+									chan->name);
 								break;
 							} else if (res)
 								break;
@@ -7610,7 +7615,13 @@ static void *ss_thread(void *data)
 						samples += res;
 						res = callerid_feed(cs, buf, res, AST_LAW(p));
 						if (res < 0) {
-							ast_log(LOG_WARNING, "CallerID feed failed: %s\n", strerror(errno));
+							/*
+							 * The previous diagnostic message output likely
+							 * explains why it failed.
+							 */
+							ast_log(LOG_WARNING,
+								"Failed to decode CallerID on channel '%s'\n",
+								chan->name);
 							break;
 						} else if (res)
 							break;
@@ -7888,7 +7899,11 @@ static void *mwi_thread(void *data)
 			samples += res;
 			if (!spill_done) {
 				if ((spill_result = callerid_feed(cs, mtd->buf, res, AST_LAW(mtd->pvt))) < 0) {
-					ast_log(LOG_WARNING, "CallerID feed failed: %s\n", strerror(errno));
+					/*
+					 * The previous diagnostic message output likely
+					 * explains why it failed.
+					 */
+					ast_log(LOG_WARNING, "Failed to decode CallerID\n");
 					break;
 				} else if (spill_result) {
 					spill_done = 1;
