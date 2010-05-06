97d5c06298b11aa65cb894d10838145c17a8669f
Tilghman Lesher <tilghman@meg.abyt.es>
2010-05-06 15:43:03 +0000

    Merged revisions 261560 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ........
      r261560 | tilghman | 2010-05-06 10:39:10 -0500 (Thu, 06 May 2010) | 8 lines
      
      Permit more lines within a SIP body to be parsed.
      
      The example given within the related issue showed 120 lines, which was mostly
      a result of the body being XML.
      
      (closes issue #17179)
       Reported by: khw
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.2@261563 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -541,7 +541,7 @@ static int mwi_expiry = DEFAULT_MWI_EXPIRY;
 #define MAX_AUTHTRIES                3                /*!< Try authentication three times, then fail */
 
 #define SIP_MAX_HEADERS              64               /*!< Max amount of SIP headers to read */
-#define SIP_MAX_LINES                64               /*!< Max amount of lines in SIP attachment (like SDP) */
+#define SIP_MAX_LINES                256              /*!< Max amount of lines in SIP attachment (like SDP) */
 #define SIP_MIN_PACKET               4096             /*!< Initialize size of memory to allocate for packets */
 #define MAX_HISTORY_ENTRIES 	     50	              /*!< Max entires in the history list for a sip_pvt */
 
