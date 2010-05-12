a0b12a5666c4ab2df3ce2fbc65822ea0955ee8b3
David Vossel <dvossel@digium.com>
2010-05-12 18:01:20 +0000

    Merged revisions 262662 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
      r262662 | dvossel | 2010-05-12 12:00:04 -0500 (Wed, 12 May 2010) | 11 lines
      
      fixes app_meetme dsp error
      
      We attempted to detect silence after translating a frame
      from signed linear.  This caused a flooding of errors.  To
      resolve this the code to detect silence was moved before the
      translation.
      
      (closes issue #17133)
      Reported by: jsdyer
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@262744 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_meetme.c
+++ apps/app_meetme.c
@@ -3471,6 +3471,10 @@ static int conf_run(struct ast_channel *chan, struct ast_conference *conf, struc
 						ast_mutex_lock(&conf->listenlock);
 						if (!conf->transframe[idx]) {
 							if (conf->origframe) {
+								if (musiconhold && !ast_dsp_silence(dsp, conf->origframe, &confsilence) && confsilence < MEETME_DELAYDETECTTALK) {
+									ast_moh_stop(chan);
+									mohtempstopped = 1;
+								}
 								if (!conf->transpath[idx]) {
 									conf->transpath[idx] = ast_translator_build_path((1 << idx), AST_FORMAT_SLINEAR);
 								}
@@ -3486,11 +3490,6 @@ static int conf_run(struct ast_channel *chan, struct ast_conference *conf, struc
  							if ((conf->transframe[idx]->frametype != AST_FRAME_NULL) &&
 							    can_write(chan, confflags)) {
 								struct ast_frame *cur;
-								if (musiconhold && !ast_dsp_silence(dsp, conf->transframe[idx], &confsilence) && confsilence < MEETME_DELAYDETECTTALK) {
-									ast_moh_stop(chan);
-									mohtempstopped = 1;
-								}
-
 								/* the translator may have returned a list of frames, so
 								   write each one onto the channel
 								*/
