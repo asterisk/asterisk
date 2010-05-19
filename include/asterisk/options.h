136073265cb142eb7989983c8e6d300b952f4baa
Tilghman Lesher <tilghman@meg.abyt.es>
2010-05-19 17:41:29 +0000

    Internal timing is now on by default, if you're using DAHDI 2.3 or above.
    The reason for ensuring DAHDI 2.3 or above is that this version ensures that
    a timer is always available, whereas in previous versions, it was possible
    for DAHDI to be loaded, but have no drivers to actually generate timing.  If
    internal_timing was turned on in this circumstance, a complete lack of audio
    would result.  This is the reason why internal_timing was not on by default.
    However, now that DAHDI ensures the availability of a timer, there is no
    reason for this setting to be off (and in fact, it solves a great many initial
    user problems).
    
    (closes issue #15932)
     Reported by: dimas
     Patches: 
           20100519__issue15932.diff.txt uploaded by tilghman (license 14)
     Tested by: tilghman
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@264248 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- include/asterisk/options.h
+++ include/asterisk/options.h
@@ -23,6 +23,8 @@
 #ifndef _ASTERISK_OPTIONS_H
 #define _ASTERISK_OPTIONS_H
 
+#include "asterisk/autoconfig.h"
+
 #if defined(__cplusplus) || defined(c_plusplus)
 extern "C" {
 #endif
@@ -73,7 +75,7 @@ enum ast_option_flags {
 	AST_OPT_FLAG_DONT_WARN = (1 << 18),
 	/*! End CDRs before the 'h' extension */
 	AST_OPT_FLAG_END_CDR_BEFORE_H_EXTEN = (1 << 19),
-	/*! Use Zaptel Timing for generators if available */
+	/*! Use DAHDI Timing for generators if available */
 	AST_OPT_FLAG_INTERNAL_TIMING = (1 << 20),
 	/*! Always fork, even if verbose or debug settings are non-zero */
 	AST_OPT_FLAG_ALWAYS_FORK = (1 << 21),
@@ -82,7 +84,11 @@ enum ast_option_flags {
 };
 
 /*! These are the options that set by default when Asterisk starts */
+#if (defined(HAVE_DAHDI_VERSION) && HAVE_DAHDI_VERSION >= 230)
+#define AST_DEFAULT_OPTIONS AST_OPT_FLAG_TRANSCODE_VIA_SLIN | AST_OPT_FLAG_INTERNAL_TIMING
+#else
 #define AST_DEFAULT_OPTIONS AST_OPT_FLAG_TRANSCODE_VIA_SLIN
+#endif
 
 #define ast_opt_exec_includes		ast_test_flag(&ast_options, AST_OPT_FLAG_EXEC_INCLUDES)
 #define ast_opt_no_fork			ast_test_flag(&ast_options, AST_OPT_FLAG_NO_FORK)
