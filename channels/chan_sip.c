ee60cb8d41465d7165c7b8477f1b24818b2cedd1
Mark Michelson <mmichelson@digium.com>
2010-05-21 20:38:14 +0000

    Be sure to set the sin_family on the proxy when allocating.
    (closes issue #17157)
    Reported by: stuarth
    
    
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@265087 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -2879,6 +2879,7 @@ static struct sip_proxy *proxy_allocate(char *name, char *port, int force)
 	proxy->force = force;
 	ast_copy_string(proxy->name, name, sizeof(proxy->name));
 	proxy->ip.sin_port = htons(port_str2int(port, STANDARD_SIP_PORT));
+	proxy->ip.sin_family = AF_INET;
 	proxy_update(proxy);
 	return proxy;
 }
