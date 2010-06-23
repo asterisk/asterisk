2f20cacc977ea44c1aec273772102db5797ee200
Paul Belanger <paul.belanger@polybeacon.com>
2010-06-23 21:01:47 +0000

    Merged revisions 272257 via svnmerge from https://origsvn.digium.com/svn/asterisk/trunk
    ................
      r272257 | pabelanger | 2010-06-23 17:00:00 -0400 (Wed, 23 Jun 2010) | 19 lines
      
      Merged revisions 272255 via svnmerge from 
      https://origsvn.digium.com/svn/asterisk/branches/1.4
      
      ........
        r272255 | pabelanger | 2010-06-23 16:57:01 -0400 (Wed, 23 Jun 2010) | 12 lines
        
        First caller into a dynamic conference now enter pin once.
        
        If MeetMe is configured to use dynamic conference
        numbers, then the first caller (which creates the
        conference) had to enter the PIN number twice.
        
        (closes issue #15878)
        Reported by: shawkris
        Patches:
              issue15878.patch uploaded by pabelanger (license 224)
        Tested by: pabelanger
      ........
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.2@272258 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_meetme.c
+++ apps/app_meetme.c
@@ -3867,10 +3867,11 @@ static int conf_exec(struct ast_channel *chan, void *data)
 					res = -1;
 				}
 			} else {
-				if ((!ast_strlen_zero(cnf->pin) &&
-				     !ast_test_flag(&confflags, CONFFLAG_ADMIN)) ||
+				if (((!ast_strlen_zero(cnf->pin) &&
+				    !ast_test_flag(&confflags, CONFFLAG_ADMIN)) ||
 				    (!ast_strlen_zero(cnf->pinadmin) &&
-				     ast_test_flag(&confflags, CONFFLAG_ADMIN))) {
+				    ast_test_flag(&confflags, CONFFLAG_ADMIN))) &&
+				    (!(cnf->users == 0 && cnf->isdynamic))) {
 					char pin[MAX_PIN] = "";
 					int j;
 
