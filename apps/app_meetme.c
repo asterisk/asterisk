b840ef081e9a4aeac1fa22b1698ebef7c8865004
Jeff Peeler <jpeeler@digium.com>
2010-07-01 20:28:15 +0000

    Merged revisions 273474 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
      r273474 | jpeeler | 2010-07-01 15:19:16 -0500 (Thu, 01 Jul 2010) | 14 lines
      
      Allow admin user to join conference without using admin mode and no user pin.
      
      Configuring the conference in meetme.conf like the following:
      conf => 2345,,6666 
      did not prompt for pin when used without admin mode. This meant that the
      conference could not be joined as an admin even if the user knew the correct
      pin. The original bug report was submitted claiming that the blank user pin
      should deny entry into the conference. I think a better way to handle this
      would be with a feature enhancement that used the following syntax:
      conf => 2345,X,6666 - where X denotes no acceptable pin allowed
      
      (closes issue #15704)
      Reported by: modelnine
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@273522 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_meetme.c
+++ apps/app_meetme.c
@@ -4154,8 +4154,7 @@ static int conf_exec(struct ast_channel *chan, const char *data)
 			} else {
 				if (((!ast_strlen_zero(cnf->pin) &&
 				    !ast_test_flag64(&confflags, CONFFLAG_ADMIN)) ||
-				    (!ast_strlen_zero(cnf->pinadmin) &&
-				    ast_test_flag64(&confflags, CONFFLAG_ADMIN))) &&
+				    !ast_strlen_zero(cnf->pinadmin)) &&
 				    (!(cnf->users == 0 && cnf->isdynamic))) {
 					char pin[MAX_PIN] = "";
 					int j;
