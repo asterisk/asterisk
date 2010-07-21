765b27597681ad554be768050341c7fa04ca1c1b
Russell Bryant <russell@russellbryant.com>
2010-07-21 16:15:00 +0000

    Use poll() instead of select() in res_timing_pthread to avoid stack corruption.
    This code did not properly check FD_SETSIZE to ensure that it did not try to
    select() on fds that were too large.  Switching to poll() removes the limitation
    on the maximum fd value.
    
    (closes issue #15915)
    Reported by: keiron
    
    (closes issue #17187)
    Reported by: Eddie Edwards
    
    (closes issue #16494)
    Reported by: Hubguru
    
    (closes issue #15731)
    Reported by: flop
    
    (closes issue #12917)
    Reported by: falves11
    
    (closes issue #14920)
    Reported by: vrban
    
    (closes issue #17199)
    Reported by: aleksey2000
    
    (closes issue #15406)
    Reported by: kowalma
    
    (closes issue #17438)
    Reported by: dcabot
    
    (closes issue #17325)
    Reported by: glwgoes
    
    (closes issue #17118)
    Reported by: erikje
    
    possibly other issues, too ...
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@278465 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_timing_pthread.c
+++ res/res_timing_pthread.c
@@ -36,6 +36,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$");
 #include "asterisk/astobj2.h"
 #include "asterisk/time.h"
 #include "asterisk/lock.h"
+#include "asterisk/poll-compat.h"
 
 static void *timing_funcs_handle;
 
@@ -376,16 +377,12 @@ static void read_pipe(struct pthread_timer *timer, unsigned int quantity)
 	do {
 		unsigned char buf[1024];
 		ssize_t res;
-		fd_set rfds;
-		struct timeval timeout = {
-			.tv_sec = 0,
+		struct pollfd pfd = {
+			.fd = rd_fd,
+			.events = POLLIN,
 		};
 
-		/* Make sure there is data to read */
-		FD_ZERO(&rfds);
-		FD_SET(rd_fd, &rfds);
-
-		if (select(rd_fd + 1, &rfds, NULL, NULL, &timeout) != 1) {
+		if (ast_poll(&pfd, 1, 0) != 1) {
 			ast_debug(1, "Reading not available on timing pipe, "
 					"quantity: %u\n", quantity);
 			break;
