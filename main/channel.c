d3700923db2a2ae1a3dc3cb25e88077ead917ca7
Russell Bryant <russell@russellbryant.com>
2010-07-26 21:53:30 +0000

    Ignore a control subclass of -1 in ast_waitfordigit_full().
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@279636 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/channel.c
+++ main/channel.c
@@ -3429,6 +3429,7 @@ int ast_waitfordigit_full(struct ast_channel *c, int ms, int audiofd, int cmdfd)
 				case AST_CONTROL_SRCCHANGE:
 				case AST_CONTROL_CONNECTED_LINE:
 				case AST_CONTROL_REDIRECTING:
+				case -1:
 					/* Unimportant */
 					break;
 				default:
