377c3bf88ee9888ca51dfe53ba66eda34f79ce0b
Jason Parker <jparker@digium.com>
2010-08-12 22:52:33 +0000

    Merged revisions 282131 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.8
    ................
      r282131 | qwell | 2010-08-12 17:51:44 -0500 (Thu, 12 Aug 2010) | 16 lines
      
      Merged revisions 282130 via svnmerge from 
      https://origsvn.digium.com/svn/asterisk/branches/1.6.2
      
      ................
        r282130 | qwell | 2010-08-12 17:50:54 -0500 (Thu, 12 Aug 2010) | 9 lines
        
        Merged revisions 282129 via svnmerge from 
        https://origsvn.digium.com/svn/asterisk/branches/1.4
        
        ........
          r282129 | qwell | 2010-08-12 17:49:28 -0500 (Thu, 12 Aug 2010) | 1 line
          
          Register CLI commands before parsing config, in case there is a config error.
        ........
      ................
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@282132 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- pbx/pbx_config.c
+++ pbx/pbx_config.c
@@ -1737,12 +1737,12 @@ static int pbx_load_module(void)
 
 static int load_module(void)
 {
-	if (pbx_load_module())
-		return AST_MODULE_LOAD_DECLINE;
- 
 	if (static_config && !write_protect_config)
 		ast_cli_register(&cli_dialplan_save);
 	ast_cli_register_multiple(cli_pbx_config, ARRAY_LEN(cli_pbx_config));
+
+	if (pbx_load_module())
+		return AST_MODULE_LOAD_DECLINE;
 
 	return AST_MODULE_LOAD_SUCCESS;
 }
