c26e47f28979bc27268e0310fc7f7e441469d937
David Vossel <dvossel@digium.com>
2010-08-26 15:22:28 +0000

    Fixed how Asterisk destroys a dialog on channel hangup before invite receives a response.
    If an ast_channel with a SIP tech pvt hangs up before the sip dialog gets a response
    to its outgoing INVITE, Asterisk used to pretend_ack the INVITE.  This is not rfc
    compliant and results in confusion at the other endpoint.  sip_pretend_ack will ack
    and remove all the packets in the retransmit queue.  This means that the INVITE will
    stop retransmitting, and that any response to that INVITE that comes after the pretend_ack
    occurs will be ignored.
    
    Instead of faking any sort of acknowledgement for an outgoing INVITE during an internal
    hangup, we should let the protocol stack process the INVITE transaction and terminate
    the dialog properly.  This is achieved by setting the PENDING_BYE flag.  When this flag
    is used, once the dialog proceeds to an escapable state the transaction will either be
    canceled with a SIP_CANCEL or completed followed immediately by a BYE.  Attempting to do
    this any other way is incorrect.  If the endpoint is not responding to the INVITE request,
    the INVITE must continue to be retransmitted until it times out which will result in the
    dialog being destroyed.
    
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@283690 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -3858,7 +3858,6 @@ static int sip_hangup(struct ast_channel *ast)
 				if (p->invitestate == INV_CALLING) {
 					/* We can't send anything in CALLING state */
 					ast_set_flag(&p->flags[0], SIP_PENDINGBYE);
-					__sip_pretend_ack(p);
 					/* Do we need a timer here if we don't hear from them at all? Yes we do or else we will get hung dialogs and those are no fun. */
 					sip_scheddestroy(p, DEFAULT_TRANS_TIMEOUT);
 					append_history(p, "DELAY", "Not sending cancel, waiting for timeout");
