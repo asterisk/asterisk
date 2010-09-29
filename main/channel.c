ddbf2ec579d9b440acfc45a34d3214c86ebb67e5
Matthew Nicholson <mnicholson@digium.com>
2010-09-29 15:04:11 +0000

    Merged revisions 289177 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
      r289177 | mnicholson | 2010-09-29 10:03:27 -0500 (Wed, 29 Sep 2010) | 8 lines
      
      Set the caller id on CDRs when it is set on the parent channel.
      
      (closes issue #17569)
      Reported by: tbelder
      Patches:
            17569.diff uploaded by tbelder (license 618)
      Tested by: tbelder
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.2@289178 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/channel.c
+++ main/channel.c
@@ -4935,6 +4935,9 @@ void ast_set_callerid(struct ast_channel *chan, const char *cid_num, const char 
 			ast_free(chan->cid.cid_ani);
 		chan->cid.cid_ani = ast_strdup(cid_ani);
 	}
+	if (chan->cdr) {
+		ast_cdr_setcid(chan->cdr, chan);
+	}
 
 	report_new_callerid(chan);
 
