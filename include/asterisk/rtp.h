8f6ca92a9d07e4a89011b4c2e341874d1f939952
Jeff Peeler <jpeeler@digium.com>
2010-10-01 22:58:38 +0000

    Change RFC2833 DTMF event duration on end to report actual elapsed time.
    The scenario here is with a non P2P early media session. The reported time
    length of DTMF presses are coming up short when sending to the remote side.
    Currently the event duration is a running total that is incremented when sending
    continuation packets. These continuation packets are only triggered upon
    incoming media from the remote side, which means that the running total probably
    is not going to end up matching the actual length of time Asterisk received
    DTMF. This patch changes the end event duration to be lengthened if it is
    detected that the end event is going to come up short.
    
    Review: https://reviewboard.asterisk.org/r/957/
    
    ABE-2476
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.4@289797 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- include/asterisk/rtp.h
+++ include/asterisk/rtp.h
@@ -174,6 +174,7 @@ int ast_rtcp_fd(struct ast_rtp *rtp);
 int ast_rtp_senddigit_begin(struct ast_rtp *rtp, char digit);
 
 int ast_rtp_senddigit_end(struct ast_rtp *rtp, char digit);
+int ast_rtp_senddigit_end_with_duration(struct ast_rtp *rtp, char digit, unsigned int duration);
 
 int ast_rtp_sendcng(struct ast_rtp *rtp, int level);
 
