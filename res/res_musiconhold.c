6ff3d6b761486c71b07516ceaca3b5e0b9744454
Tilghman Lesher <tilghman@meg.abyt.es>
2010-10-20 00:40:29 +0000

    Oops.  This module uses the generic timer and no longer uses DAHDI.
    This causes a problem with the Solaris and other system builds that have gcc
    4.1 (where optional_api is non-optional).
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@292376 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_musiconhold.c
+++ res/res_musiconhold.c
@@ -1,7 +1,7 @@
 /*
  * Asterisk -- An open source telephony toolkit.
  *
- * Copyright (C) 1999 - 2006, Digium, Inc.
+ * Copyright (C) 1999 - 2010, Digium, Inc.
  *
  * Mark Spencer <markster@digium.com>
  *
@@ -27,7 +27,6 @@
 
 /*** MODULEINFO
 	<conflict>win32</conflict>
-	<use>dahdi</use>
  ***/
 
 #include "asterisk.h"
