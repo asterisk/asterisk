4e603bd692fd8f42304ef4ebc841388d0abd03ef
Tilghman Lesher <tilghman@meg.abyt.es>
2010-10-28 19:52:52 +0000

    Merged revisions 293194 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.4
    ........
      r293194 | tilghman | 2010-10-28 14:44:37 -0500 (Thu, 28 Oct 2010) | 5 lines
      
      "!00" evaluated as false, which is incorrect.  Fixing.
      
      Reported (though the reporter did not understand he was reporting a bug) on the asterisk-users list:
      http://lists.digium.com/pipermail/asterisk-users/2010-October/255505.html
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.2@293195 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/ael/ael.tab.h
+++ res/ael/ael.tab.h
@@ -1,24 +1,23 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
 
 /* Skeleton interface for Bison's Yacc-like parsers in C
-
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
    Free Software Foundation, Inc.
-
-   This program is free software; you can redistribute it and/or modify
+   
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
+   
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -29,9 +28,10 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-
+   
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
+
 
 /* Tokens.  */
 #ifndef YYTOKENTYPE
@@ -82,66 +82,28 @@
      word = 298
    };
 #endif
-/* Tokens.  */
-#define KW_CONTEXT 258
-#define LC 259
-#define RC 260
-#define LP 261
-#define RP 262
-#define SEMI 263
-#define EQ 264
-#define COMMA 265
-#define COLON 266
-#define AMPER 267
-#define BAR 268
-#define AT 269
-#define KW_MACRO 270
-#define KW_GLOBALS 271
-#define KW_IGNOREPAT 272
-#define KW_SWITCH 273
-#define KW_IF 274
-#define KW_IFTIME 275
-#define KW_ELSE 276
-#define KW_RANDOM 277
-#define KW_ABSTRACT 278
-#define KW_EXTEND 279
-#define EXTENMARK 280
-#define KW_GOTO 281
-#define KW_JUMP 282
-#define KW_RETURN 283
-#define KW_BREAK 284
-#define KW_CONTINUE 285
-#define KW_REGEXTEN 286
-#define KW_HINT 287
-#define KW_FOR 288
-#define KW_WHILE 289
-#define KW_CASE 290
-#define KW_PATTERN 291
-#define KW_DEFAULT 292
-#define KW_CATCH 293
-#define KW_SWITCHES 294
-#define KW_ESWITCHES 295
-#define KW_INCLUDES 296
-#define KW_LOCAL 297
-#define word 298
-
 
 
 
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef union YYSTYPE
-#line 59 "ael.y"
 {
+
+/* Line 1676 of yacc.c  */
+#line 59 "ael.y"
+
 	int	intval;		/* integer value, typically flags */
 	char	*str;		/* strings */
 	struct pval *pval;	/* full objects */
-}
-/* Line 1489 of yacc.c.  */
-#line 141 "ael.tab.h"
-	YYSTYPE;
+
+
+
+/* Line 1676 of yacc.c  */
+#line 103 "ael.tab.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
-# define YYSTYPE_IS_TRIVIAL 1
 #endif
 
 
@@ -158,5 +120,6 @@ typedef struct YYLTYPE
 # define YYLTYPE_IS_DECLARED 1
 # define YYLTYPE_IS_TRIVIAL 1
 #endif
+
 
 
