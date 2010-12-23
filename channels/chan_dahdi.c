4e1350ed6286f1b11afd0e0596fb0484b0cd3070
Moises Silva <moises.silva@gmail.com>
2010-12-23 02:28:37 +0000

    Enqueue AST_CONTROL_PROGRESS after AST_CONTROL_RINGING when MFC-R2 calls are accepted
    (closes issue #18438)
    Reported by: mariner7
    Tested by: moy
    
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.6.2@299530 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_dahdi.c
+++ channels/chan_dahdi.c
@@ -3618,6 +3618,7 @@ static int dahdi_call(struct ast_channel *ast, char *rdest, int timeout)
 			ast_log(LOG_ERROR, "unable to make new MFC/R2 call!\n");
 			return -1;
 		}
+		p->mfcr2_call_accepted = 0;
 		ast_setstate(ast, AST_STATE_DIALING);
 	}
 #endif /* HAVE_OPENR2 */
@@ -7064,6 +7065,18 @@ static struct ast_frame *dahdi_read(struct ast_channel *ast)
 #ifdef HAVE_OPENR2
 	if (p->mfcr2) {
 		openr2_chan_process_event(p->r2chan);
+		if (OR2_DIR_FORWARD == openr2_chan_get_direction(p->r2chan)) {
+			struct ast_frame f = { AST_FRAME_CONTROL, AST_CONTROL_PROGRESS, };
+			/* if the call is already accepted and we already delivered AST_CONTROL_RINGING
+			 * now enqueue a progress frame to bridge the media up */
+			if (p->mfcr2_call_accepted &&
+			    !p->progress && 
+			    ast->_state == AST_STATE_RINGING) {
+				ast_log(LOG_DEBUG, "Enqueuing progress frame after R2 accept in chan %d\n", p->channel);
+				ast_queue_frame(p->owner, &f);
+				p->progress = 1;
+			}
+		}
 	}
 #endif
 
