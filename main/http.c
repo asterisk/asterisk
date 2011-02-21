679a7326d6f5131222bf253a86d26682f49304b0
Andrew Latham <lathama@gmail.com>
2011-02-21 14:24:43 +0000

    Add HTTP URI Debug logging and update notice
    enable reporting of the request URI / URL in debugging
    change funny debug note to a serious note.
    
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@308393 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/http.c
+++ main/http.c
@@ -661,6 +661,9 @@ static int handle_uri(struct ast_tcptls_session_instance *ser, char *uri,
 	struct ast_variable *get_vars = NULL, *v, *prev = NULL;
 	struct http_uri_redirect *redirect;
 
+	if (option_debug > 4)
+		ast_verbose("HTTP Request URI is %s \n", uri);
+
 	strsep(&params, "?");
 	/* Extract arguments from the request and store them in variables. */
 	if (params) {
@@ -804,7 +807,7 @@ static struct ast_variable *parse_cookies(char *cookies)
 		}
 
 		if (option_debug) {
-			ast_log(LOG_DEBUG, "mmm ... cookie!  Name: '%s'  Value: '%s'\n", name, val);
+			ast_log(LOG_DEBUG, "HTTP Cookie, Name: '%s'  Value: '%s'\n", name, val);
 		}
 
 		var = ast_variable_new(name, val, __FILE__);
