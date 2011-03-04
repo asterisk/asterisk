asterisk/asterisk

commit b4e18d5660fcc13b97cd4682ff48e33aa9d15d83

Add load priority order, such that preload becomes unnecessary in most cases
git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@278132 65c4cc65-6c06-0410-ace0-fbb531ad65f3

pbx/pbx_lua.c
@@ -84,7 +84,7 @@ static void lua_create_application_metatable(lua_State *L);
 static void lua_create_autoservice_functions(lua_State *L);
 static void lua_create_hangup_function(lua_State *L);
 
-void lua_state_destroy(void *data);
+static void lua_state_destroy(void *data);
 static lua_State *lua_get_state(struct ast_channel *chan);
 
 static int exists(struct ast_channel *chan, const char *context, const char *exten, int priority, const char *callerid, const char *data);
@@ -108,7 +108,7 @@ static const struct ast_datastore_info lua_datastore = {
 /*!
  * \brief The destructor for lua_datastore
  */
-void lua_state_destroy(void *data)
+static void lua_state_destroy(void *data)
 {
 	if (data)
 		lua_close(data);
@@ -1467,7 +1467,7 @@ static int load_module(void)
 	return AST_MODULE_LOAD_SUCCESS;
 }
 
-AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_GLOBAL_SYMBOLS, "Lua PBX Switch",
+AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_DEFAULT, "Lua PBX Switch",
 		.load = load_module,
 		.unload = unload_module,
 		.reload = reload,

commit 4988f8f6e1cf6940a9aac39cc6167f9f3307a3e6

Export global symbols from pbx_lua to allow modules to be loaded.  Fixes a regression introduced in r278132.
(closes issue #18671)
Reported by: Igels
Patches:
      pbx_lua_global_symbols1.diff uploaded by mnicholson (license 96)
Tested by: Igels


git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@309448 65c4cc65-6c06-0410-ace0-fbb531ad65f3

pbx/pbx_lua.c
@@ -1467,7 +1467,7 @@ static int load_module(void)
 	return AST_MODULE_LOAD_SUCCESS;
 }
 
-AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_DEFAULT, "Lua PBX Switch",
+AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_GLOBAL_SYMBOLS, "Lua PBX Switch",
 		.load = load_module,
 		.unload = unload_module,
 		.reload = reload,