56cd7709a51439f40eef44343d1378d7bb681b3f
Tilghman Lesher <tilghman@meg.abyt.es>
2011-03-07 00:54:42 +0000

    Merged revisions 309251 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.6.2
    ........
      r309251 | tilghman | 2011-03-01 19:06:02 -0600 (Tue, 01 Mar 2011) | 7 lines
      
      Revert previous 2 commits, and instead conditionally redefine the same macro used in flex 2.5.35 that clashed with our workaround.
      
      Not surprisingly, the workaround was exactly the same code as was provided by
      the Flex maintainers, albeit in two different places, in different macros.
      
      This should fix the FreeBSD builds, which have an older version of Flex.
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@309808 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_dahdi.c
+++ channels/chan_dahdi.c
@@ -2122,6 +2122,7 @@ static void dahdi_ami_channel_event(struct dahdi_pvt *p, struct ast_channel *cha
 		ch_name);
 }
 
+#ifdef HAVE_PRI
 /*!
  * \internal
  * \brief Post an AMI DAHDI channel association event.
@@ -2138,6 +2139,7 @@ static void my_ami_channel_event(void *pvt, struct ast_channel *chan)
 
 	dahdi_ami_channel_event(p, chan);
 }
+#endif
 
 /* linear_mode = 0 - turn linear mode off, >0 - turn linear mode on
 * 	returns the last value of the linear setting 
