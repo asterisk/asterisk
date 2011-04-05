f6f534077710f75efd0ac3530d63c58802860323
Jonathan Rose <jrose@digium.com>
2011-04-05 14:13:15 +0000

    Merged revisions 312762 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.6.2
    ........
      r312762 | jrose | 2011-04-05 09:11:36 -0500 (Tue, 05 Apr 2011) | 1 line
      
      Backporting trunk change to add verbosity to 'L' option in meetme
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@312765 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_meetme.c
+++ apps/app_meetme.c
@@ -2287,7 +2287,15 @@ static int conf_run(struct ast_channel *chan, struct ast_conference *conf, struc
  					play_warning = warning_freq = 0;
  			}
  		}
- 		
+
+		ast_verb(3, "Setting conference duration limit to: %ldms.\n", timelimit);
+		if (play_warning) {
+			ast_verb(3, "Setting warning time to %ldms from the conference duration limit.\n", play_warning);
+		}
+		if (warning_freq) {
+			ast_verb(3, "Setting warning frequency to %ldms.\n", warning_freq);
+		}
+
 		ast_channel_lock(chan);
 		if ((var = pbx_builtin_getvar_helper(chan, "CONF_LIMIT_WARNING_FILE"))) {
 			var = ast_strdupa(var);
