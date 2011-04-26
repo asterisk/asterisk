abe0351e1293c8d18d5cd4d64e8cd4066bcbdf2e
Richard Mudgett <rmudgett@digium.com>
2011-04-26 18:02:07 +0000

    Merged revisions 315452 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.8
    ........
      r315452 | rmudgett | 2011-04-26 13:00:34 -0500 (Tue, 26 Apr 2011) | 1 line
      
      Add missing set of name valid flag when dialing.
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@315453 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_dial.c
+++ apps/app_dial.c
@@ -2243,6 +2243,7 @@ static int dial_exec_full(struct ast_channel *chan, const char *data, struct ast
 				 * No hint name available.  We have a connected name supplied by
 				 * the dialplan we can use instead.
 				 */
+				caller.id.name.valid = 1;
 				caller.id.name = chan->connected.id.name;
 			}
 			ast_channel_set_caller_event(tc, &caller, NULL);
@@ -2256,6 +2257,7 @@ static int dial_exec_full(struct ast_channel *chan, const char *data, struct ast
 				 * We have a connected name supplied by the dialplan we can
 				 * use instead.
 				 */
+				caller.id.name.valid = 1;
 				caller.id.name = chan->connected.id.name;
 				ast_channel_set_caller_event(tc, &caller, NULL);
 			}
