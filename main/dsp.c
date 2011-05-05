c28e2d380c185b8cda44b468504bb8a6892714a6
Russell Bryant <russell@russellbryant.com>
2011-05-05 22:12:10 +0000

    Merged revisions 317429 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.8
    ........
      r317429 | russell | 2011-05-05 17:11:19 -0500 (Thu, 05 May 2011) | 5 lines
      
      Only display inband DTMF warning if inband DTMF detection is enabled.
      
      (closes issue #18901)
      Reported by: irroot
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@317430 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/dsp.c
+++ main/dsp.c
@@ -1595,6 +1595,9 @@ struct ast_dsp *ast_dsp_new_with_rate(unsigned int sample_rate)
 void ast_dsp_set_features(struct ast_dsp *dsp, int features)
 {
 	dsp->features = features;
+	if (!(features & DSP_FEATURE_DIGIT_DETECT)) {
+		dsp->display_inband_dtmf_warning = 0;
+	}
 }
 
 void ast_dsp_free(struct ast_dsp *dsp)
