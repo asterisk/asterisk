f7195285c936635030fc38f1a565171c6a859481
David Vossel <dvossel@digium.com>
2011-07-07 22:16:10 +0000

    Adds missing celt.h file from celt pass-through support patch.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@326900 65c4cc65-6c06-0410-ace0-fbb531ad65f3

---
+++ include/asterisk/celt.h
@@ -0,0 +1,74 @@
+/*
+ * Asterisk -- An open source telephony toolkit.
+ *
+ * Copyright (C) 2011, Digium, Inc.
+ *
+ * David Vossel <dvossel@digium.com>
+ *
+ * See http://www.asterisk.org for more information about
+ * the Asterisk project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
+ */
+
+/*!
+ * \file
+ * \brief CELT Format Attributes
+ *
+ * \author David Vossel <dvossel@digium.com>
+ */
+#ifndef _AST_FORMAT_CELT_H_
+#define _AST_FORMAT_CELT_H_
+
+#define AST_CELT_DEFAULT_FRAME_SIZE 480
+
+/*! CELT format attribute key value pairs, all are accessible through ast_format_get_value()*/
+enum celt_attr_keys {
+	CELT_ATTR_KEY_SAMP_RATE, /*!< value is an unsigned integer representing sample rate */
+	CELT_ATTR_KEY_MAX_BITRATE, /*!< value is an int */
+	CELT_ATTR_KEY_FRAME_SIZE, /*!< value is an int */
+};
+
+#endif /* _AST_FORMAT_CELT_H */
+/*
+ * Asterisk -- An open source telephony toolkit.
+ *
+ * Copyright (C) 2011, Digium, Inc.
+ *
+ * David Vossel <dvossel@digium.com>
+ *
+ * See http://www.asterisk.org for more information about
+ * the Asterisk project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
+ */
+
+/*!
+ * \file
+ * \brief CELT Format Attributes
+ *
+ * \author David Vossel <dvossel@digium.com>
+ */
+#ifndef _AST_FORMAT_CELT_H_
+#define _AST_FORMAT_CELT_H_
+
+#define AST_CELT_DEFAULT_FRAME_SIZE 480
+
+/*! CELT format attribute key value pairs, all are accessible through ast_format_get_value()*/
+enum celt_attr_keys {
+	CELT_ATTR_KEY_SAMP_RATE, /*!< value is an unsigned integer representing sample rate */
+	CELT_ATTR_KEY_MAX_BITRATE, /*!< value is an int */
+	CELT_ATTR_KEY_FRAME_SIZE, /*!< value is an int */
+};
+
+#endif /* _AST_FORMAT_CELT_H */
