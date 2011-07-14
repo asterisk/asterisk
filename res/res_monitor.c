8dc71df9d0f74a8094553944b6bfcf3a64eed047
Jonathan Rose <jrose@digium.com>
2011-07-14 19:56:19 +0000

    Merged revisions 328207 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.10
    ................
      r328207 | jrose | 2011-07-14 14:45:18 -0500 (Thu, 14 Jul 2011) | 13 lines
      
      Merged revisions 328205 via svnmerge from 
      https://origsvn.digium.com/svn/asterisk/branches/1.8
      
      ........
        r328205 | jrose | 2011-07-14 14:21:02 -0500 (Thu, 14 Jul 2011) | 6 lines
        
        Monitor application arguments requirements fixed.
        
        Monitor was requiring options in spite of no individual option on Monitor being required.
        
        Review: https://reviewboard.asterisk.org/r/1320/
      ........
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@328208 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_monitor.c
+++ res/res_monitor.c
@@ -650,12 +650,6 @@ static int start_monitor_exec(struct ast_channel *chan, const char *data)
 		AST_APP_ARG(fname_base);
 		AST_APP_ARG(options);
 	);
-	
-	/* Parse arguments. */
-	if (ast_strlen_zero(data)) {
-		ast_log(LOG_ERROR, "Monitor requires an argument\n");
-		return 0;
-	}
 
 	parse = ast_strdupa(data);
 	AST_STANDARD_APP_ARGS(args, parse);
@@ -671,7 +665,9 @@ static int start_monitor_exec(struct ast_channel *chan, const char *data)
 			stream_action &= ~X_REC_OUT;
 	}
 
-	arg = strchr(args.format, ':');
+	if (args.format) {
+		arg = strchr(args.format, ':');
+	}
 	if (arg) {
 		*arg++ = 0;
 		urlprefix = arg;
