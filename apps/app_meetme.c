f1a594068c351fd6629430362524a9ced8450846
Kinsey Moore <kmoore@digium.com>
2011-07-19 15:46:54 +0000

    Merged revisions 328770 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.8
    ........
      r328770 | kmoore | 2011-07-19 10:43:32 -0500 (Tue, 19 Jul 2011) | 11 lines
      
      MeetMe requests a PIN twice in some circumstances
      
      If a call to MeetMe includes both the dynamic(D) and always request PIN(P)
      options, MeetMe will ask for the PIN two times: once for creating the
      conference and once for entering the conference.  This behavior was introduced
      in rev 311616 when adding the CONFFLAG_ALWAYSPROMPT option to the logic branch
      controlling PIN entry for joining a conference.
      
      (closes AST-601)
      Review: https://reviewboard.asterisk.org/r/1305/
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.10@328771 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_meetme.c
+++ apps/app_meetme.c
@@ -4419,15 +4419,13 @@ static int conf_exec(struct ast_channel *chan, const char *data)
 					res = -1;
 				}
 			} else {
-				if (((!ast_strlen_zero(cnf->pin)       &&
-					!ast_test_flag64(&confflags, CONFFLAG_ADMIN)) ||
-				     (!ast_strlen_zero(cnf->pinadmin)  &&
-						 ast_test_flag64(&confflags, CONFFLAG_ADMIN)) ||
-					     (!ast_strlen_zero(cnf->pin) &&
-							 ast_strlen_zero(cnf->pinadmin) &&
-							 ast_test_flag64(&confflags, CONFFLAG_ADMIN))) &&
-				    ((!(cnf->users == 0 && cnf->isdynamic)) ||
-						ast_test_flag64(&confflags, CONFFLAG_ALWAYSPROMPT))) {
+				/* Check to see if the conference requires a pin
+				 * and we ALWAYS prompt or no pin was provided */
+				if ((!ast_strlen_zero(cnf->pin) ||
+					(!ast_strlen_zero(cnf->pinadmin) &&
+						ast_test_flag64(&confflags, CONFFLAG_ADMIN))) &&
+				    (ast_test_flag64(&confflags, CONFFLAG_ALWAYSPROMPT) ||
+						ast_strlen_zero(args.pin))) {
 					char pin[MAX_PIN] = "";
 					int j;
 
