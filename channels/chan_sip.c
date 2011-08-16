0059096c5e65e933e942d4b61d7b3680833344ae
Matthew Nicholson <mnicholson@digium.com>
2011-08-16 15:08:40 +0000

    Merged revisions 332026 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.8
    ........
      r332026 | mnicholson | 2011-08-16 10:06:31 -0500 (Tue, 16 Aug 2011) | 2 lines
      
      use DEFAULT_STORE_SIP_CAUSE to set the default value for the 'storesipcause' option
    
      AST-580
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/10@332027 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -28089,7 +28089,7 @@ static int reload_config(enum channelreloadreason reason)
 	global_shrinkcallerid = 1;
 	authlimit = DEFAULT_AUTHLIMIT;
 	authtimeout = DEFAULT_AUTHTIMEOUT;
-	global_store_sip_cause = FALSE;
+	global_store_sip_cause = DEFAULT_STORE_SIP_CAUSE;
 
 	sip_cfg.matchexternaddrlocally = DEFAULT_MATCHEXTERNADDRLOCALLY;
 
