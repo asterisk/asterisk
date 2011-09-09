d5544999b588313f05c270f3d49a69c94d7507a0
Gregory Nietsky <gregory@distrotech.co.za>
2011-09-09 07:23:53 +0000

    Move code for VALID_EXTEN from app_readexten to func_dialplan
    Mark VALID_EXTEN deprecated.
    
    Review: https://reviewboard.asterisk.org/r/1396/
    
    
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/10@335014 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_readexten.c
+++ apps/app_readexten.c
@@ -96,24 +96,6 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 			</variablelist>
 		</description>
 	</application>
-	<function name="VALID_EXTEN" language="en_US">
-		<synopsis>
-			Determine whether an extension exists or not.
-		</synopsis>
-		<syntax>
-			<parameter name="context">
-				<para>Defaults to the current context</para>
-			</parameter>
-			<parameter name="extension" required="true" />
-			<parameter name="priority">
-				<para>Priority defaults to <literal>1</literal>.</para>
-			</parameter>
-		</syntax>
-		<description>
-			<para>Returns a true value if the indicated <replaceable>context</replaceable>,
-			<replaceable>extension</replaceable>, and <replaceable>priority</replaceable> exist.</para>
-		</description>
-	</function>
  ***/
 
 enum readexten_option_flags {
@@ -280,57 +262,15 @@ static int readexten_exec(struct ast_channel *chan, const char *data)
 	return status[0] == 'H' ? -1 : 0;
 }
 
-static int acf_isexten_exec(struct ast_channel *chan, const char *cmd, char *parse, char *buffer, size_t buflen)
-{
-	int priority_int;
-	AST_DECLARE_APP_ARGS(args,
-		AST_APP_ARG(context);
-		AST_APP_ARG(extension);
-		AST_APP_ARG(priority);
-	);
-
-	AST_STANDARD_APP_ARGS(args, parse);
-
-	if (ast_strlen_zero(args.context))
-		args.context = chan->context;
-
-	if (ast_strlen_zero(args.extension)) {
-		ast_log(LOG_WARNING, "Syntax: VALID_EXTEN([<context>],<extension>[,<priority>]) - missing argument <extension>!\n");
-		return -1;
-	}
-
-	if (ast_strlen_zero(args.priority))
-		priority_int = 1;
-	else
-		priority_int = atoi(args.priority);
-
-	if (ast_exists_extension(chan, args.context, args.extension, priority_int,
-		S_COR(chan->caller.id.number.valid, chan->caller.id.number.str, NULL))) {
-	    ast_copy_string(buffer, "1", buflen);
-	} else {
-	    ast_copy_string(buffer, "0", buflen);
-	}
-
-	return 0;
-}
-
-static struct ast_custom_function acf_isexten = {
-	.name = "VALID_EXTEN",
-	.read = acf_isexten_exec,
-};
-
 static int unload_module(void)
 {
 	int res = ast_unregister_application(app);
-	res |= ast_custom_function_unregister(&acf_isexten);
-
-	return res;	
+	return res;
 }
 
 static int load_module(void)
 {
 	int res = ast_register_application_xml(app, readexten_exec);
-	res |= ast_custom_function_register(&acf_isexten);
 	return res;
 }
 
