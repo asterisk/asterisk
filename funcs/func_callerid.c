986c3e6c644bd74a3246bdf98cf264c769679c7a
Paul Belanger <paul.belanger@polybeacon.com>
2011-10-20 20:47:39 +0000

    Updated documentation for the optional CID parameter with CALLERID ........
    Merged revisions 341664 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/10@341665 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- funcs/func_callerid.c
+++ funcs/func_callerid.c
@@ -120,7 +120,8 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 				</enumlist>
 			</parameter>
 			<parameter name="CID">
-				<para>Optional Caller*ID</para>
+				<para>Optional Caller*ID to parse instead of using the Caller*ID from the
+				channel. This paramater is only optional when reading the Caller*ID.</para>
 			</parameter>
 		</syntax>
 		<description>
