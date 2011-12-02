2ff29060113428beca6b4cf72d05bb69cc64c928
Alexandr Anikin <may@telecom-service.ru>
2011-12-02 16:42:32 +0000

    Merged revisions 346762 via svnmerge from https://origsvn.digium.com/svn/asterisk/branches/1.8
    ........
      r346762 | may | 2011-12-02 20:19:19 +0400 (Fri, 02 Dec 2011) | 7 lines
      
      process null frame pointer returned by ast_rtp_instance_read correctly
      
      (closes issue ASTERISK-16697)
      Reported by: under
      Patches: 
              segfault.diff (License #5871) patch uploaded by under
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/10@346763 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_h323.c
+++ channels/chan_h323.c
@@ -772,7 +772,7 @@ static struct ast_frame *oh323_rtp_read(struct oh323_pvt *pvt)
 	if (f && (f->frametype == AST_FRAME_DTMF) && !(pvt->options.dtmfmode & (H323_DTMF_RFC2833 | H323_DTMF_CISCO))) {
 		return &ast_null_frame;
 	}
-	if (pvt->owner) {
+	if (f && pvt->owner) {
 		/* We already hold the channel lock */
 		if (f->frametype == AST_FRAME_VOICE) {
 			if (!ast_format_cap_iscompatible(pvt->owner->nativeformats, &f->subclass.format)) {
