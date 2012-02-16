706d34fade0d0613ceee273970b48994ba46431d
Sean Bright <sean@malleable.com>
2012-02-16 20:01:15 +0000

    Revert a change to audio_audiohook_write_list that had no affect.
    When I made this change initially, I was under the false impression that the
    audiohooks structure remained on the channel after all of the hooks had been
    detached.  This is not the case, ast ast_read takes care of removing the
    audiohooks structure if the lists are empty.
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@355622 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/audiohook.c
+++ main/audiohook.c
@@ -621,10 +621,6 @@ static struct ast_frame *audio_audiohook_write_list(struct ast_channel *chan, st
 	struct ast_audiohook *audiohook = NULL;
 	int samples = frame->samples;
 
-	/* Don't translate our frame if we aren't going to bother to use it */
-	if (ast_audiohook_write_list_empty(audiohook_list))
-		return end_frame;
-
 	/* ---Part_1. translate start_frame to SLINEAR if necessary. */
 	/* If the frame coming in is not signed linear we have to send it through the in_translate path */
 	if (frame->subclass.codec != AST_FORMAT_SLINEAR) {
