5b0f29d71053ca7ff28fe80084eb20a58e8927a0
Richard Mudgett <rmudgett@digium.com>
2012-02-23 00:56:31 +0000

    Revert some apparently accidental spacing changes.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@356366 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/channel_internal_api.c
+++ main/channel_internal_api.c
@@ -1,30 +1,30 @@
 /*
- *Asterisk -- An open source telephony toolkit.
+ * Asterisk -- An open source telephony toolkit.
  *
- *Copyright (C) 2012, Digium, Inc.
+ * Copyright (C) 2012, Digium, Inc.
  *
- *Mark Spencer <markster@digium.com>
+ * Mark Spencer <markster@digium.com>
  *
- *See http://www.asterisk.org for more information about
- *the Asterisk project. Please do not directly contact
- *any of the maintainers of this project for assistance;
- *the project provides a web site, mailing lists and IRC
- *channels for your use.
+ * See http://www.asterisk.org for more information about
+ * the Asterisk project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
  *
- *This program is free software, distributed under the terms of
- *the GNU General Public License Version 2. See the LICENSE file
- *at the top of the source tree.
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
  */
 
 /*! \file
  *
- *\brief Channel Accessor API
+ * \brief Channel Accessor API
  *
- *This file is intended to be the only file that ever accesses the
- *internals of an ast_channel. All other files should use the
- *accessor functions defined here.
+ * This file is intended to be the only file that ever accesses the
+ * internals of an ast_channel. All other files should use the
+ * accessor functions defined here.
  *
- *\author Terry Wilson
+ * \author Terry Wilson
  */
 
 #include "asterisk.h"
