7495f69c95c80968625d0245acf574c1e87e4114
Terry Wilson <twilson@digium.com>
2012-03-02 23:21:18 +0000

    Correct some set-but-unused variable warnings in the mISDN library.
    (from kpfleming's commit to trunk r356292)
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@358011 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_misdn.c
+++ channels/chan_misdn.c
@@ -7404,7 +7404,6 @@ static struct ast_frame *misdn_read(struct ast_channel *ast)
 static int misdn_write(struct ast_channel *ast, struct ast_frame *frame)
 {
 	struct chan_list *ch;
-	int i  = 0;
 
 	if (!ast || !(ch = MISDN_ASTERISK_TECH_PVT(ast))) {
 		return -1;
@@ -7503,7 +7502,7 @@ static int misdn_write(struct ast_channel *ast, struct ast_frame *frame)
 
 	} else {
 		/* transmit without jitterbuffer */
-		i = misdn_lib_tx2misdn_frm(ch->bc, frame->data.ptr, frame->samples);
+		misdn_lib_tx2misdn_frm(ch->bc, frame->data.ptr, frame->samples);
 	}
 
 	return 0;
