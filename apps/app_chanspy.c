2c3804e06e276f608eace3e17e073640d28c69e0
Richard Mudgett <rmudgett@digium.com>
2012-03-02 15:45:45 +0000

    Fix channel reference leak in ChanSpy.
    * Fix next_channel() channel reference leak in ChanSpy.
    
    (closes issue ASTERISK-19461)
    Reported by: Irontec
    Patches:
          app_chanspy_iteartor_next_unref.patch (license #6213) patch uploaded by Irontec
    
    (issue ASTERISK-17515)
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@357809 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_chanspy.c
+++ apps/app_chanspy.c
@@ -745,8 +745,10 @@ redo:
 	}
 
 	if (!strncmp(next->name, "DAHDI/pseudo", pseudo_len)) {
+		ast_channel_unref(next);
 		goto redo;
 	} else if (next == chan) {
+		ast_channel_unref(next);
 		goto redo;
 	}
 
