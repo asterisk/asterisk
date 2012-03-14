45205716d7353ffef2ae5aea2154305c3160c97e
Russell Bryant <russell@russellbryant.com>
2012-03-14 23:29:32 +0000

    app_chanisavail: Fix use of uninitialized variable.
    Ensure that status is set before it is used by resetting it during each loop
    iteration.  This could have resulted in incorrect results from this app.
    ........
    
    Merged revisions 359486 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    ........
    
    Merged revisions 359491 from http://svn.asterisk.org/svn/asterisk/branches/10
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@359495 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_chanisavail.c
+++ apps/app_chanisavail.c
@@ -152,7 +152,9 @@ static int chanavail_exec(struct ast_channel *chan, const char *data)
 			}
 			*number = '\0';
 			number++;
-			
+
+			status = AST_DEVICE_UNKNOWN;
+
 			if (string_compare) {
 				/* ast_parse_device_state checks for "SIP/1234" as a channel name.
 				   ast_device_state will ask the SIP driver for the channel state. */
