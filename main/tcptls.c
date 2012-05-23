563dd1be33f44917e010d4bd42a52a4419946c95
Mark Michelson <mmichelson@digium.com>
2012-05-23 20:29:03 +0000

    Only call SSL_CTX_free if DO_SSL is defined.
    Thanks to Paul Belanger for pointing out this error.
    ........
    
    Merged revisions 367416 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/10@367417 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/tcptls.c
+++ main/tcptls.c
@@ -403,10 +403,12 @@ int ast_ssl_setup(struct ast_tls_config *cfg)
 
 void ast_ssl_teardown(struct ast_tls_config *cfg)
 {
+#ifdef DO_SSL
 	if (cfg->ssl_ctx) {
 		SSL_CTX_free(cfg->ssl_ctx);
 		cfg->ssl_ctx = NULL;
 	}
+#endif
 }
 
 struct ast_tcptls_session_instance *ast_tcptls_client_start(struct ast_tcptls_session_instance *tcptls_session)
