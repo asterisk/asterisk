a7a3050de9b98aea0516e570c93f19da5f00fa61
Michael L. Young <elgueromexicano@gmail.com>
2012-06-01 03:30:01 +0000

    Add documentation to function CHANNEL for options echocan_mode and buffers
    The ability to set "echocan_mode" and "buffers" through the dialplan was added
    to chan_dahdi some time ago.  This patch adds some documentation to
    func_channel.
    
    (Closes issue ASTERISK-19911)
    Reported by: Dale Noll
    Tested by: Michael L. Young
    Patches: 
      asterisk-19911-branch18.diff uploaded by Michael L. Young (license 5026)
    
    Review: https://reviewboard.asterisk.org/r/1949/
    ........
    
    Merged revisions 368092 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    ........
    
    Merged revisions 368093 from http://svn.asterisk.org/svn/asterisk/branches/10
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@368094 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- funcs/func_channel.c
+++ funcs/func_channel.c
@@ -253,6 +253,26 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 						<para>R/O PRI Nonzero if the channel has no B channel.
 						The channel is either on hold or a call waiting call.</para>
 					</enum>
+					<enum name="buffers">
+						<para>W/O Change the channel's buffer policy (for the current call only)</para>
+						<para>This option takes two arguments:</para>
+						<para>	Number of buffers,</para>
+						<para>	Buffer policy being one of:</para>
+						<para>	    <literal>full</literal></para>
+						<para>	    <literal>immediate</literal></para>
+						<para>	    <literal>half</literal></para>
+					</enum>
+					<enum name="echocan_mode">
+						<para>W/O Change the configuration of the active echo
+						canceller on the channel (if any), for the current call
+						only.</para>
+						<para>Possible values are:</para>
+						<para>	<literal>on</literal>	Normal mode (the echo canceller is actually reinitalized)</para>
+						<para>	<literal>off</literal>	Disabled</para>
+						<para>	<literal>fax</literal>	FAX/data mode (NLP disabled if possible, otherwise
+							completely disabled)</para>
+						<para>	<literal>voice</literal>	Voice mode (returns from FAX mode, reverting the changes that were made)</para>
+					</enum>
 				</enumlist>
 				<para><emphasis>chan_ooh323</emphasis> provides the following additional options:</para>
 				<enumlist>
