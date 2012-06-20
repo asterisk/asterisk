46f6c808788f4794f455eb1fef0b4968cb50c4ff
Automerge script <automerge@asterisk.org>
2012-06-20 18:22:33 +0000

    Merged revisions 369147 via svnmerge from file:///srv/subversion/repos/asterisk/branches/10
    ................
      r369147 | may | 2012-06-20 12:36:27 -0500 (Wed, 20 Jun 2012) | 10 lines
      
      fix locking issue on empty callList
      (issue ASTERISK-19298)
      Reported by:
              Dmitry Melekhov
      Patches:
              ASTERISK-18322-2.patch
      ........
      
      Merged revisions 369146 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/10-digiumphones@369164 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- addons/ooh323c/src/ooq931.c
+++ addons/ooh323c/src/ooq931.c
@@ -2373,7 +2373,7 @@ int ooH323MakeCall(char *dest, char *callToken, ooCallOptions *opts)
       else
          OO_CLRFLAG(call->flags, OO_M_TUNNELING);
 
-      if(opts->disableGk)
+      if(opts->disableGk || gH323ep.gkClient == NULL)
          OO_SETFLAG(call->flags, OO_M_DISABLEGK);
       else
          OO_CLRFLAG(call->flags, OO_M_DISABLEGK);
