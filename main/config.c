b2cd9a1b526a445caa6ece66e00415b1b9904037
Automerge script <automerge@asterisk.org>
2012-08-16 23:25:52 +0000

    Merged revisions 371394,371398 via svnmerge from file:///srv/subversion/repos/asterisk/branches/10
    ................
      r371394 | kmoore | 2012-08-16 17:42:53 -0500 (Thu, 16 Aug 2012) | 11 lines
      
      Add module reload instrumentation for TEST_FRAMEWORK
      
      This adds AMI events for module reloads when Asterisk is built with
      TEST_FRAMEWORK enabled and corrects generation of the module load AMI
      event.
      
      (issue PQ-1126)
      ........
      
      Merged revisions 371393 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    ................
      r371398 | twilson | 2012-08-16 17:50:12 -0500 (Thu, 16 Aug 2012) | 13 lines
      
      Handle integer over/under-flow in ast_parse_args
      
      The strtol family of functions will return *_MIN/*_MAX on overflow. To
      detect when an overflow has happened, errno must be set to 0 before
      calling the function, then checked afterward.
      
      (closes issue ASTERISK-20120)
      Reported by: Matt Jordan
      Review: https://reviewboard.asterisk.org/r/2073/
      ........
      
      Merged revisions 371392 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/10-digiumphones@371424 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/config.c
+++ main/config.c
@@ -2689,8 +2689,9 @@ int ast_parse_arg(const char *arg, enum ast_parse_flags flags,
 			error = 1;
 			goto int32_done;
 		}
+		errno = 0;
 		x = strtol(arg, &endptr, 0);
-		if (*endptr || x < INT32_MIN || x > INT32_MAX) {
+		if (*endptr || errno || x < INT32_MIN || x > INT32_MAX) {
 			/* Parse error, or type out of int32_t bounds */
 			error = 1;
 			goto int32_done;
@@ -2736,8 +2737,9 @@ int32_done:
 			error = 1;
 			goto uint32_done;
 		}
+		errno = 0;
 		x = strtoul(arg, &endptr, 0);
-		if (*endptr || x > UINT32_MAX) {
+		if (*endptr || errno || x > UINT32_MAX) {
 			error = 1;
 			goto uint32_done;
 		}
