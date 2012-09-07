718dd340132f2eeb0e1ad9a47203b53f14bfb0f8
Richard Mudgett <rmudgett@digium.com>
2012-09-07 23:05:21 +0000

    Fix MALLOC_DEBUG version of ast_strndup().
    (closes issue ASTERISK-20349)
    Reported by: Brent Eagles
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@372655 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/astmm.c
+++ main/astmm.c
@@ -272,16 +272,17 @@ char *__ast_strdup(const char *s, const char *file, int lineno, const char *func
 char *__ast_strndup(const char *s, size_t n, const char *file, int lineno, const char *func) 
 {
 	size_t len;
-	void *ptr;
+	char *ptr;
 
-	if (!s)
+	if (!s) {
 		return NULL;
+	}
 
-	len = strlen(s) + 1;
-	if (len > n)
-		len = n;
-	if ((ptr = __ast_alloc_region(len, FUNC_STRNDUP, file, lineno, func, 0)))
-		strcpy(ptr, s);
+	len = strnlen(s, n);
+	if ((ptr = __ast_alloc_region(len + 1, FUNC_STRNDUP, file, lineno, func, 0))) {
+		memcpy(ptr, s, len);
+		ptr[len] = '\0';
+	}
 
 	return ptr;
 }
