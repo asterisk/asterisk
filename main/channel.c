5c76050e475c108510aa9dce77fdc54f206e269f
Brent Eagles <beagles@digium.com>
2012-11-13 18:20:13 +0000

    Patch to prevent stopping the active generator when it is not the silence generator.
    This patch introduces an internal helper function to safely check whether the
    current generator is the one that is expected before deactivating it. The
    current externally accessible ast_channel_stop_generator() function has been
    modified to be implemented in terms of the new function.
    
    (closes issue ASTERISK-19918)
    Reported by: Eduardo Abad
    ........
    
    Merged revisions 376199 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/10@376208 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/channel.c
+++ main/channel.c
@@ -3169,7 +3169,11 @@ static int generator_force(const void *data)
 
 	res = generate(chan, tmp, 0, ast_format_rate(&chan->writeformat) / 50);
 
-	chan->generatordata = tmp;
+	ast_channel_lock(chan);
+        if (chan->generator && generate == chan->generator->generate) {
+            chan->generatordata = tmp;
+        }
+	ast_channel_unlock(chan);
 
 	if (res) {
 		ast_debug(1, "Auto-deactivating generator\n");
@@ -8442,18 +8446,45 @@ struct ast_silence_generator *ast_channel_start_silence_generator(struct ast_cha
 	return state;
 }
 
+static int internal_deactivate_generator(struct ast_channel *chan, void* generator)
+{
+	ast_channel_lock(chan);
+
+	if (!chan->generatordata) {
+		ast_debug(1, "Trying to stop silence generator when there is no "
+		    "generator on '%s'\n", chan->name);
+		ast_channel_unlock(chan);
+		return 0;
+	}
+	if (chan->generator != generator) {
+		ast_debug(1, "Trying to stop silence generator when it is not the current "
+		    "generator on '%s'\n", chan->name);
+		ast_channel_unlock(chan);
+		return 0;
+	}
+	if (chan->generator && chan->generator->release) {
+		chan->generator->release(chan, chan->generatordata);
+	}
+	chan->generatordata = NULL;
+	chan->generator = NULL;
+	ast_channel_set_fd(chan, AST_GENERATOR_FD, -1);
+	ast_clear_flag(chan, AST_FLAG_WRITE_INT);
+	ast_settimeout(chan, 0, NULL, NULL);
+	ast_channel_unlock(chan);
+
+	return 1;
+}
+
 void ast_channel_stop_silence_generator(struct ast_channel *chan, struct ast_silence_generator *state)
 {
 	if (!state)
 		return;
 
-	ast_deactivate_generator(chan);
-
-	ast_debug(1, "Stopped silence generator on '%s'\n", chan->name);
-
-	if (ast_set_write_format(chan, &state->old_write_format) < 0)
-		ast_log(LOG_ERROR, "Could not return write format to its original state\n");
-
+	if (internal_deactivate_generator(chan, &silence_generator)) {
+		ast_debug(1, "Stopped silence generator on '%s'\n", chan->name);
+		if (ast_set_write_format(chan, &state->old_write_format) < 0)
+			ast_log(LOG_ERROR, "Could not return write format to its original state\n");
+	}
 	ast_free(state);
 }
 
