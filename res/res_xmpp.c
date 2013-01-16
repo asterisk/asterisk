70d22f2ab7e606cc3714e50d14a6ee6929800353
Automerge script <automerge@asterisk.org>
2013-01-16 16:20:52 +0000

    Merged revisions 379211 via svnmerge from file:///srv/subversion/repos/asterisk/trunk
    ................
      r379211 | mjordan | 2013-01-16 09:33:05 -0600 (Wed, 16 Jan 2013) | 21 lines
      
      Multiple revisions 379209-379210
      
      ........
        r379209 | mjordan | 2013-01-16 09:27:44 -0600 (Wed, 16 Jan 2013) | 8 lines
        
        Add module tags to documentation for res_jabber/res_xmpp
        
        Since res_jabber/res_xmpp provide the same APIs (app/func/manager/etc.),
        the XML documentation for each needs to call out which module is providing
        the documentation. The module attribute has been added to the various XML
        fragments for this purpose.
      ........
        r379210 | mjordan | 2013-01-16 09:30:20 -0600 (Wed, 16 Jan 2013) | 4 lines
        
        Update the dtd to actually *support* the module attribute in all elements
        
        Mea culpa.
      ........
      
      Merged revisions 379209-379210 from http://svn.asterisk.org/svn/asterisk/branches/11
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/mmichelson/threadpool@379218 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_xmpp.c
+++ res/res_xmpp.c
@@ -64,7 +64,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 #include "asterisk/config_options.h"
 
 /*** DOCUMENTATION
-	<application name="JabberSend" language="en_US">
+	<application name="JabberSend" language="en_US" module="res_xmpp">
 		<synopsis>
 			Sends an XMPP message to a buddy.
 		</synopsis>
@@ -94,7 +94,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 			<ref type="function">JABBER_RECEIVE</ref>
 		</see-also>
 	</application>
-	<function name="JABBER_RECEIVE" language="en_US">
+	<function name="JABBER_RECEIVE" language="en_US" module="res_xmpp">
 		<synopsis>
 			Reads XMPP messages.
 		</synopsis>
@@ -123,7 +123,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 			<ref type="application">JabberSend</ref>
 		</see-also>
 	</function>
-	<function name="JABBER_STATUS" language="en_US">
+	<function name="JABBER_STATUS" language="en_US" module="res_xmpp">
 		<synopsis>
 			Retrieves a buddy's status.
 		</synopsis>
@@ -153,7 +153,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 			<ref type="application">JabberSend</ref>
 		</see-also>
 	</function>
-	<application name="JabberSendGroup" language="en_US">
+	<application name="JabberSendGroup" language="en_US" module="res_xmpp">
 		<synopsis>
 			Send a Jabber Message to a specified chat room
 		</synopsis>
@@ -176,7 +176,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 			<note><para>To be able to send messages to a chat room, a user must have previously joined it. Use the <replaceable>JabberJoin</replaceable> function to do so.</para></note>
 		</description>
 	</application>
-	<application name="JabberJoin" language="en_US">
+	<application name="JabberJoin" language="en_US" module="res_xmpp">
 		<synopsis>
 			Join a chat room
 		</synopsis>
@@ -196,7 +196,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 			<para>Allows Asterisk to join a chat room.</para>
 		</description>
 	</application>
-	<application name="JabberLeave" language="en_US">
+	<application name="JabberLeave" language="en_US" module="res_xmpp">
 		<synopsis>
 			Leave a chat room
 		</synopsis>
@@ -215,7 +215,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 			<para>Allows Asterisk to leave a chat room.</para>
 		</description>
 	</application>
-	<application name="JabberStatus" language="en_US">
+	<application name="JabberStatus" language="en_US" module="res_xmpp">
 		<synopsis>
 			Retrieve the status of a jabber list member
 		</synopsis>
@@ -259,7 +259,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 			</enumlist>
 		</description>
 	</application>
-	<manager name="JabberSend" language="en_US">
+	<manager name="JabberSend" language="en_US" module="res_xmpp">
 		<synopsis>
 			Sends a message to a Jabber Client.
 		</synopsis>
