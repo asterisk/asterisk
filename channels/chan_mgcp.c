6300aa6ae4f71e9824e3b4cc2355e133bc606e9a
Kinsey Moore <kmoore@digium.com>
2013-03-16 16:00:40 +0000

    Make sure things compile...
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@383287 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_mgcp.c
+++ channels/chan_mgcp.c
@@ -507,7 +507,7 @@ static int has_voicemail(struct mgcp_endpoint *p)
 
 	ast_str_set(&uniqueid, 0, "%s@%s", mbox, cntx);
 
-	msg = stasis_cache_get(stasis_mwi_topic_cached(), stasis_mwi_state(), ast_str_buffer(uniqueid));
+	msg = stasis_cache_get(stasis_mwi_topic_cached(), stasis_mwi_state_message(), ast_str_buffer(uniqueid));
 
 	if (msg) {
 		struct stasis_mwi_state *mwi_state = stasis_message_data(msg);
@@ -4182,7 +4182,7 @@ static struct mgcp_gateway *build_gateway(char *cat, struct ast_variable *v)
 					ast_str_reset(uniqueid);
 					ast_str_set(&uniqueid, 0, "%s@%s", mbox, cntx);
 
-					maibox_specific_topic = stasis_mwi_topic(ast_str_buffer(uniqueid));
+					mailbox_specific_topic = stasis_mwi_topic(ast_str_buffer(uniqueid));
 					if (mailbox_specific_topic) {
 						e->mwi_event_sub = stasis_subscribe(mailbox_specific_topic, mwi_event_cb, NULL);
 					}
