e05870ccae05a9d1ccefd093b4be892c9b7b33b1
Andrew Latham <lathama@gmail.com>
2013-04-22 16:22:00 +0000

    Doxygen - Markup Guidelines
    Expand on a commit by OEJ to use the Coding-Guidelines
    
    (issue ASTERISK-20259)
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@386266 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- include/asterisk/srv.h
+++ include/asterisk/srv.h
@@ -1,7 +1,7 @@
 /*
  * Asterisk -- An open source telephony toolkit.
  *
- * Copyright (C) 1999 - 2005, Digium, Inc.
+ * Copyright (C) 1999 - 2013, Digium, Inc.
  *
  * Mark Spencer <markster@digium.com>
  *
@@ -23,41 +23,47 @@
 #ifndef _ASTERISK_SRV_H
 #define _ASTERISK_SRV_H
 
-/*!
-  \file srv.h
-  \brief Support for DNS SRV records, used in to locate SIP services.
-  \note Note: This SRV record support will respect the priority and
-        weight elements of the records that are returned, but there are
-	no provisions for retrying or failover between records.
-*/
+/*! \file srv.h
+ *
+ * \brief Support for DNS SRV records, used in to locate SIP services.
+ *
+ * \note Note: This SRV record support will respect the priority and weight
+ *	elements of the records that are returned, but there are no provisions
+ * 	for retrying or failover between records.
+ */
 
-/*!\brief An opaque type, for lookup usage */
+/*! \brief An opaque type, for lookup usage */
 struct srv_context;
 
-/*!\brief Retrieve set of SRV lookups, in order
+/*! \brief Retrieve set of SRV lookups, in order
+ *
  * \param[in] context A pointer in which to hold the result
  * \param[in] service The service name to look up
  * \param[out] host Result host
  * \param[out] port Associated TCP portnum
+ *
  * \retval -1 Query failed
  * \retval 0 Result exists in host and port
  * \retval 1 No more results
  */
 extern int ast_srv_lookup(struct srv_context **context, const char *service, const char **host, unsigned short *port);
 
-/*!\brief Cleanup resources associated with ast_srv_lookup
+/*! \brief Cleanup resources associated with ast_srv_lookup
+ *
  * \param context Pointer passed into ast_srv_lookup
  */
 void ast_srv_cleanup(struct srv_context **context);
 
-/*! Lookup entry in SRV records Returns 1 if found, 0 if not found, -1 on hangup 
-	Only do SRV record lookup if you get a domain without a port. If you get a port #, it's a DNS host name.
-!	\param	chan Ast channel
-	\param	host host name (return value)
-	\param	hostlen Length of string "host"
-	\param	port Port number (return value)
-	\param service Service tag for SRV lookup (like "_sip._udp" or "_stun._udp"
-*/
+/*! \brief Lookup entry in SRV records Returns 1 if found, 0 if not found, -1 on hangup 
+ *
+ * Only do SRV record lookup if you get a domain without a port. If you get a port #, it's a DNS host name.
+ *
+ * \param chan Ast channel
+ * \param host host name (return value)
+ * \param hostlen Length of string "host"
+ * \param port Port number (return value)
+ * \param service Service tag for SRV lookup (like "_sip._udp" or "_stun._udp"
+ */
 extern int ast_get_srv(struct ast_channel *chan, char *host, int hostlen, int *port, const char *service);
 
 /*!
@@ -69,6 +75,7 @@ extern int ast_get_srv(struct ast_channel *chan, char *host, int hostlen, int *p
  * SRV lookup.
  *
  * \param context The context returned by ast_srv_lookup
+ *
  * \return Number of records in context
  */
 unsigned int ast_srv_get_record_count(struct srv_context *context);
@@ -89,8 +96,9 @@ unsigned int ast_srv_get_record_count(struct srv_context *context);
  * \param[out] port The port portion of the record
  * \param[out] priority The priority portion of the record
  * \param[out] weight The weight portion of the record
- * \retval -1 Failed to retrieve information. Likely due to an out of
- * range record_num
+ *
+ * \retval -1 Failed to retrieve information. 
+ * 	Likely due to an out of range record_num
  * \retval 0 Success
  */
 int ast_srv_get_nth_record(struct srv_context *context, int record_num, const char **host,
