3aac3d2ee57bb3072e906abde03f15108df4ef27
Joshua Colp <jcolp@digium.com>
2013-04-27 19:03:39 +0000

    Tweak res_sip priority so it gets loaded first before all other SIP stuff.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@386760 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_sip.c
+++ res/res_sip.c
@@ -902,5 +902,5 @@ AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_GLOBAL_SYMBOLS | AST_MODFLAG_LOAD_
 		.load = load_module,
 		.unload = unload_module,
 		.reload = reload_module,
-		.load_pri = AST_MODPRI_CHANNEL_DEPEND,
+		.load_pri = AST_MODPRI_CHANNEL_DEPEND - 5,
 );
