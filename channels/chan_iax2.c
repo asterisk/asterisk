c2815b889e257bcaac9eb3f8772fb4f53203f867
Alec L Davis <sivad.a@paradise.net.nz>
2013-06-10 09:30:11 +0000

    chan_iax2: nativebridge refactor, missed unlock bridgecallno
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@391143 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_iax2.c
+++ channels/chan_iax2.c
@@ -11407,6 +11407,7 @@ immediatedial:
 
 				if (!(iaxs[iaxs[fr->callno]->bridgecallno]->transferring == TRANSFER_READY) &&
 				    !(iaxs[iaxs[fr->callno]->bridgecallno]->transferring == TRANSFER_MREADY)) {
+					ast_mutex_unlock(&iaxsl[iaxs[fr->callno]->bridgecallno]);
 					break;
 				}
 
