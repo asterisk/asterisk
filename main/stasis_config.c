b96d8cbc78b8dac8c9783c6bdd00650cd913d333
Richard Mudgett <rmudgett@digium.com>
2013-07-03 22:05:54 +0000

    Fix some indentation in stasis_config.c.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@393600 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/stasis_config.c
+++ main/stasis_config.c
@@ -152,18 +152,19 @@ static int process_config(int reload)
 {
 	RAII_VAR(struct stasis_conf *, conf, conf_alloc(), ao2_cleanup);
 
-		switch (aco_process_config(&cfg_info, reload)) {
-		case ACO_PROCESS_ERROR:
-			if (conf && !reload && !aco_set_defaults(&threadpool_option, "threadpool", conf->threadpool)) {
-				ast_log(AST_LOG_NOTICE, "Failed to process Stasis configuration; using defaults\n");
-				ao2_global_obj_replace(confs, conf);
-				return 0;
-			}
-			return -1;
-		case ACO_PROCESS_OK:
-		case ACO_PROCESS_UNCHANGED:
-			break;
+	switch (aco_process_config(&cfg_info, reload)) {
+	case ACO_PROCESS_ERROR:
+		if (conf && !reload
+			&& !aco_set_defaults(&threadpool_option, "threadpool", conf->threadpool)) {
+			ast_log(AST_LOG_NOTICE, "Failed to process Stasis configuration; using defaults\n");
+			ao2_global_obj_replace(confs, conf);
+			return 0;
 		}
+		return -1;
+	case ACO_PROCESS_OK:
+	case ACO_PROCESS_UNCHANGED:
+		break;
+	}
 
 	return 0;
 }
