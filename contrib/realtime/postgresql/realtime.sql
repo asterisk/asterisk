44fe8fcb91922c72601b76bae410df29faddc17d
Matthew Jordan <mjordan@digium.com>
2013-07-21 02:39:37 +0000

    Update PostgreSQL realtime scripts with schema for queue_log table
    This patch updates the realtime SQL scripts with an entry that will create the
    queue_log table. This brings the PostgreSQL scripts inline with the MySQL
    scripts, with respect to what tables they will create.
    
    (closes issue ASTERISK-21021)
    Reported by: Eugene
    patches:
      queue_log.sql uploaded by varnav (license 6360)
    ........
    
    Merged revisions 394896 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    ........
    
    Merged revisions 394897 from http://svn.asterisk.org/svn/asterisk/branches/11
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@394898 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- contrib/realtime/postgresql/realtime.sql
+++ contrib/realtime/postgresql/realtime.sql
@@ -138,6 +138,23 @@ penalty int8,
 PRIMARY KEY (queue_name, interface)
 ) WITHOUT OIDS;
 
+drop table queue_log;
+CREATE TABLE "queue_log" (
+"id" SERIAL,
+"time" TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
+"callid" character varying(50) NOT NULL,
+"queuename" character varying(50) NOT NULL,
+"agent" character varying(50) NOT NULL,
+"event" character varying(20) NOT NULL,
+"data1" character varying(50) NOT NULL,
+"data2" character varying(50) NOT NULL,
+"data3" character varying(50) NOT NULL,
+"data4" character varying(50) NOT NULL,
+"data5" character varying(50) NOT NULL,
+CONSTRAINT queue_log_pkey PRIMARY KEY (id)
+) WITHOUT OIDS;
+
+GRANT ALL ON TABLE queue_log TO asterisk;
 GRANT ALL ON TABLE cdr TO asterisk;
 GRANT ALL ON TABLE extensions_conf TO asterisk;
 GRANT ALL ON TABLE sip_conf TO asterisk;
