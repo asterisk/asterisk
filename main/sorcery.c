23e86edf6f9c13f3281ea330b5bc1ce088e0a7e0
David M. Lee <dlee@digium.com>
2013-08-01 21:19:27 +0000

    Fix sorcery for some rather picky regex implementations.
    Some regex implementations won't compile an empty string. Assuming that
    it's equivalent of a regex that will match anything, use ".?" instead.
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@396035 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/sorcery.c
+++ main/sorcery.c
@@ -585,7 +585,7 @@ int ast_sorcery_object_register(struct ast_sorcery *sorcery, const char *type, a
 
 	object_type->type.name = object_type->name;
 	object_type->type.type = ACO_ITEM;
-	object_type->type.category = "";
+	object_type->type.category = ".?";
 	object_type->type.item_alloc = alloc;
 
 	object_type->transform = transform;
