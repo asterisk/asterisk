860ab29dab3dcbe36460cc5ea108435e036396af
David M. Lee <dlee@digium.com>
2013-08-06 21:20:58 +0000

    Fixed app_meetme for cache split changes
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@396347 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_meetme.c
+++ apps/app_meetme.c
@@ -1167,7 +1167,7 @@ static int meetme_stasis_init(void)
 	STASIS_MESSAGE_TYPE_INIT(meetme_talk_request_type);
 
 	meetme_event_message_router = stasis_message_router_create(
-		ast_channel_cache());
+		ast_channel_topic_all_cached());
 
 	if (!meetme_event_message_router) {
 		meetme_stasis_cleanup();
