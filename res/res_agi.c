b41873d7e9783c10547afc415d2996acd5021303
Kevin Harwell <kharwell@digium.com>
2013-08-30 17:53:56 +0000

    Memory leak fix
    ast_xmldoc_printable returns an allocated block that must be freed by the
    caller.  Fixed manager.c and res_agi.c to stop leaking these results.
    
    (closes issue ASTERISK-22395)
    Reported by: Corey Farrell
    Patches:
         manager-leaks-11.patch uploaded by coreyfarrell (license 5909)
         res_agi-xmldoc-leaks.patch uploaded by coreyfarrell (license 5909)
    ........
    
    Merged revisions 398060 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/11@398061 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_agi.c
+++ res/res_agi.c
@@ -3841,9 +3841,6 @@ static int write_htmldump(const char *filename)
 
 	AST_RWLIST_RDLOCK(&agi_commands);
 	AST_RWLIST_TRAVERSE(&agi_commands, command, list) {
-#ifdef AST_XML_DOCS
-		char *stringptmp;
-#endif
 		char *tempstr, *stringp;
 
 		if (!command->cmda[0])	/* end ? */
@@ -3856,8 +3853,7 @@ static int write_htmldump(const char *filename)
 		fprintf(htmlfile, "<TR><TD><TABLE BORDER=\"1\" CELLPADDING=\"5\" WIDTH=\"100%%\">\n");
 		fprintf(htmlfile, "<TR><TH ALIGN=\"CENTER\"><B>%s - %s</B></TH></TR>\n", fullcmd, command->summary);
 #ifdef AST_XML_DOCS
-		stringptmp = ast_xmldoc_printable(command->usage, 0);
-		stringp = ast_strdup(stringptmp);
+		stringp = ast_xmldoc_printable(command->usage, 0);
 #else
 		stringp = ast_strdup(command->usage);
 #endif
@@ -3875,9 +3871,6 @@ static int write_htmldump(const char *filename)
 		fprintf(htmlfile, "</TD></TR>\n");
 		fprintf(htmlfile, "</TABLE></TD></TR>\n\n");
 		ast_free(stringp);
-#ifdef AST_XML_DOCS
-		ast_free(stringptmp);
-#endif
 	}
 	AST_RWLIST_UNLOCK(&agi_commands);
 	fprintf(htmlfile, "</TABLE>\n</BODY>\n</HTML>\n");
