0ca5aec494122678af35f509dd9c0a57d2f8f088
Kinsey Moore <kmoore@digium.com>
2013-09-18 19:55:46 +0000

    Fix jitter buffer log file creation
    This adjusts '/'-to-'#' replacement to replace all instances of '/'
    instead of just the first to ensure that the jitter buffer log file
    gets the correct name as per Richard Kenner's suggestion.
    
    (closes issue ASTERISK-21036)
    Reported by: Richard Kenner
    ........
    
    Merged revisions 399402 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/11@399403 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/abstract_jb.c
+++ main/abstract_jb.c
@@ -443,7 +443,7 @@ static int create_jb(struct ast_channel *chan, struct ast_frame *frr)
 		char safe_logfile[30] = "/tmp/logfile-XXXXXX";
 		int safe_fd;
 		snprintf(name2, sizeof(name2), "%s", ast_channel_name(chan));
-		if ((tmp = strchr(name2, '/'))) {
+		while ((tmp = strchr(name2, '/'))) {
 			*tmp = '#';
 		}
 
@@ -452,7 +452,7 @@ static int create_jb(struct ast_channel *chan, struct ast_frame *frr)
 		ast_assert(bridged != NULL);
 
 		snprintf(name1, sizeof(name1), "%s", ast_channel_name(bridged));
-		if ((tmp = strchr(name1, '/'))) {
+		while ((tmp = strchr(name1, '/'))) {
 			*tmp = '#';
 		}
 
