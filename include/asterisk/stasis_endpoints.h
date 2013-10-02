addbf276f5037dc5a265a1e43e383ae1d9e9d0d8
Mark Michelson <mmichelson@digium.com>
2013-10-02 22:22:17 +0000

    Multiple revisions 400318-400319
    ........
      r400318 | mmichelson | 2013-10-02 17:08:49 -0500 (Wed, 02 Oct 2013) | 12 lines
      
      Remove unnecessary waits from stasis.
      
      Since caches are updated on publisher threads, there is no need
      to wait for the cache updates to occur after a stasis message
      is published.
      
      In the case of chan_pjsip device state changes, this set of
      changes caused an improvement to performance.
      
      Review: https://reviewboard.asterisk.org/r/2890
    ........
      r400319 | mmichelson | 2013-10-02 17:10:54 -0500 (Wed, 02 Oct 2013) | 3 lines
      
      Remove svn:mergeinfo property.
    ........
    
    Merged revisions 400318-400319 from http://svn.asterisk.org/svn/asterisk/branches/12
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@400335 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- include/asterisk/stasis_endpoints.h
+++ include/asterisk/stasis_endpoints.h
@@ -194,14 +194,12 @@ struct stasis_cache *ast_endpoint_cache(void);
  *
  * \param tech Name of the endpoint's technology.
  * \param resource Resource name of the endpoint.
- * \param guaranteed Whether to require all pending messages to have been processed or not.
  * \return Snapshot of the endpoint with the given name.
  * \return \c NULL if endpoint is not found, or on error.
  * \since 12
  */
 struct ast_endpoint_snapshot *ast_endpoint_latest_snapshot(const char *tech,
-	const char *resource,
-	unsigned int guaranteed
+	const char *resource
 );
 
 /*! @} */
