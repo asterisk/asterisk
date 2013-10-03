60423755e56413f72abc1af04c7d4e1d0136c781
Matthew Jordan <mjordan@digium.com>
2013-10-03 17:55:50 +0000

    Update Alembic database scripts for external scripting and PostgreSQL, Oracle
    This patch does the following:
    1) The env scripts have been updated to be tolerant of a NULL configuration
       file. This occurs when configuration is provided by an external script,
       such that the actual config.ini file is not used.
    2) Enum types have all been given names. This is needed for PostgreSQL script
       generation.
    3) The identifier meetme_confno_starttime_endtime is greater than 30
       characters, and hence invalid for Oracle databases. This has been truncated
       down to meetme_confno_start_end.
    
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/12@400383 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- contrib/ast-db-manage/config/env.py
+++ contrib/ast-db-manage/config/env.py
@@ -9,7 +9,10 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-fileConfig(config.config_file_name)
+try:
+    fileConfig(config.config_file_name)
+except:
+    pass
 
 # add your model's MetaData object here
 # for 'autogenerate' support
