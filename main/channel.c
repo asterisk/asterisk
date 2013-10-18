8a6db34dc837f02a594070ac5b95b9cd3f15ca27
Walter Doekes <walter+asterisk@wjd.nu>
2013-10-18 14:43:26 +0000

    Properly copy/remove the device state cache flag over a masquerade.
    In r378303 the AST_FLAG_DISABLE_DEVSTATE_CACHE flag was added that tells
    the devstate system to not cache states for non-real devices. However,
    when optimizing away channels (ast_do_masquerade), that flag wasn't
    copied.
    
    In my case, using Local devices as queue members created a situation
    where the endpoint was considered in use, but the state change of the
    device being available again was ignored (not cached). The endpoint
    channel was optimized into the (previously) Local channel, but kept
    the do-not-cache flag. The end result being that the queue member
    apparently stayed in use forever.
    
    (closes issue ASTERISK-22718)
    Reported by: Walter Doekes
    
    Review: https://reviewboard.asterisk.org/r/2925/
    ........
    
    Merged revisions 401178 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/11@401179 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/channel.c
+++ main/channel.c
@@ -6793,6 +6793,8 @@ int ast_do_masquerade(struct ast_channel *original)
 {
 	int x;
 	int origstate;
+	unsigned int orig_disablestatecache;
+	unsigned int clone_disablestatecache;
 	int visible_indication;
 	int clone_was_zombie = 0;/*!< TRUE if the clonechan was a zombie before the masquerade. */
 	struct ast_frame *current;
@@ -7028,6 +7030,20 @@ int ast_do_masquerade(struct ast_channel *original)
 	origstate = ast_channel_state(original);
 	ast_channel_state_set(original, ast_channel_state(clonechan));
 	ast_channel_state_set(clonechan, origstate);
+
+	/* And the swap the cachable state too. Otherwise we'd start caching
+	 * Local channels and ignoring real ones. */
+	orig_disablestatecache = ast_test_flag(ast_channel_flags(original), AST_FLAG_DISABLE_DEVSTATE_CACHE);
+	clone_disablestatecache = ast_test_flag(ast_channel_flags(clonechan), AST_FLAG_DISABLE_DEVSTATE_CACHE);
+	if (orig_disablestatecache != clone_disablestatecache) {
+		if (orig_disablestatecache) {
+			ast_clear_flag(ast_channel_flags(original), AST_FLAG_DISABLE_DEVSTATE_CACHE);
+			ast_set_flag(ast_channel_flags(clonechan), AST_FLAG_DISABLE_DEVSTATE_CACHE);
+		} else {
+			ast_set_flag(ast_channel_flags(original), AST_FLAG_DISABLE_DEVSTATE_CACHE);
+			ast_clear_flag(ast_channel_flags(clonechan), AST_FLAG_DISABLE_DEVSTATE_CACHE);
+		}
+	}
 
 	/* Mangle the name of the clone channel */
 	snprintf(zombn, sizeof(zombn), "%s<ZOMBIE>", orig); /* quick, hide the brains! */
