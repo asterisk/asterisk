037dee9eaa14bf3b3140b398c9a4f09add51570d
Jonathan Rose <jrose@digium.com>
2013-10-24 16:40:59 +0000

    memory leaks: Memory leak cleanup patch by Corey Farrell (second set)
    Also covers ast_app_parse_timelen-fail-zero-length.patch, but the patch was
    replaced with one of my own.
    
    (issue ASTERISK-22467)
    Reported by: Corey Farrell
    Patches:
        chan_dahdi-cleanup_push.patch uploaded by coreyfarrell (license 5909)
        clicompat-r2.patch uploaded by coreyfarrell (license 5909)
        codecs-ilbc-doCPLC.patch uploaded by coreyfarrell (license 5909)
        data-cleanup-test-registration.patch uploaded by coreyfarrell (license 5909)
        main-asterisk-kill-listener.patch uploaded by coreyfarrell (license 5909)
    ........
    
    Merged revisions 401704 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/11@401705 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_dahdi.c
+++ channels/chan_dahdi.c
@@ -11949,6 +11949,11 @@ static struct dahdi_pvt *handle_init_event(struct dahdi_pvt *i, int event)
 	return NULL;
 }
 
+static void monitor_pfds_clean(void *arg) {
+	struct pollfd **pfds = arg;
+	ast_free(*pfds);
+}
+
 static void *do_monitor(void *data)
 {
 	int count, res, res2, spoint, pollres=0;
@@ -11972,6 +11977,7 @@ static void *do_monitor(void *data)
 #endif
 	pthread_setcancelstate(PTHREAD_CANCEL_DISABLE, NULL);
 
+	pthread_cleanup_push(monitor_pfds_clean, &pfds);
 	for (;;) {
 		/* Lock the interface list */
 		ast_mutex_lock(&iflock);
@@ -12231,6 +12237,7 @@ static void *do_monitor(void *data)
 		ast_mutex_unlock(&iflock);
 	}
 	/* Never reached */
+	pthread_cleanup_pop(1);
 	return NULL;
 
 }
