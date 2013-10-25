eb6293eeaa61ebefef8b084dd18a371991311d61
Jonathan Rose <jrose@digium.com>
2013-10-25 16:22:59 +0000

    revert clicompat-r2.patch from r401704
    Patch caused the following build errors against testsuite
    https://bamboo.asterisk.org/bamboo/browse/AST-ATRUNKBUILD4-244
    
    (issue ASTERISK-22467)
    Reported by: Corey Farrell
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@401895 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- utils/clicompat.c
+++ utils/clicompat.c
@@ -9,18 +9,8 @@ void ast_cli(int fd, const char *fmt, ...)
 
 struct ast_cli_entry;
 
-int ast_register_atexit(void (*func)(void));
-int ast_register_atexit(void (*func)(void))
-{
-	return 0;
-}
 int ast_cli_register_multiple(struct ast_cli_entry *e, int len);
 int ast_cli_register_multiple(struct ast_cli_entry *e, int len)
-{
-	return 0;
-}
-int ast_cli_unregister_multiple(struct ast_cli_entry *e, int len);
-int ast_cli_unregister_multiple(struct ast_cli_entry *e, int len)
 {
 	return 0;
 }
