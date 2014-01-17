9e6407596b789c2e2828c3767d228ffbacf7259b
Rusty Newton <rnewton@digium.com>
2014-01-17 15:40:37 +0000

    Documentation: doc fixes across various parts of the code for ASTERISK issues 23061,23028,23046,23027
    Fixes typos of "transfered" instead of "transferred" in various code. Fixes incorrect gosub param help text for app_queue.
    Fixes Asterisk man pages containing unquoted minus signs. Adds note about the "textsupport" option in sip.conf.sample.
    
    (issue ASTERISK-23061)
    (issue ASTERISK-23028)
    (issue ASTERISK-23046)
    (issue ASTERISK-23027)
    (closes issue ASTERISK-23061)
    (closes issue ASTERISK-23028)
    (closes issue ASTERISK-23046)
    (closes issue ASTERISK-23027)
    Reported by: Eugene, Jeremy Laine, Denis Pantsyrev
    Patches:
     transferred.patch uploaded by Jeremy Laine (license 6561)
     hyphen.patch uploaded by Jeremy Laine (license 6561)
     sip.conf.sample.patch uploaded by Eugene (license 6360)
    ........
    
    Merged revisions 405791 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/11@405792 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/features.c
+++ main/features.c
@@ -2497,7 +2497,7 @@ static const char *real_ctx(struct ast_channel *transferer, struct ast_channel *
 
 /*!
  * \brief Blind transfer user to another extension
- * \param chan channel to be transfered
+ * \param chan channel to be transferred
  * \param peer channel initiated blind transfer
  * \param config
  * \param code
@@ -2661,7 +2661,7 @@ static void atxfer_fail_cleanup(struct ast_channel *transferee, struct ast_chann
 
 /*!
  * \brief Attended transfer
- * \param chan transfered user
+ * \param chan transferred user
  * \param peer person transfering call
  * \param config
  * \param code
@@ -3845,7 +3845,7 @@ static void set_config_flags(struct ast_channel *chan, struct ast_bridge_config 
  * \details
  * Request channel, set channel variables, initiate call,
  * check if they want to disconnect, go into loop, check if timeout has elapsed,
- * check if person to be transfered hung up, check for answer break loop,
+ * check if person to be transferred hung up, check for answer break loop,
  * set cdr return channel.
  *
  * \retval Channel Connected channel for transfer.
@@ -4463,7 +4463,7 @@ int ast_bridge_call(struct ast_channel *chan, struct ast_channel *peer, struct a
 				ast_set_flag(peer_cdr, AST_CDR_FLAG_BRIDGED);
 			}
 		}
-		/* the DIALED flag may be set if a dialed channel is transfered
+		/* the DIALED flag may be set if a dialed channel is transferred
 		 * and then bridged to another channel.  In order for the
 		 * bridge CDR to be written, the DIALED flag must not be
 		 * present. */
@@ -8157,7 +8157,7 @@ int ast_bridge_timelimit(struct ast_channel *chan, struct ast_bridge_config *con
  *
  * Split data, check we aren't bridging with ourself, check valid channel,
  * answer call if not already, check compatible channels, setup bridge config
- * now bridge call, if transfered party hangs up return to PBX extension.
+ * now bridge call, if transferred party hangs up return to PBX extension.
  */
 static int bridge_exec(struct ast_channel *chan, const char *data)
 {
