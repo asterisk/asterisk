66cce0927e582b326b080e77d7fc5a679cc8293a
Rusty Newton <rnewton@digium.com>
2014-01-17 15:39:23 +0000

    Documentation: doc fixes across various parts of the code for ASTERISK issues 23061,23028,23046,23027
    Fixes typos of "transfered" instead of "transferred" in various code. Fixes incorrect gosub param help text for app_queue.
    Fixes Asterisk man pages containing unquoted minus signs. Adds note about the "textsupport" option in sip.conf.sample.
    
    (issue ASTERISK-23061)
    (issue ASTERISK-23028)
    (issue ASTERISK-23046)
    (issue ASTERISK-23027)
    (closes issue ASTERISK-23061)
    (closes issue ASTERISK-23028)
    (closes issue ASTERISK-23046)
    (closes issue ASTERISK-23027)
    Reported by: Eugene, Jeremy Laine, Denis Pantsyrev
    Patches:
     transferred.patch uploaded by Jeremy Laine (license 6561)
     hyphen.patch uploaded by Jeremy Laine (license 6561)
     sip.conf.sample.patch uploaded by Eugene (license 6360)
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@405791 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_transfer.c
+++ apps/app_transfer.c
@@ -54,7 +54,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 		<description>
 			<para>Requests the remote caller be transferred
 			to a given destination. If TECH (SIP, IAX2, LOCAL etc) is used, only
-			an incoming call with the same channel technology will be transfered.
+			an incoming call with the same channel technology will be transferred.
 			Note that for SIP, if you transfer before call is setup, a 302 redirect
 			SIP message will be returned to the caller.</para>
 			<para>The result of the application will be reported in the <variable>TRANSFERSTATUS</variable>
