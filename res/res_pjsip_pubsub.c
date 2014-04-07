46cdbe5863d472b0aa79913806376778e563cde5
Kinsey Moore <kmoore@digium.com>
2014-04-07 20:39:55 +0000

    PJSIP: Ensure test event has new state
    The change that fixed the pubsub test event's use of a dangling pointer
    also changed when it was processed relative to the pjsip subscription
    state change processing. This change corrects the order of events while
    holding a reference to the pointer that was previously dangling.
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/12@411883 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_pjsip_pubsub.c
+++ res/res_pjsip_pubsub.c
@@ -466,16 +466,21 @@ pjsip_dialog *ast_sip_subscription_get_dlg(struct ast_sip_subscription *sub)
 int ast_sip_subscription_send_request(struct ast_sip_subscription *sub, pjsip_tx_data *tdata)
 {
 	struct ast_sip_endpoint *endpoint = ast_sip_subscription_get_endpoint(sub);
+	int res;
+
+	ao2_ref(sub, +1);
+	res = pjsip_evsub_send_request(ast_sip_subscription_get_evsub(sub),
+			tdata) == PJ_SUCCESS ? 0 : -1;
 
 	ast_test_suite_event_notify("SUBSCRIPTION_STATE_SET",
 		"StateText: %s\r\n"
 		"Endpoint: %s\r\n",
 		pjsip_evsub_get_state_name(ast_sip_subscription_get_evsub(sub)),
 		ast_sorcery_object_get_id(endpoint));
+	ao2_cleanup(sub);
 	ao2_cleanup(endpoint);
 
-	return pjsip_evsub_send_request(ast_sip_subscription_get_evsub(sub),
-			tdata) == PJ_SUCCESS ? 0 : -1;
+	return res;
 }
 
 static void subscription_datastore_destroy(void *obj)
