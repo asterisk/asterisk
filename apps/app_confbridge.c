e91f65bb9187e3caecedcec1ce44ff33bbd39418
Kinsey Moore <kmoore@digium.com>
2014-04-21 14:58:12 +0000

    Confbridge: Fix ConfbridgeKick AMI documentation
    This adds documentation for the "all" channel option for the
    ConfbridgeKick AMI action and adjusts AMI responses accordingly.
    
    (issue ASTERISK-23282)
    Reported by: Dorian Logan
    ........
    
    Merged revisions 412730 from http://svn.asterisk.org/svn/asterisk/branches/12
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@412731 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_confbridge.c
+++ apps/app_confbridge.c
@@ -236,7 +236,10 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 		<syntax>
 			<xi:include xpointer="xpointer(/docs/manager[@name='Login']/syntax/parameter[@name='ActionID'])" />
 			<parameter name="Conference" required="true" />
-			<parameter name="Channel" required="true" />
+			<parameter name="Channel" required="true" >
+				<para>If this parameter is not a complete channel name, the first channel with this prefix will be used.</para>
+				<para>If this parameter is "all", all channels will be kicked from the conference.</para>
+			</parameter>
 		</syntax>
 		<description>
 		</description>
@@ -2966,7 +2969,7 @@ static int action_confbridgekick(struct mansession *s, const struct message *m)
 	ao2_ref(conference, -1);
 
 	if (found) {
-		astman_send_ack(s, m, "User kicked");
+		astman_send_ack(s, m, !strcmp("all", channel) ? "All participants kicked" : "User kicked");
 	} else {
 		astman_send_error(s, m, "No Channel by that name found in Conference.");
 	}
