ec93bb462b2bb3b34a9e9a4deaef3bc2fe3601ac
Matthew Jordan <mjordan@digium.com>
2014-05-19 01:09:39 +0000

    Undo r414122
    The Test Suite caught a few problems, undoing until those are resolved
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/12@414137 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- bridges/bridge_native_rtp.c
+++ bridges/bridge_native_rtp.c
@@ -230,10 +230,8 @@ static void native_rtp_bridge_stop(struct ast_bridge *bridge, struct ast_channel
 		break;
 	case AST_RTP_GLUE_RESULT_REMOTE:
 		if (!target) {
-			if (ast_channel_is_leaving_bridge(c0->chan)) {
-				glue0->update_peer(c0->chan, NULL, NULL, NULL, NULL, 0);
-			}
-			if (glue1 && ast_channel_is_leaving_bridge(c1->chan)) {
+			glue0->update_peer(c0->chan, NULL, NULL, NULL, NULL, 0);
+			if (glue1) {
 				glue1->update_peer(c1->chan, NULL, NULL, NULL, NULL, 0);
 			}
 		} else {
@@ -487,9 +485,8 @@ static void native_rtp_bridge_leave(struct ast_bridge *bridge, struct ast_bridge
 	}
 
 	/* Direct RTP may have occurred, tear it down */
-	if (ast_channel_is_leaving_bridge(bridge_channel->chan)) {
-		glue->update_peer(bridge_channel->chan, NULL, NULL, NULL, NULL, 0);
-	}
+	glue->update_peer(bridge_channel->chan, NULL, NULL, NULL, NULL, 0);
+
 	native_rtp_bridge_stop(bridge, NULL);
 }
 
