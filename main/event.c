6b14886dc747ee8e876e982aa7c892810fb8783b
Kinsey Moore <kmoore@digium.com>
2014-05-23 14:36:40 +0000

    Fix signed/unsigned build warnings ........
    Merged revisions 414474 from http://svn.asterisk.org/svn/asterisk/branches/12
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@414475 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/event.c
+++ main/event.c
@@ -200,7 +200,7 @@ const char *ast_event_get_type_name(const struct ast_event *event)
 	type = ast_event_get_type(event);
 
 	if (type < 0 || type >= ARRAY_LEN(event_names)) {
-		ast_log(LOG_ERROR, "Invalid event type - '%d'\n", type);
+		ast_log(LOG_ERROR, "Invalid event type - '%u'\n", type);
 		return "";
 	}
 
