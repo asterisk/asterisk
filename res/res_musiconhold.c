0acc6265303822e0c5b4044d8152f38896fdbe83
Kinsey Moore <kmoore@digium.com>
2014-06-13 13:11:38 +0000

    MoH: Don't restart stream on repeated start calls
    Currently, music on hold will stop and then start again from the
    beginning if ast_moh_start() is called multiple times. This can happen
    if a call is put on hold repeatedly (the channel receives multiple
    HOLD control frames) and can be triggered from ARI by starting MoH on a
    channel multiple times. This is fairly jarring/annoying to users.
    
    This change prevents MoH from being restarted if the requested music
    class is the same as the one currently playing.
    
    Review: https://reviewboard.asterisk.org/r/3615/
    ........
    
    Merged revisions 416150 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    ........
    
    Merged revisions 416151 from http://svn.asterisk.org/svn/asterisk/branches/11
    ........
    
    Merged revisions 416152 from http://svn.asterisk.org/svn/asterisk/branches/12
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@416153 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_musiconhold.c
+++ res/res_musiconhold.c
@@ -1413,7 +1413,7 @@ static int local_ast_moh_start(struct ast_channel *chan, const char *mclass, con
 	struct mohclass *mohclass = NULL;
 	struct moh_files_state *state = ast_channel_music_state(chan);
 	struct ast_variable *var = NULL;
-	int res;
+	int res = 0;
 	int realtime_possible = ast_check_realtime("musiconhold");
 
 	/* The following is the order of preference for which class to use:
@@ -1601,10 +1601,12 @@ static int local_ast_moh_start(struct ast_channel *chan, const char *mclass, con
 		}
 	}
 
-	if (mohclass->total_files) {
-		res = ast_activate_generator(chan, &moh_file_stream, mohclass);
-	} else {
-		res = ast_activate_generator(chan, &mohgen, mohclass);
+	if (!state || strcmp(mohclass->name, state->class->name)) {
+		if (mohclass->total_files) {
+			res = ast_activate_generator(chan, &moh_file_stream, mohclass);
+		} else {
+			res = ast_activate_generator(chan, &mohgen, mohclass);
+		}
 	}
 	if (!res) {
 		ast_channel_latest_musicclass_set(chan, mohclass->name);
