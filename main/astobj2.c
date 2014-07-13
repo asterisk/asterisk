01492e96d97fe5b67501f0dd67ebb650b23fe87f
Corey Farrell <git@cfware.com>
2014-07-13 21:47:06 +0000

    astobj2: work around REF_DEBUG race which causes out of order log entries
    * Update refcounter.py to use delta's to track the current reference count.
    * Use result from internal_ao2_ref to write old_refcount to refs_log.
    
    Review: https://reviewboard.asterisk.org/r/3756/
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/1.8@418504 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/astobj2.c
+++ main/astobj2.c
@@ -219,23 +219,24 @@ void *ao2_object_get_lockaddr(void *obj)
 int __ao2_ref_debug(void *user_data, const int delta, const char *tag, const char *file, int line, const char *funcname)
 {
 	struct astobj2 *obj = INTERNAL_OBJ(user_data);
+	int old_refcount = -1;
+
+	if (obj) {
+		old_refcount = internal_ao2_ref(user_data, delta);
+	}
 
 	if (ref_log && user_data) {
-		if (obj && obj->priv_data.ref_counter + delta == 0) {
+		if (obj && old_refcount + delta == 0) {
 			fprintf(ref_log, "%p,%d,%d,%s,%d,%s,**destructor**,%s\n", user_data, delta, ast_get_tid(), file, line, funcname, tag);
 			fflush(ref_log);
 		} else if (delta != 0) {
 			fprintf(ref_log, "%p,%s%d,%d,%s,%d,%s,%d,%s\n", user_data, (delta < 0 ? "" : "+"),
-				delta, ast_get_tid(), file, line, funcname, obj ? obj->priv_data.ref_counter : -1, tag);
+				delta, ast_get_tid(), file, line, funcname, old_refcount, tag);
 			fflush(ref_log);
 		}
 	}
 
-	if (obj == NULL) {
-		return -1;
-	}
-
-	return internal_ao2_ref(user_data, delta);
+	return old_refcount;
 }
 
 int __ao2_ref(void *user_data, const int delta)
