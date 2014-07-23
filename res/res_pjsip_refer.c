0470766999d776434d0c664d6045f06104d7dc0e
Matthew Jordan <mjordan@digium.com>
2014-07-23 16:41:27 +0000

    res_pjsip_refer: remove stray debugging line
    How'd those @ symbols get in there...
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/12@419316 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_pjsip_refer.c
+++ res/res_pjsip_refer.c
@@ -204,7 +204,6 @@ static struct ast_frame *refer_progress_framehook(struct ast_channel *chan, stru
 		/* Media is passing without progress, this means the call has been answered */
 		notification = refer_progress_notification_alloc(progress, 200, PJSIP_EVSUB_STATE_TERMINATED);
 	} else if (f->frametype == AST_FRAME_CONTROL) {
-		ast_debug(5, "@@@@ GOT FRAME TYPE: %d\n", f->subclass.integer);
 		/* Based on the control frame being written we can send a NOTIFY advising of the progress */
 		if ((f->subclass.integer == AST_CONTROL_RING) || (f->subclass.integer == AST_CONTROL_RINGING)) {
 			progress->subclass = f->subclass.integer;
