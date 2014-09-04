9a9006d5dbf9887685b6ac92772a35b48b535c38
Jonathan Rose <jrose@digium.com>
2014-09-04 20:46:46 +0000

    Manager: Require read permission for SYSTEM in order to send FullyBooted
    Review: https://reviewboard.asterisk.org/r/3969/
    ........
    
    Merged revisions 422584 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    ........
    
    Merged revisions 422625 from http://svn.asterisk.org/svn/asterisk/branches/11
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/12@422626 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/manager.c
+++ main/manager.c
@@ -3615,6 +3615,7 @@ static int action_login(struct mansession *s, const struct message *m)
 	}
 	astman_send_ack(s, m, "Authentication accepted");
 	if ((s->session->send_events & EVENT_FLAG_SYSTEM)
+		&& (s->session->readperm & EVENT_FLAG_SYSTEM)
 		&& ast_test_flag(&ast_options, AST_OPT_FLAG_FULLY_BOOTED)) {
 		struct ast_str *auth = ast_str_alloca(80);
 		const char *cat_str = authority_to_str(EVENT_FLAG_SYSTEM, &auth);
