56540d82586112641c63e3180d765fb3e0b0ae6b
Corey Farrell <git@cfware.com>
2014-10-14 16:17:52 +0000

    res_fax: Resolve module reference leak caused by reserved sessions
    Remove reference to module providing reserved session after
    adding a reference to the final module.  This re-reference
    is done to ensure that module references are correct even
    if the final session selects a different module than the
    reserved session.
    
    ASTERISK-18923 #close
    Reported by: Grigoriy Puzankin
    Review: https://reviewboard.asterisk.org/r/4048/
    ........
    
    Merged revisions 425405 from http://svn.asterisk.org/svn/asterisk/branches/1.8
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/11@425407 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_fax.c
+++ res/res_fax.c
@@ -1057,6 +1057,10 @@ static struct ast_fax_session *fax_session_new(struct ast_fax_session_details *d
 			}
 			ast_debug(4, "Requesting a new FAX session from '%s'.\n", faxmod->tech->description);
 			ast_module_ref(faxmod->tech->module);
+			if (reserved) {
+				/* Balance module ref from reserved session */
+				ast_module_unref(reserved->tech->module);
+			}
 			s->tech = faxmod->tech;
 			break;
 		}
