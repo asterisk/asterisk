asterisk/asterisk

commit 13b2c25687e336b53637fa58db48565b63dd2e58

Fix issue with no sound in both way in case of previous call to chan_unistim phone was canceled.
(related to ASTERISK-20183)



git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/11@391379 65c4cc65-6c06-0410-ace0-fbb531ad65f3

channels/chan_unistim.c
@@ -4777,7 +4777,7 @@ static int unistim_hangup(struct ast_channel *ast)
 	struct unistim_line *l;
 	struct unistim_device *d;
 	struct unistimsession *s;
-	int i;
+	int i,end_call = 1;
 
 	s = channel_to_session(ast);
 	sub = ast_channel_tech_pvt(ast);
@@ -4822,10 +4822,8 @@ static int unistim_hangup(struct ast_channel *ast)
 		unistim_unalloc_sub(d, sub);
 		return 0;
 	}
-
 	if (sub->subtype == SUB_REAL) {
 		sub_stop_silence(s, sub);
-		send_end_call(s); /* Send end call packet only if ending active call, in other way sound should be loosed */
 	} else if (sub->subtype == SUB_RING) {
 		send_no_ring(s);
 		for (i = 0; i < FAVNUM; i++) {
@@ -4836,8 +4834,16 @@ static int unistim_hangup(struct ast_channel *ast)
 			if (is_key_line(d, i) && !strcmp(l->name, d->sline[i]->name)) {
 				send_favorite_short(i, FAV_LINE_ICON, s);
 				d->ssub[i] = NULL;
+				continue;
+			}
+			if (d->ssub[i] != NULL) { /* Found other subchannel active other then hangup'ed one */
+				ast_log(LOG_WARNING, "There is not only one call here %p %p %i\n",d->ssub[i], sub, i);
+				end_call = 0;
 			}
 		}
+	}
+	if (end_call) {
+		send_end_call(s); /* Send end call packet only if ending active call, in other way sound should be loosed */
 	}
 	sub->moh = 0;
 	if (sub->softkey >= 0) {
@@ -6132,7 +6138,7 @@ static int parse_bookmark(const char *text, struct unistim_device *d)
 			return 0;
 		}
 		if (d->softkeyicon[p] != 0) {
-			ast_log(LOG_WARNING, "Invalid position %d for bookmark : already used\n:", p);
+			ast_log(LOG_WARNING, "Invalid position %d for bookmark : already used:\n", p);
 			return 0;
 		}
 		memmove(line, line + 2, sizeof(line) - 2);

commit 195abe370e89d6f75e0a3c08f457eb4619c5ecf0

Fix loss of voice after second call drops (on a second line) in case using multiple lines on unistim phones. There is regression was introduced in r391379.
Reported by: Rustam Khankishyiev
(closes issue ASTERISK-23846)



git-svn-id: https://origsvn.digium.com/svn/asterisk/branches/11@425667 65c4cc65-6c06-0410-ace0-fbb531ad65f3

channels/chan_unistim.c
@@ -4841,17 +4841,19 @@ static int unistim_hangup(struct ast_channel *ast)
 	} else if (sub->subtype == SUB_RING) {
 		send_no_ring(s);
 		for (i = 0; i < FAVNUM; i++) {
-			if (!soft_key_visible(s->device, i))
+			if (!soft_key_visible(s->device, i)) {
 				continue;
-			if (d->ssub[i] != sub)
+			}
+			if (d->ssub[i] != sub) {
+				if (d->ssub[i] != NULL) { /* Found other subchannel active other then hangup'ed one */
+					end_call = 0;
+				}
 				continue;
+			}
 			if (is_key_line(d, i) && !strcmp(l->name, d->sline[i]->name)) {
 				send_favorite_short(i, FAV_LINE_ICON, s);
 				d->ssub[i] = NULL;
 				continue;
-			}
-			if (d->ssub[i] != NULL) { /* Found other subchannel active other then hangup'ed one */
-				end_call = 0;
 			}
 		}
 	}