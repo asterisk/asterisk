0a38e10c43f8160fac00cc3934298d2a9352687c
George Joseph <george.joseph@fairview5.com>
2015-04-11 16:04:32 -0600

    res_pjsip: Add global option to limit the maximum time for initial qualifies
    Currently when Asterisk starts initial qualifies of contacts are spread out
    randomly between 0 and qualify_timeout to prevent network and system overload.
    If a contact's qualify_frequency is 5 minutes however, that contact may be
    unavailable to accept calls for the entire 5 minutes after startup.  So while
    staggering the initial qualifies is a good idea, basing the time on
    qualify_timeout could leave contacts unavailable for too long.
    
    This patch adds a new global parameter "max_initial_qualify_time" that sets the
    maximum time for the initial qualifies.  This way you could make sure that all
    your contacts are initialy, ransomly qualified within say 30 seconds but still
    have the contact's ongoing qualifies at a 5 minute interval.
    
    If max_initial_qualify_time is > 0, the formula is initial_interval =
    min(max_initial_interval, qualify_timeout * random().  If not set,
    qualify_timeout is used.
    
    The default is "0" (disabled).
    
    Testing Done:
    Tested in my own pbx with 15 endpoints.
    If max_initial_qualify_time is > 0 and < qualify_time, it's used, otherwise
    qualify_timeout is used.
    
    Testsuite test forthcoming.
    
    Change-Id: Ib80498aa1ea9923277bef51d6a9015c9c79740f4
    Tested-by: George Joseph <george.joseph@fairview5.com>

--- res/res_pjsip.c
+++ res/res_pjsip.c
@@ -1226,6 +1226,9 @@
 				<configOption name="keep_alive_interval" default="0">
 					<synopsis>The interval (in seconds) to send keepalives to active connection-oriented transports.</synopsis>
 				</configOption>
+				<configOption name="max_initial_qualify_time" default="0">
+					<synopsis>The maximum amount of time from startup that qualifies should be attempted on all contacts.</synopsis>
+				</configOption>
 				<configOption name="type">
 					<synopsis>Must be of type 'global'.</synopsis>
 				</configOption>
