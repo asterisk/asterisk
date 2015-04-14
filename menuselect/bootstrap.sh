84e5ffad55f2736882c7a6e878088b7916730e53
George Joseph <george.joseph@fairview5.com>
2015-04-13 18:34:55 -0600

    Backport menuselect to 12,11,1.8
    Backport menuselect from 13->12->11->1.8
    
    Change-Id: I54c4dd2bdacd3c9d858be3acab08706941f2e585

---
+++ menuselect/bootstrap.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+check_for_app() {
+	$1 --version 2>&1 >/dev/null
+	if [ $? != 0 ]
+	then
+		echo "Please install $1 and run bootstrap.sh again!"
+		exit 1
+	fi
+}
+
+uname -s | grep -q FreeBSD
+if [ $? = 0 ]
+then
+	check_for_app autoconf259
+	check_for_app autoheader259
+	check_for_app automake19
+	check_for_app aclocal19
+	echo "Generating the configure script ..."
+	aclocal19 -I ../autoconf 2>/dev/null
+	autoconf259
+	autoheader259
+	automake19 --add-missing --copy 2>/dev/null
+else
+	AUTOCONF_VERSION=2.59
+	AUTOMAKE_VERSION=1.9
+	export AUTOCONF_VERSION
+	export AUTOMAKE_VERSION
+
+	check_for_app autoconf
+	check_for_app autoheader
+	check_for_app automake
+	check_for_app aclocal
+	echo "Generating the configure script ..."
+	aclocal -I ../autoconf 2>/dev/null
+	autoconf
+	autoheader
+	automake --add-missing --copy 2>/dev/null
+fi
+
+exit 0
