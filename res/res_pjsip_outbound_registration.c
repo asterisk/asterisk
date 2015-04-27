8803389b901de6cc898411eb7dda67fbfc6c310c
Mark Michelson <mmichelson@digium.com>
2015-04-27 16:13:22 -0500

    res_pjsip_outbound_registration: Don't fail on delayed processing.
    Odd behaviors have been observed during outbound registrations. The most
    common problem witnessed has been one where a request with
    authentication credentials cannot be created after receiving a 401
    response. Other behaviors include apparently processing an incorrect SIP
    response.
    
    Inspecting the code led to an apparent issue with regards to how we
    handle transactions in outbound registration code. When a response to a
    REGISTER arrives, we save a pointer to the transaction and then push a
    task onto the registration serializer. Between the time that we save the
    pointer and push the task, it's possible for the transaction to be
    destroyed due to a timeout. It's also possible for the address to be
    reused by the transaction layer for a new transaction.
    
    There are two fixes introduced here.
    
    First, we prevent the transaction from being destroyed before we are
    ready by adding a reference to the transaction group lock and
    decrementing that reference once we are completely finished processing
    the REGISTER response.
    
    Even though we now ensure that the transaction memory cannot be freed
    before we are ready, this is not enough to ensure that we can
    successfully perform actions like creating a REGISTER with
    authentication credentials. This is because when the transaction times
    out, most data is wiped from the transaction. Even though the
    transaction is not freed, it's a shell of its former self.
    
    To allow for authentication of a REGISTER request to be authenticated
    after the transaction has timed out, we now also hold a reference to the
    original REGISTER request. The function for creating a request with
    authentication has been altered to take the original request instead of
    the transaction where the original request was sent.
    
    ASTERISK-25020
    Reported by Mark Michelson
    
    Change-Id: I756c19ab05ada5d0503175db9676acf87c686d0a

--- res/res_pjsip_outbound_registration.c
+++ res/res_pjsip_outbound_registration.c
@@ -565,6 +565,8 @@ struct registration_response {
 	pjsip_rx_data *rdata;
 	/*! \brief The response transaction */
 	pjsip_transaction *tsx;
+	/*! \brief Request for which the response was received */
+	pjsip_tx_data *old_request;
 };
 
 /*! \brief Registration response structure destructor */
@@ -574,6 +576,14 @@ static void registration_response_destroy(void *obj)
 
 	if (response->rdata) {
 		pjsip_rx_data_free_cloned(response->rdata);
+	}
+
+	if (response->tsx) {
+		pj_grp_lock_dec_ref(response->tsx->grp_lock);
+	}
+
+	if (response->old_request) {
+		pjsip_tx_data_dec_ref(response->old_request);
 	}
 
 	ao2_cleanup(response->client_state);
@@ -640,7 +650,7 @@ static int handle_registration_response(void *data)
 			(response->code == 401 || response->code == 407)) {
 		pjsip_tx_data *tdata;
 		if (!ast_sip_create_request_with_auth(&response->client_state->outbound_auths,
-				response->rdata, response->tsx, &tdata)) {
+				response->rdata, response->old_request, &tdata)) {
 			ao2_ref(response->client_state, +1);
 			response->client_state->auth_attempted = 1;
 			ast_debug(1, "Sending authenticated REGISTER to '%s'\n", server_uri);
@@ -743,6 +753,9 @@ static void sip_outbound_registration_response_cb(struct pjsip_regc_cbparam *par
 
 		response->retry_after = retry_after ? retry_after->ivalue : 0;
 		response->tsx = pjsip_rdata_get_tsx(param->rdata);
+		response->old_request = response->tsx->last_tx;
+		pjsip_tx_data_add_ref(response->old_request);
+		pj_grp_lock_add_ref(response->tsx->grp_lock);
 		pjsip_rx_data_clone(param->rdata, 0, &response->rdata);
 	}
 
