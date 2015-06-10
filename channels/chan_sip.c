9c021b37adda556d1dce0d7482940b3917754e71
Damian Ivereigh <damo@launtel.net.au>
2015-06-10 17:38:48 +1000

    Bug ASTERISK-25154. Dialog fromtag not updated by request with auth
    If an INVITE is rejected with a 401 unathorised, and then the second
    INVITE comes through with a different fromtag, the dialog will be
    matched properly in find_call(), but will continue to save the old
    (now incorrect) fromtag. This gives problems later on with matching
    future dialog packets. This fix updates the fromtag
    
    Change-Id: I16534adef310303c59dd5989020f580d4305591c

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -9285,6 +9285,13 @@ static struct sip_pvt *find_call(struct sip_request *req, struct ast_sockaddr *a
 
 			switch (found) {
 			case SIP_REQ_MATCH:
+				if (args.method != SIP_RESPONSE && args.authentication_present) {
+					/* If we have a request that uses athentication, the fromtag may
+					 * different from the original call dialog. So update the fromtag */
+					sip_pvt_lock(sip_pvt_ptr);
+					ast_string_field_set(sip_pvt_ptr, theirtag, args.fromtag);
+					sip_pvt_unlock(sip_pvt_ptr);
+				}
 				ao2_iterator_destroy(iterator);
 				dialog_unref(fork_pvt, "unref fork_pvt");
 				free_via(via);
