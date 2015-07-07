48e2dacebdc9affa1622bbd8efff5d0dcb0a1ff3
Benjamin Ford <bford@digium.com>
2015-07-07 14:09:20 -0500

    ARI: Added new functionality to load a single module.
    An http request can be sent to load an Asterisk module. If the
    module can not be loaded or is loaded already, an error response
    will be returned.
    
    The command "curl -v -u user:pass -X POST 'http://localhost:8088/ari
    /asterisk/modules/{moduleName}'" (or something similar, depending on
    configuration) can be run in the terminal to access this new
    functionality.
    
    For more information, see:
    https://wiki.asterisk.org/wiki.display/~bford/Asterisk+ARI+Resource
    
    * Added new ARI functionality
    * Asterisk modules can be loaded through http requests
    
    ASTERISK-25173
    
    Change-Id: I9e05d5b8c5c666ecfef341504f9edc1aa84fda33

--- res/ari/resource_asterisk.c
+++ res/ari/resource_asterisk.c
@@ -33,6 +33,7 @@ ASTERISK_REGISTER_FILE()
 
 #include "asterisk/ast_version.h"
 #include "asterisk/buildinfo.h"
+#include "asterisk/module.h"
 #include "asterisk/paths.h"
 #include "asterisk/pbx.h"
 #include "resource_asterisk.h"
@@ -138,6 +139,50 @@ void ast_ari_asterisk_get_info(struct ast_variable *headers,
 	}
 
 	ast_ari_response_ok(response, ast_json_ref(json));
+}
+
+void ast_ari_asterisk_load_module(struct ast_variable *headers,
+	struct ast_ari_asterisk_load_module_args *args,
+	struct ast_ari_response *response)
+{
+	enum ast_module_load_result load_result;
+
+	ast_assert(response != NULL);
+
+	if (ast_strlen_zero(args->module_name)) {
+		ast_ari_response_error(
+			response, 400, "Bad Request",
+			"Module name is required");
+		return;
+	}
+
+	if (ast_module_check(args->module_name)) {
+		ast_ari_response_error(
+			response, 409, "Conflict",
+			"Module is already loaded");
+		return;
+	}
+
+	load_result = ast_load_resource(args->module_name);
+
+	if (load_result == AST_MODULE_LOAD_DECLINE) {
+		ast_ari_response_error(
+			response, 409, "Conflict",
+			"Module is not configured");
+		return;
+	} else if (load_result == AST_MODULE_LOAD_SKIP) {
+		ast_ari_response_error(
+			response, 409, "Conflict",
+			"Module was skipped");
+		return;
+	} else if (load_result == AST_MODULE_LOAD_FAILURE) {
+		ast_ari_response_error(
+			response, 409, "Conflict",
+			"Module could not be loaded properly");
+		return;
+	}
+
+	ast_ari_response_no_content(response);
 }
 
 void ast_ari_asterisk_get_global_var(struct ast_variable *headers,
