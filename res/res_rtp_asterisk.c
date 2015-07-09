a43e9c29bff022b10b2e162841aeb99ccc6694be
Mark Michelson <mmichelson@digium.com>
2015-07-09 14:17:53 -0500

    res_pjsip: Add rtp_keepalive endpoint option.
    This adds an "rtp_keepalive" option for PJSIP endpoints. Similar to the
    chan_sip option, this specifies an interval, in seconds, at which we
    will send RTP comfort noise frames. This can be useful for keeping RTP
    sessions alive as well as keeping NAT associations alive during lulls.
    
    ASTERISK-25242 #close
    Reported by Mark Michelson
    
    Change-Id: I683bdc206c8c7def586ecaa64dcf2b86550be3bf

--- res/res_rtp_asterisk.c
+++ res/res_rtp_asterisk.c
@@ -2117,6 +2117,7 @@ static int __rtp_sendto(struct ast_rtp_instance *instance, void *buf, size_t siz
 	void *temp = buf;
 	struct ast_rtp *rtp = ast_rtp_instance_get_data(instance);
 	struct ast_srtp *srtp = ast_rtp_instance_get_srtp(instance);
+	int res;
 
 	*ice = 0;
 
@@ -2135,7 +2136,11 @@ static int __rtp_sendto(struct ast_rtp_instance *instance, void *buf, size_t siz
 	}
 #endif
 
-	return ast_sendto(rtcp ? rtp->rtcp->s : rtp->s, temp, len, flags, sa);
+	res = ast_sendto(rtcp ? rtp->rtcp->s : rtp->s, temp, len, flags, sa);
+	if (res > 0) {
+		ast_rtp_instance_set_last_tx(instance, time(NULL));
+	}
+	return res;
 }
 
 static int rtcp_sendto(struct ast_rtp_instance *instance, void *buf, size_t size, int flags, struct ast_sockaddr *sa, int *ice)
