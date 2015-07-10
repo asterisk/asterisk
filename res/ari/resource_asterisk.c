fa11e992f0e829a349125aaf051aab5e31eb01f6
Benjamin Ford <bford@digium.com>
2015-07-10 10:57:46 -0500

    ARI: Added new functionality to get information on a single module.
    An http request can be sent to retrieve information on a single
    module, including the resource name, description, use count, status,
    and support level.
    
    The command "curl -v -u user:pass -X GET 'http://localhost:8088/ari
    /asterisk/modules/{moduleName}'" (or something similar, depending on
    configuration) can be run in the terminal to access this new
    functionality.
    
    For more information, see:
    https://wiki.asterisk.org/wiki.display/~bford/Asterisk+ARI+Resource
    
    * Added new ARI functionality
    * Information on a single module can now be retrieved
    
    ASTERISK-25173
    
    Change-Id: Ibce5a94e70ecdf4e90329cf0ba66c33a62d37463

--- res/ari/resource_asterisk.c
+++ res/ari/resource_asterisk.c
@@ -33,6 +33,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 
 #include "asterisk/ast_version.h"
 #include "asterisk/buildinfo.h"
+#include "asterisk/module.h"
 #include "asterisk/paths.h"
 #include "asterisk/pbx.h"
 #include "resource_asterisk.h"
@@ -138,6 +139,72 @@ void ast_ari_asterisk_get_info(struct ast_variable *headers,
 	}
 
 	ast_ari_response_ok(response, ast_json_ref(json));
+}
+
+/*!
+ * \brief Identify module by name and process resource information
+ * \param module Resource name
+ * \param description Resource description
+ * \param usecnt Resource use count
+ * \param status Resource running status
+ * \param like
+ * \param support_level Resource support level
+ * \param data JSON body for resource
+ * \param condition Name to match resource to
+ *
+ * \retval 0 if no resource exists
+ * \retval 1 if resource exists
+ */
+static int identify_module(const char *module, const char *description, int usecnt,
+                           const char *status, const char *like,
+                           enum ast_module_support_level support_level, void *data,
+                           const char *condition)
+{
+	if (strcmp(condition, module) != 0) {
+		return 0;
+	}
+
+	ast_json_object_set(data, "name", ast_json_string_create(module));
+	ast_json_object_set(data, "description", ast_json_string_create(description));
+	ast_json_object_set(data, "use_count", ast_json_integer_create(usecnt));
+	ast_json_object_set(data, "status", ast_json_string_create(status));
+	ast_json_object_set(data, "support_level", ast_json_string_create(
+	                    ast_module_support_level_to_string(support_level)));
+
+	return 1;
+}
+
+void ast_ari_asterisk_get_module(struct ast_variable *headers,
+	struct ast_ari_asterisk_get_module_args *args,
+	struct ast_ari_response *response)
+{
+	struct ast_json *json;
+
+	ast_assert(response != NULL);
+
+	if (ast_strlen_zero(args->module_name)) {
+		ast_ari_response_error(
+			response, 400, "Bad Request",
+			"Module name is required");
+		return;
+	}
+
+	if (!ast_module_check(args->module_name)) {
+		ast_ari_response_error(
+			response, 404, "Not Found",
+			"Module does not exist");
+		return;
+	}
+
+	json = ast_json_object_create();
+	if (!json) {
+			ast_ari_response_alloc_failed(response);
+		return;
+	}
+
+	ast_update_module_list_condition(&identify_module, NULL, json, args->module_name);
+
+	ast_ari_response_ok(response, json);
 }
 
 void ast_ari_asterisk_get_global_var(struct ast_variable *headers,
