27dc2094e98b59b8a50b059ddd6048285a42e6b9
Mark Michelson <mmichelson@digium.com>
2015-08-04 09:47:34 -0500

    res_http_websocket: Debug write lengths.
    Commit 39cc28f6ea2140ad6d561fd4c9e9a66f065cecee attempted to fix a
    test failure observed on 32 bit test agents by ensuring that a cast from
    a 32 bit unsigned integer to a 64 bit unsigned integer was happening in
    a predictable place. As it turns out, this did not cause test runs to
    succeed.
    
    This commit adds several redundant debug messages that print the payload
    lengths of websocket frames. The idea here is that this commit will not
    cause tests to succeed for the faulty test agent, but we might deduce
    where the fault lies more easily this way by observing at what point the
    expected value (537) changes to some ungangly huge number.
    
    If you are wondering why something like this is being committed to the
    branch, keep in mind that in commit
    39cc28f6ea2140ad6d561fd4c9e9a66f065cecee I noted that the observed test
    failures only happen when automated tests are run. Attempts to run the
    tests by hand manually on the test agent result in the tests passing.
    
    Change-Id: I14a65c19d8af40dadcdbd52348de3b0016e1ae8d

--- res/ari/ari_websockets.c
+++ res/ari/ari_websockets.c
@@ -174,7 +174,7 @@ int ast_ari_websocket_session_write(struct ast_ari_websocket_session *session,
 		return -1;
 	}
 
-	ast_debug(3, "Examining ARI event: \n%s\n", str);
+	ast_debug(3, "Examining ARI event (length %zu): \n%s\n", strlen(str), str);
 	if (ast_websocket_write_string(session->ws_session, str)) {
 		ast_log(LOG_NOTICE, "Problem occurred during websocket write, websocket closed\n");
 		return -1;
