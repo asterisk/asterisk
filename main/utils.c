ffa26a0c2e88ab294e6f0407faa71cc053a4e511
Guido Falsi <madpilot@freebsd.org>
2015-09-01 17:16:55 +0200

    Core/General: Add #ifdef needed on FreeBSD.
    pthread_attr_init() defaults to PTHREAD_EXPLICIT_SCHED on FreeBSD
    too.
    
    ASTERISK-25310 #close
    Reported by: Guido Falsi
    
    Change-Id: Iae6befac9028b5b9795f86986a4a08a1ae6ab7c4

--- main/utils.c
+++ main/utils.c
@@ -1242,8 +1242,8 @@ int ast_pthread_create_stack(pthread_t *thread, pthread_attr_t *attr, void *(*st
 		pthread_attr_init(attr);
 	}
 
-#ifdef __linux__
-	/* On Linux, pthread_attr_init() defaults to PTHREAD_EXPLICIT_SCHED,
+#if defined(__linux__) || defined(__FreeBSD__)
+	/* On Linux and FreeBSD , pthread_attr_init() defaults to PTHREAD_EXPLICIT_SCHED,
 	   which is kind of useless. Change this here to
 	   PTHREAD_INHERIT_SCHED; that way the -p option to set realtime
 	   priority will propagate down to new threads by default.
