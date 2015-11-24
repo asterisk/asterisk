fa2072903238ddb8dbba7d75ccf70edb36581f36
Richard Mudgett <rmudgett@digium.com>
2015-11-24 12:44:53 -0600

    Audit improper usage of scheduler exposed by 5c713fdf18f.
    channels/chan_iax2.c:
    * Initialize struct chan_iax2_pvt scheduler ids earlier because of
    iax2_destroy_helper().
    
    channels/chan_sip.c:
    channels/sip/config_parser.c:
    * Fix initialization of scheduler id struct members.  Some off nominal
    paths had 0 as a scheduler id to be destroyed when it was never started.
    
    chan_skinny.c:
    * Fix some scheduler id comparisons that excluded the valid 0 id.
    
    channel.c:
    * Fix channel initialization of the video stream scheduler id.
    
    pbx_dundi.c:
    * Fix channel initialization of the packet retransmission scheduler id.
    
    ASTERISK-25476
    
    Change-Id: I07a3449f728f671d326a22fcbd071f150ba2e8c8

--- pbx/pbx_dundi.c
+++ pbx/pbx_dundi.c
@@ -3264,6 +3264,7 @@ static int dundi_send(struct dundi_transaction *trans, int cmdresp, int flags, i
 	pack = ast_calloc(1, len);
 	if (pack) {
 		pack->h = (struct dundi_hdr *)(pack->data);
+		pack->retransid = -1;
 		if (cmdresp != DUNDI_COMMAND_ACK) {
 			pack->retransid = ast_sched_add(sched, trans->retranstimer, dundi_rexmit, pack);
 			pack->retrans = DUNDI_DEFAULT_RETRANS - 1;
