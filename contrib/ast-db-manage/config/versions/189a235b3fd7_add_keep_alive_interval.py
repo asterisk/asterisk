c5e16fe33a47563ebb38a377ebe6c06b7f302cab
Mark Michelson <mmichelson@digium.com>
2015-12-16 11:25:13 -0600

    Alembic: Add PJSIP global keep_alive_interval.
    The keep_alive_interval option was added about a year ago, but no
    alembic revision was created to add the appropriate column to the
    database.
    
    This commit fixes the problem and adds the column. This was discovered
    by running the testsuite with automatic conversion to realtime enabled.
    
    Change-Id: If3ef92a7c4f4844d08f8aae170d2178aec5c4c1a

---
+++ contrib/ast-db-manage/config/versions/189a235b3fd7_add_keep_alive_interval.py
@@ -0,0 +1,22 @@
+"""add_keep_alive_interval
+
+Revision ID: 189a235b3fd7
+Revises: 28ce1e718f05
+Create Date: 2015-12-16 11:23:16.994523
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '189a235b3fd7'
+down_revision = '339a3bdf53fc'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('ps_globals', sa.Column('keep_alive_interval', sa.Integer))
+
+
+def downgrade():
+    op.drop_column('ps_globals', 'keep_alive_interval')
