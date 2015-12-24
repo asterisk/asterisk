b5c60b62dd91144f17451f5acf625eac8c3a5a73
Kevin Harwell <kharwell@digium.com>
2015-12-24 13:45:14 -0600

    pbx: Deadlock between contexts container and context_merge locks
    Recent changes (ASTERISK-25394 commit 2bd27d12223fe33b58c453965ed5c6ed3af7c4f5)
    introduced the possibility of a deadlock. Due to the mentioned modifications
    ast_change_hints now needs to keep both merge/delete and state callbacks from
    occurring while it executes. Unfortunately, sometimes ast_change_hints can be
    called with the contexts container locked. When this happens it's possible for
    another thread to grab the context_merge_lock before the thread calling into
    ast_change_hints does and then try to obtain the contexts container lock. This
    of course causes a deadlock between the two threads. The thread calling into
    ast_change_hints waits for the other thread to release context_merge_lock and
    the other thread is waiting on that one to release the contexts container lock.
    
    Unfortunately, there is not a great way to fix this problem. When hints change
    the merge/delete cannot run, nor the state callbacks. Picking the least of
    evils this patch adds yet another lock: state_callbacks_lock. This lock is held
    during state callbacks and ast_change_hints. In order to keep the merge/delete
    code from executing at the same time ast_change_hints locks the contexts
    container as well. The context_merge_lock no longer needs to be held during
    ast_change_hints, thus relieving the possible tension between threads.
    
    ASTERISK-25640 #close
    Reported by: Krzysztof Trempala
    
    Change-Id: I7f67ce4b4c94b731c23932cc2ba9e0a4a720813d

--- main/pbx.c
+++ main/pbx.c
@@ -1434,6 +1434,11 @@ AST_MUTEX_DEFINE_STATIC(conlock);
 AST_MUTEX_DEFINE_STATIC(context_merge_lock);
 
 /*!
+ * \brief Lock used to protect state data during callbacks
+ */
+AST_MUTEX_DEFINE_STATIC(state_callbacks_lock);
+
+/*!
  * \brief Registered applications container.
  *
  * It is sorted by application name.
@@ -5380,12 +5385,14 @@ static void device_state_cb(void *unused, struct stasis_subscription *sub, struc
 	strcpy(cmpdevice->hintdevice, dev_state->device);
 
 	ast_mutex_lock(&context_merge_lock);/* Hold off ast_merge_contexts_and_delete */
+	ast_mutex_lock(&state_callbacks_lock);
 	dev_iter = ao2_t_callback(hintdevices,
 		OBJ_POINTER | OBJ_MULTIPLE,
 		hintdevice_cmp_multiple,
 		cmpdevice,
 		"find devices in container");
 	if (!dev_iter) {
+		ast_mutex_unlock(&state_callbacks_lock);
 		ast_mutex_unlock(&context_merge_lock);
 		ast_free(hint_app);
 		return;
@@ -5481,6 +5488,7 @@ static void device_state_cb(void *unused, struct stasis_subscription *sub, struc
 
 		ao2_cleanup(device_state_info);
 	}
+	ast_mutex_unlock(&state_callbacks_lock);
 	ast_mutex_unlock(&context_merge_lock);
 
 	ao2_iterator_destroy(dev_iter);
@@ -5876,7 +5884,14 @@ static int ast_change_hint(struct ast_exten *oe, struct ast_exten *ne)
 		return -1;
 	}
 
-	ast_mutex_lock(&context_merge_lock); /* Hold off ast_merge_contexts_and_delete and state changes */
+	/* Lock state callbacks so hints can be manipulated */
+	ast_mutex_lock(&state_callbacks_lock);
+
+	/*
+	 * Hold off ast_merge_contexts_and_delete, but do it by holding the contexts lock.
+	 * If the context_merge_lock is held there is the potential for a deadlock.
+	 */
+	ast_rdlock_contexts();
 
 	ao2_lock(hints);/* Locked to hold off others while we move the hint around. */
 
@@ -5887,7 +5902,8 @@ static int ast_change_hint(struct ast_exten *oe, struct ast_exten *ne)
 	hint = ao2_find(hints, oe, OBJ_UNLINK);
 	if (!hint) {
 		ao2_unlock(hints);
-		ast_mutex_unlock(&context_merge_lock);
+		ast_unlock_contexts();
+		ast_mutex_unlock(&state_callbacks_lock);
 		ast_free(hint_app);
 		return -1;
 	}
@@ -5928,9 +5944,10 @@ static int ast_change_hint(struct ast_exten *oe, struct ast_exten *ne)
 
 	ao2_unlock(hints);
 
-	/* Locking for state callbacks is respected here and only the context_merge_lock lock is
-	 * held during the state callback invocation. This will stop the normal state callback
-	 * thread from being able to handle incoming state changes if they occur.
+	/* Locking for state callbacks is respected here and only the context_merge_lock and
+	 * state_callbacks_lock are held during the state callback invocation. This will stop
+	 * the normal state callback thread from being able to handle incoming state changes
+	 * if they occur.
 	 */
 
 	/* Determine if presence state has changed due to the change of the hint extension */
@@ -6013,7 +6030,8 @@ static int ast_change_hint(struct ast_exten *oe, struct ast_exten *ne)
 
 	ao2_ref(hint, -1);
 
-	ast_mutex_unlock(&context_merge_lock);
+	ast_unlock_contexts();
+	ast_mutex_unlock(&state_callbacks_lock);
 
 	ast_free(hint_app);
 	ast_free(previous_message);
@@ -11993,6 +12011,7 @@ static void presence_state_cb(void *unused, struct stasis_subscription *sub, str
 	}
 
 	ast_mutex_lock(&context_merge_lock);/* Hold off ast_merge_contexts_and_delete */
+	ast_mutex_lock(&state_callbacks_lock);
 	hint_iter = ao2_iterator_init(hints, 0);
 	for (; (hint = ao2_iterator_next(&hint_iter)); ao2_cleanup(hint)) {
 		struct ast_state_cb *state_cb;
@@ -12089,6 +12108,7 @@ static void presence_state_cb(void *unused, struct stasis_subscription *sub, str
 		ao2_iterator_destroy(&cb_iter);
 	}
 	ao2_iterator_destroy(&hint_iter);
+	ast_mutex_unlock(&state_callbacks_lock);
 	ast_mutex_unlock(&context_merge_lock);
 
 	ast_free(hint_app);
