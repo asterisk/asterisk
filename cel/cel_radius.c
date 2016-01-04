f4c299c3c8f3232fd9fa33f72dedc6470bad61fb
Aaron An <anjb@ti-net.com.cn>
2016-01-04 18:26:55 +0800

    cel/cel_radius: Fix wrong pointer.
    The macro ADD_VENDOR_CODE defined in the cel_radius.c should use the parameter
    y not the address of y.
    
    The radius udp packet av pairs it not corret, then review the source
    code and compare it with cdr/cdr_radius.c i find the bug, after fix it work
    
    ASTERISK-25647 #close
    Reported by: Aaron An
    Tested by: Aaron An
    
    Change-Id: I72889bccd8fde120d47aa659edc0e7e6d4d019f0

--- cel/cel_radius.c
+++ cel/cel_radius.c
@@ -95,7 +95,7 @@ static rc_handle *rh = NULL;
 
 #define RADIUS_BACKEND_NAME "CEL Radius Logging"
 
-#define ADD_VENDOR_CODE(x,y) (rc_avpair_add(rh, send, x, &y, strlen(y), VENDOR_CODE))
+#define ADD_VENDOR_CODE(x,y) (rc_avpair_add(rh, send, x, y, strlen(y), VENDOR_CODE))
 
 static int build_radius_record(VALUE_PAIR **send, struct ast_cel_event_record *record)
 {
