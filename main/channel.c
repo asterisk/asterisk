e6e67adcc13683d7a213add720405efc17bbe6dd
Joshua Colp <jcolp@digium.com>
2016-01-12 13:14:29 -0400

    channel: Queue hangup if channel is hung up during connected line sub.
    When connected or redirected subroutines or macros are executed it is
    expected that the underlying applications and logic invoked are fast
    and do not consume frames. In practice this constraint is not enforced
    and if not adhered to will cause channels to continue when they shouldn't.
    This is because each caller of the connected or redirected logic does not
    check whether the channel has been hung up on return. As a result the
    the hung up channel continues.
    
    This change makes it so when a connected or redirected subroutine or
    macro is finished the channel is checked. If the channel has been hung
    up a hangup is queued again so the caller will see this and stop.
    
    ASTERISK-25690 #close
    
    Change-Id: I1f9a8ceb1487df0389f0d346ce0f6dcbcaf476ea

--- main/channel.c
+++ main/channel.c
@@ -10950,6 +10950,10 @@ int ast_channel_connected_line_macro(struct ast_channel *autoservice_chan, struc
 		ast_party_connected_line_free(&saved_connected);
 	}
 
+	if (ast_check_hangup_locked(macro_chan)) {
+		ast_queue_hangup(macro_chan);
+	}
+
 	return retval;
 }
 
@@ -11000,6 +11004,10 @@ int ast_channel_redirecting_macro(struct ast_channel *autoservice_chan, struct a
 		ast_party_redirecting_free(&saved_redirecting);
 	}
 
+	if (ast_check_hangup_locked(macro_chan)) {
+		ast_queue_hangup(macro_chan);
+	}
+
 	return retval;
 }
 
@@ -11043,6 +11051,10 @@ int ast_channel_connected_line_sub(struct ast_channel *autoservice_chan, struct 
 		ast_party_connected_line_free(&saved_connected);
 	}
 
+	if (ast_check_hangup_locked(sub_chan)) {
+		ast_queue_hangup(sub_chan);
+	}
+
 	return retval;
 }
 
@@ -11084,6 +11096,10 @@ int ast_channel_redirecting_sub(struct ast_channel *autoservice_chan, struct ast
 		ast_channel_unlock(sub_chan);
 		ast_channel_update_redirecting(sub_chan, &saved_redirecting, NULL);
 		ast_party_redirecting_free(&saved_redirecting);
+	}
+
+	if (ast_check_hangup_locked(sub_chan)) {
+		ast_queue_hangup(sub_chan);
 	}
 
 	return retval;
