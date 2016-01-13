38cd738a24ec6c2e94f1cc1230e081bae6c3eb2a
Daniel Journo <dan@keshercommunications.com>
2016-01-13 21:58:22 +0000

    pjsip/alembic:  Fix qualify_timeout column definition
    Corrects the qualify_timeout column type from Integer to Decimal
    
    ASTERISK-25686 #close
    Reported-by: Marcelo Terres
    
    Change-Id: I757d0e3c011ee9be6cd5abd48bc92441a405d3c8

---
+++ contrib/ast-db-manage/config/versions/423f34ad36e2_fix_pjsip_qualify_ti.py
@@ -0,0 +1,26 @@
+"""fix pjsip qualify timeout
+
+Revision ID: 423f34ad36e2
+Revises: 136885b81223
+Create Date: 2016-01-13 21:49:21.557734
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '423f34ad36e2'
+down_revision = '136885b81223'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.alter_column('ps_aors', 'qualify_timeout', type_=sa.Decimal)
+    op.alter_column('ps_contacts', 'qualify_timeout', type_=sa.Decimal)
+    pass
+
+
+def downgrade():
+    op.alter_column('ps_aors', 'qualify_timeout', type_=sa.Integer)
+    op.alter_column('ps_contacts', 'qualify_timeout', type_=sa.Integer)
+    pass
