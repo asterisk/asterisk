asterisk/asterisk

commit 48dd99d2b4d6938b528f9f6aca98d36b60c5513b

taskprocessor.c: Fix CLI "core show taskprocessors" unref.
Change-Id: I1d9f4e532caa6dfabe034745dd16d06134efdce5

main/taskprocessor.c
@@ -284,7 +284,7 @@ static char *cli_tps_report(struct ast_cli_entry *e, int cmd, struct ast_cli_arg
 		maxqsize = p->stats->max_qsize;
 		processed = p->stats->_tasks_processed_count;
 		ast_cli(a->fd, "\n%24s   %17lu %12lu %12lu", name, processed, qsize, maxqsize);
-		ao2_ref(p, -1);
+		ast_taskprocessor_unreference(p);
 	}
 	ao2_iterator_destroy(&i);
 	tcount = ao2_container_count(tps_singletons);

commit aef26eff794fef5ed7cf0c7ef600a459facf80f6

taskprocessor.c: Fix endless loop in CLI "core show taskprocessor"
Revert 48dd99d2b4d6938b528f9f6aca98d36b60c5513b which caused an infinite
loop only in v11.

ASTERISK-25701 #close
Reported by: ibercom

Change-Id: I0d9103f4dc03fa31bb8d5e7a7a73b467bbac5349

main/taskprocessor.c
@@ -284,7 +284,7 @@ static char *cli_tps_report(struct ast_cli_entry *e, int cmd, struct ast_cli_arg
 		maxqsize = p->stats->max_qsize;
 		processed = p->stats->_tasks_processed_count;
 		ast_cli(a->fd, "\n%24s   %17lu %12lu %12lu", name, processed, qsize, maxqsize);
-		ast_taskprocessor_unreference(p);
+		ao2_ref(p, -1);
 	}
 	ao2_iterator_destroy(&i);
 	tcount = ao2_container_count(tps_singletons);