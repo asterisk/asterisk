8b47ce1051134730d2b7bba6fed5333a35f48759
George Joseph <george.joseph@fairview5.com>
2016-01-18 20:54:28 -0700

    build-system: Allow building with static pjproject
    Background here:
    http://lists.digium.com/pipermail/asterisk-dev/2016-January/075266.html
    
    From CHANGES:
     * To help insure that Asterisk is compiled and run with the same known
       version of pjproject, a new option (--with-pjproject-bundled) has been
       added to ./configure.  When specified, the version of pjproject specified
       in third-party/versions.mak will be downloaded and configured.  When you
       make Asterisk, the build process will also automatically build pjproject
       and Asterisk will be statically linked to it.  Once a particular version
       of pjproject is configured and built, it won't be configured or built
       again unless you run a 'make distclean'.
    
       To facilitate testing, when 'make install' is run, the pjsua and pjsystest
       utilities and the pjproject python bindings will be installed in
       ASTDATADIR/third-party/pjproject.
    
       The default behavior remains building with the shared pjproject
       installation, if any.
    
    Building:
    
       All you have to do is include the --with-pjproject-bundled option on
       the ./configure command line (and remove any existing --with-pjproject
       option if specified).  Everything else is automatic.
    
    Behind the scenes:
    
       The top-level Makefile was modified to include 'third-party' in the
       list of MOD_SUBDIRS.
    
       The third-party directory was created to contain any third party
       packages that may be needed in the future.  Its Makefile automatically
       iterates over any subdirectories passing on targets.
    
       The third-party/pjproject directory was created to house the pjproject
       source distribution.  Its Makefile contains targets to download, patch
       configure, generate dependencies, compile libs, apps and python bindings,
       sanitized build.mak and generate a symbols list.
    
       When bootstrap.sh is run, it automatically includes the configure.m4
       file in third-party/pjproject.  This file has a macro to download and
       conifgure pjproject and get and set PJPROJECT_INCLUDE, PJPROJECT_DIR
       and PJPROJECT_BUNDLED.  It also tests for the capabilities like
       PJ_TRANSACTION_GRP_LOCK by parsing preprocessor output as opposed to
       trying to compile.  Of course, bootstrap.sh is only run once and the
       configure file is incldued in the patch.
    
       When configure is run with the new options, the macro in configure.m4
       triggers the download, patch, conifgure and tests.  No compilation is
       performed at this time.
    
       When make is run in the top-level Asterisk source directory, it will
       automatically descend all the subdirectories in third_party just as it
       does for addons, apps, etc.  The top-level Makefile makes sure that
       the 'third-party' is built before 'main' so that dependencies from the
       other directories are built first.
    
       When main does build, a new shared library (libasteriskpj) is created that
       links statically to the pjproject .a files and exports all their symbols.
       The asterisk binary links to that, just as it does with libasteriskssl.
    
       When Asterisk is installed, the pjsua and pjsystest apps, and the pjproject
       python bindings are installed in ASTDATADIR/third-party/pjproject.  This
       will facilitate testing, including running the testsuite which will be
       updated to check that directory for the pjsua module ahead of the system
       python library.
    
    Modules should continue to depend on pjproject if they use pjproject APIs
    directly.  They should not care about the implementation.  No changes to any
    res_pjsip modules were made.
    
    Change-Id: Ia7a60c28c2e9ba9537c5570f933c1ebcb20a3103

---
+++ main/libasteriskpj.c
@@ -0,0 +1,52 @@
+/*
+ * Asterisk -- An open source telephony toolkit.
+ *
+ * Copyright (C) 2009-2012, Digium, Inc.
+ * Copyright (C) 2015, Fairview 5 Engineering, LLC
+ *
+ * Russell Bryant <russell@digium.com>
+ * George Joseph <george.joseph@fairview5.com>
+ *
+ * See http://www.asterisk.org for more information about
+ * the Asterisk project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
+ */
+
+/*!
+ * \file
+ * \brief Loader stub for static pjproject libraries
+ *
+ * \author George Joseph <george.joseph@fairview5.com>
+ */
+
+/*** MODULEINFO
+	<support_level>core</support_level>
+ ***/
+
+#include "asterisk.h"
+
+ASTERISK_REGISTER_FILE()
+
+#ifdef HAVE_PJPROJECT
+#include <pjlib.h>
+#endif
+
+#include "asterisk/_private.h" /* ast_pj_init() */
+
+/*!
+ * \internal
+ * \brief Initialize static pjproject implementation
+ */
+int ast_pj_init(void)
+{
+#ifdef HAVE_PJPROJECT_BUNDLED
+	pj_init();
+#endif
+	return 0;
+}
