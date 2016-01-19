fef0d63f69878c97cf3f231927b0c2ae87d3f026
George Joseph <george.joseph@fairview5.com>
2016-01-18 20:54:28 -0700

    build-system: Allow building with static pjproject
    Background here:
    http://lists.digium.com/pipermail/asterisk-dev/2016-January/075266.html
    
    From CHANGES:
     * To help insure that Asterisk is compiled and run with the same known
       version of pjproject, a new option (--with-pjproject-bundled) has been
       added to ./configure.  When specified, the version of pjproject specified
       in third-party/versions.mak will be downloaded and configured.  When you
       make Asterisk, the build process will also automatically build pjproject
       and Asterisk will be statically linked to it.  Once a particular version
       of pjproject is configured and built, it won't be configured or built
       again unless you run a 'make distclean'.
    
       To facilitate testing, when 'make install' is run, the pjsua and pjsystest
       utilities and the pjproject python bindings will be installed in
       ASTDATADIR/third-party/pjproject.
    
       The default behavior remains building with the shared pjproject
       installation, if any.
    
    Building:
    
       All you have to do is include the --with-pjproject-bundled option on
       the ./configure command line (and remove any existing --with-pjproject
       option if specified).  Everything else is automatic.
    
    Behind the scenes:
    
       The top-level Makefile was modified to include 'third-party' in the
       list of MOD_SUBDIRS.
    
       The third-party directory was created to contain any third party
       packages that may be needed in the future.  Its Makefile automatically
       iterates over any subdirectories passing on targets.
    
       The third-party/pjproject directory was created to house the pjproject
       source distribution.  Its Makefile contains targets to download, patch
       configure, generate dependencies, compile libs, apps and python bindings,
       sanitized build.mak and generate a symbols list.
    
       When bootstrap.sh is run, it automatically includes the configure.m4
       file in third-party/pjproject.  This file has a macro to download and
       conifgure pjproject and get and set PJPROJECT_INCLUDE, PJPROJECT_DIR
       and PJPROJECT_BUNDLED.  It also tests for the capabilities like
       PJ_TRANSACTION_GRP_LOCK by parsing preprocessor output as opposed to
       trying to compile.  Of course, bootstrap.sh is only run once and the
       configure file is incldued in the patch.
    
       When configure is run with the new options, the macro in configure.m4
       triggers the download, patch, conifgure and tests.  No compilation is
       performed at this time.  The downloaded tarball is cached in /tmp so
       it doesn't get downloaded again on a distclean.
    
       When make is run in the top-level Asterisk source directory, it will
       automatically descend all the subdirectories in third_party just as it
       does for addons, apps, etc.  The top-level Makefile makes sure that
       the 'third-party' is built before 'main' so that dependencies from the
       other directories are built first.
    
       When main does build, a new shared library (libasteriskpj) is created that
       links statically to the pjproject .a files and exports all their symbols.
       The asterisk binary links to that, just as it does with libasteriskssl.
    
       When Asterisk is installed, the pjsua and pjsystest apps, and the pjproject
       python bindings are installed in ASTDATADIR/third-party/pjproject.  This
       will facilitate testing, including running the testsuite which will be
       updated to check that directory for the pjsua module ahead of the system
       python library.
    
    Modules should continue to depend on pjproject if they use pjproject APIs
    directly.  They should not care about the implementation.  No changes to any
    res_pjsip modules were made.
    
    Change-Id: Ia7a60c28c2e9ba9537c5570f933c1ebcb20a3103

---
+++ third-party/pjproject/patches/config_site.h
@@ -0,0 +1,34 @@
+/*
+ * Asterisk config_site.h
+ */
+
+#include <sys/select.h>
+
+#define PJ_HAS_IPV6 1
+#define NDEBUG 1
+#define PJ_MAX_HOSTNAME (256)
+#define PJSIP_MAX_URL_SIZE (512)
+#ifdef PJ_HAS_LINUX_EPOLL
+#define PJ_IOQUEUE_MAX_HANDLES	(5000)
+#else
+#define PJ_IOQUEUE_MAX_HANDLES	(FD_SETSIZE)
+#endif
+#define PJ_IOQUEUE_HAS_SAFE_UNREG 1
+#define PJ_IOQUEUE_MAX_EVENTS_IN_SINGLE_POLL (16)
+
+#define PJ_SCANNER_USE_BITWISE	0
+#define PJ_OS_HAS_CHECK_STACK	0
+#define PJ_LOG_MAX_LEVEL		3
+#define PJ_ENABLE_EXTRA_CHECK	0
+#define PJSIP_MAX_TSX_COUNT		((64*1024)-1)
+#define PJSIP_MAX_DIALOG_COUNT	((64*1024)-1)
+#define PJSIP_UDP_SO_SNDBUF_SIZE	(512*1024)
+#define PJSIP_UDP_SO_RCVBUF_SIZE	(512*1024)
+#define PJ_DEBUG			0
+#define PJSIP_SAFE_MODULE		0
+#define PJ_HAS_STRICMP_ALNUM		0
+#define PJ_HASH_USE_OWN_TOLOWER		1
+#define PJSIP_UNESCAPE_IN_PLACE		1
+
+#undef PJ_TODO
+#define PJ_TODO(x)
