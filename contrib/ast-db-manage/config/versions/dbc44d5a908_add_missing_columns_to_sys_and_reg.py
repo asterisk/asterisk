953d1cc11a1b28f8a43f4d68d39f845e47db2d68
George Joseph <george.joseph@fairview5.com>
2016-02-03 13:25:23 -0700

    pjsip/alembic:  Add missing columns to system and registration
    ps_systems needed disable_tcp_switch
    ps_registrations needed line and endpoint
    
    ASTERISK-25737 #close
    
    Change-Id: Iaf9c2d69e62243d9fa53104c28c5339c47d4ac19

---
+++ contrib/ast-db-manage/config/versions/dbc44d5a908_add_missing_columns_to_sys_and_reg.py
@@ -0,0 +1,37 @@
+"""Add missing columns to system and registration
+
+Revision ID: dbc44d5a908
+Revises: 423f34ad36e2
+Create Date: 2016-02-03 13:15:15.083043
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'dbc44d5a908'
+down_revision = '423f34ad36e2'
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import ENUM
+
+YESNO_NAME = 'yesno_values'
+YESNO_VALUES = ['yes', 'no']
+
+def upgrade():
+    ############################# Enums ##############################
+
+    # yesno_values have already been created, so use postgres enum object
+    # type to get around "already created" issue - works okay with mysql
+    yesno_values = ENUM(*YESNO_VALUES, name=YESNO_NAME, create_type=False)
+
+    op.add_column('ps_systems', sa.Column('disable_tcp_switch', yesno_values))
+    op.add_column('ps_registrations', sa.Column('line', yesno_values))
+    op.add_column('ps_registrations', sa.Column('endpoint', sa.String(40)))
+    pass
+
+
+def downgrade():
+    op.drop_column('ps_systems', 'disable_tcp_switch')
+    op.drop_column('ps_registrations', 'line')
+    op.drop_column('ps_registrations', 'endpoint')
+    pass
