b170b45681e9a57aad798913686029f25d3c7b75
Andrew Nagy <andrew.nagy@the159.com>
2016-03-15 11:31:19 -0700

    ASTERISK-25846 return 0 when/if the app fails
    Change-Id: I63216a61f30706d5362bc0906b50b6f0544aebce

--- apps/app_stasis.c
+++ apps/app_stasis.c
@@ -111,6 +111,8 @@ static int app_exec(struct ast_channel *chan, const char *data)
 	}
 
 	if (ret == -1) {
+	    /* set ret to 0 so pbx_core doesnt hangup the channel */		
+	    ret = 0; 
 	    pbx_builtin_setvar_helper(chan, "STASISSTATUS", "FAILED");
 	} else {
 	    pbx_builtin_setvar_helper(chan, "STASISSTATUS", "SUCCESS");
