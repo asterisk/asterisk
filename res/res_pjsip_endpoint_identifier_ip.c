ce85aa1db46d871df27e4255870ba9288db4ed12
Alexei Gradinari <alex2grad@gmail.com>
2016-05-04 16:16:24 -0400

    res_pjsip: module load priority
    The res_pjsip_endpoint_identifier_* and res_pjsip_registrar modules
    should load ASAP to avoid "No matching endpoint found"
    for legitimate endpoint.
    
    ASTERISK-25994
    
    Change-Id: Iac95d95ad031e0be104189d29e923a2ad7c24a1b

--- res/res_pjsip_endpoint_identifier_ip.c
+++ res/res_pjsip_endpoint_identifier_ip.c
@@ -515,5 +515,5 @@ AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_LOAD_ORDER, "PJSIP IP endpoint ide
 		.load = load_module,
 		.reload = reload_module,
 		.unload = unload_module,
-		.load_pri = AST_MODPRI_APP_DEPEND,
+		.load_pri = AST_MODPRI_CHANNEL_DEPEND - 4,
 	       );
