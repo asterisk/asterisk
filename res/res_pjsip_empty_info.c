39fedfa4235da9f2d5de63bafae0521d7591ac8e
snuffy <snuffy22@gmail.com>
2016-05-10 12:40:08 +1000

    res_pjsip_empty_info: Respond to empty SIP INFO packets
    Some SBCs require responses to empty SIP INFO packets
    after establishing call via INVITE, if not responded to
    they may drop your call after unspecified timeout of X minutes.
    
    They are identified by having no Content-Type, check for this
    and respond with 200 - OK message.
    
    ASTERISK-24986 #close
    Reported-by: Ilya Trikoz, Federico Santulli
    
    Change-Id: Ib27e4f07151e5aef28fa587e4ead36c5b87c43e0

---
+++ res/res_pjsip_empty_info.c
@@ -0,0 +1,89 @@
+/*
+ * Asterisk -- An open source telephony toolkit.
+ *
+ * Copyright (C) 2016, Digium, Inc.
+ *
+ * Bradley Latus <brad.latus@gmail.com>
+ *
+ * See http://www.asterisk.org for more information about
+ * the Asterisk project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
+ */
+
+/*** MODULEINFO
+	<depend>pjproject</depend>
+	<depend>res_pjsip</depend>
+	<depend>res_pjsip_session</depend>
+	<support_level>core</support_level>
+ ***/
+
+#include "asterisk.h"
+
+ASTERISK_REGISTER_FILE()
+
+#include <pjsip.h>
+#include <pjsip_ua.h>
+
+#include "asterisk/res_pjsip.h"
+#include "asterisk/res_pjsip_session.h"
+#include "asterisk/module.h"
+
+static void send_response(struct ast_sip_session *session,
+		struct pjsip_rx_data *rdata, int code)
+{
+	pjsip_tx_data *tdata;
+	pjsip_dialog *dlg = session->inv_session->dlg;
+
+	if (pjsip_dlg_create_response(dlg, rdata, code, NULL, &tdata) == PJ_SUCCESS) {
+		struct pjsip_transaction *tsx = pjsip_rdata_get_tsx(rdata);
+		pjsip_dlg_send_response(dlg, tsx, tdata);
+	}
+}
+
+static int empty_info_incoming_request(struct ast_sip_session *session,
+		struct pjsip_rx_data *rdata)
+{
+	if (!rdata->msg_info.ctype) {
+		/* Need to return 200 OK on empty body */
+		/* Some SBCs use empty INFO as a KEEPALIVE */
+		send_response(session, rdata, 200);
+		return 1;
+	}
+
+	/* Let another module respond */
+	return 0;
+
+}
+
+static struct ast_sip_session_supplement empty_info_supplement = {
+	.method = "INFO",
+	.priority = AST_SIP_SUPPLEMENT_PRIORITY_LAST,
+	.incoming_request = empty_info_incoming_request,
+};
+
+static int load_module(void)
+{
+	CHECK_PJSIP_SESSION_MODULE_LOADED();
+
+	ast_sip_session_register_supplement(&empty_info_supplement);
+	return AST_MODULE_LOAD_SUCCESS;
+}
+
+static int unload_module(void)
+{
+	ast_sip_session_unregister_supplement(&empty_info_supplement);
+	return 0;
+}
+
+AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_LOAD_ORDER, "PJSIP Empty INFO Support",
+	.support_level = AST_MODULE_SUPPORT_CORE,
+	.load = load_module,
+	.unload = unload_module,
+	.load_pri = AST_MODPRI_APP_DEPEND,
+);
