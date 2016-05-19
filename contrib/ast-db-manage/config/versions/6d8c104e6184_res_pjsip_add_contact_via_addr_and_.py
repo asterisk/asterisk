2a71f1d0fbf03427b24b7d9020c700e9f4a781df
Alexei Gradinari <alex2grad@gmail.com>
2016-05-19 15:56:26 -0400

    res_pjsip: add "via_addr", "via_port", "call_id" to contact
    As res_pjsip_nat rewrites contact's address, only the last Via header
    can contain the source address of registered endpoint.
    Also Call-Id header may contain the source address of registered
    endpoint.
    
    Added "via_addr", "via_port", "call_id" to contact.
    Added new fields ViaAddress, CallID to AMI event ContactStatus.
    
    ASTERISK-26011
    
    Change-Id: I36bcc0bf422b3e0623680152d80486aeafe4c576

---
+++ contrib/ast-db-manage/config/versions/6d8c104e6184_res_pjsip_add_contact_via_addr_and_.py
@@ -0,0 +1,25 @@
+"""res_pjsip: add contact via_addr and callid
+
+Revision ID: a845e4d8ade8
+Revises: bca7113d796f
+Create Date: 2016-05-19 15:51:33.410852
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'a845e4d8ade8'
+down_revision = 'bca7113d796f'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.add_column('ps_contacts', sa.Column('via_addr', sa.String(40)))
+    op.add_column('ps_contacts', sa.Column('via_port', sa.Integer))
+    op.add_column('ps_contacts', sa.Column('call_id', sa.String(255)))
+
+def downgrade():
+    op.drop_column('ps_contacts', 'via_addr')
+    op.drop_column('ps_contacts', 'via_port')
+    op.drop_column('ps_contacts', 'call_id')
