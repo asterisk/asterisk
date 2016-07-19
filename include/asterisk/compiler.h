05cfe1a76e7c7e0162b3196c8ff16c064726f53f
Corey Farrell <git@cfware.com>
2016-07-18 23:46:19 -0400

    Add conditional support for noreturn functions.
    This adds support for tagging functions with the noreturn attribute.
    If DO_CRASH is enabled then ast_do_crash never returns.  If AST_DEVMODE
    and DO_CRASH are enabled then failed assertions never return.  This can
    resolve a large number of false positives with static analyzers.
    
    ASTERISK-26220 #close
    
    Change-Id: Icfb61e5fe54574eced4c3e88b317244f467ec753

--- include/asterisk/compiler.h
+++ include/asterisk/compiler.h
@@ -77,6 +77,12 @@
 #define attribute_may_alias
 #endif
 
+#ifdef HAVE_ATTRIBUTE_noreturn
+#define attribute_noreturn __attribute__((noreturn))
+#else
+#define attribute_noreturn
+#endif
+
 /* Some older version of GNU gcc (3.3.5 on OpenBSD 4.3 for example) dont like 'NULL' as sentinel */
 #define SENTINEL ((char *)NULL)
 
