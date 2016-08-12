094c26aa689664177e156d17bf5e9ab67c442a53
frahaase <fra.haase@googlemail.com>
2016-08-12 18:23:18 +0200

    Binaural synthesis (confbridge): Adds binaural synthesis to bridge_softmix.
    Adds binaural synthesis to bridge_softmix (via convolution using libfftw3).
    Binaural synthesis is conducted at 48kHz.
    For a conference, only one spatial representation is rendered.
    The default rendering is applied for mono-capable channels.
    
    ASTERISK-26292
    
    Change-Id: Iecdb381b6adc17c961049658678f6219adae1ddf

---
+++ utils/conf_bridge_binaural_hrir_importer.h
@@ -0,0 +1,46 @@
+#define FILE_HEADER "\
+ * Asterisk -- An open source telephony toolkit.\n\
+ *\n\
+ * Copyright (C) 2016, Frank Haase, Dennis Guse\n\
+ *\n\
+ * Frank Haase <fra.haase@gmail.com>\n\
+ * Dennis Guse <dennis.guse@alumni.tu-berlin.de>\n\
+ *\n\
+ * See http://www.asterisk.org for more information about\n\
+ * the Asterisk project. Please do not directly contact\n\
+ * any of the maintainers of this project for assistance;\n\
+ * the project provides a web site, mailing lists and IRC\n\
+ * channels for your use.\n\
+ *\n\
+ * Copyright (c) 2001 The Regents of the University of California. All Rights Reserved.\n\
+ *\n\
+ * The HRIRs used here are obtained from The CIPIC HRTF Database\n\
+ * (http://interface.cipic.ucdavis.edu/CIL_html/CIL_HRTF_database.htm)\n\
+ * Note that the above mentioned material is Copyright (c) 2001 The\n\
+ * Regents of the University of California. All Rights Reserved.\n\
+ *\n\
+ * Download the file\n\
+ * http://interface.cipic.ucdavis.edu/data/special_kemar_hrir.tar and\n\
+ * uncompress it in the folder where this Matlab script resides. Finally,\n\
+ * run the script.\n\
+ *\n\
+ * This program is free software, distributed under the terms of\n\
+ * the GNU General Public License Version 2. See the LICENSE file\n\
+ * at the top of the source tree.\n\
+ */\n\
+\n\
+/*! \\file\n\
+ *\n\
+ * \\brief Multi-party software binaural channel HRIRS\n\
+ *\n\
+ * \\author Frank Haase <fra.haase@googlemail.com>\n\
+ * \\author Dennis Guse <dennis.guse@alumni.tu-berlin.de>\n\
+ *\n\
+ * \\ingroup bridges\n\
+ */\n\
+\n\
+/*\n\
+ * This file was created with command:\n\
+ * $ conf_bridge_binaural_hrir_importer %s %d %d\n\
+ */\n\
+"
