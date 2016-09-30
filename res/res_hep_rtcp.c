80ade7aa5d485b6875e8b1f96a92afb63ed26af2
Nir Simionovich <nirs@greenfieldtech.net>
2016-10-01 02:00:41 +0300

    Resolve an issue with chan_sip Call-Id not reported reported
    Change-Id: I466d7503ea73e5b09c5d0fb5568ba8c9e6f248fa

--- res/res_hep_rtcp.c
+++ res/res_hep_rtcp.c
@@ -55,21 +55,28 @@ static char *assign_uuid(struct ast_json *json_channel)
 		return NULL;
 	}
 
-	if (uuid_type == HEP_UUID_TYPE_CALL_ID && ast_begins_with(channel_name, "PJSIP")) {
-		struct ast_channel *chan = ast_channel_get_by_name(channel_name);
-		char buf[128];
-
-		if (chan && !ast_func_read(chan, "CHANNEL(pjsip,call-id)", buf, sizeof(buf))) {
-			uuid = ast_strdup(buf);
-		}
-
-		ast_channel_cleanup(chan);
+	/* We need the channel to continue properly, so let's extract that one first */
+	struct ast_channel *chan = ast_channel_get_by_name(channel_name);
+	switch(uuid_type) {
+		case HEP_UUID_TYPE_CHANNEL:
+			uuid = ast_strdup(channel_name);
+			break;
+		case HEP_UUID_TYPE_CALL_ID:
+		default:
+			char buf[128];
+			if(ast_begins_with(channel_name, "PJSIP")) {
+				if (chan && !ast_func_read(chan, "CHANNEL(pjsip,call-id)", buf, sizeof(buf))) {
+					uuid = ast_strdup(buf);
+				}
+			} else {
+				if (chan && !ast_func_read(chan, "SIP_HEADER(call-id)", buf, sizeof(buf))) {
+					uuid = ast_strdup(buf);
+				}
+			}
+			break;
 	}
 
-	/* If we couldn't get the call-id or didn't want it, just use the channel name */
-	if (!uuid) {
-		uuid = ast_strdup(channel_name);
-	}
+	ast_channel_cleanup(chan);
 
 	return uuid;
 }
