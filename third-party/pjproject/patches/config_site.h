461f4c6544829aeed42a62c0ff4b23df15995bb3
George Joseph <gjoseph@digium.com>
2016-10-05 13:53:10 -0600

    pjproject_bundled:  Add MALLOC_DEBUG capability
    pjproject_bundled will now use the asterisk memory debugging APIs
    if MALLOC_DEBUG is turned on in menuselect.
    
    Because this required stubs for the executable programs and the python
    bindings, some Makefile reorganization was needed to properly handle
    the dependencies.  As a result, the makefile now individually makes
    each of the pjproject libraries separately instead of making them all
    in 1 shot.  The only visible change is that there are separate status
    lines printed for each library instead oif 1 for all libs.  Also, the
    making of the pjproject dependency files was eliminated.  They're not
    needed for building unless you're actively modifying pjproject source
    files and it makes the build process faster.  Finally, any issues with
    parallel builds should be resolved again making the build faster.
    
    Change-Id: Icc5e3d658fbfb00e0a46b44c66dcc2522d5171b0

--- third-party/pjproject/patches/config_site.h
+++ third-party/pjproject/patches/config_site.h
@@ -5,6 +5,14 @@
 #include <sys/select.h>
 
 /*
+ * Since both pjproject and asterisk source files will include config_site.h,
+ * we need to make sure that only pjproject source files include asterisk_malloc_debug.h.
+ */
+#if defined(MALLOC_DEBUG) && !defined(_ASTERISK_ASTMM_H)
+#include "asterisk_malloc_debug.h"
+#endif
+
+/*
  * Defining PJMEDIA_HAS_SRTP to 0 does NOT disable Asterisk's ability to use srtp.
  * It only disables the pjmedia srtp transport which Asterisk doesn't use.
  * The reason for the disable is that while Asterisk works fine with older libsrtp
