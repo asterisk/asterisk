6c2ef61647c52ac3a8a7438eb59eeb0172ae0e54
Richard Mudgett <rmudgett@digium.com>
2016-10-11 18:14:39 -0500

    json: Add UTF-8 check call.
    Since the json library does not make the check function public we
    recreate/copy the function in our interface module.
    
    ASTERISK-26466
    Reported by: Richard Mudgett
    
    Change-Id: I36d3d750b6f5f1a110bc69ea92b435ecdeeb2a99

--- include/asterisk/json.h
+++ include/asterisk/json.h
@@ -217,6 +217,33 @@ const char *ast_json_typename(enum ast_json_type type);
 /*!@{*/
 
 /*!
+ * \brief Check the string of the given length for UTF-8 format.
+ * \since 13.12.0
+ *
+ * \param str String to check.
+ * \param len Length of string to check.
+ *
+ * \retval 0 if not UTF-8 encoded or str is NULL.
+ * \retval 1 if UTF-8 encoded.
+ */
+int ast_json_utf8_check_len(const char *str, size_t len);
+
+/*!
+ * \brief Check the nul terminated string for UTF-8 format.
+ * \since 13.12.0
+ *
+ * \param str String to check.
+ *
+ * \retval 0 if not UTF-8 encoded or str is NULL.
+ * \retval 1 if UTF-8 encoded.
+ */
+int ast_json_utf8_check(const char *str);
+
+/*!@}*/
+
+/*!@{*/
+
+/*!
  * \brief Get the JSON true value.
  * \since 12.0.0
  *
