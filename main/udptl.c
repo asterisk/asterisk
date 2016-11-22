7c6c09c499330a2c7f1681e572e27d440b0d2bfb
Guido Falsi <mad@madpilot.net>
2016-11-22 18:20:06 +0100

    res_rtp: Fix regression observed in FreeBSD when IPv6 is not available.
    The latest Release candidate fails to create RTP streams when IPv6
    is not available. Due to the changes made in September the ast_sockaddr
    structure passed around to create these streams is always of AF_INET6
    type, causing failure when used for IPv4, at least on FreeBSD. This
    patch add some code (wrapped in @ifdefs) to reallocate the ast_sockaddr
    structure on the fly with an AF_INET family, allowing it to work
    correctly when creating the cosket with the passed structure fails.
    
    ASTERISK-26617
    
    Change-Id: I627a4e91795e821111e1cda523f083a40d0e0c3e

--- main/udptl.c
+++ main/udptl.c
@@ -1014,6 +1014,9 @@ struct ast_udptl *ast_udptl_new_with_bindaddr(struct ast_sched_context *sched, s
 	int i;
 	long int flags;
 	RAII_VAR(struct udptl_config *, cfg, ao2_global_obj_ref(globals), ao2_cleanup);
+#if defined(__FreeBSD__)
+	int portretryipv4 = 1;
+#endif
 
 	if (!cfg || !cfg->general) {
 		ast_log(LOG_ERROR, "Could not access global udptl options!\n");
@@ -1037,8 +1040,18 @@ struct ast_udptl *ast_udptl_new_with_bindaddr(struct ast_sched_context *sched, s
 		udptl->tx[i].buf_len = -1;
 	}
 
-	if ((udptl->fd = socket(ast_sockaddr_is_ipv6(addr) ?
-					AF_INET6 : AF_INET, SOCK_DGRAM, 0)) < 0) {
+#if defined(__FreeBSD__)
+	udptl->fd = socket(AF_INET6, SOCK_DGRAM, 0);
+	if (udptl->fd < 0) {
+		ast_sockaddr_parse(addr, "0.0.0.0", 0);
+		udptl->fd = socket(AF_INET, SOCK_DGRAM, 0);
+		portretryipv4 = 0;
+	}
+#else
+	udptl->fd = socket(ast_sockaddr_is_ipv6(addr) ?
+			AF_INET6 : AF_INET, SOCK_DGRAM, 0);
+#endif
+	if (udptl->fd < 0) {
 		ast_free(udptl);
 		ast_log(LOG_WARNING, "Unable to allocate socket: %s\n", strerror(errno));
 		return NULL;
@@ -1077,8 +1090,28 @@ struct ast_udptl *ast_udptl_new_with_bindaddr(struct ast_sched_context *sched, s
 		if (x > cfg->general->end)
 			x = cfg->general->start;
 		if (x == startplace) {
+			/* Try again with IPv4 if not IPv6 port could be found */
+#if defined(__FreeBSD__)
+			if (portretryipv4 == 1) {
+				close(udptl->fd);
+				ast_sockaddr_parse(addr, "0.0.0.0", 0);
+				udptl->fd = socket(AF_INET, SOCK_DGRAM, 0);
+				if (udptl->fd >= 0) {
+					/* Reset the RTP port search parameters */
+					x = (cfg->general->start == cfg->general->end) ? cfg->general->start : (ast_random() % (cfg->general->end - cfg->general->start)) + cfg->general->start;
+					if (cfg->general->use_even_ports && (x & 1)) {
+						++x;
+					}
+					startplace = x;
+					portretryipv4 = 0;
+					continue;
+				}
+			}
+#endif
 			ast_log(LOG_WARNING, "No UDPTL ports remaining\n");
-			close(udptl->fd);
+			if (udptl->fd >= 0) {
+				close(udptl->fd);
+			}
 			ast_free(udptl);
 			return NULL;
 		}
--- res/res_rtp_asterisk.c
+++ res/res_rtp_asterisk.c
@@ -2636,6 +2636,9 @@ static int ast_rtp_new(struct ast_rtp_instance *instance,
 {
 	struct ast_rtp *rtp = NULL;
 	int x, startplace;
+#if defined(__FreeBSD__)
+	int portretryipv4 = 1;
+#endif
 
 	/* Create a new RTP structure to hold all of our data */
 	if (!(rtp = ast_calloc(1, sizeof(*rtp)))) {
@@ -2656,10 +2659,20 @@ static int ast_rtp_new(struct ast_rtp_instance *instance,
 	}
 
 	/* Create a new socket for us to listen on and use */
-	if ((rtp->s =
-	     create_new_socket("RTP",
-			       ast_sockaddr_is_ipv4(addr) ? AF_INET  :
-			       ast_sockaddr_is_ipv6(addr) ? AF_INET6 : -1)) < 0) {
+#if defined(__FreeBSD__)
+	rtp->s = create_new_socket("RTP", AF_INET6);
+	if (rtp->s < 0) {
+		/* create correct addr structure for AF_INET */
+		ast_sockaddr_parse(addr, "0.0.0.0", 0);
+		rtp->s = create_new_socket("RTP", AF_INET);
+		portretryipv4 = 0;
+	}
+#else
+	rtp->s = create_new_socket("RTP",
+			ast_sockaddr_is_ipv4(addr) ? AF_INET  :
+			ast_sockaddr_is_ipv6(addr) ? AF_INET6 : -1);
+#endif
+	if (rtp->s < 0) {
 		ast_log(LOG_WARNING, "Failed to create a new socket for RTP instance '%p'\n", instance);
 		ast_free(rtp);
 		return -1;
@@ -2686,8 +2699,26 @@ static int ast_rtp_new(struct ast_rtp_instance *instance,
 
 		/* See if we ran out of ports or if the bind actually failed because of something other than the address being in use */
 		if (x == startplace || (errno != EADDRINUSE && errno != EACCES)) {
+#if defined(__FreeBSD__)
+			/* Try again with IPv4 if not IPv6 port could be found */
+			if (portretryipv4 == 1) {
+				close(rtp->s);
+				ast_sockaddr_parse(addr, "0.0.0.0", 0);
+		                rtp->s = create_new_socket("RTP", AF_INET);
+				if (rtp->s >= 0) {
+					/* Reset the RTP port search parameters */
+					x = (rtpend == rtpstart) ? rtpstart : (ast_random() % (rtpend - rtpstart)) + rtpstart;
+					x = x & ~1;
+					startplace = x;
+					portretryipv4 = 0;
+					continue;
+				}
+			}
+#endif
 			ast_log(LOG_ERROR, "Oh dear... we couldn't allocate a port for RTP instance '%p'\n", instance);
-			close(rtp->s);
+			if (rtp->s >= 0) {
+				close(rtp->s);
+			}
 			ast_free(rtp);
 			return -1;
 		}
