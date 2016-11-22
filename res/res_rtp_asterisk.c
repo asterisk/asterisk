98d3eca2f00fdf8c7456ee753620954009db8206
Guido Falsi <mad@madpilot.net>
2016-11-22 18:20:06 +0100

    res_rtp: Fix regression observed in FreeBSD when IPv6 is not available.
    The latest Release candidate fails to create RTP streams when IPv6
    is not available. Due to the changes made in September the ast_sockaddr
    structure passed around to create these streams is always of AF_INET6
    type, causing failure when used for IPv4, at least on FreeBSD. This
    patch add some code (wrapped in @ifdefs) to reallocate the ast_sockaddr
    structure on the fly with an AF_INET family, allowing it to work
    correctly when creating the cosket with the passed structure fails.
    
    ASTERISK-26617
    
    Change-Id: I627a4e91795e821111e1cda523f083a40d0e0c3e

--- res/res_rtp_asterisk.c
+++ res/res_rtp_asterisk.c
@@ -2656,10 +2656,19 @@ static int ast_rtp_new(struct ast_rtp_instance *instance,
 	}
 
 	/* Create a new socket for us to listen on and use */
-	if ((rtp->s =
-	     create_new_socket("RTP",
-			       ast_sockaddr_is_ipv4(addr) ? AF_INET  :
-			       ast_sockaddr_is_ipv6(addr) ? AF_INET6 : -1)) < 0) {
+#if defined(__FreeBSD__)
+	rtp->s = create_new_socket("RTP", AF_INET6);
+	if (rtp->s < 0) {
+		/* create correct addr structure for AF_INET */
+		ast_sockaddr_parse(addr, "0.0.0.0", 0);
+		rtp->s = create_new_socket("RTP", AF_INET);
+	}
+#else
+	rtp->s = create_new_socket("RTP",
+			ast_sockaddr_is_ipv4(addr) ? AF_INET  :
+			ast_sockaddr_is_ipv6(addr) ? AF_INET6 : -1);
+#endif
+	if (rtp->s < 0) {
 		ast_log(LOG_WARNING, "Failed to create a new socket for RTP instance '%p'\n", instance);
 		ast_free(rtp);
 		return -1;
