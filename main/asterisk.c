be21705e4a70eed7a4687d9b75744d069bbbcb3e
Richard Mudgett <rmudgett@digium.com>
2016-11-23 18:27:54 -0600

    PJPROJECT logging: Made easier to get available logging levels.
    Use of the new logging is as simple as issuing the new CLI command or
    setting the new asterisk.conf option.
    
    Other options that can affect the logging are how you have the pjproject
    log levels mapped to Asterisk log types in pjproject.conf and if you have
    configured Asterisk to log the DEBUG type messages.  Altering the
    pjproject.conf level mapping shouldn't be necessary for most installations
    as the default mapping is sensible.  Configuring Asterisk to log the DEBUG
    message type is standard practice for collecting debug information.
    
    * Added CLI "pjproject set log level" command to dynamically adjust the
    enabled pjproject log messages.
    
    * Added pjproject.conf boot section "log_level" option to set the initial
    pjproject log message level so all messages could be captured from
    initialization.
    
    * Set PJ_LOG_MAX_LEVEL to 6 to compile in all defined logging levels into
    bundled pjproject.  Pjproject will use the currently set run time log
    level to determine if a log message is generated just like Asterisk
    verbose and debug logging levels.
    
    * In log_forwarder(), made always log enabled and mapped pjproject log
    messages.  DEBUG mapped log messages are no longer gated by the current
    Asterisk debug logging level.
    
    * Removed RAII_VAR() from res_pjproject.c:get_log_level().
    
    ASTERISK-26630 #close
    
    Change-Id: I6dca12979f482ffb0450aaf58db0fe0f6d2e5389

--- main/asterisk.c
+++ main/asterisk.c
@@ -325,6 +325,7 @@ int ast_verb_sys_level;
 
 int option_verbose;				/*!< Verbosity level */
 int option_debug;				/*!< Debug level */
+int ast_option_pjproject_log_level;
 double ast_option_maxload;			/*!< Max load avg on system */
 int ast_option_maxcalls;			/*!< Max number of active calls */
 int ast_option_maxfiles;			/*!< Max number of open file handles (files, sockets) */
@@ -647,6 +648,7 @@ static char *handle_show_settings(struct ast_cli_entry *e, int cmd, struct ast_c
 	ast_cli(a->fd, "  Root console verbosity:      %d\n", option_verbose);
 	ast_cli(a->fd, "  Current console verbosity:   %d\n", ast_verb_console_get());
 	ast_cli(a->fd, "  Debug level:                 %d\n", option_debug);
+	ast_cli(a->fd, "  Pjproject log level:         %d\n", ast_option_pjproject_log_level);
 	ast_cli(a->fd, "  Maximum load average:        %lf\n", ast_option_maxload);
 #if defined(HAVE_SYSINFO)
 	ast_cli(a->fd, "  Minimum free memory:         %ld MB\n", option_minmemfree);
@@ -3892,6 +3894,37 @@ static void ast_readconfig(void)
 	ast_config_destroy(cfg);
 }
 
+static void read_pjproject_boot_options(void)
+{
+	struct ast_config *cfg;
+	struct ast_variable *v;
+	struct ast_flags config_flags = { CONFIG_FLAG_NOCACHE | CONFIG_FLAG_NOREALTIME };
+
+	ast_option_pjproject_log_level = DEFAULT_PJ_LOG_MAX_LEVEL;
+
+	cfg = ast_config_load2("pjproject.conf", "" /* core, can't reload */, config_flags);
+	if (!cfg
+		|| cfg == CONFIG_STATUS_FILEUNCHANGED
+		|| cfg == CONFIG_STATUS_FILEINVALID) {
+		/* We'll have to use defaults */
+		return;
+	}
+
+	for (v = ast_variable_browse(cfg, "boot"); v; v = v->next) {
+		if (!strcasecmp(v->name, "log_level")) {
+			if (sscanf(v->value, "%30d", &ast_option_pjproject_log_level) != 1) {
+				ast_option_pjproject_log_level = DEFAULT_PJ_LOG_MAX_LEVEL;
+			} else if (ast_option_pjproject_log_level < 0) {
+				ast_option_pjproject_log_level = 0;
+			} else if (MAX_PJ_LOG_MAX_LEVEL < ast_option_pjproject_log_level) {
+				ast_option_pjproject_log_level = MAX_PJ_LOG_MAX_LEVEL;
+			}
+		}
+	}
+
+	ast_config_destroy(cfg);
+}
+
 static void *monitor_sig_flags(void *unused)
 {
 	for (;;) {
@@ -4613,6 +4646,7 @@ static void asterisk_daemon(int isroot, const char *runuser, const char *rungrou
 
 	check_init(ast_timing_init(), "Timing");
 	check_init(ast_ssl_init(), "SSL");
+	read_pjproject_boot_options();
 	check_init(ast_pj_init(), "Embedded PJProject");
 	check_init(app_init(), "App Core");
 	check_init(devstate_init(), "Device State Core");
