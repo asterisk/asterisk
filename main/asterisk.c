7451abf125563a146a5a6e5d048424d62eae3ad3
Richard Mudgett <rmudgett@digium.com>
2016-11-23 18:27:54 -0600

    PJPROJECT logging: Made easier to get available logging levels.
    Use of the new logging is as simple as issuing the new CLI command or
    setting the new asterisk.conf option.
    
    Other options that can affect the logging are how you have the pjproject
    log levels mapped to Asterisk log types in pjproject.conf and if you have
    configured Asterisk to log the DEBUG type messages.  Altering the
    pjrpoject.conf level mapping shouldn't be necessary for most installations
    as the default mapping is sensible.  Configuring Asterisk to log the DEBUG
    message type is standard practice for collecting debug information.
    
    * Added CLI "pjproject set log level" command to dynamically adjust the
    enabled pjproject log messages.
    
    * Added asterisk.conf pjproject_log_level option to set the initial pjproject
    log message level so all messages could be captured from initialization.
    
    * Set PJ_LOG_MAX_LEVEL to 6 to compile in all defined logging levels into
    bundled pjproject.  Pjproject will use the currently set run time log
    level to determine if a log message is generated just like Asterisk
    verbose and debug logging levels.
    
    * In log_forwarder(), made always log enabled and mapped pjproject log
    messages.  DEBUG mapped log messages are no longer gated by the current
    Asterisk debug logging level.
    
    * Removed RAII_VAR() from res_pjproject.c:get_log_level().
    
    ASTERISK-26630 #close
    
    Change-Id: I6dca12979f482ffb0450aaf58db0fe0f6d2e5389

--- main/asterisk.c
+++ main/asterisk.c
@@ -325,6 +325,7 @@ int ast_verb_sys_level;
 
 int option_verbose;				/*!< Verbosity level */
 int option_debug;				/*!< Debug level */
+int ast_option_pjproject_log_level = DEFAULT_PJ_LOG_MAX_LEVEL;
 double ast_option_maxload;			/*!< Max load avg on system */
 int ast_option_maxcalls;			/*!< Max number of active calls */
 int ast_option_maxfiles;			/*!< Max number of open file handles (files, sockets) */
@@ -647,6 +648,7 @@ static char *handle_show_settings(struct ast_cli_entry *e, int cmd, struct ast_c
 	ast_cli(a->fd, "  Root console verbosity:      %d\n", option_verbose);
 	ast_cli(a->fd, "  Current console verbosity:   %d\n", ast_verb_console_get());
 	ast_cli(a->fd, "  Debug level:                 %d\n", option_debug);
+	ast_cli(a->fd, "  Pjproject log level:         %d\n", ast_option_pjproject_log_level);
 	ast_cli(a->fd, "  Maximum load average:        %lf\n", ast_option_maxload);
 #if defined(HAVE_SYSINFO)
 	ast_cli(a->fd, "  Minimum free memory:         %ld MB\n", option_minmemfree);
@@ -3737,6 +3739,14 @@ static void ast_readconfig(void)
 			option_debug = 0;
 			if (sscanf(v->value, "%30d", &option_debug) != 1) {
 				option_debug = ast_true(v->value) ? 1 : 0;
+			}
+		} else if (!strcasecmp(v->name, "pjproject_log_level")) {
+			if (sscanf(v->value, "%30d", &ast_option_pjproject_log_level) != 1) {
+				ast_option_pjproject_log_level = DEFAULT_PJ_LOG_MAX_LEVEL;
+			} else if (ast_option_pjproject_log_level < 0) {
+				ast_option_pjproject_log_level = 0;
+			} else if (MAX_PJ_LOG_MAX_LEVEL < ast_option_pjproject_log_level) {
+				ast_option_pjproject_log_level = MAX_PJ_LOG_MAX_LEVEL;
 			}
 #if HAVE_WORKING_FORK
 		/* Disable forking (-f at startup) */
