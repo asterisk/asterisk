2e137efae6becae3098d5b14dd9d1036d098ca7d
Jonathan R. Rose <jonathan.rose@motorolasolutions.com>
2016-12-19 15:03:52 -0600

    core/pbx: dialplan show - display filename/line#
    Adds the ability for extensions to be registered to include filename and
    line number so that dialplan show output can show the filename and line
    number of a config file responsible for generating a given extension.
    
    This only affects config modules that are written to use the new extension
    registering functions. In this patch, that only includes pbx_config, so
    extensions registered in extensions.conf and any included extension will
    be shown in this manner. Extensions registered in this manner will show
    the filename and line number *instead* of the registrar.
    
    ASTERISK-26658 #close
    Reported by: Jonathan R. Rose
    
    Change-Id: Ieccc6abccdff34ed5c7da3511fd24972b8f2dd30

--- res/parking/parking_bridge_features.c
+++ res/parking/parking_bridge_features.c
@@ -633,7 +633,7 @@ static int parking_duration_callback(struct ast_bridge_channel *bridge_channel, 
 		ast_debug(3, "An extension for '%s@%s' was already registered by another registrar '%s'\n",
 			dial_string_flat, PARK_DIAL_CONTEXT, ast_get_extension_registrar(existing_exten));
 	} else if (ast_add_extension2_nolock(park_dial_context, 1, dial_string_flat, 1, NULL, NULL,
-			"Dial", duplicate_returnexten, ast_free_ptr, BASE_REGISTRAR)) {
+			"Dial", duplicate_returnexten, ast_free_ptr, BASE_REGISTRAR, NULL, 0)) {
 			ast_free(duplicate_returnexten);
 		ast_log(LOG_ERROR, "Failed to create parking redial parker extension %s@%s - Dial(%s)\n",
 			dial_string_flat, PARK_DIAL_CONTEXT, returnexten);
