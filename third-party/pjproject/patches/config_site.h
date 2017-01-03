b0de39a46f02682c4da24f54b4051bb433ed513c
George Joseph <gjoseph@digium.com>
2017-01-03 14:14:09 -0700

    pjproject_bundled:  Compile pjsua with max log level = 2
    A while back, we changed config_site.h to set PJ_LOG_MAX_LEVEL = 6.
    This allowed us to control the log level better from inside Asterisk.
    An unfortunate side effect of this was that the pjsua binary and
    python bindings were also compiled with log level set to 6 so whenever
    a testsuite test that uses pjsua runs, it spits out 6795 lines of
    debug in an instant even before the test starts.  I believe this
    overruns the Jenkins capture buffer and prevents the test from
    properly terminating.  In turn, this results in the testsuite just
    hanging until the job is killed.  It's more frequent on the higher
    end agents because they can spit out the messages faster.
    
    Unfortunately, the messages are all spit out before we have control
    of the python pj.Lib instance where we can set logging levels so the
    only alternative was to actually compile pjsua and _pjsua.so with an
    overridden PJ_LOG_MAX_LEVEL.  Although defining a lower max level was
    done in the Makefile, the define in config_site.h had to be wrapped
    with "#ifndef" so the change would take effect.  NDEBUG and PJ_DEBUG
    were also wrapped in anticipation of future issues.
    
    Change-Id: I2af9e7d48dde1927279c586c9c725d868fe6f3ff

--- third-party/pjproject/patches/config_site.h
+++ third-party/pjproject/patches/config_site.h
@@ -21,7 +21,11 @@
 #define PJMEDIA_HAS_SRTP 0
 
 #define PJ_HAS_IPV6 1
+
+#ifndef NDEBUG
 #define NDEBUG 1
+#endif
+
 #define PJ_MAX_HOSTNAME (256)
 #define PJSIP_MAX_URL_SIZE (512)
 #ifdef PJ_HAS_LINUX_EPOLL
@@ -34,13 +38,21 @@
 
 #define PJ_SCANNER_USE_BITWISE	0
 #define PJ_OS_HAS_CHECK_STACK	0
+
+#ifndef PJ_LOG_MAX_LEVEL
 #define PJ_LOG_MAX_LEVEL		6
+#endif
+
 #define PJ_ENABLE_EXTRA_CHECK	1
 #define PJSIP_MAX_TSX_COUNT		((64*1024)-1)
 #define PJSIP_MAX_DIALOG_COUNT	((64*1024)-1)
 #define PJSIP_UDP_SO_SNDBUF_SIZE	(512*1024)
 #define PJSIP_UDP_SO_RCVBUF_SIZE	(512*1024)
+
+#ifndef PJ_DEBUG
 #define PJ_DEBUG			0
+#endif
+
 #define PJSIP_SAFE_MODULE		0
 #define PJ_HAS_STRICMP_ALNUM		0
 
