c572e172665647abd27ea65a4c45f893161c8c44
Tzafrir Cohen <tzafrir.cohen@xorcom.com>
2017-01-21 07:57:33 +0200

    openssl 1.1 support: use OPENSSL_VERSION_NUMBER
    Use OPENSSL_VERSION_NUMBER instead of OPENSSL_API_COMPAT to detect
    the openssl 1.1 API.
    
    ASTERISK-26109 #close
    
    Change-Id: I4e448f55ef516aedf6ad154037c35577a421a458

--- main/tcptls.c
+++ main/tcptls.c
@@ -399,19 +399,19 @@ static int tcptls_stream_close(void *cookie)
 					SSL_get_error(stream->ssl, res));
 			}
 
-#if defined(OPENSSL_API_COMPAT) && OPENSSL_API_COMPAT >= 0x10100000L
+#if defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER >= 0x10100000L
 			if (!SSL_is_server(stream->ssl)) {
 #else
 			if (!stream->ssl->server) {
 #endif
 				/* For client threads, ensure that the error stack is cleared */
-#if !defined(OPENSSL_API_COMPAT) || OPENSSL_API_COMPAT < 0x10100000L
-#if OPENSSL_VERSION_NUMBER >= 0x10000000L
+#if !defined(OPENSSL_VERSION_NUMBER) || OPENSSL_VERSION_NUMBER < 0x10100000L
+#if !defined(OPENSSL_VERSION_NUMBER) || OPENSSL_VERSION_NUMBER >= 0x10000000L
 				ERR_remove_thread_state(NULL);
 #else
 				ERR_remove_state(0);
-#endif	/* OPENSSL_VERSION_NUMBER >= 0x10000000L */
-#endif  /* !defined(OPENSSL_API_COMPAT) || OPENSSL_API_COMPAT < 0x10100000L */
+#endif	/* openssl = 1.0 */
+#endif  /* openssl < 1.1 */
 			}
 
 			SSL_free(stream->ssl);
