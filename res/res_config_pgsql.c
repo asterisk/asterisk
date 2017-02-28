8c8bc89daa901bc32779a75fce4e2ab0ffd786c7
Sean Bright <sean.bright@gmail.com>
2017-02-28 10:41:45 -0500

    res_config_pgsql: Release table locks where appropriate
    The find_table() functions NULL or a locked table pointer. We are
    not consistently calling release_table() in failure paths.
    
    Change-Id: I6f665b455799c84b036e5b34904b82b05eab9544

--- res/res_config_pgsql.c
+++ res/res_config_pgsql.c
@@ -735,6 +735,7 @@ static int update_pgsql(const char *database, const char *tablename, const char 
 	ast_mutex_lock(&pgsql_lock);
 	if (!pgsql_reconnect(database)) {
 		ast_mutex_unlock(&pgsql_lock);
+		release_table(table);
 		return -1;
 	}
 
@@ -880,6 +881,7 @@ static int update2_pgsql(const char *database, const char *tablename, const stru
 	ast_mutex_lock(&pgsql_lock);
 	if (!pgsql_reconnect(database)) {
 		ast_mutex_unlock(&pgsql_lock);
+		release_table(table);
 		return -1;
 	}
 
@@ -1324,6 +1326,7 @@ static int require_pgsql(const char *database, const char *tablename, va_list ap
 
 			        if (pgsql_exec(database, tablename, ast_str_buffer(sql), &result) != 0) {
 					ast_mutex_unlock(&pgsql_lock);
+					release_table(table);
 				        return -1;
 			        }
 
