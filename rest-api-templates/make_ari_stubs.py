063c9a935f2133e1726ce0851935745118ca04cd
George Joseph <gjoseph@digium.com>
2017-07-21 10:17:38 -0600

    Update make_ari_stubs in master to make the version 16
    Ready for next major version
    
    Change-Id: If9dc99b3b78768529e69a297d8f87e23582ca6d0

--- rest-api-templates/make_ari_stubs.py
+++ rest-api-templates/make_ari_stubs.py
@@ -41,7 +41,7 @@ def rel(file):
     """
     return os.path.join(TOPDIR, file)
 
-WIKI_PREFIX = 'Asterisk 13'
+WIKI_PREFIX = 'Asterisk 16'
 
 API_TRANSFORMS = [
     Transform(rel('api.wiki.mustache'),
