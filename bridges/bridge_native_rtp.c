6b7d5671d1c8b12f1fc968eeb34898e762c1fb1f
Jean Aunis <jean.aunis@prescom.fr>
2017-09-07 11:41:09 +0200

    bridge : Fix one-way direct-media when early bridging with native_rtp
    When two channels were early bridged in a native_rtp bridge, the RTP description
    on one side was not updated when the other side answered.
    This patch forbids non-answered channels to enter a native_rtp bridge, and
    triggers a bridge reconfiguration when an ANSWER frame is received.
    
    ASTERISK-27257
    
    Change-Id: If1aaee1b4ed9658a1aa91ab715ee0a6413b878df

--- bridges/bridge_native_rtp.c
+++ bridges/bridge_native_rtp.c
@@ -598,7 +598,8 @@ static int native_rtp_framehook_consume(void *data, enum ast_frame_type type)
  */
 static int native_rtp_bridge_capable(struct ast_channel *chan)
 {
-	return !ast_channel_has_hook_requiring_audio(chan);
+	return !ast_channel_has_hook_requiring_audio(chan)
+			&& ast_channel_state(chan) == AST_STATE_UP;
 }
 
 /*!
