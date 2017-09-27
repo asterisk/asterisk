47620ea8626fa45929b212c18b8f20954ee1376c
George Joseph <gjoseph@digium.com>
2017-09-27 12:45:21 -0600

    logger:  Bring back ability to  turn debug on by source file
    Somewhere along the way we lost the ability to debug individual
    source files.  For modules, this wasn't a big deal but all the
    source files in ./main are in the one "core" module so debugging
    individual core capabilities was almost impossible.
    
    * Added a test to DEBUG_ATLEAST that also checks __FILE__ instead
    of just module name.  Any source file will work even if it's in
    a module subdirectory.
    
    Change-Id: Icc0af41837f3b1679dec7af21fa32cd1f7469f6e

--- include/asterisk/logger.h
+++ include/asterisk/logger.h
@@ -447,7 +447,9 @@ void ast_callid_strnprint(char *buffer, size_t buffer_size, struct ast_callid *c
 
 #define DEBUG_ATLEAST(level) \
 	(option_debug >= (level) \
-		|| (ast_opt_dbg_module && (int)ast_debug_get_by_module(AST_MODULE) >= (level)))
+		|| (ast_opt_dbg_module \
+        	&& ((int)ast_debug_get_by_module(AST_MODULE) >= (level) \
+				|| (int)ast_debug_get_by_module(__FILE__) >= (level))))
 
 /*!
  * \brief Log a DEBUG message
