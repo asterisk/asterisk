e8c9cb6c40790f0c54eae1ef70841e9545147e53
Corey Farrell <git@cfware.com>
2017-11-20 14:08:57 -0500

    res_mwi_external_ami: Remove incorrect load priority.
    res_mwi_external_ami specified AST_MODFLAG_LOAD_ORDER but didn't set
    load_pri, resulting in an actual load priority of 0.  This module only
    provides AMI actions so it has no reason to load early.
    
    Change-Id: I82987fcf10d3ea42716b2f9df915b16687fd5839

--- res/res_mwi_external_ami.c
+++ res/res_mwi_external_ami.c
@@ -366,7 +366,7 @@ static int load_module(void)
 	return AST_MODULE_LOAD_SUCCESS;
 }
 
-AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_LOAD_ORDER, "AMI support for external MWI",
+AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_DEFAULT, "AMI support for external MWI",
 	.support_level = AST_MODULE_SUPPORT_CORE,
 	.load = load_module,
 	.unload = unload_module,
