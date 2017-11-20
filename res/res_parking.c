8e1506154fb2e17645dca632ef4d805ac5ef94e6
Corey Farrell <git@cfware.com>
2017-11-20 14:05:46 -0500

    res_parking: Set load_pri more appropriately.
    res_parking had an inplicit load_pri of 0 meaning it was one of the very
    first modules loaded after modules with global symbols.  Set it to
    AST_MODPRI_DEVSTATE_PROVIDER as it provides device state for parking
    lots.
    
    Change-Id: I297b6fb3ff6993ec004e667b22a74f5925906259

--- res/res_parking.c
+++ res/res_parking.c
@@ -1293,4 +1293,5 @@ AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_LOAD_ORDER, "Call Parking Resource
 	.load = load_module,
 	.unload = unload_module,
 	.reload = reload_module,
+	.load_pri = AST_MODPRI_DEVSTATE_PROVIDER,
 );
