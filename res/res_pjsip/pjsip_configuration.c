c3bc44fa1bfe40b027788c338aea1b112bb8049f
Sean Bright <sean.bright@gmail.com>
2017-12-08 15:58:54 -0500

    pjsip_configuration: Add correct file header
    Change-Id: I25348c386a222bb704aff07f54375108a6402906

--- res/res_pjsip/pjsip_configuration.c
+++ res/res_pjsip/pjsip_configuration.c
@@ -1,8 +1,19 @@
 /*
- * sip_cli_commands.c
+ * Asterisk -- An open source telephony toolkit.
  *
- *  Created on: Jan 25, 2013
- *      Author: mjordan
+ * Copyright (C) 2013, Digium, Inc.
+ *
+ * Matt Jordan <mjordan@digium.com>
+ *
+ * See http://www.asterisk.org for more information about
+ * the Asterisk project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
  */
 
 #include "asterisk.h"
