3d79c34350b9cadd5be274218907411329032a21
Corey Farrell <git@cfware.com>
2017-12-07 20:41:14 -0500

    res_stasis and res_speech: Fix load order.
    res_stasis was missing AST_MODFLAG_LOAD_ORDER.  Set res_stasis and
    res_speech to start at (AST_MODPRI_APP_DEPEND - 1) so they are ready for
    dependent modules.
    
    Change-Id: I27f4f3810a95b6be8a5bfbf62be2ace6bfab6ff3

--- res/res_speech.c
+++ res/res_speech.c
@@ -365,5 +365,5 @@ AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_GLOBAL_SYMBOLS | AST_MODFLAG_LOAD_
 		.support_level = AST_MODULE_SUPPORT_CORE,
 		.load = load_module,
 		.unload = unload_module,
-		.load_pri = AST_MODPRI_APP_DEPEND,
+		.load_pri = AST_MODPRI_APP_DEPEND - 1,
 		);
