e3c9314a2ed93147475d3f04ab9d58b0474c9356
Corey Farrell <git@cfware.com>
2018-01-03 20:07:44 -0500

    func_odbc: Add missing unlock's to acf_odbc_read.
    Change-Id: I828329ecbd252ae8f27a369a046d2b03102b07c6

--- funcs/func_odbc.c
+++ funcs/func_odbc.c
@@ -791,6 +791,7 @@ static int acf_odbc_read(struct ast_channel *chan, const char *cmd, char *s, cha
 		if (!(resultset = ast_calloc(1, sizeof(*resultset)))) {
 			pbx_builtin_setvar_helper(chan, "ODBCROWS", rowcount);
 			pbx_builtin_setvar_helper(chan, "ODBCSTATUS", status);
+			AST_RWLIST_UNLOCK(&queries);
 			ast_autoservice_stop(chan);
 			return -1;
 		}
@@ -807,6 +808,7 @@ static int acf_odbc_read(struct ast_channel *chan, const char *cmd, char *s, cha
 			if (!(resultset = ast_calloc(1, sizeof(*resultset)))) {
 				pbx_builtin_setvar_helper(chan, "ODBCROWS", rowcount);
 				pbx_builtin_setvar_helper(chan, "ODBCSTATUS", status);
+				AST_RWLIST_UNLOCK(&queries);
 				ast_autoservice_stop(chan);
 				return -1;
 			}
