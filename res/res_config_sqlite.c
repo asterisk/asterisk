e6738b79b3b610d8332aa4de7c137295adce9f64
Richard Mudgett <rmudgett@digium.com>
2018-03-08 13:53:09 -0600

    Complete deprecating legacy modules.
    The menuselect comment was updated to deprecate these modules but the
    AST_MODULE_INFO block at the end of file was missed.
    
    ASTERISK-27671
    
    Change-Id: I63070b5c4d4f08af010c6034acd4793c1bcef839

--- res/res_config_sqlite.c
+++ res/res_config_sqlite.c
@@ -1773,7 +1773,7 @@ static int load_module(void)
 }
 
 AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_LOAD_ORDER, "Realtime SQLite configuration",
-	.support_level = AST_MODULE_SUPPORT_EXTENDED,
+	.support_level = AST_MODULE_SUPPORT_DEPRECATED,
 	.load = load_module,
 	.unload = unload_module,
 	.load_pri = AST_MODPRI_REALTIME_DRIVER,
