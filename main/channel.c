7d874c1af78a74efa226bab9501d5ba63a907ab7
Richard Mudgett <rmudgett@digium.com>
2018-06-13 13:05:03 -0500

    AMI PlayDTMF Action: Make not compete with channel's media thread.
    There can be one and only one thread handling a channel's media at a time.
    Otherwise, we don't know which thread is going to handle the media frames.
    
    ASTERISK-27625
    
    Change-Id: Ia341f1a6f4d54f2022261abec9021fe5b2eb4905

--- main/channel.c
+++ main/channel.c
@@ -4810,12 +4810,28 @@ int ast_senddigit_end(struct ast_channel *chan, char digit, unsigned int duratio
 
 int ast_senddigit(struct ast_channel *chan, char digit, unsigned int duration)
 {
+	if (duration < AST_DEFAULT_EMULATE_DTMF_DURATION) {
+		duration = AST_DEFAULT_EMULATE_DTMF_DURATION;
+	}
 	if (ast_channel_tech(chan)->send_digit_begin) {
 		ast_senddigit_begin(chan, digit);
-		ast_safe_sleep(chan, (duration >= AST_DEFAULT_EMULATE_DTMF_DURATION ? duration : AST_DEFAULT_EMULATE_DTMF_DURATION));
+		ast_safe_sleep(chan, duration);
 	}
 
-	return ast_senddigit_end(chan, digit, (duration >= AST_DEFAULT_EMULATE_DTMF_DURATION ? duration : AST_DEFAULT_EMULATE_DTMF_DURATION));
+	return ast_senddigit_end(chan, digit, duration);
+}
+
+int ast_senddigit_external(struct ast_channel *chan, char digit, unsigned int duration)
+{
+	if (duration < AST_DEFAULT_EMULATE_DTMF_DURATION) {
+		duration = AST_DEFAULT_EMULATE_DTMF_DURATION;
+	}
+	if (ast_channel_tech(chan)->send_digit_begin) {
+		ast_senddigit_begin(chan, digit);
+		usleep(duration * 1000);
+	}
+
+	return ast_senddigit_end(chan, digit, duration);
 }
 
 int ast_prod(struct ast_channel *chan)
