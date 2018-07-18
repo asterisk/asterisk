cf63a4b0c7c3cefa4d9fe3ee84d640d6394e4963
Chris-Savinovich <csavinovich@digium.com>
2018-07-18 12:34:04 -0400

    stasis: Improve message type "Use of before/init after destruction"
    Fixes issue where error msg
    "Use of before/init after destruction"
    was being printed on disabled messages
    in dev mode.  With this
    fix if message is disabled
    a warning will print.
    
    ASTERISK-25548
    Change-Id: Ie0d866d1cbc60c16dbef08bc65e99505c3c1adfa

--- main/stasis.c
+++ main/stasis.c
@@ -1199,7 +1199,9 @@ struct stasis_topic *stasis_topic_pool_get_topic(struct stasis_topic_pool *pool,
 void stasis_log_bad_type_access(const char *name)
 {
 #ifdef AST_DEVMODE
-	ast_log(LOG_ERROR, "Use of %s() before init/after destruction\n", name);
+	if (!stasis_message_type_declined(name)) {
+		ast_log(LOG_ERROR, "Use of %s() before init/after destruction\n", name);
+	}
 #endif
 }
 
