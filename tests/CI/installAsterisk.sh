914b9e193451bdfbf2037603c650c83f4af3a419
George Joseph <gjoseph@digium.com>
2018-07-27 12:23:02 -0600

    CI: Add optional uninstall step before installing asterisk
    Change-Id: I7dedf1e925eafc3a0adf01dd9dfbe44eb642aab7

--- tests/CI/installAsterisk.sh
+++ tests/CI/installAsterisk.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 CIDIR=$(dirname $(readlink -fn $0))
+UNINSTALL=0
+UNINSTALL_ALL=0
 source $CIDIR/ci.functions
 
 MAKE=`which make`
@@ -9,6 +11,9 @@ if [ x"$DESTDIR" != x ] ; then
 	mkdir -p "$DESTDIR"
 fi
 destdir=${DESTDIR:+DESTDIR=$DESTDIR}
+
+[ $UNINSTALL -gt 0 ] && ${MAKE} ${destdir} uninstall
+[ $UNINSTALL_ALL -gt 0 ] && ${MAKE} ${destdir} uninstall-all
 
 ${MAKE} ${destdir} install || ${MAKE} ${destdir} NOISY_BUILD=yes install || exit 1
 ${MAKE} ${destdir} samples
