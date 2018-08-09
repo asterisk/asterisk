a1b0db826a2bbbd185fe69c755f55c1c401a5562
Torrey Searle <torrey@voxbone.com>
2018-08-09 09:34:17 +0200

    res/res_pjsip_sdp_rtp:  put rtcp-mux in answer only if offered
    If in the initial sdp the caller doesn't include the line
    a=rtcp-mux
    
    Then asterisk shoud not include rtcp-mux in the response regardless
    of rtcp-mux being enabled on the endpoint
    
    ASTERISK-28007 #close
    
    Change-Id: I58e9b9f40a139afc0da5de41906cc608fb62adc7

--- res/res_pjsip_session.c
+++ res/res_pjsip_session.c
@@ -1342,6 +1342,7 @@ static int add_session_media(void *obj, void *arg, int flags)
 	}
 	session_media->encryption = session->endpoint->media.rtp.encryption;
 	session_media->remote_ice = session->endpoint->media.rtp.ice_support;
+	session_media->remote_rtcp_mux = session->endpoint->rtcp_mux;
 	session_media->keepalive_sched_id = -1;
 	session_media->timeout_sched_id = -1;
 	/* Safe use of strcpy */
