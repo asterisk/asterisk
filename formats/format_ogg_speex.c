1fad6b907980f66e87c21851ea6b54e0d0a148ac
Richard Mudgett <rmudgett@digium.com>
2018-10-17 14:34:55 -0500

    modules: Add missing run time module support levels.
    Change-Id: I29b9dbfa4bbfc49f21eba356858e38b1d3041824

--- formats/format_ogg_speex.c
+++ formats/format_ogg_speex.c
@@ -338,6 +338,7 @@ static int load_module(void)
 }
 
 AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_LOAD_ORDER, "OGG/Speex audio",
+	.support_level = AST_MODULE_SUPPORT_EXTENDED,
 	.load = load_module,
 	.unload = unload_module,
 	.load_pri = AST_MODPRI_APP_DEPEND
