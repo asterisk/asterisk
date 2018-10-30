719daf32990e7fa89d05f09f153b1508cb574c0c
Richard Mudgett <rmudgett@digium.com>
2018-10-30 10:52:28 -0500

    alembic: Fix use_callerid_contact option add script.
    ASTERISK-28087
    
    Change-Id: I046d018015427d0916fab571b5a4f5367476f729

--- contrib/ast-db-manage/config/versions/2bb1a85135ad_pjsip_add_use_callerid_contact.py
+++ contrib/ast-db-manage/config/versions/2bb1a85135ad_pjsip_add_use_callerid_contact.py
@@ -14,21 +14,26 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects.postgresql import ENUM
 
-YESNO_NAME = 'yesno_values'
-YESNO_VALUES = ['yes', 'no']
+AST_BOOL_NAME = 'ast_bool_values'
+# We'll just ignore the n/y and f/t abbreviations as Asterisk does not write
+# those aliases.
+AST_BOOL_VALUES = [ '0', '1',
+                    'off', 'on',
+                    'false', 'true',
+                    'no', 'yes' ]
 
 
 def upgrade():
     ############################# Enums ##############################
 
-    # yesno_values have already been created, so use postgres enum object
+    # ast_bool_values has already been created, so use postgres enum object
     # type to get around "already created" issue - works okay with mysql
-    yesno_values = ENUM(*YESNO_VALUES, name=YESNO_NAME, create_type=False)
+    ast_bool_values = ENUM(*AST_BOOL_VALUES, name=AST_BOOL_NAME, create_type=False)
 
     op.add_column('ps_globals', sa.Column('use_callerid_contact', yesno_values))
 
 
 def downgrade():
     if op.get_context().bind.dialect.name == 'mssql':
-        op.drop_constraint('ck_ps_globals_use_callerid_contact_yesno_values','ps_globals')
+        op.drop_constraint('ck_ps_globals_use_callerid_contact_ast_bool_values', 'ps_globals')
     op.drop_column('ps_globals', 'use_callerid_contact')
