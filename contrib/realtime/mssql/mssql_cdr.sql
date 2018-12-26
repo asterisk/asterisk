7ac8bf4698653328dc6c0916dda6213d30912fa7
Asterisk Development Team <asteriskteam@digium.com>
2018-12-26 13:45:13 -0500

    Update for 16.1.1

--- contrib/realtime/mssql/mssql_cdr.sql
+++ contrib/realtime/mssql/mssql_cdr.sql
@@ -1,7 +1,8 @@
 BEGIN TRANSACTION;
 
 CREATE TABLE alembic_version (
-    version_num VARCHAR(32) NOT NULL
+    version_num VARCHAR(32) NOT NULL, 
+    CONSTRAINT alembic_version_pkc PRIMARY KEY (version_num)
 );
 
 GO
