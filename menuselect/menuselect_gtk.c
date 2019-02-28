625af27a28d68f53e1974caf4c5d6ea1bf05a289
Sean Bright <sean.bright@gmail.com>
2019-02-28 16:36:36 -0500

    menuselect: Add license header to menuselect_gtk.c
    This file was added to the Subversion repository on 2007-03-15 by
    Russell Bryant, a Digium employee at the time.
    
    ASTERISK-24173 #close
    
    Change-Id: Ie866fa9d31d550467613d362b35b03c031ee594d

--- menuselect/menuselect_gtk.c
+++ menuselect/menuselect_gtk.c
@@ -1,3 +1,29 @@
+/*
+ * Asterisk -- An open source telephony toolkit.
+ *
+ * Copyright (C) 2007, Russell Bryant
+ *
+ * Russell Bryant <russell@digium.com>
+ *
+ * See http://www.asterisk.org for more information about
+ * the Asterisk project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
+ */
+
+/*
+ * \file
+ *
+ * \author Russell Bryant <russell@digium.com>
+ *
+ * \brief GTK 2.x frontend for selection maintenance
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
