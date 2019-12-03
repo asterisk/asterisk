9c9296c635314d671897cd601377cc66982599b0
Jean Aunis <jean.aunis@prescom.fr>
2019-12-03 12:58:26 +0100

    chan_sip: voice frames are no longer transmitted after emitting a COLP
    The SIP transaction state was reset when emitting an UPDATE or a re-INVITE
    related to a COLP, preventing RTP packets to be emitted.
    
    ASTERISK-28647
    
    Change-Id: Ie7a30fa7a97f711e7ba6cc17f221a0993d48bd8b

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -15746,7 +15746,6 @@ static void update_connectedline(struct sip_pvt *p, const void *data, size_t dat
 			initialize_initreq(p, &req);
 			p->lastinvite = p->ocseq;
 			ast_set_flag(&p->flags[0], SIP_OUTGOING);
-			p->invitestate = INV_CALLING;
 			send_request(p, &req, XMIT_CRITICAL, p->ocseq);
 		} else if ((is_method_allowed(&p->allowed_methods, SIP_UPDATE)) && (!ast_strlen_zero(p->okcontacturi))) {
 			reqprep(&req, p, SIP_UPDATE, 0, 1);
