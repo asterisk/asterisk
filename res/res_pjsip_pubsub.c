e14fd3b22a832f1e49fbdf340727c12f0c0d2929
Joshua C. Colp <jcolp@sangoma.com>
2020-01-28 15:18:45 +0000

    res_pjsip_pubsub: Increment persistence data ref when recreating.
    Each subscription needs to have a reference to the persisted data
    for it, as well as the main JSON contained within the tree. When
    recreating a subscription this did not occur and they both shared
    the same reference.
    
    ASTERISK-28714
    
    Change-Id: I706abd49ea182ea367a4ac3feca2706460ae9f4a

--- res/res_pjsip_pubsub.c
+++ res/res_pjsip_pubsub.c
@@ -1287,7 +1287,7 @@ static struct ast_sip_subscription *allocate_subscription(const struct ast_sip_s
 	 */
 
 	if (tree->persistence && tree->persistence->generator_data) {
-		sub->persistence_data = ast_json_object_get(tree->persistence->generator_data, resource);
+		sub->persistence_data = ast_json_ref(ast_json_object_get(tree->persistence->generator_data, resource));
 	}
 
 	sub->handler = handler;
