1c8c560f39b29ee70d11b52e0e73b55e2c2f3432
Torrey Searle <torrey@voxbone.com>
2020-02-24 16:00:08 +0100

    res/res_pjsip_sdp_rtp: Fix MOH transitions
    Update the state of remote_hold immediately on receipt of remote
    SDP so that the information is available when building the SDP
    answer
    
    ASTERISK-28754 #close
    
    Change-Id: I7026032a807e9c95081cb8f060400b05deb4836f

--- include/asterisk/res_pjsip_session.h
+++ include/asterisk/res_pjsip_session.h
@@ -95,6 +95,8 @@ struct ast_sip_session_media {
 	unsigned int remote_rtcp_mux:1;
 	/*! \brief Does remote support ice */
 	unsigned int remote_ice:1;
+	/*! \brief Stream is held by remote side changed during this negotiation*/
+	unsigned int remotely_held_changed:1;
 	/*! \brief Media type of this session media */
 	enum ast_media_type type;
 	/*! \brief The write callback when writing frames */
