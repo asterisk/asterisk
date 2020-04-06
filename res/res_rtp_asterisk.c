1ef1b1b0c2df9c4e8251f8854968863715cb64c8
traud <pabstraud@compuserve.com>
2020-04-06 15:00:10 +0200

    res_rtp_asterisk: Avoid absolute value on unsigned subtraction.
    ASTERISK-28809
    
    Change-Id: I269731715347c8e5ef7db1b6ffd3f8d15fc04be4

--- res/res_rtp_asterisk.c
+++ res/res_rtp_asterisk.c
@@ -7750,7 +7750,7 @@ static struct ast_frame *ast_rtp_read(struct ast_rtp_instance *instance, int rtc
 		}
 
 		return AST_LIST_FIRST(&frames);
-	} else if (((abs(seqno - rtp->expectedrxseqno) > 100) && timestamp > rtp->lastividtimestamp) ||
+	} else if ((((seqno - rtp->expectedrxseqno) > 100) && timestamp > rtp->lastividtimestamp) ||
 		ast_data_buffer_count(rtp->recv_buffer) == ast_data_buffer_max(rtp->recv_buffer)) {
 		int inserted = 0;
 
