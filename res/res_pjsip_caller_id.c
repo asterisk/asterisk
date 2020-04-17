008f46bf1e5822d64d9f192aa2367e9f2fea1a09
Alexander Traud <pabstraud@compuserve.com>
2020-04-17 11:47:01 +0200

    res_pjsip: Sync load- and build-time deps.
    MODULEINFO is checked while buidling/linking the module.
    AST_MODULE_INFO is checked while loading/running the module.
    
    ASTERISK-28838
    
    Change-Id: I4bb868532ca217fec1351885d99eb55c21b58042

--- res/res_pjsip_caller_id.c
+++ res/res_pjsip_caller_id.c
@@ -19,6 +19,7 @@
 /*** MODULEINFO
 	<depend>pjproject</depend>
 	<depend>res_pjsip</depend>
+	<depend>res_pjsip_session</depend>
 	<support_level>core</support_level>
  ***/
 
@@ -794,5 +795,5 @@ AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_LOAD_ORDER, "PJSIP Caller ID Suppo
 	.load = load_module,
 	.unload = unload_module,
 	.load_pri = AST_MODPRI_APP_DEPEND,
-	.requires = "res_pjsip",
+	.requires = "res_pjsip,res_pjsip_session",
 );
