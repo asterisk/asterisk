52d6a982290195127deb6103c3d9d0e363649cab
Dioris Moreno <dmoreno@libersys.io>
2020-05-12 14:39:39 -0500

    Module setup

--- res/res_pjsip_json_info.c
+++ res/res_pjsip_json_info.c
@@ -44,24 +44,33 @@ static void send_response(struct ast_sip_session *session,
 	}
 }
 
+static int is_json_type(pjsip_rx_data *rdata, char *subtype)
+{
+	return rdata->msg_info.ctype
+		&& !pj_strcmp2(&rdata->msg_info.ctype->media.type, "application")
+		&& !pj_strcmp2(&rdata->msg_info.ctype->media.subtype, subtype);
+}
+
 static int json_info_incoming_request(struct ast_sip_session *session,
 		struct pjsip_rx_data *rdata)
 {
-	if (!rdata->msg_info.ctype) {
-		/* Need to return 200 OK on empty body */
-		/* Some SBCs use empty INFO as a KEEPALIVE */
-		send_response(session, rdata, 200);
-		return 1;
+
+	if (!is_json_type(rdata, "json")) {
+		/* Let another module respond */
+		return 0;
 	}
 
-	/* Let another module respond */
-	return 0;
+	ast_log(LOG_NOTICE, "Received SIP INFO from channel %s\n", ast_channel_name(session->channel));
+
+	/* Need to return 200 OK */
+	send_response(session, rdata, 200);
+	return 1;
 
 }
 
 static struct ast_sip_session_supplement json_info_supplement = {
 	.method = "INFO",
-	.priority = AST_SIP_SUPPLEMENT_PRIORITY_LAST,
+	.priority = AST_SIP_SUPPLEMENT_PRIORITY_FIRST,
 	.incoming_request = json_info_incoming_request,
 };
 
@@ -84,3 +93,174 @@ AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_LOAD_ORDER, "PJSIP JSON INFO Suppo
 	.load_pri = AST_MODPRI_APP_DEPEND,
 	.requires = "res_pjsip,res_pjsip_session",
 );
+
+
+	// if (!strcmp(event, "presence") || !strcmp(event, "dialog")) { /* Presence, RFC 3842 */
+	// 	int gotdest;
+	// 	const char *accept;
+	// 	int start = 0;
+	// 	enum subscriptiontype subscribed = NONE;
+	// 	const char *unknown_accept = NULL;
+
+    //             /* Get destination right away */
+    //             gotdest = get_destination(p, NULL, NULL);
+	// 	if (gotdest != SIP_GET_DEST_EXTEN_FOUND) {
+	// 		if (gotdest == SIP_GET_DEST_INVALID_URI) {
+	// 			transmit_response(p, "416 Unsupported URI scheme", req);
+	// 		} else {
+	// 			transmit_response(p, "404 Not Found", req);
+	// 		}
+	// 		pvt_set_needdestroy(p, "subscription target not found");
+	// 		if (authpeer) {
+	// 			sip_unref_peer(authpeer, "sip_unref_peer, from handle_request_subscribe (authpeer 2)");
+	// 		}
+	// 		return 0;
+	// 	}
+
+	// 	/* Header from Xten Eye-beam Accept: multipart/related, application/rlmi+xml, application/pidf+xml, application/xpidf+xml */
+	// 	accept = __get_header(req, "Accept", &start);
+	// 	while ((subscribed == NONE) && !ast_strlen_zero(accept)) {
+	// 		if (strstr(accept, "application/pidf+xml")) {
+	// 			if (strstr(p->useragent, "Polycom")) {
+	// 				subscribed = XPIDF_XML; /* Older versions of Polycom firmware will claim pidf+xml, but really they only support xpidf+xml */
+	// 			} else {
+	// 				subscribed = PIDF_XML; /* RFC 3863 format */
+	// 			}
+	// 		} else if (strstr(accept, "application/dialog-info+xml")) {
+	// 			subscribed = DIALOG_INFO_XML;
+	// 			/* IETF draft: draft-ietf-sipping-dialog-package-05.txt */
+	// 		} else if (strstr(accept, "application/cpim-pidf+xml")) {
+	// 			subscribed = CPIM_PIDF_XML;    /* RFC 3863 format */
+	// 		} else if (strstr(accept, "application/xpidf+xml")) {
+	// 			subscribed = XPIDF_XML;        /* Early pre-RFC 3863 format with MSN additions (Microsoft Messenger) */
+	// 		} else {
+	// 			unknown_accept = accept;
+	// 		}
+	// 		/* check to see if there is another Accept header present */
+	// 		accept = __get_header(req, "Accept", &start);
+	// 	}
+
+	// 	if (!start) {
+	// 		if (p->subscribed == NONE) { /* if the subscribed field is not already set, and there is no accept header... */
+	// 			transmit_response(p, "489 Bad Event", req);
+	// 			ast_log(LOG_WARNING,"SUBSCRIBE failure: no Accept header: pvt: "
+	// 				"stateid: %d, laststate: %d, dialogver: %u, subscribecont: "
+	// 				"'%s', subscribeuri: '%s'\n",
+	// 				p->stateid,
+	// 				p->laststate,
+	// 				p->dialogver,
+	// 				p->subscribecontext,
+	// 				p->subscribeuri);
+	// 			pvt_set_needdestroy(p, "no Accept header");
+	// 			if (authpeer) {
+	// 				sip_unref_peer(authpeer, "sip_unref_peer, from handle_request_subscribe (authpeer 2)");
+	// 			}
+	// 			return 0;
+	// 		}
+	// 		/* if p->subscribed is non-zero, then accept is not obligatory; according to rfc 3265 section 3.1.3, at least.
+	// 		   so, we'll just let it ride, keeping the value from a previous subscription, and not abort the subscription */
+	// 	} else if (subscribed == NONE) {
+	// 		/* Can't find a format for events that we know about */
+	// 		char buf[200];
+
+	// 		if (!ast_strlen_zero(unknown_accept)) {
+	// 			snprintf(buf, sizeof(buf), "489 Bad Event (format %s)", unknown_accept);
+	// 		} else {
+	// 			snprintf(buf, sizeof(buf), "489 Bad Event");
+	// 		}
+	// 		transmit_response(p, buf, req);
+	// 		ast_log(LOG_WARNING,"SUBSCRIBE failure: unrecognized format:"
+	// 			"'%s' pvt: subscribed: %d, stateid: %d, laststate: %d,"
+	// 			"dialogver: %u, subscribecont: '%s', subscribeuri: '%s'\n",
+	// 			unknown_accept,
+	// 			(int)p->subscribed,
+	// 			p->stateid,
+	// 			p->laststate,
+	// 			p->dialogver,
+	// 			p->subscribecontext,
+	// 			p->subscribeuri);
+	// 		pvt_set_needdestroy(p, "unrecognized format");
+	// 		if (authpeer) {
+	// 			sip_unref_peer(authpeer, "sip_unref_peer, from handle_request_subscribe (authpeer 2)");
+	// 		}
+	// 		return 0;
+	// 	} else {
+	// 		p->subscribed = subscribed;
+	// 	}
+	// } else if (!strcmp(event, "message-summary")) {
+	// 	int start = 0;
+	// 	int found_supported = 0;
+	// 	const char *accept;
+
+	// 	accept = __get_header(req, "Accept", &start);
+	// 	while (!found_supported && !ast_strlen_zero(accept)) {
+	// 		found_supported = strcmp(accept, "application/simple-message-summary") ? 0 : 1;
+	// 		if (!found_supported) {
+	// 			ast_debug(3, "Received SIP mailbox subscription for unknown format: %s\n", accept);
+	// 		}
+	// 		accept = __get_header(req, "Accept", &start);
+	// 	}
+	// 	/* If !start, there is no Accept header at all */
+	// 	if (start && !found_supported) {
+	// 		/* Format requested that we do not support */
+	// 		transmit_response(p, "406 Not Acceptable", req);
+	// 		ast_debug(2, "Received SIP mailbox subscription for unknown format: %s\n", accept);
+	// 		pvt_set_needdestroy(p, "unknown format");
+	// 		if (authpeer) {
+	// 			sip_unref_peer(authpeer, "sip_unref_peer, from handle_request_subscribe (authpeer 3)");
+	// 		}
+	// 		return 0;
+	// 	}
+	// 	/* Looks like they actually want a mailbox status
+	// 	  This version of Asterisk supports mailbox subscriptions
+	// 	  The subscribed URI needs to exist in the dial plan
+	// 	  In most devices, this is configurable to the voicemailmain extension you use
+	// 	*/
+	// 	if (!authpeer || AST_LIST_EMPTY(&authpeer->mailboxes)) {
+	// 		if (!authpeer) {
+	// 			transmit_response(p, "404 Not found", req);
+	// 		} else {
+	// 			transmit_response(p, "404 Not found (no mailbox)", req);
+	// 			ast_log(LOG_NOTICE, "Received SIP subscribe for peer without mailbox: %s\n", S_OR(authpeer->name, ""));
+	// 		}
+	// 		pvt_set_needdestroy(p, "received 404 response");
+
+	// 		if (authpeer) {
+	// 			sip_unref_peer(authpeer, "sip_unref_peer, from handle_request_subscribe (authpeer 3)");
+	// 		}
+	// 		return 0;
+	// 	}
+
+	// 	p->subscribed = MWI_NOTIFICATION;
+	// 	if (ast_test_flag(&authpeer->flags[1], SIP_PAGE2_SUBSCRIBEMWIONLY)) {
+	// 		ao2_unlock(p);
+	// 		add_peer_mwi_subs(authpeer);
+	// 		ao2_lock(p);
+	// 	}
+	// 	if (authpeer->mwipvt != p) {	/* Destroy old PVT if this is a new one */
+	// 		/* We only allow one subscription per peer */
+	// 		if (authpeer->mwipvt) {
+	// 			dialog_unlink_all(authpeer->mwipvt);
+	// 			authpeer->mwipvt = dialog_unref(authpeer->mwipvt, "unref dialog authpeer->mwipvt");
+	// 		}
+	// 		authpeer->mwipvt = dialog_ref(p, "setting peers' mwipvt to p");
+	// 	}
+
+	// 	if (p->relatedpeer != authpeer) {
+	// 		if (p->relatedpeer) {
+	// 			sip_unref_peer(p->relatedpeer, "Unref previously stored relatedpeer ptr");
+	// 		}
+	// 		p->relatedpeer = sip_ref_peer(authpeer, "setting dialog's relatedpeer pointer");
+	// 	}
+	// 	/* Do not release authpeer here */
+	// } else if (!strcmp(event, "call-completion")) {
+	// 	handle_cc_subscribe(p, req);
+	// } else { /* At this point, Asterisk does not understand the specified event */
+	// 	transmit_response(p, "489 Bad Event", req);
+	// 	ast_debug(2, "Received SIP subscribe for unknown event package: %s\n", event);
+	// 	pvt_set_needdestroy(p, "unknown event package");
+	// 	if (authpeer) {
+	// 		sip_unref_peer(authpeer, "sip_unref_peer, from handle_request_subscribe (authpeer 5)");
+	// 	}
+	// 	return 0;
+	// }
