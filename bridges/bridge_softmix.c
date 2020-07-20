82178a02950e36f46f45cf02871ad3bf8e1eeed3
Dioris Moreno <dmoreno@libersys.io>
2020-07-20 01:43:25 -0500

    Exp

--- bridges/bridge_softmix.c
+++ bridges/bridge_softmix.c
@@ -321,6 +321,12 @@ static void set_softmix_bridge_data(int rate, int interval, struct ast_bridge_ch
 
 	slin_format = ast_format_cache_get_slin_by_rate(rate);
 
+	ast_debug(1, "Adding channel %s, rate=%s, slin_format=%s, rawread=%s \n",
+		ast_channel_name(bridge_channel->chan),
+		rate,
+		ast_format_get_name(slin_format),
+		ast_format_get_name(ast_channel_rawreadformat(bridge_channel->chan));
+
 	ast_mutex_lock(&sc->lock);
 	if (reset) {
 		ast_slinfactory_destroy(&sc->factory);
