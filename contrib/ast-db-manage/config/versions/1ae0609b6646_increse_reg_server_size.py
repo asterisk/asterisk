1fd12b88c7a7a833f084db778af1ee8d5f38601d
Sungtae Kim <pchero21@gmail.com>
2020-08-31 14:21:09 +0200

    realtime: Increased reg_server character size
    Currently, the ps_contacts table's reg_server column in realtime database type is varchar(20).
    This is fine for normal cases, but if the hostname is longer than 20, it returns error and then
    failed to register the contact address of the peer.
    
    Normally, 20 characters limitation for the hostname is fine, but with the cloud env.
    So, increased the size to 255.
    
    ASTERISK-29056
    
    Change-Id: Iac52c8c35030303cfa551bb39f410b33bffc507d

---
+++ contrib/ast-db-manage/config/versions/1ae0609b6646_increse_reg_server_size.py
@@ -0,0 +1,22 @@
+"""increse reg server size
+
+Revision ID: 1ae0609b6646
+Revises: 61797b9fced6
+Create Date: 2020-08-31 13:50:19.772439
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1ae0609b6646'
+down_revision = '61797b9fced6'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.alter_column('ps_contacts', 'reg_server', type_=sa.String(255))
+
+
+def downgrade():
+    op.alter_column('ps_contacts', 'reg_server', type_=sa.String(20))
