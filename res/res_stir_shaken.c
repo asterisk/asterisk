26059f8616a639a162f15065ddfbfcfdb869b43d
Sean Bright <sean.bright@gmail.com>
2021-06-10 17:24:52 -0400

    menuselect: Fix description of several modules.
    The text description needs to be the last thing on the AST_MODULE_INFO
    line to be pulled in properly by menuselect.
    
    Change-Id: I0c913e36fea8b661f42e56920b6c5513ae8fd832

--- res/res_stir_shaken.c
+++ res/res_stir_shaken.c
@@ -1667,8 +1667,7 @@ static int load_module(void)
 	return res;
 }
 
-AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_GLOBAL_SYMBOLS | AST_MODFLAG_LOAD_ORDER,
-				"STIR/SHAKEN Module for Asterisk",
+AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_GLOBAL_SYMBOLS | AST_MODFLAG_LOAD_ORDER, "STIR/SHAKEN Module for Asterisk",
 	.support_level = AST_MODULE_SUPPORT_CORE,
 	.load = load_module,
 	.unload = unload_module,
