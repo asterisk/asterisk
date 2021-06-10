c0fc8adbb6a509927b5f84afaa377cf37e42343a
Sean Bright <sean.bright@gmail.com>
2021-06-10 17:24:52 -0400

    menuselect: Fix description of several modules.
    The text description needs to be the last thing on the AST_MODULE_INFO
    line to be pulled in properly by menuselect.
    
    Change-Id: I0c913e36fea8b661f42e56920b6c5513ae8fd832

--- res/res_format_attr_ilbc.c
+++ res/res_format_attr_ilbc.c
@@ -171,8 +171,7 @@ static int unload_module(void)
 	return 0;
 }
 
-AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_LOAD_ORDER,
-	"iLBC Format Attribute Module",
+AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_LOAD_ORDER, "iLBC Format Attribute Module",
 	.support_level = AST_MODULE_SUPPORT_CORE,
 	.load = load_module,
 	.unload = unload_module,
