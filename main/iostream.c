b17177c419fd098b52d30f29ac54db298a56bc1d
Fabrice Fontaine <fontaine.fabrice@gmail.com>
2023-02-25 11:27:12 +0100

    main/iostream.c: fix build with libressl
    Fix the following build failure with libressl by using SSL_is_server
    which is available since version 2.7.0 and
    https://github.com/libressl-portable/openbsd/commit/d7ec516916c5eaac29b02d7a8ac6570f63b458f7:
    
    iostream.c: In function 'ast_iostream_close':
    iostream.c:559:41: error: invalid use of incomplete typedef 'SSL' {aka 'struct ssl_st'}
      559 |                         if (!stream->ssl->server) {
          |                                         ^~
    
    ASTERISK-30107 #close
    
    Fixes: - http://autobuild.buildroot.org/results/ce4d62d00bb77ba5b303cacf6be7e350581a62f9
    Change-Id: Iea7f34970297f2fb50285d73462d0174ba7e9587
    (cherry picked from commit 030b7b900942df6c795fcabd112d08d1ed858467)

--- main/iostream.c
+++ main/iostream.c
@@ -553,7 +553,7 @@ int ast_iostream_close(struct ast_iostream *stream)
 					ERR_error_string(sslerr, err), ssl_error_to_string(sslerr, res));
 			}
 
-#if !defined(LIBRESSL_VERSION_NUMBER) && (OPENSSL_VERSION_NUMBER >= 0x10100000L)
+#if !(defined(LIBRESSL_VERSION_NUMBER) && (LIBRESSL_VERSION_NUMBER < 0x2070000L)) && (OPENSSL_VERSION_NUMBER >= 0x10100000L)
 			if (!SSL_is_server(stream->ssl)) {
 #else
 			if (!stream->ssl->server) {
