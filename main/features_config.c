1ad52a126322f045094b6d32625f4bc27883ab10
Mike Bradeen <mbradeen@sangoma.com>
2023-03-01 15:39:15 -0700

    bridge_builtin_features: add beep via touch variable
    Add periodic beep option to one-touch recording by setting
    the touch variable TOUCH_MONITOR_BEEP or
    TOUCH_MIXMONITOR_BEEP to the desired interval in seconds.
    
    If the interval is less than 5 seconds, a minimum of 5
    seconds will be imposed.  If the interval is set to an
    invalid value, it will default to 15 seconds.
    
    A new test event PERIODIC_HOOK_ENABLED was added to the
    func_periodic_hook hook_on function to indicate when
    a hook is started.  This is so we can test that the touch
    variable starts the hook as expected.
    
    ASTERISK-30446
    
    Change-Id: I800e494a789ba7a930bbdcd717e89d86040d6661

--- main/features_config.c
+++ main/features_config.c
@@ -199,6 +199,9 @@
 						channel variable or <literal>auto</literal> if the variable is not set. The timestamp
 						is a UNIX timestamp. The suffix is either the value of the <replaceable>TOUCH_MONITOR</replaceable>
 						channel variable or the callerID of the channels if the variable is not set.</para>
+						<para>To play a periodic beep while this call is being recorded, set the
+						<replaceable>TOUCH_MONITOR_BEEP</replaceable> to the interval in seconds. The interval will default
+						to 15 seconds if invalid.  The minimum interval is 5 seconds.</para>
 					</description>
 					<see-also><ref type="configOption">automixmon</ref></see-also>
 				</configOption>
@@ -215,6 +218,9 @@
 						channel variable or <literal>auto</literal> if the variable is not set. The timestamp
 						is a UNIX timestamp. The suffix is either the value of the <replaceable>TOUCH_MIXMONITOR</replaceable>
 						channel variable or the callerID of the channels if the variable is not set.</para>
+						<para>To play a periodic beep while this call is being recorded, set the
+						<replaceable>TOUCH_MIXMONITOR_BEEP</replaceable> to the interval in seconds. The interval will default
+						to 15 seconds if invalid.  The minimum interval is 5 seconds.</para>
 					</description>
 					<see-also><ref type="configOption">automon</ref></see-also>
 				</configOption>
