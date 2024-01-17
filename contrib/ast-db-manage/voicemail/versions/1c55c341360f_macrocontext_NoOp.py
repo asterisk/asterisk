4b03427c3af3adf02994e5fab1e42a0c0f7a26b5
Mike Bradeen <mbradeen@sangoma.com>
2024-01-17 09:21:13 -0700

    app_voicemail: add NoOp alembic script to maintain sync
    Adding a NoOp alembic script for the voicemail database to maintain
    version sync with other branches.
    
    Fixes: #527
    (cherry picked from commit 96d20e92bfb1525807ee01da840895754532d7a8)

---
+++ contrib/ast-db-manage/voicemail/versions/1c55c341360f_macrocontext_NoOp.py
@@ -0,0 +1,20 @@
+"""Macrocontext NoOp for sync
+Revision ID: 1c55c341360f
+Revises: 39428242f7f5
+Create Date: 2024-01-09 15:01:39.698918
+"""
+
+# revision identifiers, used by Alembic.
+revision = '1c55c341360f'
+down_revision = '39428242f7f5'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
