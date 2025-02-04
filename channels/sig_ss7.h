d65af637ac9b7472feabb21bad52b61585e4f013
Luz Paz <luzpaz@pm.me>
2025-02-04 06:44:31 -0500

    docs: Fix various typos in channels/ Found via `codespell -q 3 -S "./CREDITS,*.po" -L abd,asent,atleast,cachable,childrens,contentn,crypted,dne,durationm,enew,exten,inout,leapyear,mye,nd,oclock,offsetp,ot,parm,parms,preceeding,pris,ptd,requestor,re-use,re-used,re-uses,ser,siz,slanguage,slin,thirdparty,varn,varns,ues`

--- channels/sig_ss7.h
+++ channels/sig_ss7.h
@@ -309,8 +309,8 @@ struct sig_ss7_chan {
 	/*!
 	 * \brief Indication of the call being a CUG call and its permissions.
 	 * \note 0 or 1 - non-CUG call
-	 * \note 2 - CUG call, outgoing access alowed
-	 * \note 3 - CUG call, outgoing access not alowed
+	 * \note 2 - CUG call, outgoing access allowed
+	 * \note 3 - CUG call, outgoing access not allowed
 	 */
 	unsigned char cug_indicator;
 };
