asterisk/asterisk

commit 3cab4e7ab4a3ae483430d5f5e8fa167d02a8128c

config.c Make ast_variable_update update last match.
ast_variable_update currently sets the first match for a variable, as
opposed to the last one. This issue is complementary to that raised
in #244.

This is incorrect and results in the wrong (or no) action being taken
in cases where a section inherits from a template section. When the
traversal occurs to update the setting, the existing code erroneously
would use the first of possibly multiple matches in its update logic,
which is wrong. Now, explicitly use the last match in the traversal,
which will ensure that the actual setting is updated properly, and
not skipped or ignored because a template from which the setting's
section inherits was used for comparison.

Resolves: #960

UpgradeNote: Config variables, when set/updated, such as via AMI,
will now have the corresponding setting updated, even if their
sections inherit from template sections.

main/config.c
@@ -1534,13 +1534,19 @@ int ast_variable_delete(struct ast_category *category, const char *variable, con
 int ast_variable_update(struct ast_category *category, const char *variable,
 						const char *value, const char *match, unsigned int object)
 {
-	struct ast_variable *cur, *prev=NULL, *newer=NULL;
+	struct ast_variable *cur, *prev=NULL, *newer=NULL, *matchvar = NULL;
 
 	for (cur = category->root; cur; prev = cur, cur = cur->next) {
 		if (strcasecmp(cur->name, variable) ||
 			(!ast_strlen_zero(match) && strcasecmp(cur->value, match)))
 			continue;
+		matchvar = cur;
+	}
 
+	for (cur = category->root; cur; prev = cur, cur = cur->next) {
+		if (cur != matchvar) {
+			continue;
+		}
 		if (!(newer = ast_variable_new(variable, value, cur->file)))
 			return -1;
 

commit 6159dbb6f8f9e2c19bf215a96e103840e0368694

config.c: Fix inconsistent pointer logic in ast_variable_update.
Commit 3cab4e7ab4a3ae483430d5f5e8fa167d02a8128c introduced a
regression by causing the wrong pointers to be used in certain
(more complex) cases. We now take care to ensure the exact
same pointers are used as before that commit, and simplify
by eliminating the unnecessary second for loop.

Resolves: #1147

main/config.c
@@ -1645,41 +1645,51 @@ int ast_variable_delete(struct ast_category *category, const char *variable, con
 int ast_variable_update(struct ast_category *category, const char *variable,
 						const char *value, const char *match, unsigned int object)
 {
-	struct ast_variable *cur, *prev=NULL, *newer=NULL, *matchvar = NULL;
+	struct ast_variable *cur, *prev = NULL, *newer = NULL;
+	struct ast_variable *matchcur = NULL, *matchprev = NULL;
 
+	/* Find the last match. See comments in ast_variable_retrieve,
+	 * but this ensures updating config sections that inherit from
+	 * templates works properly. */
 	for (cur = category->root; cur; prev = cur, cur = cur->next) {
-		if (strcasecmp(cur->name, variable) ||
-			(!ast_strlen_zero(match) && strcasecmp(cur->value, match)))
-			continue;
-		matchvar = cur;
-	}
-
-	for (cur = category->root; cur; prev = cur, cur = cur->next) {
-		if (cur != matchvar) {
+		if (strcasecmp(cur->name, variable) || (!ast_strlen_zero(match) && strcasecmp(cur->value, match))) {
+			/* Not the same variable,
+			 * or its value doesn't match. */
 			continue;
 		}
-		if (!(newer = ast_variable_new(variable, value, cur->file)))
-			return -1;
-
-		ast_variable_move(newer, cur);
-		newer->object = newer->object || object;
-
-		/* Replace the old node in the list with the new node. */
-		newer->next = cur->next;
-		if (prev)
-			prev->next = newer;
-		else
-			category->root = newer;
-		if (category->last == cur)
-			category->last = newer;
-
-		ast_variable_destroy(cur);
-
-		return 0;
+		matchprev = prev;
+		matchcur = cur;
 	}
 
-	/* Could not find variable to update */
-	return -1;
+	if (!matchcur) {
+		/* Could not find variable to update */
+		return -1;
+	}
+
+	/* Restore pointers from the matching var */
+	prev = matchprev;
+	cur = matchcur;
+
+	if (!(newer = ast_variable_new(variable, value, cur->file))) {
+		return -1;
+	}
+
+	ast_variable_move(newer, cur);
+	newer->object = newer->object || object;
+
+	/* Replace the old node in the list with the new node. */
+	newer->next = cur->next;
+	if (prev) {
+		prev->next = newer;
+	} else {
+		category->root = newer;
+	}
+	if (category->last == cur) {
+		category->last = newer;
+	}
+
+	ast_variable_destroy(cur);
+	return 0;
 }
 
 struct ast_category *ast_category_delete(struct ast_config *config,