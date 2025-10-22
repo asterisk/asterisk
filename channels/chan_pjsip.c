asterisk/asterisk

commit b333ee3be7b08e17fea78838b58380308659e23c

chan_pjsip.c: Change SSRC after media source change
When the RTP media source changes, such as after a blind transfer, the new source introduces a discontinuous timestamp. According to RFC 3550, Section 5.1, an RTP stream's timestamp for a given SSRC must increment monotonically and linearly.
To comply with the standard and avoid a large timestamp jump on the existing SSRC, a new SSRC is generated for the new media stream.
This change resolves known interoperability issues with certain SBCs (like Sonus/Ribbon) that stop forwarding media when they detect such a timestamp violation. This code uses the existing implementation from chan_sip.

Resolves: #927

channels/chan_pjsip.c
@@ -1820,6 +1820,14 @@ static int chan_pjsip_indicate(struct ast_channel *ast, int condition, const voi
 	case AST_CONTROL_SRCUPDATE:
 		break;
 	case AST_CONTROL_SRCCHANGE:
+		/* Generate a new SSRC due to media source change and RTP timestamp reset.
+		   Ensures RFC 3550 compliance and avoids SBC interoperability issues (Sonus/Ribbon)*/
+		for (i = 0; i < AST_VECTOR_SIZE(&channel->session->active_media_state->sessions); ++i) {
+			media = AST_VECTOR_GET(&channel->session->active_media_state->sessions, i);
+			if (media && media->rtp) {
+				ast_rtp_instance_change_source(media->rtp);
+			}
+		}
 		break;
 	case AST_CONTROL_REDIRECTING:
 		if (ast_channel_state(ast) != AST_STATE_UP) {

commit 39ba714f72758f9bff2afbcdac11ef158f4a0d80

chan_pjsip: Disable SSRC change for WebRTC endpoints.
Commit b333ee3b introduced a fix to chan_pjsip that addressed RTP issues with
blind transfers and some SBCs.  Unfortunately, the fix broke some WebRTC
clients that are sensitive to SSRC changes and non-monotonic timestamps so
the fix is now disabled for endpoints with the "bundle" parameter set to true.

Resolves: #1535

channels/chan_pjsip.c
@@ -1820,12 +1820,14 @@ static int chan_pjsip_indicate(struct ast_channel *ast, int condition, const voi
 	case AST_CONTROL_SRCUPDATE:
 		break;
 	case AST_CONTROL_SRCCHANGE:
-		/* Generate a new SSRC due to media source change and RTP timestamp reset.
-		   Ensures RFC 3550 compliance and avoids SBC interoperability issues (Sonus/Ribbon)*/
-		for (i = 0; i < AST_VECTOR_SIZE(&channel->session->active_media_state->sessions); ++i) {
-			media = AST_VECTOR_GET(&channel->session->active_media_state->sessions, i);
-			if (media && media->rtp) {
-				ast_rtp_instance_change_source(media->rtp);
+		if (!channel->session->endpoint->media.bundle) {
+			/* Generate a new SSRC due to media source change and RTP timestamp reset.
+			   Ensures RFC 3550 compliance and avoids SBC interoperability issues (Sonus/Ribbon)*/
+			for (i = 0; i < AST_VECTOR_SIZE(&channel->session->active_media_state->sessions); ++i) {
+				media = AST_VECTOR_GET(&channel->session->active_media_state->sessions, i);
+				if (media && media->rtp) {
+					ast_rtp_instance_change_source(media->rtp);
+				}
 			}
 		}
 		break;