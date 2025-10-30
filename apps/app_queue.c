f676c148d0faa78b71174f5d50b0370e92df3baa
Kristian F. Høgh <kfh@uni-tel.dk>
2025-10-30 14:43:37 +0100

    Queue: Only make announcements to head caller if announce_to_first_user is true

--- apps/app_queue.c
+++ apps/app_queue.c
@@ -8972,13 +8972,12 @@ check_turns:
 					goto stop;
 				}
 			}
-		}
-		makeannouncement = 1;
 
-		/* Make a periodic announcement, if enabled */
-		if (qe.parent->periodicannouncefrequency) {
-			if ((res = say_periodic_announcement(&qe, ringing))) {
-				goto stop;
+			/* Make a periodic announcement, if enabled */
+			if (qe.parent->periodicannouncefrequency) {
+				if ((res = say_periodic_announcement(&qe, ringing))) {
+					goto stop;
+				}
 			}
 		}
 
