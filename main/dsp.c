asterisk/asterisk

commit ec0581fef4e44b029588cd1ce0539dffa36db94a

restarts busydetector (if enabled) when DTMF is received after call is bridged.
  (closes issue 0016389)
  Reported by: alecdavis
  Tested by: alecdavis
  Patch
    dtmf_busydetector.diff2.txt uploaded by alecdavis (license 585)


git-svn-id: https://origsvn.digium.com/svn/asterisk/trunk@235774 65c4cc65-6c06-0410-ace0-fbb531ad65f3

main/dsp.c
@@ -1376,8 +1376,8 @@ struct ast_frame *ast_dsp_process(struct ast_channel *chan, struct ast_dsp *dsp,
 		}
 	}
 
-	if ((dsp->features & DSP_FEATURE_DIGIT_DETECT)) {
-		if ((dsp->digitmode & DSP_DIGITMODE_MF))
+	if (dsp->features & (DSP_FEATURE_DIGIT_DETECT | DSP_FEATURE_BUSY_DETECT)) {
+		if (dsp->digitmode & DSP_DIGITMODE_MF)
 			digit = mf_detect(dsp, &dsp->digit_state, shortdata, len, (dsp->digitmode & DSP_DIGITMODE_NOQUELCH) == 0, (dsp->digitmode & DSP_DIGITMODE_RELAXDTMF));
 		else
 			digit = dtmf_detect(dsp, &dsp->digit_state, shortdata, len, (dsp->digitmode & DSP_DIGITMODE_NOQUELCH) == 0, (dsp->digitmode & DSP_DIGITMODE_RELAXDTMF));
@@ -1389,15 +1389,18 @@ struct ast_frame *ast_dsp_process(struct ast_channel *chan, struct ast_dsp *dsp,
 			if (!dsp->dtmf_began) {
 				/* We have not reported DTMF_BEGIN for anything yet */
 
-				event = AST_FRAME_DTMF_BEGIN;
-				event_digit = dsp->digit_state.digits[0];
+				if (dsp->features & DSP_FEATURE_DIGIT_DETECT) {
+					event = AST_FRAME_DTMF_BEGIN;
+					event_digit = dsp->digit_state.digits[0];
+				}
 				dsp->dtmf_began = 1;
 
 			} else if (dsp->digit_state.current_digits > 1 || digit != dsp->digit_state.digits[0]) {
 				/* Digit changed. This means digit we have reported with DTMF_BEGIN ended */
-	
-				event = AST_FRAME_DTMF_END;
-				event_digit = dsp->digit_state.digits[0];
+				if (dsp->features & DSP_FEATURE_DIGIT_DETECT) {
+					event = AST_FRAME_DTMF_END;
+					event_digit = dsp->digit_state.digits[0];
+				}
 				memmove(dsp->digit_state.digits, dsp->digit_state.digits + 1, dsp->digit_state.current_digits);
 				dsp->digit_state.current_digits--;
 				dsp->dtmf_began = 0;
@@ -1406,6 +1409,7 @@ struct ast_frame *ast_dsp_process(struct ast_channel *chan, struct ast_dsp *dsp,
 					/* Reset Busy Detector as we have some confirmed activity */ 
 					memset(dsp->historicsilence, 0, sizeof(dsp->historicsilence));
 					memset(dsp->historicnoise, 0, sizeof(dsp->historicnoise));
+					ast_debug(1, "DTMF Detected - Reset busydetector\n");
 				}
 			}
 

commit e9bf69da888f25fe67b32ed5bde22c294bf7bda2

dsp.c: Avoid running digit detection twice if busydetect is enabled.
When setting busydetect=yes in chan_dahdi.conf, chan_dahdi uses a
second DSP to process audio on the channel, in addition to the one
that exists normally. Within ast_dsp_process, the conditional
guarding digit detection (either mf_detect or dtmf_detect) was
true if either DSP_FEATURE_DIGIT_DETECT or DSP_FEATURE_BUSY_DETECT
was set. Because digits are normally quelched unless
DSP_DIGITMODE_NOQUELCH is set, the first pass would quelch digits
on the channel and the second pass would see discrete DTMF tones
instead of the continuous tone that is actually present.

This second digit detection on the frame should not be happening,
and DSP_FEATURE_BUSY_DETECT should not be part of this condition
in the first place. While the busy detector gets reset if a digit is
detected, this is done even without it being part of this conditional.
This regression was introduced in commit ec0581fef4e44b029588cd1ce0539dffa36db94a.

Removing it ensures that mf_detect/dtmf_detect is only called once
per frame, since busy detection doesn't need digit detection to work
and digit detection shouldn't be performed twice.

Resolves: #1570

main/dsp.c
@@ -1571,7 +1571,7 @@ struct ast_frame *ast_dsp_process(struct ast_channel *chan, struct ast_dsp *dsp,
 		dsp->f.frametype = AST_FRAME_CONTROL;
 		dsp->f.subclass.integer = AST_CONTROL_BUSY;
 		ast_frfree(af);
-		ast_debug(1, "Requesting Hangup because the busy tone was detected on channel %s\n", ast_channel_name(chan));
+		ast_log(LOG_NOTICE, "Requesting hangup because busy tone was detected on channel %s\n", ast_channel_name(chan));
 		return ast_frisolate(&dsp->f);
 	}
 
@@ -1591,7 +1591,7 @@ struct ast_frame *ast_dsp_process(struct ast_channel *chan, struct ast_dsp *dsp,
 		}
 	}
 
-	if (dsp->features & (DSP_FEATURE_DIGIT_DETECT | DSP_FEATURE_BUSY_DETECT)) {
+	if (dsp->features & (DSP_FEATURE_DIGIT_DETECT)) {
 		if (dsp->digitmode & DSP_DIGITMODE_MF) {
 			digit = mf_detect(dsp, &dsp->digit_state, shortdata, len, (dsp->digitmode & DSP_DIGITMODE_NOQUELCH) == 0, (dsp->digitmode & DSP_DIGITMODE_RELAXDTMF));
 		} else {