asterisk/asterisk

commit ae7182a5f2900cb5721c833d5e3bed8eace4e930

main/channel.c: Route real-time text frames through stream logic
Real-time text (T.140 and T.140/RED) frames must be handled the same
way as voice and video frames so they are associated with the correct
media stream when read and written. Add AST_FRAME_TEXT to the frame
type checks used to locate the originating and default stream in
__ast_queue_frame(), __ast_read() and ast_write_stream(). Accept
the ast_format_t140_red format in addition to ast_format_t140 when
invoking the channel technology write_text callback.

main/channel.c
@@ -1121,7 +1121,7 @@ static int __ast_queue_frame(struct ast_channel *chan, struct ast_frame *fin, in
 			/* Save the most recent frame */
 			if (!AST_LIST_NEXT(cur, frame_list)) {
 				break;
-			} else if (cur->frametype == AST_FRAME_VOICE || cur->frametype == AST_FRAME_VIDEO || cur->frametype == AST_FRAME_NULL) {
+			} else if (cur->frametype == AST_FRAME_VOICE || cur->frametype == AST_FRAME_VIDEO || cur->frametype == AST_FRAME_TEXT || cur->frametype == AST_FRAME_NULL) {
 				if (++count > 64) {
 					break;
 				}
@@ -3717,7 +3717,7 @@ static struct ast_frame *__ast_read(struct ast_channel *chan, int dropaudio, int
 			default:
 				break;
 			}
-		} else if (f->frametype == AST_FRAME_VOICE || f->frametype == AST_FRAME_VIDEO) {
+		} else if (f->frametype == AST_FRAME_VOICE || f->frametype == AST_FRAME_VIDEO || f->frametype == AST_FRAME_TEXT) {
 			if (ast_channel_tech(chan) && ast_channel_tech(chan)->read_stream) {
 				stream = ast_stream_topology_get_stream(ast_channel_get_stream_topology(chan), f->stream_num);
 				default_stream = ast_channel_get_default_stream(chan, ast_format_get_type(f->subclass.format));
@@ -3756,7 +3756,7 @@ static struct ast_frame *__ast_read(struct ast_channel *chan, int dropaudio, int
 			 * thing different is that we need to find the default stream so we know whether to invoke the
 			 * default stream logic or not (such as transcoding).
 			 */
-			if (f && (f->frametype == AST_FRAME_VOICE || f->frametype == AST_FRAME_VIDEO)) {
+			if (f && (f->frametype == AST_FRAME_VOICE || f->frametype == AST_FRAME_VIDEO || f->frametype == AST_FRAME_TEXT)) {
 				stream = ast_stream_topology_get_stream(ast_channel_get_stream_topology(chan), f->stream_num);
 				default_stream = ast_channel_get_default_stream(chan, ast_format_get_type(f->subclass.format));
 			}
@@ -3766,7 +3766,7 @@ static struct ast_frame *__ast_read(struct ast_channel *chan, int dropaudio, int
 			/* Since this channel driver does not support multistream determine the default stream this frame
 			 * originated from and update the frame to include it.
 			 */
-			if (f && (f->frametype == AST_FRAME_VOICE || f->frametype == AST_FRAME_VIDEO)) {
+			if (f && (f->frametype == AST_FRAME_VOICE || f->frametype == AST_FRAME_VIDEO || f->frametype == AST_FRAME_TEXT)) {
 				stream = default_stream = ast_channel_get_default_stream(chan, ast_format_get_type(f->subclass.format));
 				if (!stream) {
 					ast_frfree(f);
@@ -5218,12 +5218,12 @@ int ast_write_stream(struct ast_channel *chan, int stream_num, struct ast_frame 
 		}
 		stream = ast_stream_topology_get_stream(ast_channel_get_stream_topology(chan), stream_num);
 		default_stream = ast_channel_get_default_stream(chan, ast_stream_get_type(stream));
-	} else if (fr->frametype == AST_FRAME_VOICE || fr->frametype == AST_FRAME_VIDEO || fr->frametype == AST_FRAME_MODEM) {
+	} else if (fr->frametype == AST_FRAME_VOICE || fr->frametype == AST_FRAME_VIDEO || fr->frametype == AST_FRAME_TEXT || fr->frametype == AST_FRAME_MODEM) {
 		/* If we haven't been told of a stream then we need to figure out which once we need */
 		enum ast_media_type type = AST_MEDIA_TYPE_UNKNOWN;
 
 		/* Some frame types have a fixed media type */
-		if (fr->frametype == AST_FRAME_VOICE || fr->frametype == AST_FRAME_VIDEO) {
+		if (fr->frametype == AST_FRAME_VOICE || fr->frametype == AST_FRAME_VIDEO || fr->frametype == AST_FRAME_TEXT) {
 			type = ast_format_get_type(fr->subclass.format);
 		} else if (fr->frametype == AST_FRAME_MODEM) {
 			type = AST_MEDIA_TYPE_IMAGE;
@@ -5308,7 +5308,8 @@ int ast_write_stream(struct ast_channel *chan, int stream_num, struct ast_frame 
 				res = 0;
 			}
 		} else if (stream == default_stream) {
-			if (ast_format_cmp(fr->subclass.format, ast_format_t140) == AST_FORMAT_CMP_EQUAL) {
+			if (ast_format_cmp(fr->subclass.format, ast_format_t140) == AST_FORMAT_CMP_EQUAL ||
+				ast_format_cmp(fr->subclass.format, ast_format_t140_red) == AST_FORMAT_CMP_EQUAL) {
 				res = (ast_channel_tech(chan)->write_text == NULL) ? 0 :
 					ast_channel_tech(chan)->write_text(chan, fr);
 			} else {

commit 3545454bf55cfe21e85aced81b00d4e0104b0014

main/channel.c: fix regression related to SIP MESSAGE text bridging
fix regression related to SIP MESSAGE text bridging introduced in ae7182a5f2

main/channel.c
@@ -3717,7 +3717,11 @@ static struct ast_frame *__ast_read(struct ast_channel *chan, int dropaudio, int
 			default:
 				break;
 			}
-		} else if (f->frametype == AST_FRAME_VOICE || f->frametype == AST_FRAME_VIDEO || f->frametype == AST_FRAME_TEXT) {
+		/* Text messages over SIP MESSAGE have no stream codec topology, leaving f->subclass.format as NULL.
+		 * We must bypass this block for standalone text frames to prevent them from being dropped.
+		 */
+		} else if (f->frametype == AST_FRAME_VOICE || f->frametype == AST_FRAME_VIDEO ||
+					( f->frametype == AST_FRAME_TEXT && f->subclass.format)) {
 			if (ast_channel_tech(chan) && ast_channel_tech(chan)->read_stream) {
 				stream = ast_stream_topology_get_stream(ast_channel_get_stream_topology(chan), f->stream_num);
 				default_stream = ast_channel_get_default_stream(chan, ast_format_get_type(f->subclass.format));
@@ -3756,7 +3760,11 @@ static struct ast_frame *__ast_read(struct ast_channel *chan, int dropaudio, int
 			 * thing different is that we need to find the default stream so we know whether to invoke the
 			 * default stream logic or not (such as transcoding).
 			 */
-			if (f && (f->frametype == AST_FRAME_VOICE || f->frametype == AST_FRAME_VIDEO || f->frametype == AST_FRAME_TEXT)) {
+			/* Text messages based on SIP MESSAGE) have no stream codec topology, leaving f->subclass.format as NULL.
+			 * We must bypass this block for standalone text frames to prevent them from being dropped.
+			 */
+			if (f && (f->frametype == AST_FRAME_VOICE || f->frametype == AST_FRAME_VIDEO ||
+				(f->frametype == AST_FRAME_TEXT && f->subclass.format))) {
 				stream = ast_stream_topology_get_stream(ast_channel_get_stream_topology(chan), f->stream_num);
 				default_stream = ast_channel_get_default_stream(chan, ast_format_get_type(f->subclass.format));
 			}
@@ -3766,7 +3774,11 @@ static struct ast_frame *__ast_read(struct ast_channel *chan, int dropaudio, int
 			/* Since this channel driver does not support multistream determine the default stream this frame
 			 * originated from and update the frame to include it.
 			 */
-			if (f && (f->frametype == AST_FRAME_VOICE || f->frametype == AST_FRAME_VIDEO || f->frametype == AST_FRAME_TEXT)) {
+			/* Text messages based on SIP MESSAGE) have no stream codec topology, leaving f->subclass.format as NULL.
+			 * We must bypass this block for standalone text frames to prevent them from being dropped.
+			 */
+			if (f && (f->frametype == AST_FRAME_VOICE || f->frametype == AST_FRAME_VIDEO ||
+				( f->frametype == AST_FRAME_TEXT && f->subclass.format))) {
 				stream = default_stream = ast_channel_get_default_stream(chan, ast_format_get_type(f->subclass.format));
 				if (!stream) {
 					ast_frfree(f);